e91faa67b4ab277dc8132649c3dc4388248800c9
Andrew Bresticker <abrestic@chromium.org>
2013-08-13 13:39:48 -0700

    CHROMIUM: cpufreq: interactive: validate above_hispeed_delay
    We rely on the frequencies in above_hispeed_delay being in ascending
    order.  Ensure that is the case for the values the user gives us.
    
    BUG=chrome-os-partner:20830
    TEST=Boot Pit; write invalid above_hispeed_delay value:
    localhost ~ # echo "20000 1800000:20000 700000:0" > above_hispeed_delay
    -bash: echo: write error: Invalid argument
    
    Change-Id: Ifb431e58ad4b6b371152d7b09fcbefa127b0cbe2
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/65742
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -889,7 +889,7 @@ static ssize_t store_above_hispeed_delay(
 	struct cpufreq_interactive_tunables *tunables,
 	const char *buf, size_t count)
 {
-	int ntokens;
+	int ntokens, i;
 	unsigned int *new_above_hispeed_delay = NULL;
 	unsigned long flags;
 
@@ -899,6 +899,15 @@ static ssize_t store_above_hispeed_delay(
 	new_above_hispeed_delay = get_tokenized_data(buf, &ntokens);
 	if (IS_ERR(new_above_hispeed_delay))
 		return PTR_RET(new_above_hispeed_delay);
+
+	/* Make sure frequencies are in ascending order. */
+	for (i = 3; i < ntokens; i += 2) {
+		if (new_above_hispeed_delay[i] <=
+		    new_above_hispeed_delay[i - 2]) {
+			kfree(new_above_hispeed_delay);
+			return -EINVAL;
+		}
+	}
 
 	spin_lock_irqsave(&tunables->above_hispeed_delay_lock, flags);
 	if (tunables->above_hispeed_delay != default_above_hispeed_delay)
