cd6360285b6688cc350e1370317074ab826f56ad
flar2 <asegaert@gmail.com>
2013-11-06 23:24:29 -0500

    AIO: Don't plug the I/O queue in do_io_submit()
    Signed-off-by: flar2 <asegaert@gmail.com>

--- fs/aio.c
+++ fs/aio.c
@@ -1600,7 +1600,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
-	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1616,8 +1615,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		pr_debug("EINVAL: invalid context id\n");
 		return -EINVAL;
 	}
-
-	blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1641,7 +1638,6 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
 
 	percpu_ref_put(&ctx->users);
 	return i ? i : ret;
