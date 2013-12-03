2cb364fcc184f8a1e40b282df12d512e4faaed75
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-12-03 09:24:02 -0800

    rcu: Add ACCESS_ONCE() to ->n_force_qs_lh accesses
    The ->n_force_qs_lh field is accessed without the benefit of any
    synchronization, so this commit adds the needed ACCESS_ONCE() wrappers.
    Yes, increments to ->n_force_qs_lh can be lost, but contention should
    be low and the field is strictly statistical in nature, so this is not
    a problem.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -2268,7 +2268,7 @@ static void force_quiescent_state(struct rcu_state *rsp)
 		if (rnp_old != NULL)
 			raw_spin_unlock(&rnp_old->fqslock);
 		if (ret) {
-			rsp->n_force_qs_lh++;
+			ACCESS_ONCE(rsp->n_force_qs_lh)++;
 			return;
 		}
 		rnp_old = rnp;
@@ -2279,7 +2279,7 @@ static void force_quiescent_state(struct rcu_state *rsp)
 	raw_spin_lock_irqsave(&rnp_old->lock, flags);
 	raw_spin_unlock(&rnp_old->fqslock);
 	if (ACCESS_ONCE(rsp->gp_flags) & RCU_GP_FLAG_FQS) {
-		rsp->n_force_qs_lh++;
+		ACCESS_ONCE(rsp->n_force_qs_lh)++;
 		raw_spin_unlock_irqrestore(&rnp_old->lock, flags);
 		return;  /* Someone beat us to it. */
 	}
