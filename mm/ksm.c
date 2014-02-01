180a46c8b58679d070ca9f463180207e4d197ec9
Pranav Vashi <neobuddy89@gmail.com>
2014-02-01 23:22:50 +0530

    mm: ksm: Optimize defaults
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/ksm.c
+++ mm/ksm.c
@@ -219,10 +219,10 @@ static unsigned long ksm_pages_unshared;
 static unsigned long ksm_rmap_items;
 
 /* Number of pages ksmd should scan in one batch */
-static unsigned int ksm_thread_pages_to_scan = 100;
+static unsigned int ksm_thread_pages_to_scan = 256;
 
 /* Milliseconds ksmd should sleep between batches */
-static unsigned int ksm_thread_sleep_millisecs = 20;
+static unsigned int ksm_thread_sleep_millisecs = 1500;
 
 /* Boolean to indicate whether to use deferred timer or not */
 static bool use_deferred_timer = true;
