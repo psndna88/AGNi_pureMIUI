4f9366622831af82d4820c53712b07538435232a
Dirk Brandewie <dirk.j.brandewie@intel.com>
2014-03-19 08:45:53 -0700

    cpufreq: Add stop CPU callback to cpufreq_driver interface
    This callback allows the driver to do clean up before the CPU is
    completely down and its state cannot be modified.  This is used
    by the intel_pstate driver to reduce the requested P state prior to
    the core going away.  This is required because the requested P state
    of the offline core is used to select the package P state. This
    effectively sets the floor package P state to the requested P state on
    the offline core.
    
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    [rjw: Minor modifications]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1339,6 +1339,8 @@ static int __cpufreq_remove_dev_prepare(struct device *dev,
 						__func__, new_cpu, cpu);
 			}
 		}
+	} else if (cpufreq_driver->stop_cpu && cpufreq_driver->setpolicy) {
+		cpufreq_driver->stop_cpu(policy);
 	}
 
 	return 0;
