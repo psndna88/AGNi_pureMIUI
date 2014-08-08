7d6167946071b1d4d39ab699fa6a54ce1d260534
Paul Moore <pmoore@redhat.com>
2014-08-07 20:55:30 -0400

    selinux: remove unused variabled in the netport, netnode, and netif caches
    This patch removes the unused return code variable in the netport,
    netnode, and netif initialization functions.
    
    Reported-by: fengguang.wu@intel.com
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- security/selinux/netnode.c
+++ security/selinux/netnode.c
@@ -302,7 +302,6 @@ void sel_netnode_flush(void)
 static __init int sel_netnode_init(void)
 {
 	int iter;
-	int ret;
 
 	if (!selinux_enabled)
 		return 0;
@@ -312,7 +311,7 @@ static __init int sel_netnode_init(void)
 		sel_netnode_hash[iter].size = 0;
 	}
 
-	return ret;
+	return 0;
 }
 
 __initcall(sel_netnode_init);
