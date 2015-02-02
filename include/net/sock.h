11361c54eea5eaddcaaa79a4c95992a1b5aa781a
Harout Hedeshian <harouth@codeaurora.org>
2015-02-02 13:30:42 -0700

    net: tcp: Scale the TCP backlog queue to absorb packet bursts
    A large momentary influx of packets flooding the TCP layer may cause
    packets to get dropped at the socket's backlog queue. Bump this up to
    prevent these drops. Note that this change may cause the socket memory
    accounting to allow the total backlog queue length to exceed the user
    space configured values, sometimes by a substantial amount, which can
    lead to out of order packets to be dropped instead of being queued. To
    avoid these ofo drops, the condition to drop an out of order packet is
    modified to allow out of order queuing to continue as long as it falls
    within the now increased backlog queue limit.
    
    Change-Id: I447ffc8560cb149fe84193c72bf693862f7ec740
    Signed-off-by: Harout Hedeshian <harouth@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: dev_harsh1998 <harshitjain6751@gmail.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -69,6 +69,8 @@
 #include <net/checksum.h>
 #include <net/tcp_states.h>
 
+#define TCP_BACKLOG_SCALE 4
+
 struct cgroup;
 struct cgroup_subsys;
 #ifdef CONFIG_NET
@@ -784,7 +786,7 @@ static inline bool sk_rcvqueues_full(const struct sock *sk, const struct sk_buff
 static inline __must_check int sk_add_backlog(struct sock *sk, struct sk_buff *skb,
 					      unsigned int limit)
 {
-	if (sk_rcvqueues_full(sk, skb, limit))
+	if (sk_rcvqueues_full(sk, skb, limit * TCP_BACKLOG_SCALE))
 		return -ENOBUFS;
 
 	/*
