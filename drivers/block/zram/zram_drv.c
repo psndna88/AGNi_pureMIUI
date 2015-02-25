8b5b1c7178c68bf63ef0565ada813b8d834c2155
Vinayak Menon <vinmenon@codeaurora.org>
2015-02-25 19:43:59 +0530

    mm: swap: don't delay swap free for fast swap devices
    There are couple of issues with swapcache usage when ZRAM is used
    as swap device.
    1) Kernel does a swap readahead which can be around 6 to 8 pages
    depending on total ram, which is not required for zram since
    accesses are fast.
    2) Kernel delays the freeing up of swapcache expecting a later hit,
    which again is useless in the case of zram.
    3) This is not related to swapcache, but zram usage itself.
    As mentioned in (2) kernel delays freeing of swapcache, but along with
    that it delays zram compressed page free also. i.e. there can be 2 copies,
    though one is compressed.
    
    This patch addresses these issues using two new flags
    QUEUE_FLAG_FAST and SWP_FAST, to indicate that accesses to the device
    will be fast and cheap, and instructs the swap layer to free up
    swap space agressively, and not to do read ahead.
    
    Change-Id: Ifbeb38dd4839d7aa7e608e777fa47dded9dc431b
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1180,6 +1180,7 @@ static int zram_add(void)
 	zram->disk->private_data = zram;
 	snprintf(zram->disk->disk_name, 16, "zram%d", device_id);
 
+	__set_bit(QUEUE_FLAG_FAST, &queue->queue_flags);
 	/* Actual capacity set using syfs (/sys/block/zram<id>/disksize */
 	set_capacity(zram->disk, 0);
 	/* zram devices sort of resembles non-rotational disks */
