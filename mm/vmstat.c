5cc26f5be616eb9c788f0966f7dafe8479c2fa74
Liam Mark <lmark@codeaurora.org>
2015-02-27 12:59:00 -0800

    lowmemorykiller: Don't count swap cache pages twice
    The lowmem_shrink function discounts all the swap cache pages from
    the file cache count. The zone aware code also discounts all file
    cache pages from a certain zone.  This results in some swap cache
    pages being discounted twice, which can result in the low memory
    killer being unnecessarily aggressive.
    
    Fix the low memory killer to only discount the swap cache pages
    once.
    
    Change-Id: I650bbfbf0fbbabd01d82bdb3502b57ff59c3e14f
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -746,6 +746,7 @@ const char * const vmstat_text[] = {
 #ifdef CONFIG_UKSM
 	"nr_uksm_zero_pages",
 #endif
+	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
