3ea26a63a2a67616e2305f73b59ed535c16c94f5
Paul Reioux <reioux@gmail.com>
2015-06-07 17:03:58 +0200

    android: binder: use GFP_HIGHUSER flag since binder is designed for userspace
    Change-Id: I8b5b6cc759f99bc1241ea32dd526d62f3c77101c
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/android/binder_n.c
+++ drivers/android/binder_n.c
@@ -593,7 +593,7 @@ static int binder_update_page_range(struct binder_proc *proc, int allocate,
 		page = &proc->pages[(page_addr - proc->buffer) / PAGE_SIZE];
 
 		BUG_ON(*page);
-		*page = alloc_page(GFP_KERNEL | __GFP_HIGHMEM | __GFP_ZERO);
+		*page = alloc_page(GFP_HIGHUSER);
 		if (*page == NULL) {
 			pr_err("%d: binder_alloc_buf failed for page at %pK\n",
 				proc->pid, page_addr);
