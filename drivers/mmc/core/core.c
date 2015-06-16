4160dc2f9b5fe8f7432dcd20bf8021cda00d58f7
Talel Shenhar <tatias@codeaurora.org>
2015-06-16 15:58:35 +0300

    mmc: clk_scaling: add sysfs for avoiding scale down in write
    Change Ia7a578434d7db8983493788ab2d7f9383d703169 introduced
    a degradation in performance.
    That change tried to fix a problem for clock scaling
    during write requests.
    During write request the invalid_state is always set due to
    CMD13 that is sent after each write.
    In the attempt to fix that issue the change above added the
    ability to scale down although invalid_state was set.
    Thus, creating a favor for downscale over upscale.
    Some customers prefer performance over power.
    This change introduces a sysfs entry that will allow the
    driver to retain its old behavior which had better performance.
    
    It can be controlled from new sysfs entry:
    "scale_down_in_low_wr_load" found under
    /sys/class/mmc_host/mmcX/clk_scaling/
    The default value for it is "0". In case customers
    want to gain performance over power they should set
    it to "1".
    
    Change-Id: I8fb1f430b6287526567521382486c2b11952aa73
    Signed-off-by: Talel Shenhar <tatias@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3398,6 +3398,8 @@ static bool mmc_is_vaild_state_for_clk_scaling(struct mmc_host *host,
 	 * If the current partition type is RPMB, clock switching may not
 	 * work properly as sending tuning command (CMD21) is illegal in
 	 * this mode.
+	 * In case invalid_state is set, we forbid clock scaling, unless,
+	 * its down-scale and "scale_down_in_low_wr_load" is set.
 	 */
 	if (!card || (mmc_card_mmc(card) &&
 		card->part_curr == EXT_CSD_PART_CONFIG_ACC_RPMB) ||
