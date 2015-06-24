03b914f933ba66cede457d25bb768cd5127399e8
Piotr Kwapulinski <kwapulinski.piotr@gmail.com>
2015-06-24 16:58:39 -0700

    mm/mmap.c: optimization of do_mmap_pgoff function
    The simple check for zero length memory mapping may be performed
    earlier.  So that in case of zero length memory mapping some unnecessary
    code is not executed at all.  It does not make the code less readable
    and saves some CPU cycles.
    
    Signed-off-by: Piotr Kwapulinski <kwapulinski.piotr@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1239,10 +1239,7 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 
 	*populate = 0;
 
-	while (file && (file->f_mode & FMODE_NONMAPPABLE))
-		file = file->f_op->get_lower_file(file);
-
-        if (!len)
+	if (!len)
 		return -EINVAL;
 
 	/*
