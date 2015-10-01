9581f833af789499aaea96e8e6ba77bac5750993
anarkia1976 <stefano.villa1976@gmail.com>
2015-10-01 18:03:08 +0200

    gcc5: fix: tcp_input.c: array subscript is above array bound

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4049,7 +4049,8 @@ static void tcp_sack_remove(struct tcp_sock *tp)
 
 			/* Zap this SACK, by moving forward any other SACKS. */
 			for (i=this_sack+1; i < num_sacks; i++)
-				tp->selective_acks[i-1] = tp->selective_acks[i];
+				if (i < 4)
+					tp->selective_acks[i-1] = tp->selective_acks[i];
 			num_sacks--;
 			continue;
 		}
