12e0679bbfcce08872fa9ccc25c55f1cb0d37aff
Harshdeep Dhatt <hdhatt@codeaurora.org>
2015-10-07 16:10:36 -0600

    msm: kgsl: Clear the interrupt immediately
    Sometimes an interrupt from GPU is ignored while we
    are still executing the previous interrupt. In order
    to service any interrupt that was fired while executing
    the interrupt handler, clear the interrupt register
    immediately.
    Also, clear the ADRENO_INT_RBBM_AHB_ERROR bit not before
    but after it's serviced in its respective handler. This
    will avoid firing the main interrupt handler a second
    time.
    
    CRs-Fixed: 1072781
    Change-Id: Ie6b5a511f5b3077adae7d464de437f2aa893b0c9
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/adreno.c
    	drivers/gpu/msm/adreno.h

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -171,6 +171,10 @@ enum adreno_start_type {
 
 struct adreno_gpudev;
 
+#define ADRENO_INT_BIT(a, _bit) (((a)->gpucore->gpudev->int_bits) ? \
+		(adreno_get_int(a, _bit) < 0 ? 0 : \
+		BIT(adreno_get_int(a, _bit))) : 0)
+
 struct adreno_busy_data {
 	unsigned int gpu_busy;
 	unsigned int vbif_ram_cycles;
@@ -472,6 +476,11 @@ enum adreno_regs {
 	ADRENO_REG_REGISTER_MAX,
 };
 
+enum adreno_int_bits {
+	ADRENO_INT_RBBM_AHB_ERROR,
+	ADRENO_INT_BITS_MAX,
+};
+
 /**
  * adreno_reg_offsets: Holds array of register offsets
  * @offsets: Offset array of size defined by enum adreno_regs
@@ -487,6 +496,7 @@ struct adreno_reg_offsets {
 #define ADRENO_REG_UNUSED	0xFFFFFFFF
 #define ADRENO_REG_SKIP	0xFFFFFFFE
 #define ADRENO_REG_DEFINE(_offset, _reg) [_offset] = _reg
+#define ADRENO_INT_DEFINE(_offset, _val) ADRENO_REG_DEFINE(_offset, _val)
 
 /*
  * struct adreno_vbif_data - Describes vbif register value pair
@@ -623,6 +633,7 @@ struct adreno_gpudev {
 	 * so define them in the structure and use them as variables.
 	 */
 	const struct adreno_reg_offsets *reg_offsets;
+	unsigned int *const int_bits;
 	const struct adreno_ft_perf_counters *ft_perf_counters;
 	unsigned int ft_perf_counters_count;
 
@@ -1009,6 +1020,23 @@ static inline unsigned int adreno_getreg(struct adreno_device *adreno_dev,
 	if (!adreno_checkreg_off(adreno_dev, offset_name))
 		return ADRENO_REG_REGISTER_MAX;
 	return gpudev->reg_offsets->offsets[offset_name];
+}
+
+/*
+ * adreno_get_int() - Returns the offset value of an interrupt bit from
+ * the interrupt bit array in the gpudev node
+ * @adreno_dev:		Pointer to the the adreno device
+ * @bit_name:		The interrupt bit enum whose bit is returned
+ */
+static inline unsigned int adreno_get_int(struct adreno_device *adreno_dev,
+				enum adreno_int_bits bit_name)
+{
+	struct adreno_gpudev *gpudev = ADRENO_GPU_DEVICE(adreno_dev);
+
+	if (bit_name >= ADRENO_INT_BITS_MAX)
+		return -ERANGE;
+
+	return gpudev->int_bits[bit_name];
 }
 
 /**
