09a7e33084b3514061c4ae7b5f3ea7f65cc4647b
Patrick Daly <pdaly@codeaurora.org>
2015-10-16 12:58:52 -0700

    cpuidle: Add need_resched() check
    The cpuidle framework is disabled as part of suspend. In this scenario,
    the cpu may enter wfi without checking the need_resched() flag.
    Prevent this from occuring.
    
    CRs-fixed: 920501
    Change-Id: Ib2077833279d84b2aea25c61198dfbdcf4566ea4
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -108,6 +108,11 @@ int cpuidle_idle_call(void)
 	struct cpuidle_driver *drv;
 	int next_state, entered_state;
 
+	if (need_resched()) {
+		local_irq_enable();
+		return 0;
+	}
+
 	if (off)
 		return -ENODEV;
 
