8dedc1d76408628f99a619a50ca1c4411e46b378
anarkia1976 <stefano.villa1976@gmail.com>
2015-10-30 21:59:07 +0100

    soc: qcom: msm_perf: prevent perfd to change workload
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -1048,6 +1048,10 @@ static int set_workload_detect(const char *buf, const struct kernel_param *kp)
 	unsigned int val, i;
 	struct cluster *i_cl;
 	unsigned long flags;
+	int msm_perf = strcmp(current->comm, "perfd");
+
+       if (msm_perf == 0)
+               return -EINVAL;
 
 	if (!clusters_inited)
 		return -EINVAL;
