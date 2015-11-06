d00a74e0327eb304f3682686d2c4a3d12f00dd38
Tetsuo Handa <penguin-kernel@i-love.sakura.ne.jp>
2015-11-05 18:47:51 -0800

    UPSTREAM: mm/oom_kill.c: fix potentially killing unrelated process
    At the for_each_process() loop in oom_kill_process(), we are comparing
    address of OOM victim's mm without holding a reference to that mm.  If
    there are a lot of processes to compare or a lot of "Kill process %d (%s)
    sharing same memory" messages to print, for_each_process() loop could take
    very long time.
    
    It is possible that meanwhile the OOM victim exits and releases its mm,
    and then mm is allocated with the same address and assigned to some
    unrelated process.  When we hit such race, the unrelated process will be
    killed by error.  To make sure that the OOM victim's mm does not go away
    until for_each_process() loop finishes, get a reference on the OOM
    victim's mm before calling task_unlock(victim).
    
    [oleg@redhat.com: several fixes]
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    BUG=chromium:727707
    TEST=Build and boot Caroline, try to load it to start ooming
    
    (cherry picked from commit 880b768937e90c433c0c8254a22b1eb63df005a4)
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Change-Id: If1ae193d756ee952aa253b91ca794d4a6926bd3d
    Reviewed-on: https://chromium-review.googlesource.com/521811
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -519,8 +519,9 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 		victim = p;
 	}
 
-	/* mm cannot safely be dereferenced after task_unlock(victim) */
+	/* Get a reference to safely compare mm after task_unlock(victim) */
 	mm = victim->mm;
+	atomic_inc(&mm->mm_count);
 	/*
 	 * We should send SIGKILL before setting TIF_MEMDIE in order to prevent
 	 * the OOM victim from depleting the memory reserves from the user
@@ -559,6 +560,7 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 		}
 	rcu_read_unlock();
 
+	mmdrop(mm);
 	put_task_struct(victim);
 }
 #undef K
