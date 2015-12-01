f0841045514b2f8a5305aae90e1bce061e5775f2
Senthil Kumar Rajagopal <skrajago@codeaurora.org>
2015-12-01 03:40:21 -0500

    [PATCH] Fix extern inline use for gcc > 4.3 in c99 mode

--- lib/mpi/mpi-inline.h
+++ lib/mpi/mpi-inline.h
@@ -30,7 +30,11 @@
 #define G10_MPI_INLINE_H
 
 #ifndef G10_MPI_INLINE_DECL
-#define G10_MPI_INLINE_DECL  extern inline
+#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
+#define G10_MPI_INLINE_DECL extern inline __attribute__ ((__gnu_inline__))
+#else
+#define G10_MPI_INLINE_DECL extern inline
+#endif
 #endif
 
 G10_MPI_INLINE_DECL mpi_limb_t
