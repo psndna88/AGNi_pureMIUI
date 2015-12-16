49fc9e7635dc78dbae19aa8af2facc0e9eafdb79
Todd Kjos <tkjos@google.com>
2015-12-16 13:49:37 -0800

    Notify scheduler of cpuidle state changes
    Change-Id: If48c8c5f2347113d90fa40e7a4a335e1fb57ce69
    Signed-off-by: Joe Maples <joe@frap129.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9027,15 +9027,4 @@ sched_set_cpu_cstate(int cpu, int cstate, struct cpuidle_state *cpuidle,
 
 	idle_set_state_idx(rq, cstate);
 	idle_set_state(rq, cpuidle);
-	/*
-	if ((cstate == 0 && wakeup_energy == 0 && wakeup_latency == 0) ||
-	    (cstate == 1 && cpu < 4 && wakeup_energy == 5800 && wakeup_latency == 50) ||
-	    (cstate == 1 && cpu >= 4 && wakeup_energy == 24120 && wakeup_latency == 60) ||
-	    (cstate == 3 && cpu >= 4 && wakeup_energy == 155775 && wakeup_latency == 475) ||
-	    (cstate == 4 && cpu < 4 && wakeup_energy == 46400 && wakeup_latency == 480) ||
-	    (cstate == 4 && cpu >= 4 && wakeup_energy == 192156 && wakeup_latency == 636)) {
-	} else {
-		pr_info("TJK: CPU%d ->cstate=%d  nrg=%d latency=%d\n", cpu, cstate, wakeup_energy, wakeup_latency);
-	}
-	*/
 }
