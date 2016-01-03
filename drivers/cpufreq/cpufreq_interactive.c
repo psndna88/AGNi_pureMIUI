2acbac32c945899866d7cee6ec1b1fa69bcbcf29
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-03 22:08:55 +0000

    cpufreq: interactive: Consider changed min limit a frequency boost
    A changed policy->min can safely be considered a frequency boost.
    Process this change analogous to the cpufreq_interactive_boost()
    routine.
    
    Picked from https://github.com/CyanogenMod/android_kernel_lge_hammerhead/commit/b09fb164a64b07c2d993542e6747ba720a60a6c9 and adapted for Angler.
    
    Change-Id: Ie4ca2be2dad0f5bd541c2b79afee3bad3a349d80
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1539,6 +1539,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 	struct cpufreq_frequency_table *freq_table;
 	struct cpufreq_interactive_tunables *tunables;
 	unsigned long flags;
+	unsigned int anyboost;
 
 	if (have_governor_per_policy())
 		tunables = policy->governor_data;
@@ -1675,10 +1676,12 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		down_read(&ppol->enable_sem);
 		if (ppol->governor_enabled) {
 			spin_lock_irqsave(&ppol->target_freq_lock, flags);
-			if (policy->max < ppol->target_freq)
+			if (policy->max < ppol->target_freq) {
 				ppol->target_freq = policy->max;
-			else if (policy->min > ppol->target_freq)
+			} else if (policy->min > ppol->target_freq) {
 				ppol->target_freq = policy->min;
+				anyboost = 1;
+			}
 			spin_unlock_irqrestore(&ppol->target_freq_lock, flags);
 
 			if (policy->min < ppol->min_freq)
@@ -1688,6 +1691,17 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		}
 
 		up_read(&ppol->enable_sem);
+
+		if (anyboost) {
+			u64 now = ktime_to_us(ktime_get());
+
+			cpumask_set_cpu(policy->cpu, &speedchange_cpumask);
+			ppol->hispeed_validate_time = now;
+			ppol->floor_freq = policy->min;
+			ppol->floor_validate_time = now;
+
+			wake_up_process(speedchange_task);
+		}
 
 		break;
 	}
