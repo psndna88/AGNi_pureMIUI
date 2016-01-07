e97e90c8d4cbe59b61b49ac945d645670aa9588f
Walter Yang <yandongy@codeaurora.org>
2016-01-07 12:19:54 +0800

    ASoC: msm8x16-wcd: Update HPH PA gain for Tombak
    HPH PA gain is configurable and same as Conga, 0dB or -4.5 dB.
    And the impedance-gain mapping rule is also the same as Conga.
    Update the HPH PA gain for Tombak as well.
    
    CRs-Fixed: 958890
    Change-Id: I49901ab36d39b37846c10e52c3fd5f8e0e2ae5cd
    Signed-off-by: Walter Yang <yandongy@codeaurora.org>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -3910,8 +3910,6 @@ void wcd_imped_config(struct snd_soc_codec *codec,
 		switch (codec_version) {
 		case TOMBAK_1_0:
 		case TOMBAK_2_0:
-			pr_debug("%s: Default gain is set\n", __func__);
-			break;
 		case CONGA:
 			/*
 			 * For 32Ohm load and higher loads, Set 0x19E
