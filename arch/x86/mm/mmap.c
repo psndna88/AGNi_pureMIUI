2a1d9956cf72d71ef736973dacbfed203918b469
dcashman <dcashman@google.com>
2016-02-24 13:31:22 -0800

    BACKPORT: FROMLIST: mm: ASLR: use get_random_long()
    (cherry picked from commit https://lkml.org/lkml/2016/2/4/833)
    
    Replace calls to get_random_int() followed by a cast to (unsigned long)
    with calls to get_random_long().  Also address shifting bug which, in case
    of x86 removed entropy mask for mmap_rnd_bits values > 31 bits.
    
    Bug: 26963541
    Signed-off-by: Daniel Cashman <dcashman@android.com>
    Signed-off-by: Daniel Cashman <dcashman@google.com>
    Change-Id: Iac34b63294ec120edcbf8760186667a84a5cf556
    
    Conflicts:
    	arch/x86/mm/mmap.c
    Signed-off-by: Ashish Kotnala <ashishkotnala1.ak@gmail.com>

--- arch/x86/mm/mmap.c
+++ arch/x86/mm/mmap.c
@@ -75,9 +75,13 @@ static unsigned long mmap_rnd(void)
 	*/
 	if (current->flags & PF_RANDOMIZE) {
 		if (mmap_is_ia32())
-			rnd = get_random_int() % (1<<8);
+#ifdef CONFIG_COMPAT
+			rnd = get_random_long() & ((1UL << mmap_rnd_compat_bits) - 1);
+#else
+			rnd = get_random_long() & ((1UL << mmap_rnd_bits) - 1);
+#endif
 		else
-			rnd = get_random_int() % (1<<28);
+			rnd = get_random_long() & ((1UL << mmap_rnd_bits) - 1);
 	}
 	return rnd << PAGE_SHIFT;
 }
