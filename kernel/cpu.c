151f2c768aed3364d7d614b2780da3dddaaac7c4
Nick Desaulniers <ndesaulniers@google.com>
2016-02-24 10:36:43 +0530

    Silences PCIe and CPU suspend state kernel messages Bug: 27134656 Change-Id: I681ec2171472514489365ca4bfc4ef16d9b344fe
    # Conflicts:
    #	kernel/cpu.c

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -522,7 +522,7 @@ int disable_nonboot_cpus(void)
 	 */
 	cpumask_clear(frozen_cpus);
 
-	printk("Disabling non-boot CPUs ...\n");
+	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
 			continue;
@@ -566,14 +566,14 @@ void __ref enable_nonboot_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	printk(KERN_INFO "Enabling non-boot CPUs ...\n");
+	pr_debug("Enabling non-boot CPUs ...\n");
 
 	arch_enable_nonboot_cpus_begin();
 
 	for_each_cpu(cpu, frozen_cpus) {
 		error = _cpu_up(cpu, 1);
 		if (!error) {
-			printk(KERN_INFO "CPU%d is up\n", cpu);
+			pr_debug(KERN_INFO "CPU%d is up\n", cpu);
 			cpu_device = get_cpu_device(cpu);
 			if (!cpu_device)
 				pr_err("%s: failed to get cpu%d device\n",
