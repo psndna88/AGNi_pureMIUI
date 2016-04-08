ed631ac32732d0cf13e558f2ce88e4a5222e132e
David S. Miller <davem@davemloft.net>
2016-04-07 20:40:25 -0400

    net: Fix build failure due to lockdep_sock_is_held().
    Needs to be protected with CONFIG_LOCKDEP.
    
    Based upon a patch by Hannes Frederic Sowa.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1484,6 +1484,7 @@ do {									\
 	lockdep_init_map(&(sk)->sk_lock.dep_map, (name), (key), 0);	\
 } while (0)
 
+#ifdef CONFIG_LOCKDEP
 static inline bool lockdep_sock_is_held(const struct sock *csk)
 {
 	struct sock *sk = (struct sock *)csk;
@@ -1491,6 +1492,7 @@ static inline bool lockdep_sock_is_held(const struct sock *csk)
 	return lockdep_is_held(&sk->sk_lock) ||
 	       lockdep_is_held(&sk->sk_lock.slock);
 }
+#endif
 
 extern void lock_sock_nested(struct sock *sk, int subclass);
 
