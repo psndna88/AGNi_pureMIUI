6b6f889ba7db9d6e25b647d42cd78ec2d98afb85
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-05-05 16:19:00 +0800

    mdss: livedisplay: Use CABC command chain for CE
    Some devices make use of the same command for both colour enhance
    and content adaptive backlight control. This allows such devices
    to be able to make use of both colour enhance and CABC simultaneously.
    
    Change-Id: I99e1ee1d1371466381525e38cb83ce72ce9aeda4

--- drivers/video/msm/mdss/mdss_livedisplay.h
+++ drivers/video/msm/mdss/mdss_livedisplay.h
@@ -31,6 +31,7 @@ struct mdss_livedisplay_ctx {
 	uint8_t sre_medium_value;
 	uint8_t sre_strong_value;
 	uint8_t aco_value;
+	uint8_t cabc_ce_value;
 
 	const uint8_t *ce_off_cmds;
 	const uint8_t *ce_on_cmds;
@@ -90,6 +91,7 @@ enum {
 	MODE_COLOR_ENHANCE	= 0x08,
 	MODE_PRESET		= 0x10,
 	MODE_RGB		= 0x20,
+	MODE_CABC_COLOR_ENHANCE	= 0x40,
 	MODE_UPDATE_ALL		= 0xFF,
 };
 
@@ -99,7 +101,10 @@ int mdss_livedisplay_create_sysfs(struct msm_fb_data_type *mfd);
 
 static inline bool is_cabc_cmd(uint32_t value)
 {
-    return (value & MODE_CABC) || (value & MODE_SRE) || (value & MODE_AUTO_CONTRAST);
+	return (value & MODE_CABC) ||
+			(value & MODE_SRE) ||
+			(value & MODE_AUTO_CONTRAST) ||
+			(value & MODE_CABC_COLOR_ENHANCE);
 }
 
 static inline struct mdss_livedisplay_ctx* get_ctx(struct msm_fb_data_type *mfd)
