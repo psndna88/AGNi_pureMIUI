28c1a9282d97f1a29aedc57bb76022090b7f8a6c
Sivaram Vempati <svempati@codeaurora.org>
2016-07-21 10:34:01 -0700

    msm: thermal: Update the hotplug initialization
    Hotplug initialization will trigger a hotplug if temperature stays above
    the trip threshold and will try to online if temperature is below the
    clear threshold. Since KTM does boot mitigation till thermal-engine is
    running, hotplug init can be called twice. First from post init script
    and second when the hotplug task is initialized. There is a possiblity
    the first init call can set the hotplug local mask and the second init
    call wont clear or update the mask with correct value if the temperature
    is between the hotplug trip and clear threshold.
    
    Update the hotplug init API to perform the check only if the hotplug
    task is initialized. Also the hotplug check will clear the hotplug local
    mask if the temperature is not above the trip threshold.
    
    Bug: 29464076
    Change-Id: Ica1325f8aa65c338ea0e5b201f566607c3ddf904
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    Signed-off-by: Sivaram Vemapti <svempati@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3473,7 +3473,7 @@ static int hotplug_init_cpu_offlined(void)
 	long temp = 0;
 	uint32_t cpu = 0;
 
-	if (!hotplug_enabled)
+	if (!hotplug_enabled || !hotplug_task)
 		return 0;
 
 	mutex_lock(&core_control_mutex);
@@ -3490,8 +3490,7 @@ static int hotplug_init_cpu_offlined(void)
 
 		if (temp >= msm_thermal_info.hotplug_temp_degC)
 			cpus[cpu].offline = 1;
-		else if (temp <= (msm_thermal_info.hotplug_temp_degC -
-			msm_thermal_info.hotplug_temp_hysteresis_degC))
+		else
 			cpus[cpu].offline = 0;
 	}
 	mutex_unlock(&core_control_mutex);
