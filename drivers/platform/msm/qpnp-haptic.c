09411af1c09a99fd4cb2f83a11106b4c70f0fbdb
Sultanxda <sultanxda@gmail.com>
2016-08-02 23:59:42 -0700

    msm: qpnp-haptic: Process haptics asynchronously
    Blocking the timed_output sysfs node due to mutex contention causes severe
    device wake-up latency, as a process crucial to system resume writes to the
    timed_output vibrator node every time the device is resumed via the
    fingerprint reader (fingerprint reader triggers a haptic response on
    success).
    
    By processing haptics asynchronously via a worker, the timed_output sysfs
    won't get stalled for long periods of time (>30ms), and thus the device
    will consistently wake from sleep faster.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    msm: qpnp-haptic: Prevent redundant calls to disable vibrator
    
    A redundant call to disable the vibrator right before something wants to
    enable it results in lack of vibration.
    
    Avoid re-disabling haptics to fix this.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    msm: qpnp-haptic: Start shutoff timer after haptics actually start
    
    qpnp_hap_set() sometimes lags when enabling haptics, causing the shutoff
    timer to kill the haptics prematurely.
    
    Start the shutoff timer after haptics are enabled so that the requested
    duration is actually carried out.
    
    Change-Id: I91bad94d18415476b5b05c1e1a4a60ae180947df
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -296,6 +296,8 @@ struct qpnp_hap {
 	struct mutex lock;
 	struct mutex wf_lock;
 	struct mutex set_lock;
+	spinlock_t td_lock;
+	struct work_struct td_work;
 	struct completion completion;
 	enum qpnp_hap_mode play_mode;
 	enum qpnp_hap_auto_res_mode auto_res_mode;
@@ -337,6 +339,7 @@ struct qpnp_hap {
 	bool sup_brake_pat;
 	bool correct_lra_drive_freq;
 	bool misc_trim_error_rc19p2_clk_reg_present;
+	int td_value;
 };
 
 static struct qpnp_hap *ghap;
@@ -1656,11 +1659,20 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 	return rc;
 }
 
-/* enable interface from timed output class */
-static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
+static void qpnp_timed_enable_worker(struct work_struct *work)
 {
-	struct qpnp_hap *hap = container_of(dev, struct qpnp_hap,
-					 timed_dev);
+	struct qpnp_hap *hap = container_of(work, struct qpnp_hap,
+					 td_work);
+	int value;
+
+	spin_lock(&hap->td_lock);
+	value = hap->td_value;
+	spin_unlock(&hap->td_lock);
+
+	/* Vibrator already disabled */
+	if (!value && !hap->state)
+		return;
+
 	flush_work(&hap->work);
 
 	mutex_lock(&hap->lock);
@@ -1676,15 +1688,30 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 		value = (value > hap->timeout_ms ?
 				 hap->timeout_ms : value);
 		hap->state = 1;
-		hrtimer_start(&hap->hap_timer,
-			      ktime_set(value / 1000, (value % 1000) * 1000000),
-			      HRTIMER_MODE_REL);
 	}
 	mutex_unlock(&hap->lock);
 	if (hap->play_mode == QPNP_HAP_DIRECT)
 		qpnp_hap_set(hap, hap->state);
 	else
 		schedule_work(&hap->work);
+
+	if (value)
+		hrtimer_start(&hap->hap_timer,
+			      ktime_set(value / 1000, (value % 1000) * 1000000),
+			      HRTIMER_MODE_REL);
+}
+
+/* enable interface from timed output class */
+static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
+{
+	struct qpnp_hap *hap = container_of(dev, struct qpnp_hap,
+					 timed_dev);
+
+	spin_lock(&hap->td_lock);
+	hap->td_value = value;
+	spin_unlock(&hap->td_lock);
+
+	schedule_work(&hap->td_work);
 }
 
 /* play pwm bytes */
@@ -1798,6 +1825,10 @@ static enum hrtimer_restart qpnp_hap_timer(struct hrtimer *timer)
 {
 	struct qpnp_hap *hap = container_of(timer, struct qpnp_hap,
 							 hap_timer);
+
+	/* Vibrator already disabled */
+	if (!hap->state)
+		return HRTIMER_NORESTART;
 
 	hap->state = 0;
 	schedule_work(&hap->work);
@@ -2389,10 +2420,12 @@ static int qpnp_haptic_probe(struct spmi_device *spmi)
 	mutex_init(&hap->lock);
 	mutex_init(&hap->wf_lock);
 	mutex_init(&hap->set_lock);
+	spin_lock_init(&hap->td_lock);
 
 	INIT_WORK(&hap->work, qpnp_hap_worker);
 	INIT_DELAYED_WORK(&hap->sc_work, qpnp_handle_sc_irq);
 	init_completion(&hap->completion);
+	INIT_WORK(&hap->td_work, qpnp_timed_enable_worker);
 
 	hrtimer_init(&hap->hap_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hap->hap_timer.function = qpnp_hap_timer;
