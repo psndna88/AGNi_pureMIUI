a93b1021900593823ea1e2a6a06e336f9b1dcaab
Francisco Franco <franciscofranco.1990@gmail.com>
2016-08-26 00:13:56 +0000

    cpufreq: cpu-boost: boost the input boost notifier to max prio
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -186,6 +186,7 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 
 static struct notifier_block boost_adjust_nb = {
 	.notifier_call = boost_adjust_notify,
+	.priority = INT_MAX,
 };
 
 static void do_boost_rem(struct work_struct *work)
