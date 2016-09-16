4ecfcf6e25851d1331764fcbdc9ea8b4729cb08e
Paul Reioux <reioux@gmail.com>
2016-09-16 20:25:49 +0530

    Add PowerSuspend Driver
    Changed Files:
    - drivers/video/msm/mdss/mdss_dsi_panel.c
    - include/linux/powersuspend.h
    - kernel/power/Kconfig
    - kernel/power/Makefile
    - kernel/power/powersuspend.c
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -26,6 +26,10 @@
 #include "mdss_dba_utils.h"
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -748,6 +752,13 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	display_on = true;
 
+	#ifdef CONFIG_LAZYPLUG
+		lazyplug_enter_lazy(false); 
+	#endif
+
+	#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+	#endif
 
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -857,6 +868,14 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 #endif
 
 	display_on = false;
+
+	#ifdef CONFIG_LAZYPLUG
+		lazyplug_enter_lazy(true);
+	#endif
+
+	#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+	#endif
 
 end:
 	pr_debug("%s:-\n", __func__);
