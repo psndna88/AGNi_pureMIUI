fccef257a5371bcfc5c37bd233991e2a8ca2bee2
psndna88 <psndna88@gmail.com>
2016-09-23 19:40:05 +0530

    selinux: add security in-core xattr support for esdfs,f2fs,tmpfs,exfat

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -436,7 +436,11 @@ static int sb_finish_set_opts(struct super_block *sb)
 	if (!strcmp(sb->s_type->name, "sysfs") ||
 	    !strcmp(sb->s_type->name, "pstore") ||
 	    !strcmp(sb->s_type->name, "debugfs") ||
-	    !strcmp(sb->s_type->name, "rootfs"))
+	    !strcmp(sb->s_type->name, "rootfs") ||
+	    !strcmp(sb->s_type->name, "esdfs") ||
+	    !strcmp(sb->s_type->name, "f2fs") ||
+	    !strcmp(sb->s_type->name, "tmpfs") ||
+	    !strcmp(sb->s_type->name, "exfat"))
 		sbsec->flags |= SE_SBLABELSUPP;
 
 	/*
