e205eaa733aeddf79fa27178b10dde337a16d5ab
psndna88 <psndna88@gmail.com>
2016-09-23 18:17:38 +0530

    drivers/cpufreq: Implement Zzmoove 1.0 beta8 for hexacore Snapdragon devices

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -442,6 +442,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
