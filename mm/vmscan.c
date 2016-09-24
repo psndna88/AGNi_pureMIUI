a7b7d09b7ac387262a46a3a7fb9f8c40121f4fdc
psndna88 <psndna88@gmail.com>
2016-09-24 10:12:02 +0530

    Swappiness set to 60

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -137,7 +137,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 30;
+int vm_swappiness = 60;
 unsigned long vm_total_pages;	/* The total number of pages which the VM controls */
 
 #ifdef CONFIG_KSWAPD_CPU_AFFINITY_MASK
