0ac9ffd752c8ff63a9eec66ed545d26343ffed7a
Sunil Khatri <sunilkh@codeaurora.org>
2016-10-04 18:53:22 +0530

    msm: kgsl: Disable UCHE global filter
    Disable UCHE global filter for invalidate/flush,
    SP will handle the invalidate/flush for each SP
    independently on A5XX GPUs.
    
    CRs-Fixed: 1073853
    Change-Id: I3daf3722cfbdfff080161f9a0248fd8096550328
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>
    Signed-off-by: Sudeep Yedalapure <sudeepy@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/a5xx_reg.h
    	drivers/gpu/msm/adreno_a5xx.c

--- drivers/gpu/msm/a5xx_reg.h
+++ drivers/gpu/msm/a5xx_reg.h
@@ -629,6 +629,7 @@
 #define A5XX_VPC_PERFCTR_VPC_SEL_3          0xE67
 
 /* UCHE registers */
+#define A5XX_UCHE_MODE_CNTL                 0xE81
 #define A5XX_UCHE_WRITE_THRU_BASE_LO        0xE87
 #define A5XX_UCHE_WRITE_THRU_BASE_HI        0xE88
 #define A5XX_UCHE_TRAP_BASE_LO              0xE89
