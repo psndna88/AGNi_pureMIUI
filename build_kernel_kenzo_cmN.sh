8c8dcb13220fca22b3eb282f74ce7efaad26bc50
psndna88 <psndna88@gmail.com>
2016-10-30 16:12:55 +0530

    Update AGNi pureCM-N v2.6r3 configs

--- build_kernel_kenzo_cmN.sh
+++ build_kernel_kenzo_cmN.sh
@@ -7,36 +7,46 @@ export KERNELDIR=`readlink -f .`
 #. ~/gcc-7.x-uber_aarch64.sh
 
 export ARCH=arm64
-mv .git .git-halt
 
 echo ""
 echo " Cross-compiling AGNi pureCM-N kernel ..."
 echo ""
+
 cd $KERNELDIR/
-rm $KERNELDIR/arch/arm64/boot/Image
-rm $KERNELDIR/arch/arm64/boot/Image.gz
-rm $KERNELDIR/arch/arm64/boot/Image.gz-dtb
-rm $KERNELDIR/arch/arm/boot/dts/*.dtb
-rm $KERNELDIR/drivers/staging/prima/wlan.ko
 
 if [ ! -f $KERNELDIR/.config ];
 then
 make agni_kenzo-cmN_defconfig
 fi
 
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+rm $KERNELDIR/drivers/staging/prima/wlan.ko
 make -j3 || exit 1
+mv .git-halt .git
 
 rm -rf $KERNELDIR/BUILT_kenzo-cmN
 mkdir -p $KERNELDIR/BUILT_kenzo-cmN
 
 find -name '*.ko' -exec mv -v {} $KERNELDIR/BUILT_kenzo-cmN/ \;
-cp $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo-cmN/
+#find -name '*.ko' -exec mv -v {} $KERNELDIR/BUILT_kenzo-cmN/system/lib/modules/ \;
+
+# Goodix X
+mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo-cmN/
+
+# Goodix V
+echo ""
+git apply goodix.patch && echo "   Goodix Patch applied ..."
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
+mv .git-halt .git
+mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo-cmN/Image.gz-dtb_goodix
+git apply -R goodix.patch && echo "   Goodix Patch Cleaned UP."
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+rm $KERNELDIR/arch/arm64/boot/Image
+rm $KERNELDIR/arch/arm64/boot/Image.gz
 
 echo ""
-echo " BUILT_kenzo-cmN is ready."
-
-mv .git-halt .git
-
 echo "AGNi pureCM-N has been built for kenzo !!!"
-
 
--- build_kernel_kenzo_cmNwm.sh
+++ build_kernel_kenzo_cmNwm.sh
@@ -7,35 +7,45 @@ export KERNELDIR=`readlink -f .`
 #. ~/gcc-7.x-uber_aarch64.sh
 
 export ARCH=arm64
-mv .git .git-halt
 
 echo ""
 echo " Cross-compiling AGNi pureCM-Nwm kernel ..."
 echo ""
+
 cd $KERNELDIR/
-rm $KERNELDIR/arch/arm64/boot/Image
-rm $KERNELDIR/arch/arm64/boot/Image.gz
-rm $KERNELDIR/arch/arm64/boot/Image.gz-dtb
-rm $KERNELDIR/arch/arm/boot/dts/*.dtb
 
 if [ ! -f $KERNELDIR/.config ];
 then
 make agni_kenzo-cmNwm_defconfig
 fi
 
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+rm $KERNELDIR/drivers/staging/prima/wlan.ko
 make -j3 || exit 1
+mv .git-halt .git
 
 rm -rf $KERNELDIR/BUILT_kenzo-cmNwm
 mkdir -p $KERNELDIR/BUILT_kenzo-cmNwm/system/lib/modules/
 
 find -name '*.ko' -exec mv -v {} $KERNELDIR/BUILT_kenzo-cmNwm/system/lib/modules/ \;
-cp $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo-cmNwm/
+
+# Goodix X
+mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo-cmNwm/
+
+# Goodix V
+echo ""
+git apply goodix.patch && echo "   Goodix Patch applied ..."
+mv .git .git-halt
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+make -j3 || exit 1
+mv .git-halt .git
+mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/BUILT_kenzo-cmNwm/Image.gz-dtb_goodix
+git apply -R goodix.patch && echo "   Goodix Patch Cleaned UP."
+rm $KERNELDIR/arch/arm/boot/dts/*.dtb
+rm $KERNELDIR/arch/arm64/boot/Image
+rm $KERNELDIR/arch/arm64/boot/Image.gz
 
 echo ""
-echo " BUILT_kenzo-cmNwm is ready."
-
-mv .git-halt .git
-
-echo "AGNi pureCM-N has been built for kenzo !!!"
-
+echo "AGNi pureCM-Nwm has been built for kenzo !!!"
 
