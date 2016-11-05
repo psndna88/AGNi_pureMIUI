eb7f8ae51738d145d0d60cecc405c76d39727aeb
MEGAX91 <blackdeath1370@gmail.com>
2016-11-05 11:07:29 +0330

    mdss_fb: backlight dimmer option (default off)
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -80,7 +80,9 @@
 					out = ((v) * (v) * 255  / 4095 + (v) * (255 - (v)) / 32);\
 					} while (0)
 
-bool backlight_dimmer = true;
+
+bool backlight_dimmer = false;
+
 module_param(backlight_dimmer, bool, 0755);
 
 static struct fb_info *fbi_list[MAX_FBI_LIST];
