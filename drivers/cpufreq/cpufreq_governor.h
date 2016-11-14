edd88d3a1a7ca5267e316d46b48aee1eba59b22a
psndna88 <psndna88@gmail.com>
2016-11-14 18:18:25 +0530

    cpufreq: Add Chill cpu governor

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -222,7 +222,11 @@ struct cs_dbs_tuners {
 	unsigned int sampling_down_factor;
 	unsigned int up_threshold;
 	unsigned int down_threshold;
+	unsigned int down_threshold_suspended;
 	unsigned int freq_step;
+	unsigned int sleep_depth;
+	unsigned int boost_enabled;
+	unsigned int boost_count;
 };
 
 struct ex_dbs_tuners {
--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -448,6 +448,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
+extern struct cpufreq_governor cpufreq_gov_chill;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
 #endif
 
 /*********************************************************************
