21b37e87d07041c9e909246a391bb2a4eb26eb80
Francis Yan <francisyyan@gmail.com>
2016-11-27 23:07:17 -0800

    [BACKPORT] tcp: export sender limits chronographs to TCP_INFO
    This patch exports all the sender chronograph measurements collected
    in the previous patches to TCP_INFO interface. Note that busy time
    exported includes all the other sending limits (rwnd-limited,
    sndbuf-limited). Internally the time unit is jiffy but externally
    the measurements are in microseconds for future extensions.
    
    Signed-off-by: Francis Yan <francisyyan@gmail.com>
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2676,6 +2676,25 @@ int compat_tcp_setsockopt(struct sock *sk, int level, int optname,
 EXPORT_SYMBOL(compat_tcp_setsockopt);
 #endif
 
+static void tcp_get_info_chrono_stats(const struct tcp_sock *tp,
+				      struct tcp_info *info)
+{
+	u64 stats[__TCP_CHRONO_MAX], total = 0;
+	enum tcp_chrono i;
+
+	for (i = TCP_CHRONO_BUSY; i < __TCP_CHRONO_MAX; ++i) {
+		stats[i] = tp->chrono_stat[i - 1];
+		if (i == tp->chrono_type)
+			stats[i] += tcp_time_stamp - tp->chrono_start;
+		stats[i] *= USEC_PER_SEC / HZ;
+		total += stats[i];
+	}
+
+	info->tcpi_busy_time = total;
+	info->tcpi_rwnd_limited = stats[TCP_CHRONO_RWND_LIMITED];
+	info->tcpi_sndbuf_limited = stats[TCP_CHRONO_SNDBUF_LIMITED];
+}
+
 /* Return information about state of tcp endpoint in API format. */
 void tcp_get_info(struct sock *sk, struct tcp_info *info)
 {
@@ -2760,6 +2779,7 @@ void tcp_get_info(struct sock *sk, struct tcp_info *info)
 	/* Expose reference count for socket */
 	if (sk->sk_socket) {
 		struct file *filep = sk->sk_socket->file;
+	tcp_get_info_chrono_stats(tp, info);
 
 		if (filep)
 			info->tcpi_count = file_count(filep);
