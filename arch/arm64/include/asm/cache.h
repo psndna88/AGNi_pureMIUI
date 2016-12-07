0b43f23d9f2b8a3ed01d905c9f3e1dea32bef40b
Catalin Marinas <catalin.marinas@arm.com>
2016-12-07 15:10:41 +0530

    arm64: cache: change ARCH_DMA_MINALIGN and L1_CACHE_SHIFT values
    This reverts commit 2f1c27479e8a641d654d7f6696bd326ad80e109e
    and also sets ARM_DMA_MINALIGN to 128.
    ARCH_DMA_MINALIGN is dependent on L1_CACHE_SHIFT but it should
    be set to maximum *known* cache line size on ARMv8 systems to
    avoid DMA coherecy issues. So setting ARM_DMA_MINALIGN to 128.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Change-Id: I3fcdba32d0bf574f0af7c924ae926dd47df75e07
    Patch-mainline: linux-kernel @ 21/03/16, 17:14:03
    Signed-off-by: Imran Khan <kimran@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- arch/arm64/include/asm/cache.h
+++ arch/arm64/include/asm/cache.h
@@ -23,12 +23,12 @@
 
 /*
  * Memory returned by kmalloc() may be used for DMA, so we must make
- * sure that all such allocations are cache aligned. Otherwise,
- * unrelated code may cause parts of the buffer to be read into the
- * cache before the transfer is done, causing old data to be seen by
- * the CPU.
+ * sure that all such allocations are aligned to the maximum *known*
+ * cache line size on ARMv8 systems. Otherwise, unrelated code may cause
+ * parts of the buffer to be read into the cache before the transfer is
+ * done, causing old data to be seen by the CPU.
  */
-#define ARCH_DMA_MINALIGN	L1_CACHE_BYTES
+#define ARCH_DMA_MINALIGN	(128)
 
 #ifndef __ASSEMBLY__
 
