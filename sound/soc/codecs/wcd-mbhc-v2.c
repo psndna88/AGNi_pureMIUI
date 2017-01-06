8c06e9d1b8253427d0615c5ac3c337473a4a7c47
Yeleswarapu Nagaradhesh <nagaradh@codeaurora.org>
2017-01-06 18:57:07 +0530

    ASoC: wcd-mbhc: disable electrical interrupts when cable type is NONE
    Electrical interrupts are not disabled when cable type is NONE,
    due to which SW is getting spurious interrupts when cable is removed.
    Disable electrical interrupts if cable type is NONE.
    
    CRs-Fixed: 1072485
    Change-Id: I5eb77eba3999aff929b659b783f3ed0776530479
    Signed-off-by: Yeleswarapu Nagaradhesh <nagaradh@codeaurora.org>
    
    Conflicts:
    	sound/soc/codecs/wcd-mbhc-v2.c
    
    # Conflicts:
    #	sound/soc/codecs/wcd-mbhc-v2.c

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
  * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -1627,6 +1627,9 @@ static void wcd_mbhc_swch_irq_handler(struct wcd_mbhc *mbhc)
 		/* Disable HW FSM */
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_FSM_EN, 0);
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
+		wcd_mbhc_hs_elec_irq(mbhc, WCD_MBHC_ELEC_HS_INS, false);
+		wcd_mbhc_hs_elec_irq(mbhc, WCD_MBHC_ELEC_HS_REM, false);
+
 	}
 
 	mbhc->in_swch_irq_handler = false;
