65bb64713428e662e627d3f470b98a660bc2ae2b
umang96 <umangleekha3@gmail.com>
2017-01-11 19:06:27 +0530

    zram: Max compression streams 4
    * Reference from francofrancisco's commit

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -858,7 +858,7 @@ static void zram_reset_device(struct zram *zram)
 	/* Reset stats */
 	memset(&zram->stats, 0, sizeof(zram->stats));
 	zram->disksize = 0;
-	zram->max_comp_streams = 1;
+	zram->max_comp_streams = 4;
 
 	set_capacity(zram->disk, 0);
 	part_stat_set_all(&zram->disk->part0, 0);
@@ -1245,7 +1245,7 @@ static int create_device(struct zram *zram, int device_id)
 	}
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 	zram->meta = NULL;
-	zram->max_comp_streams = 1;
+	zram->max_comp_streams = 4;
 	return 0;
 
 out_free_disk:
