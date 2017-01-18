891159f9994a3bd534753fc8ca386de4b3228d13
Joe Maples <joe@frap129.org>
2017-01-17 19:19:27 -0500

    lazyplug: Remove Power/earlysuspend hooks
    Paving the way to use franco's display bool method
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/cpufreq/lazyplug.c
+++ drivers/cpufreq/lazyplug.c
@@ -4,6 +4,7 @@
  *
  * Copyright 2012~2014 Paul Reioux
  * Copyright 2015 Park Ju Hyung
+ * Copyright 2017 Joe Maples
  *
  *
  ** Introduction
@@ -72,19 +73,11 @@
 #include <linux/input.h>
 #include <linux/cpufreq.h>
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-#include <linux/earlysuspend.h>
-#endif
-
 //#define DEBUG_LAZYPLUG
 #undef DEBUG_LAZYPLUG
 
 #define LAZYPLUG_MAJOR_VERSION	1
-#define LAZYPLUG_MINOR_VERSION	0
+#define LAZYPLUG_MINOR_VERSION	1
 
 #define DEF_SAMPLING_MS			(268)
 #define DEF_IDLE_COUNT			(19) /* 268 * 19 = 5092, almost equals to 5 seconds */
@@ -402,71 +395,6 @@ static void wakeup_boost(void)
 	}
 }
 
-#if defined(CONFIG_POWERSUSPEND) || defined(CONFIG_HAS_EARLYSUSPEND)
-#ifdef CONFIG_POWERSUSPEND
-static void lazyplug_suspend(struct power_suspend *handler)
-#else
-static void lazyplug_suspend(struct early_suspend *handler)
-#endif
-{
-	if (lazyplug_active) {
-		pr_info("lazyplug: screen-off, turn off cores\n");
-		flush_workqueue(lazyplug_wq);
-
-		mutex_lock(&lazyplug_mutex);
-		suspended = true;
-		mutex_unlock(&lazyplug_mutex);
-
-		// put rest of the cores to sleep unconditionally!
-		cpu_all_ctrl(false);
-	}
-}
-
-static void cpu_all_up(struct work_struct *work);
-static DECLARE_WORK(cpu_all_up_work, cpu_all_up);
-
-static void cpu_all_up(struct work_struct *work)
-{
-	cpu_all_ctrl(true);
-	wakeup_boost();
-}
-
-#ifdef CONFIG_POWERSUSPEND
-static void lazyplug_resume(struct power_suspend *handler)
-#else
-static void lazyplug_resume(struct early_suspend *handler)
-#endif
-{
-	if (lazyplug_active) {
-		pr_info("lazyplug: screen-on, turn on cores\n");
-		mutex_lock(&lazyplug_mutex);
-		/* keep cores awake long enough for faster wake up */
-		persist_count = BUSY_PERSISTENCE;
-		suspended = false;
-		mutex_unlock(&lazyplug_mutex);
-
-		schedule_work(&cpu_all_up_work);
-	}
-	queue_delayed_work_on(0, lazyplug_wq, &lazyplug_work,
-		msecs_to_jiffies(10));
-}
-#endif
-
-#ifdef CONFIG_POWERSUSPEND
-static struct power_suspend lazyplug_power_suspend_driver = {
-	.suspend = lazyplug_suspend,
-	.resume = lazyplug_resume,
-};
-#endif  /* CONFIG_POWERSUSPEND */
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static struct early_suspend lazyplug_early_suspend_driver = {
-        .level = EARLY_SUSPEND_LEVEL_DISABLE_FB + 10,
-        .suspend = lazyplug_suspend,
-        .resume = lazyplug_resume,
-};
-#endif	/* CONFIG_HAS_EARLYSUSPEND */
-
 static unsigned int Lnr_run_profile_sel = 0;
 static unsigned int Ltouch_boost_active = true;
 static bool Lprevious_state = false;
@@ -587,13 +515,6 @@ int __init lazyplug_init(void)
 	}
 
 	rc = input_register_handler(&lazyplug_input_handler);
-
-#ifdef CONFIG_POWERSUSPEND
-	register_power_suspend(&lazyplug_power_suspend_driver);
-#endif
-#ifdef CONFIG_HAS_EARLYSUSPEND
-	register_early_suspend(&lazyplug_early_suspend_driver);
-#endif
 
 	lazyplug_wq = alloc_workqueue("lazyplug",
 				WQ_HIGHPRI | WQ_UNBOUND, 1);
