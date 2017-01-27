f5fca31d0cb6775c8ce351741a2b2b2d3912ac0b
Nathan Chancellor <natechancellor@gmail.com>
2017-01-26 19:43:12 -0700

    drivers: crypto: msm: qce50: fix misleading-indentation warning
    drivers/crypto/msm/qce50.c: In function 'qce_ablk_cipher_req':
    drivers/crypto/msm/qce50.c:4766:2: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
      if (rc)
      ^~
    drivers/crypto/msm/qce50.c:4768:3: note: ...this statement, but the latter is misleadingly indented as if it is guarded by the 'if'
       return 0;
       ^~~~~~
    drivers/crypto/msm/qce50.c: In function 'qce_process_sha_req':
    drivers/crypto/msm/qce50.c:4840:2: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
      if (rc)
      ^~
    drivers/crypto/msm/qce50.c:4842:3: note: ...this statement, but the latter is misleadingly indented as if it is guarded by the 'if'
       return 0;
       ^~~~~~
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/crypto/msm/qce50.c
+++ drivers/crypto/msm/qce50.c
@@ -4718,7 +4718,7 @@ int qce_ablk_cipher_req(void *handle, struct qce_req *c_req)
 	rc = _qce_sps_transfer(pce_dev);
 	if (rc)
 		goto bad;
-		return 0;
+	return 0;
 bad:
 	if (areq->src != areq->dst) {
 		if (pce_dev->dst_nents) {
@@ -4803,7 +4803,7 @@ int qce_process_sha_req(void *handle, struct qce_sha_req *sreq)
 	rc = _qce_sps_transfer(pce_dev);
 	if (rc)
 		goto bad;
-		return 0;
+	return 0;
 bad:
 	if (pce_dev->src_nents) {
 		qce_dma_unmap_sg(pce_dev->pdev, sreq->src,
