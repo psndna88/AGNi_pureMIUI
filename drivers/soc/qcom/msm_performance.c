e41060b76c9f483dd35c27665475e41b54af6c39
Nathan Chancellor <natechancellor@gmail.com>
2017-02-01 21:53:47 -0700

    msm_performance: disable touchboost by default
    This is crazy overkill (boosts to either 1344MHz or 1555MHz for 1.5s
    or something like that) so not great for battery but users can turn
    it back on if they don't want the loss of performance.
    
    Also, make sure that the max frequency can't be tampered with when
    touchboost is disabled.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: TheDarkStrix <thedarkstrix@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -25,7 +25,8 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 
-static int touchboost = 1;
+static int touchboost = 0;
+
 
 static struct mutex managed_cpus_lock;
 
@@ -434,6 +435,9 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+
+	if (!touchboost)
+		return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
