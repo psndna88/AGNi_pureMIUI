884040b014e33288cafa1a66989721f0067a3421
psndna88 <psndna88@gmail.com>
2017-02-18 18:57:47 +0530

    revert "SafetyNet bypass: Show androidboot.verifiedbootstate=green
    Change-Id: I584e0de8f5602eb2631390b47c8916fa22c50375"
    
    This reverts commit cc8da9b58952b4ae7934c5da4d307723e91c467c.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,11 +4,11 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
-static char proc_cmdline[COMMAND_LINE_SIZE];
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", proc_cmdline);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -26,23 +26,29 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-	/* SafetyNet bypass: show androidboot.verifiedbootstate=green */
-	char *a1, *a2;
-
-	a1 = strstr(saved_command_line, "androidboot.verifiedbootstate=");
-	if (a1) {
-		a1 = strchr(a1, '=');
-		a2 = strchr(a1, ' ');
-		if (!a2) /* last argument on the cmdline */
-			a2 = "";
-
-		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
-			  (int)(a1 - saved_command_line + 1),
-			  saved_command_line, a2);
-	} else {
-		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
+	char *offset_addr, *cmd = new_command_line;
+	
+	strcpy(cmd, saved_command_line);
+	
+	/*
+	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
+	 * by userspace in order to pass SafetyNet CTS check.
+	 */
+	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
+	if (offset_addr) {
+		size_t i, len, offset;
+		
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+		
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+		
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
 	}
-
+	
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
