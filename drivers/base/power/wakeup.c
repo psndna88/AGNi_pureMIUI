8f2bda8d4e8a646ea4d7621c82f327ae1fee344c
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-10 14:21:13 +0000

    drivers: wakeup: add options to disable wakelocks
    As usual it's the user's entire responsibility of breaking (or not) functionality when disabling wakelocks.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,8 +16,16 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
 
 #include "power.h"
+
+static bool enable_qcom_rx_wakelock_ws = true;
+module_param(enable_qcom_rx_wakelock_ws, bool, 0644);
+static bool enable_timerfd_ws = true;
+module_param(enable_timerfd_ws, bool, 0644);
+static bool enable_netlink_ws = true;
+module_param(enable_netlink_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -427,6 +435,12 @@ static bool wakeup_source_not_registered(struct wakeup_source *ws)
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
+
+	if ((!enable_qcom_rx_wakelock_ws && !strncmp(ws->name, "qcom_rx_wakelock", 16)) ||
+			(!enable_timerfd_ws && !strncmp(ws->name, "[timerfd]", 9)) ||
+			(!enable_netlink_ws && !strncmp(ws->name, "NETLINK", 7))) {
+		return;
+	}
 
 	if (WARN(wakeup_source_not_registered(ws),
 			"unregistered wakeup source\n"))
