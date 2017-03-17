90e78f4dce26cd30ad9c69d3fa5ef1196cb39fea
AshishM94 <im.ashish994@gmail.com>
2017-03-17 11:30:40 +0530

    fs: nfs: fscache: fix warning: 'smp_mb__before_clear_bit' is deprecated [-Wdeprecated-declarations] *smp_mb__before_clear_bit is deprecated after @7d9e69c77fd3e992f022484798be475a2905ac8f

--- fs/nfs/fscache.c
+++ fs/nfs/fscache.c
@@ -284,7 +284,7 @@ static inline void nfs_fscache_inode_unlock(struct inode *inode)
 {
 	struct nfs_inode *nfsi = NFS_I(inode);
 
-	smp_mb__before_clear_bit();
+	smp_mb__before_atomic();
 	clear_bit(NFS_INO_FSCACHE_LOCK, &nfsi->flags);
 	smp_mb__after_clear_bit();
 	wake_up_bit(&nfsi->flags, NFS_INO_FSCACHE_LOCK);
