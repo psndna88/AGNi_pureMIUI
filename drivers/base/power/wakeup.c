ae0c545d1aeb162fd84348565f4a3c799a9496e1
psndna88 <psndna88@gmail.com>
2017-04-07 08:50:18 +0530

    revert "drivers: wakeup: add option to block wlan, alarmtimer & bluetooth_timer wakelocks"
    This reverts commit 817c311292c7f33c409ad0a0dc5c694dfa702149.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -19,7 +19,6 @@
 #include <trace/events/power.h>
 #include <linux/moduleparam.h>
 #include <linux/display_state.h>
-#include "power.h"
 
 static bool enable_si_ws = true;
 module_param(enable_si_ws, bool, 0644);
@@ -33,6 +32,9 @@ static bool wlan_wake = true;
 module_param(wlan_wake, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
+
+#include "power.h"
+
 static bool enable_qcom_rx_wakelock_ws = true;
 module_param(enable_qcom_rx_wakelock_ws, bool, 0644);
 static bool enable_ipa_ws = false;
@@ -41,12 +43,6 @@ static bool enable_timerfd_ws = true;
 module_param(enable_timerfd_ws, bool, 0644);
 static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
-static bool enable_wlan_ws = true;
-module_param(enable_wlan_ws, bool, 0644);
-static bool enable_alarmtimer_ws = true;
-module_param(enable_alarmtimer_ws, bool, 0644);
-static bool enable_bluetooth_timer_ws = true;
-module_param(enable_bluetooth_timer_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -483,13 +479,7 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 			(!enable_timerfd_ws &&
 				!strncmp(ws->name, "[timerfd]", wslen)) ||
 			(!enable_netlink_ws &&
-				!strncmp(ws->name, "NETLINK", wslen)) ||
-			(!enable_timerfd_ws &&
-				!strncmp(ws->name, "wlan", wslen)) ||
-			(!enable_timerfd_ws &&
-				!strncmp(ws->name, "alarmtimer", wslen)) ||
-			(!enable_timerfd_ws &&
-				!strncmp(ws->name, "bluetooth_timer", wslen))) {
+				!strncmp(ws->name, "NETLINK", wslen))) {
 			if (ws->active) {
 				wakeup_source_deactivate(ws);
 				pr_info("forcefully deactivate wakeup source: %s\n",
