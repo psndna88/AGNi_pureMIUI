2549f9e96a789baec131350d35f5b8dfdff90201
psndna88 <psndna88@gmail.com>
2017-04-11 07:34:42 +0530

    revert hall sensor from "drivers/input: update sensors from Xiaomi ido-l-oss"

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 1999-2002 Vojtech Pavlik
- * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published by
@@ -476,11 +475,6 @@ struct input_keymap_entry {
 #define KEY_RFKILL		247	/* Key that controls all radios */
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
-
-#define KEY_HALL_SENSOR		249
-#define KEY_NEXTSONG_NEW		250
-#define KEY_PREVIOUSSONG_NEW	251
-
 
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
