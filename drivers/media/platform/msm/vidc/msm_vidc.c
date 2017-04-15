smac0628/android_kernel_huawei_angler

qcom: goodbye pr_info, hello pr_debug, you won't be missed on my dmesg
Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1275,7 +1275,7 @@ void *msm_vidc_open(int core_id, int session_type)
 		goto err_invalid_core;
 	}
 
-	pr_info(VIDC_DBG_TAG "Opening video instance: %p, %d\n",
+	pr_debug(VIDC_DBG_TAG "Opening video instance: %p, %d\n",
 		VIDC_MSG_PRIO2STRING(VIDC_INFO), inst, session_type);
 	mutex_init(&inst->sync_lock);
 	mutex_init(&inst->bufq[CAPTURE_PORT].lock);
@@ -1466,7 +1466,7 @@ int msm_vidc_close(void *instance)
 
 	msm_smem_delete_client(inst->mem_client);
 
-	pr_info(VIDC_DBG_TAG "Closed video instance: %p\n",
+	pr_debug(VIDC_DBG_TAG "Closed video instance: %p\n",
 			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
 	kfree(inst);
 

psndna88/AGNi_pureMIUI

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1301,7 +1301,7 @@ void *msm_vidc_open(int core_id, int session_type)
 		goto err_invalid_core;
 	}
 
-	pr_info(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
+	pr_debug(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
 		VIDC_MSG_PRIO2STRING(VIDC_INFO), inst, session_type);
 	mutex_init(&inst->sync_lock);
 	mutex_init(&inst->bufq[CAPTURE_PORT].lock);
@@ -1509,7 +1509,7 @@ int msm_vidc_close(void *instance)
 
 	msm_smem_delete_client(inst->mem_client);
 
-	pr_info(VIDC_DBG_TAG "Closed video instance: %pK\n",
+	pr_debug(VIDC_DBG_TAG "Closed video instance: %pK\n",
 			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
 	kfree(inst);
 