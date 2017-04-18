6eb7238b3c51be2932a65cdf4d5e1a356443aca9
Nathan Chancellor <natechancellor@gmail.com>
2017-04-17 22:17:50 -0700

    sched: core: fix warning
    kernel/sched/core.c: In function 'idle_cpu_relaxed':
    kernel/sched/core.c:6474:39: warning: comparison between pointer and integer
      if (cpu_relaxed_read_long(&rq->curr) != rq->idle)
                                           ^~
    
    Fixes: e5434290a23c ("kernel: cpuidle: use idle_cpu_relaxed and cpu_relaxed")
    Change-Id: I2b5e4b67f8655fef806149410db983a34f0eba13
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6496,7 +6496,7 @@ int idle_cpu_relaxed(int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 
-	if (cpu_relaxed_read_long(&rq->curr) != rq->idle)
+	if (cpu_relaxed_read_long(&rq->curr) != (u64)rq->idle)
 		return 0;
 
 	if (cpu_relaxed_read(&rq->nr_running))
