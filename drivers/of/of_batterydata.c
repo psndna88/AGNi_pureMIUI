5477966072a387119e0eb6de34da2ae8632a005d
TheScarastic <warabhishek@gmail.com>
2017-05-16 14:46:41 +0000

    of_batterydata: ifdef hydrogen related code
    Change-Id: I4e6158844e728122e413756b47bba64750e04149

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -315,7 +315,10 @@ struct device_node *of_batterydata_get_best_profile(
 		const char *psy_name,  const char  *batt_type)
 {
 	struct batt_ids batt_ids;
-	struct device_node *node, *best_node = NULL, *generic_node = NULL;
+	struct device_node *node, *best_node = NULL;
+#ifdef CONFIG_MACH_XIAOMI_HYDROGEN
+	struct device_node *generic_node = NULL;
+#endif
 	struct power_supply *psy;
 	const char *battery_type = NULL;
 	union power_supply_propval ret = {0, };
@@ -385,17 +388,23 @@ struct device_node *of_batterydata_get_best_profile(
 				}
 			}
 		}
+#ifdef CONFIG_MACH_XIAOMI_HYDROGEN
 		rc = of_property_read_string(node, "qcom,battery-type",
 							&battery_type);
 		if (!rc && strcmp(battery_type, "itech_3020mah") == 0)
 				generic_node = node;
+#endif
 	}
 
 	if (best_node == NULL) {
-		/* now that best_node is null, there is no need to
-		 * check whether generic node is null. */
+#ifdef CONFIG_MACH_XIAOMI_HYDROGEN
+		/* now that best_node is null and device is hydrogen,
+		 * there is no need to check whether generic node is null. */
 		best_node = generic_node;
 		pr_err("No battery data found,use generic one\n");
+#else
+		pr_err("No battery data found\n");
+#endif
 		return best_node;
 	}
 
