e6daccd5b00a4c7bc2fe8cef4d5e182836878c8f
psndna88 <psndna88@gmail.com>
2017-05-21 00:03:52 +0530

    staging: ion: revert caf/LA.BR.1.3.6_rb1.18

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -465,7 +465,7 @@ static struct ion_handle *pass_to_user(struct ion_handle *handle)
 /* Must hold the client lock */
 static int user_ion_handle_put_nolock(struct ion_handle *handle)
 {
-	int ret = 0;
+	int ret;
 
 	if (--handle->user_ref_count == 0)
 		ret = ion_handle_put_nolock(handle);
@@ -698,7 +698,7 @@ static void user_ion_free_nolock(struct ion_client *client,
 		WARN(1, "%s: invalid handle passed to free.\n", __func__);
 		return;
 	}
-	if (!(handle->user_ref_count > 0)) {
+	if (handle->user_ref_count == 0) {
 		WARN(1, "%s: User does not have access!\n", __func__);
 		return;
 	}
