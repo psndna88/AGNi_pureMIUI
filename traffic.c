2e80731fd6be718dae6bf3476aa98f2b8082f6d4
Rakesh Sunki <rsunki@qti.qualcomm.com>
2017-06-22 15:50:30 -0700

    NAN: Fix ping issue when ndp is enabled
    Ping failed because the pings were routed to an incorrect interface. Fix
    the issue by routing the pings to nan0 interface when ndp is enabled.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

--- traffic.c
+++ traffic.c
@@ -129,7 +129,9 @@ static int cmd_traffic_send_ping(struct sigma_dut *dut,
 	if (rate != 1)
 		snprintf(int_arg, sizeof(int_arg), " -i %f", interval);
 	intf_arg[0] = '\0';
-	if (type == 2)
+	if (dut->ndp_enable)
+		strlcpy(intf_arg, " -I nan0", sizeof(intf_arg));
+	else if (type == 2)
 		snprintf(intf_arg, sizeof(intf_arg), " -I %s",
 			 get_station_ifname());
 	fprintf(f, "#!" SHELL "\n"
