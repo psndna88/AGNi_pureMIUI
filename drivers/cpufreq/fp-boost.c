93418407cfee206926c8938009539b9332217e6c
Joe Maples <joe@frap129.org>
2017-07-14 07:15:40 -0500

    fp-boost: Decrease duration to 2 seconds
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/cpufreq/fp-boost.c
+++ drivers/cpufreq/fp-boost.c
@@ -39,7 +39,7 @@
 #define FINGERPRINT_KEY 0x2ee
 
 /* The duration in milliseconds for the fingerprint boost */
-#define FP_BOOST_MS (3000)
+#define FP_BOOST_MS (2000)
 
 /*
  * "fp_config" = "fingerprint boost configuration". This contains the data and
