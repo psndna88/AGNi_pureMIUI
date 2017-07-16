5d58c0ca80d5abcde7e67e50c72a1839116dcfc3
Pranav Vashi <neobuddy89@gmail.com>
2017-07-17 00:53:05 +0530

    msm: vidc: Fix memory leak
    Change-Id: I5ecb597e1d716806654574726203956d3dba8216
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -3968,8 +3968,11 @@ static struct v4l2_ctrl **get_super_cluster(struct msm_vidc_inst *inst,
 	struct v4l2_ctrl **cluster = kmalloc(sizeof(struct v4l2_ctrl *) *
 			NUM_CTRLS, GFP_KERNEL);
 
-	if (!size || !cluster || !inst)
+	if (!size || !cluster || !inst) {
+		if (cluster)
+			kfree(cluster);
 		return NULL;
+	}
 
 	for (c = 0; c < NUM_CTRLS; c++)
 		cluster[sz++] =  inst->ctrls[c];
