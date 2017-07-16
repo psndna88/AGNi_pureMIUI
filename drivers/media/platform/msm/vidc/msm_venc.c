fd71640828eb56de1d00ed7ca8df9498cb39d605
Pranav Vashi <neobuddy89@gmail.com>
2017-07-17 00:47:18 +0530

    msm: vidc: Clean session when failed to open instance
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -1570,6 +1570,7 @@ static inline int start_streaming(struct msm_vidc_inst *inst)
 	if (rc) {
 		dprintk(VIDC_ERR,
 			"Failed to move inst: %pK to start done state\n", inst);
+		msm_comm_session_clean(inst);
 		goto fail_start;
 	}
 	msm_dcvs_init_load(inst);
