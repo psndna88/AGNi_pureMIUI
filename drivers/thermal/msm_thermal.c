20242b101be2ed9d4ad9fb8738283ea142fae393
psndna88 <psndna88@gmail.com>
2017-08-05 19:20:33 +0530

    revert "drivers: thermal: queue work on system_power_efficient_wq
    There doesn't seem to be any real dependency of scheduling these on
    the cpu which scheduled them, so moving every *_delayed to the
    power efficient wq save potential needlessly idle cpu wake ups
    leaving the scheduler to decide the most appropriate cpus to wake up.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>"
    
    This reverts commit 832d55fac51ac709189ce56b23c451a36b6c3af1.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3385,9 +3385,8 @@ static void check_temp(struct work_struct *work)
 
 reschedule:
 	//if (polling_enabled)
-		queue_delayed_work(system_power_efficient_wq,
-			&check_temp_work,
-			msecs_to_jiffies(msm_thermal_info.poll_ms));
+		schedule_delayed_work(&check_temp_work,
+				msecs_to_jiffies(msm_thermal_info.poll_ms));
 }
 
 static int __ref msm_thermal_cpu_callback(struct notifier_block *nfb,
