9288412e0fe1e148421f91b829bd7457384e6f53
psndna88 <psndna88@gmail.com>
2017-08-07 21:05:34 +0530

    revert "msm: sensor: actuator: add conditional check for ioctl
    Add conditional check when operating VIDIOC_MSM_ACTUATOR_CFG32.
    
    CRs-Fixed: 2060371
    Change-Id: I9cfaff05b1fff6969a2543c3816d41ed1fabf897
    Signed-off-by: Haibin Liu <haibinl@codeaurora.org>"
    
    This reverts commit 11972e53901da42cb2ce662a00ed18b15d3ae966.

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1664,6 +1664,7 @@ static long msm_actuator_subdev_do_ioctl(
 
 	switch (cmd) {
 	case VIDIOC_MSM_ACTUATOR_CFG32:
+		cmd = VIDIOC_MSM_ACTUATOR_CFG;
 		switch (u32->cfgtype) {
 		case CFG_SET_ACTUATOR_INFO:
 			actuator_data.cfgtype = u32->cfgtype;
@@ -1781,14 +1782,11 @@ static long msm_actuator_subdev_do_ioctl(
 		}
 	}
 
-	if (cmd == VIDIOC_MSM_ACTUATOR_CFG32)
-		rc = msm_actuator_subdev_ioctl(sd, VIDIOC_MSM_ACTUATOR_CFG, parg);
-	else
-		rc = msm_actuator_subdev_ioctl(sd, cmd, parg);
+	rc = msm_actuator_subdev_ioctl(sd, cmd, parg);
 
 	switch (cmd) {
 
-	case VIDIOC_MSM_ACTUATOR_CFG32:
+	case VIDIOC_MSM_ACTUATOR_CFG:
 
 		switch (u32->cfgtype) {
 
