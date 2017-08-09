cdad54e9661fcacb8abac084eb592ba1732ea038
TheScarastic <warabhishek@gmail.com>
2017-08-09 15:44:02 +0200

    Increase maximum charging current

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -109,9 +109,9 @@ enum msm_usb_phy_type {
 #define IDEV_CHG_MIN	500
 #define IUNIT		100
 
-#define IDEV_ACA_CHG_MAX	1500
+#define IDEV_ACA_CHG_MAX	2000
 #define IDEV_ACA_CHG_LIMIT	500
-#define IDEV_HVDCP_CHG_MAX	1800
+#define IDEV_HVDCP_CHG_MAX	2000
 
 /**
  * Different states involved in USB charger detection.
