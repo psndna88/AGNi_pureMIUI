631670b5c7b94875a3502dc67341633c8b06cc78
psndna88 <psndna88@gmail.com>
2017-08-14 13:27:31 +0530

    mdss_fb: backlight dimmer (default on)

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -80,7 +80,7 @@
 					out = ((v) * (v) * 255  / 4095 + (v) * (255 - (v)) / 32);\
 					} while (0)
 
-bool backlight_dimmer = false;
+bool backlight_dimmer = true;
 module_param(backlight_dimmer, bool, 0755);
 
 static struct fb_info *fbi_list[MAX_FBI_LIST];
