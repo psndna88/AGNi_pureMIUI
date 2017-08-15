6eec96058da15e098d09b9c981d45b925483e97e
Bartosz Golaszewski <brgl@bgdev.pl>
2017-08-15 11:07:59 +0200

    irq_work: improve the flag definitions
    IRQ_WORK_FLAGS is defined simply to 3UL. This is confusing as it
    says nothing about its purpose. Define IRQ_WORK_FLAGS as a bitwise
    OR of IRQ_WORK_PENDING and IRQ_WORK_BUSY.
    
    While we're at it: use the BIT() macro for all flags.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- include/linux/irq_work.h
+++ include/linux/irq_work.h
@@ -12,10 +12,10 @@
  * busy      NULL, 2 -> {free, claimed} : callback in progress, can be claimed
  */
 
-#define IRQ_WORK_PENDING	1UL
-#define IRQ_WORK_BUSY		2UL
-#define IRQ_WORK_FLAGS		3UL
-#define IRQ_WORK_LAZY		4UL /* Doesn't want IPI, wait for tick */
+#define IRQ_WORK_PENDING	BIT(0)
+#define IRQ_WORK_BUSY		BIT(1)
+#define IRQ_WORK_FLAGS		(IRQ_WORK_PENDING | IRQ_WORK_BUSY)
+#define IRQ_WORK_LAZY		BIT(2) /* Doesn't want IPI, wait for tick */
 
 struct irq_work {
 	unsigned long flags;
