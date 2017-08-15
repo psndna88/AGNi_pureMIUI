cbc0bf63067f56ba7ee4788d4a8ca6e076086e16
Suren Baghdasaryan <surenb@google.com>
2017-08-15 15:12:24 -0700

    ANDROID: check dir value of xfrm_userpolicy_id
    Check user provided dir value to prevent out-of-bound access
    which may occur if dir is not less than XFRM_POLICY_MAX.
    
    (url: http://seclists.org/bugtraq/2017/Jul/30)
    
    Bug: 64257838
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I5bbdf95e14a61bdf5207977d9a5a4465bc848da0

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -1590,6 +1590,10 @@ static struct sk_buff *xfrm_policy_netlink(struct sk_buff *in_skb,
 	struct sk_buff *skb;
 	int err;
 
+	err = verify_policy_dir(dir);
+	if (err)
+		return ERR_PTR(err);
+
 	skb = nlmsg_new(NLMSG_DEFAULT_SIZE, GFP_KERNEL);
 	if (!skb)
 		return ERR_PTR(-ENOMEM);
@@ -2136,6 +2140,10 @@ static int xfrm_do_migrate(struct sk_buff *skb, struct nlmsghdr *nlh,
 	int err;
 	int n = 0;
 
+	err = verify_policy_dir(pi->dir);
+	if (err)
+		return err;
+
 	if (attrs[XFRMA_MIGRATE] == NULL)
 		return -EINVAL;
 
@@ -2250,6 +2258,11 @@ static int xfrm_send_migrate(const struct xfrm_selector *sel, u8 dir, u8 type,
 {
 	struct net *net = &init_net;
 	struct sk_buff *skb;
+	int err;
+
+	err = verify_policy_dir(dir);
+	if (err)
+		return err;
 
 	skb = nlmsg_new(xfrm_migrate_msgsize(num_migrate, !!k), GFP_ATOMIC);
 	if (skb == NULL)
@@ -2878,6 +2891,11 @@ out_free_skb:
 
 static int xfrm_send_policy_notify(struct xfrm_policy *xp, int dir, const struct km_event *c)
 {
+	int err;
+
+	err = verify_policy_dir(dir);
+	if (err)
+		return err;
 
 	switch (c->event) {
 	case XFRM_MSG_NEWPOLICY:
