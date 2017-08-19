5d659bcfcf086c1cf87087fa6d2975fb99c61fa8
AshishM94 <im.ashish994@gmail.com>
2017-08-19 12:43:21 +0530

    techpack: video: kill opening/closing video instance logspam
    Change-Id: Ie7a66a1271c9f602c2954b8aa1f38a16b45e1875
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- techpack/video/msm/vidc/msm_vidc.c
+++ techpack/video/msm/vidc/msm_vidc.c
@@ -1519,7 +1519,7 @@ void *msm_vidc_open(int core_id, int session_type)
 		goto err_invalid_sid;
 	}
 
-	pr_info(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
+	pr_debug(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
 		"high", inst->sid, get_codec_name(inst->sid),
 		inst, session_type);
 	mutex_init(&inst->sync_lock);
@@ -1792,7 +1792,7 @@ int msm_vidc_destroy(struct msm_vidc_inst *inst)
 
 	msm_vidc_debugfs_deinit_inst(inst);
 
-	pr_info(VIDC_DBG_TAG "Closed video instance: %pK\n",
+	pr_debug(VIDC_DBG_TAG "Closed video instance: %pK\n",
 			"high", inst->sid, get_codec_name(inst->sid),
 			inst);
 	put_sid(inst->sid);
