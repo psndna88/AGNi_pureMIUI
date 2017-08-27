1c5c70086f4c45feaebfa65712a9b96e1c7f6318
psndna88 <psndna88@gmail.com>
2017-08-27 17:39:31 +0530

    Update AGNi pureMIUI/LOS v10.4 configs

--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -2,45 +2,66 @@
 export KERNELDIR=`readlink -f .`
 cd $KERNELDIR;
 
-TEMP_DIR=$KERNELDIR/../TEMP_KENZO_BUILDS_RUN_STORE
+TEMP_DIR=$KERNELDIR/TEMP_KENZO_BUILDS_RUN_STORE
 mkdir -p $TEMP_DIR
-rm -rf BUILT_kenzo-miuiMM BUILT_kenzo-cmMM BUILT_kenzo-cmN .config
+rm -rf BUILT_kenzo-miuiMM BUILT_kenzo-miuiN BUILT_kenzo-cmMM BUILT_kenzo-cmN .config .config.old
+
 echo "-----------------------------------------------------------------------"
 echo " "
-echo "          BATCH MODE: Building all AGNi N variants..."
-# AGNI pureCM-N
+echo "          BATCH MODE: Building AGNi LOS-N-O variant..."
+# AGNI pureLOS-N
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_cmN.sh
-mv -f $KERNELDIR/BUILT_kenzo-cmN $TEMP_DIR
+mv -f $KERNELDIR/BUILT_kenzo-cmN $TEMP_DIR/
 rm .config
-mv -f $TEMP_DIR/* $KERNELDIR
-rm -rf $TEMP_DIR
 echo " "
-echo "          BATCH MODE: Built all AGNi N variants !!!"
+echo "          BATCH MODE: Built AGNi LOS-N-O variant!!!"
 echo "-----------------------------------------------------------------------"
 echo " "
-echo " "
-echo "-----------------------------------------------------------------------"
-echo " "
-echo "          BATCH MODE: Building all AGNi MM variants..."
 
-# AGNI pureCM-MM
+echo " "
+echo "-----------------------------------------------------------------------"
+echo " "
+echo "          BATCH MODE: Building AGNi MIUI-N variant..."
+# AGNI pureMIUI-N
+rm drivers/staging/prima/wlan.ko
+./build_kernel_kenzo_miuiN.sh
+mv -f $KERNELDIR/BUILT_kenzo-miuiN $TEMP_DIR/
+rm .config
+echo " "
+echo "          BATCH MODE: Built AGNi MIUI-N variant!!!"
+echo "-----------------------------------------------------------------------"
+echo " "
+
+echo " "
+echo "-----------------------------------------------------------------------"
+echo " "
+echo "          BATCH MODE: Building AGNi LOS-MM variant..."
+# AGNI pureLOS-MM
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_cmMM.sh
-mv -f $KERNELDIR/BUILT_kenzo-cmMM $TEMP_DIR
+mv -f $KERNELDIR/BUILT_kenzo-cmMM $TEMP_DIR/
 rm .config
-
+echo " "
+echo "          BATCH MODE: Built AGNi LOS-MM variant!!!"
 echo "-----------------------------------------------------------------------"
 echo " "
 
+echo " "
+echo "-----------------------------------------------------------------------"
+echo " "
+echo "          BATCH MODE: Building AGNi MIUI-MM variant..."
 # AGNI pureMIUI-MM
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_miuiMM.sh
-mv -f $KERNELDIR/BUILT_kenzo-miuiMM $TEMP_DIR
+mv -f $KERNELDIR/BUILT_kenzo-miuiMM $TEMP_DIR/
 rm .config
-
 echo " "
-echo "          BATCH MODE: Built all AGNi MM variants !!!"
+echo "          BATCH MODE: Built AGNi MIUI-MM variant !!!"
 echo "-----------------------------------------------------------------------"
 echo " "
+
+mv -f $TEMP_DIR/* $KERNELDIR
+rm -rf $TEMP_DIR
+echo "          BATCH MODE: Built ALL variants !!!"
 
