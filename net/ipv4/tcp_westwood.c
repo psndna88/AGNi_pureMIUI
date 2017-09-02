eb097ee8cba03c6f118cceb70dde71e10da53986
MOVZX <movzx@yahoo.com>
2017-09-02 14:30:15 +0300

    net: Westwood+ Tweaks
    Changed Files:
    - net/ipv4/tcp_westwood.c
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -44,8 +44,8 @@ struct westwood {
 
 
 /* TCP Westwood functions and constants */
-#define TCP_WESTWOOD_RTT_MIN   (HZ/20)	/* 50ms */
-#define TCP_WESTWOOD_INIT_RTT  (20*HZ)	/* maybe too conservative?! */
+#define TCP_WESTWOOD_RTT_MIN   (HZ/18)	/* 50ms */
+#define TCP_WESTWOOD_INIT_RTT  (18*HZ)	/* maybe too conservative?! */
 
 /*
  * @tcp_westwood_create
