ce53756649d83c88287ce2c577c3ed52f8fa1a81
Julien Thierry <julien.thierry@arm.com>
2017-10-13 14:32:55 +0100

    arm_arch_timer: Expose event stream status
    The arch timer configuration for a CPU might get reset after suspending
    said CPU.
    
    In order to reliably use the event stream in the kernel (e.g. for delays),
    we keep track of the state where we can safely consider the event stream as
    properly configured. After writing to cntkctl, we issue an ISB to ensure
    that subsequent delay loops can rely on the event stream being enabled.
    
    Signed-off-by: Julien Thierry <julien.thierry@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/clocksource/arm_arch_timer.h
+++ include/clocksource/arm_arch_timer.h
@@ -94,6 +94,8 @@ extern u32 arch_timer_get_rate(void);
 extern u64 (*arch_timer_read_counter)(void);
 extern struct arch_timer_kvm_info *arch_timer_get_kvm_info(void);
 extern void arch_timer_mem_get_cval(u32 *lo, u32 *hi);
+extern bool arch_timer_evtstrm_available(void);
+
 #else
 
 static inline u32 arch_timer_get_rate(void)
@@ -110,6 +112,12 @@ static void arch_timer_mem_get_cval(u32 *lo, u32 *hi)
 {
 	*lo = *hi = ~0U;
 }
+
+static inline bool arch_timer_evtstrm_available(void)
+{
+	return false;
+}
+
 #endif
 
 #endif
