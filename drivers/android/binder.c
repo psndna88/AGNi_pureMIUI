6d495093dfbd324bc5f30323ffef62bae6cd0784
Stephen Smalley <sds@tycho.nsa.gov>
2017-10-24 17:46:56 +0530

    binder: Add security hooks to binder and implement the hooks for SELinux.
    Add security hooks to the binder and implement the hooks for SELinux.
    The security hooks enable security modules such as SELinux to implement
    controls over binder IPC.  The security hooks include support for
    controlling what process can become the binder context manager
    (binder_set_context_mgr), controlling the ability of a process
    to invoke a binder transaction/IPC to another process (binder_transaction),
    controlling the ability a process to transfer a binder reference to
    another process (binder_transfer_binder), and controlling the ability
    of a process to transfer an open file to another process (binder_transfer_file).
    
    This support is used by SE Android, http://selinuxproject.org/page/SEAndroid.
    
    Change-Id: I34266b66320b6a3df9ac01833d7f94daf742920e
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -38,6 +38,7 @@
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/pid_namespace.h>
+#include <linux/security.h>
 #include <linux/ratelimit.h>
 
 #ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
@@ -1667,6 +1668,8 @@ static int binder_translate_binder(struct flat_binder_object *fp,
 				  (u64)node->cookie);
 		return -EINVAL;
 	}
+	if (security_binder_transfer_binder(proc->tsk, target_proc->tsk))
+		return -EPERM;
 
 	ref = binder_get_ref_for_node(target_proc, node);
 	if (!ref)
@@ -1705,6 +1708,8 @@ static int binder_translate_handle(struct flat_binder_object *fp,
 				  proc->pid, thread->pid, fp->handle);
 		return -EINVAL;
 	}
+	if (security_binder_transfer_binder(proc->tsk, target_proc->tsk))
+		return -EPERM;
 
 	if (ref->node->proc == target_proc) {
 		if (fp->hdr.type == BINDER_TYPE_HANDLE)
@@ -1774,6 +1779,12 @@ static int binder_translate_fd(int fd,
 		goto err_fget;
 	}
 
+	ret = security_binder_transfer_file(proc->tsk, target_proc->tsk, file);
+	if (ret < 0) {
+		ret = -EPERM;
+		goto err_security;
+	}
+
 	target_fd = task_get_unused_fd_flags(target_proc, O_CLOEXEC);
 	if (target_fd < 0) {
 		ret = -ENOMEM;
@@ -1787,6 +1798,8 @@ static int binder_translate_fd(int fd,
 	return target_fd;
 
 err_get_unused_fd:
+err_security:
+	fput(file);
 err_fget:
 err_fd_not_accepted:
 	return ret;
@@ -1840,7 +1853,7 @@ static int binder_translate_fd_array(struct binder_fd_array_object *fda,
 
 err_translate_fd_failed:
 	/*
-	 * Failed to allocate fd error, free fds
+	 * Failed to allocate fd or security error, free fds
 	 * installed so far.
 	 */
 	num_installed_fds = fdi;
@@ -1990,6 +2003,10 @@ static void binder_transaction(struct binder_proc *proc,
 		if (target_proc == NULL) {
 			return_error = BR_DEAD_REPLY;
 			goto err_dead_binder;
+		}
+		if (security_binder_transaction(proc->tsk, target_proc->tsk) < 0) {
+			return_error = BR_FAILED_REPLY;
+			goto err_invalid_target_handle;
 		}
 		if (!(tr->flags & TF_ONE_WAY) && thread->transaction_stack) {
 			struct binder_transaction *tmp;
@@ -3287,6 +3304,9 @@ static int binder_ioctl_set_ctx_mgr(struct file *filp)
 		ret = -EBUSY;
 		goto out;
 	}
+	ret = security_binder_set_context_mgr(proc->tsk);
+	if (ret < 0)
+		goto out;
 	if (uid_valid(context->binder_context_mgr_uid)) {
 		if (!uid_eq(context->binder_context_mgr_uid, curr_euid)) {
 			pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
