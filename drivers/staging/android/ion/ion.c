cb51fe6174ce6e2467b24451dfdfed3d2876cf5a
Liam Mark <lmark@codeaurora.org>
2017-11-21 10:46:38 -0800

    ion: add kernel support to get buffer flags
    Allow ION kernel clients to get an ion buffer's flags.
    This will allow ION clients to be able to determine if a buffer
    is cached, uncached, secure ect.
    
    Change-Id: I5460478e69f28e74906fd298f57dabd9e62c1e1f
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1407,6 +1407,15 @@ static void ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf, size_t start,
 	mutex_unlock(&buffer->lock);
 }
 
+static int ion_dma_buf_get_flags(struct dma_buf *dmabuf,
+				 unsigned long *flags)
+{
+	struct ion_buffer *buffer = dmabuf->priv;
+	*flags = buffer->flags;
+
+ 	return 0;
+}
+
 static struct dma_buf_ops dma_buf_ops = {
 	.map_dma_buf = ion_map_dma_buf,
 	.unmap_dma_buf = ion_unmap_dma_buf,
@@ -1417,6 +1426,7 @@ static struct dma_buf_ops dma_buf_ops = {
 	.kmap_atomic = ion_dma_buf_kmap,
 	.kunmap_atomic = ion_dma_buf_kunmap,
 	.kmap = ion_dma_buf_kmap,
+	.get_flags = ion_dma_buf_get_flags,
 	.kunmap = ion_dma_buf_kunmap,
 };
 
