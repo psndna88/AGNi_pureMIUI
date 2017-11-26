869d48bca2236fa630621ee68d8bd2bd1d2ff89a
psndna88 <psndna88@gmail.com>
2017-11-26 18:15:02 +0530

    Update AGNi pureMIUI/LOS v11.6 configs
    Signed-off-by: psndna88 <psndna88@gmail.com>
    
    defconfig: Restrict kswapd to power cluster
    Restrict kswapd to the power cluster in order to reduce power consumption.

--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -2,8 +2,8 @@
 export KERNELDIR=`readlink -f .`
 cd $KERNELDIR;
 
-TEMP_DIR=$KERNELDIR/../TEMP_KENZO_BUILDS_RUN_STORE
-mkdir -p $TEMP_DIR
+#TEMP_DIR=$KERNELDIR/../TEMP_KENZO_BUILDS_RUN_STORE
+#mkdir -p $TEMP_DIR
 rm -rf BUILT_kenzo-miuiMM BUILT_kenzo-miuiN BUILT_kenzo-losMM BUILT_kenzo-losN BUILT_kenzo-losO .config .config.old
 
 echo "-----------------------------------------------------------------------"
@@ -12,7 +12,7 @@ echo "          BATCH MODE: Building AGNi LOS-O variant..."
 # AGNI pureLOS-O
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_losO.sh
-mv -f $KERNELDIR/BUILT_kenzo-losO $TEMP_DIR/
+#mv -f $KERNELDIR/BUILT_kenzo-losO $TEMP_DIR/
 rm .config
 echo " "
 echo "          BATCH MODE: Built AGNi LOS-O variant!!!"
@@ -25,7 +25,7 @@ echo "          BATCH MODE: Building AGNi LOS-N variant..."
 # AGNI pureLOS-N
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_losN.sh
-mv -f $KERNELDIR/BUILT_kenzo-losN $TEMP_DIR/
+#mv -f $KERNELDIR/BUILT_kenzo-losN $TEMP_DIR/
 rm .config
 echo " "
 echo "          BATCH MODE: Built AGNi LOS-N variant!!!"
@@ -39,7 +39,7 @@ echo "          BATCH MODE: Building AGNi MIUI-N variant..."
 # AGNI pureMIUI-N
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_miuiN.sh
-mv -f $KERNELDIR/BUILT_kenzo-miuiN $TEMP_DIR/
+#mv -f $KERNELDIR/BUILT_kenzo-miuiN $TEMP_DIR/
 rm .config
 echo " "
 echo "          BATCH MODE: Built AGNi MIUI-N variant!!!"
@@ -67,14 +67,14 @@ echo "          BATCH MODE: Building AGNi MIUI-MM variant..."
 # AGNI pureMIUI-MM
 rm drivers/staging/prima/wlan.ko
 ./build_kernel_kenzo_miuiMM.sh
-mv -f $KERNELDIR/BUILT_kenzo-miuiMM $TEMP_DIR/
+#mv -f $KERNELDIR/BUILT_kenzo-miuiMM $TEMP_DIR/
 rm .config
 echo " "
 echo "          BATCH MODE: Built AGNi MIUI-MM variant !!!"
 echo "-----------------------------------------------------------------------"
 echo " "
 
-mv -f $TEMP_DIR/* $KERNELDIR
-rm -rf $TEMP_DIR
+#mv -f $TEMP_DIR/* $KERNELDIR
+#rm -rf $TEMP_DIR
 echo "          BATCH MODE: Built ALL variants !!!"
 
