344b6cd42e276feb21653c4612589c387b1964d3
AshishM94 <im.ashish994@gmail.com>
2017-12-08 17:37:51 +0530

    net: wireguard: fix build errors

--- net/wireguard/compat/compat.h
+++ net/wireguard/compat/compat.h
@@ -208,10 +208,6 @@ static inline void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 12, 0) || defined(ISUBUNTU1404)) && !defined(ISRHEL7)
 #include <linux/random.h>
-static inline u32 prandom_u32_max(u32 ep_ro)
-{
-	return (u32)(((u64) prandom_u32() * ep_ro) >> 32);
-}
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 75) && !defined(ISRHEL7)
