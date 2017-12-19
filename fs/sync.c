393e9f202a48ff7d12fefe0543742a1836404f28
psndna88 <psndna88@gmail.com>
2017-12-19 13:13:19 +0530

    fs: Remove dynamic fsync completely
    Signed-off-by: psndna88 <psndna88@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -17,10 +17,6 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-#include <linux/dyn_sync_cntrl.h>
-#endif
-
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
@@ -183,17 +179,10 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !dyn_fsync_only_emergency_sync && suspend_active))
-		return 0;
-	else {
-#endif
 	if (!file->f_op || !file->f_op->fsync)
 		return -EINVAL;
+
 	return file->f_op->fsync(file, start, end, datasync);
-#ifdef CONFIG_DYNAMIC_FSYNC
-	}
-#endif
 }
 EXPORT_SYMBOL(vfs_fsync_range);
 
@@ -228,21 +217,11 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !dyn_fsync_only_emergency_sync && suspend_active))
-		return 0;
-	else
-#endif
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !dyn_fsync_only_emergency_sync && suspend_active))
-		return 0;
-	else
-#endif
 	return do_fsync(fd, 1);
 }
 
@@ -313,11 +292,6 @@ EXPORT_SYMBOL(generic_write_sync);
 SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 				unsigned int, flags)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !dyn_fsync_only_emergency_sync && suspend_active))
-		return 0;
-	else {
-#endif
 	int ret;
 	struct fd f;
 	struct address_space *mapping;
@@ -397,9 +371,6 @@ out_put:
 	fdput(f);
 out:
 	return ret;
-#ifdef CONFIG_DYNAMIC_FSYNC
-	}
-#endif
 }
 
 /* It would be nice if people remember that not all the world's an i386
@@ -407,10 +378,5 @@ out:
 SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !dyn_fsync_only_emergency_sync && suspend_active))
-		return 0;
-	else
-#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
