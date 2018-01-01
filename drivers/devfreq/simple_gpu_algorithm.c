97b33cd0ac8d90899c4125d012448e785c3a7590
Ryan Andri <ryanandri@linuxmail.org>
2018-01-02 02:50:33 +0700

    devfreq: tune Simple GPU algorithm
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>
    Signed-off-by: Haseo97 <jawgattus@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/devfreq/simple_gpu_algorithm.c
+++ drivers/devfreq/simple_gpu_algorithm.c
@@ -22,7 +22,7 @@
 static int default_laziness = 4;
 module_param_named(simple_laziness, default_laziness, int, 0664);
 
-static int ramp_up_threshold = 5000;
+static int ramp_up_threshold = 3000;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
 int simple_gpu_active = 0;
