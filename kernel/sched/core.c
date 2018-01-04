068f5dcf32d3058397d4adcea9cf5f31e7398908
Francisco Franco <franciscofranco.1990@gmail.com>
2018-01-04 04:17:00 +0000

    sched: core: the 8956 is an hexa-core so allow the cpu 0, 1, 2 and 3
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7809,7 +7809,7 @@ void __cpuinit init_idle(struct task_struct *idle, int cpu)
 static const struct cpumask *adjust_cpumask(const struct task_struct *p,
 	const struct cpumask *old_mask)
 {
-	static const unsigned long allowed_cpus = 0x3;
+	static const unsigned long allowed_cpus = 0xf;
 
 	if (!(p->flags & PF_KTHREAD) || p->kthread_per_cpu)
 		return old_mask;
