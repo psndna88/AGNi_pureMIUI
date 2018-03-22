6f934b8422b94f8b50bc363c69a89995ec275e61
Dmitry-777 <dmitryporshnevdev@gmail.com>
2018-03-22 15:51:52 +0300

    irq_work: use llist_empty_relaxed

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -114,7 +114,7 @@ bool irq_work_needs_cpu(void)
 
 	raised = &__get_cpu_var(raised_list);
 	lazy = &__get_cpu_var(lazy_list);
-	if (llist_empty(raised) && llist_empty(lazy))
+	if (llist_empty_relaxed(raised) && llist_empty_relaxed(lazy))
 		return false;
 
 	/* All work should have been flushed before going offline */
@@ -131,7 +131,7 @@ static void irq_work_run_list(struct llist_head *list)
 
 	BUG_ON(!irqs_disabled());
 
-	if (llist_empty(list))
+	if (llist_empty_relaxed(list))
 		return;
 
 	llnode = llist_del_all(list);
