99568ed5d55799fe23effe7bbfad59dd3a5ef601
Parvinder Singh <psndna88@gmail.com>
2018-04-05 12:20:16 +0530

    qpnp-smbcharger: set 4.4v as max battery full voltage
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -1016,11 +1016,7 @@ static int get_property_from_fg(struct smbchg_chip *chip,
 
 static int get_prop_batt_voltage_now(struct smbchg_chip *chip);
 #define DEFAULT_BATT_CAPACITY	50
-#ifdef CONFIG_MACH_XIAOMI_KENZO
-#define DEFAULT_BATT_VOLTAGE_MAX	4200000
-#else
-#define DEFAULT_BATT_VOLTAGE_MAX	4300000
-#endif
+#define DEFAULT_BATT_VOLTAGE_MAX	4400000
 static int get_prop_batt_capacity(struct smbchg_chip *chip)
 {
 	int capacity, rc;
@@ -1101,7 +1097,7 @@ static int get_prop_batt_voltage_now(struct smbchg_chip *chip)
 	return uv;
 }
 
-#define DEFAULT_BATT_VOLTAGE_MAX_DESIGN	4200000
+#define DEFAULT_BATT_VOLTAGE_MAX_DESIGN	4400000
 static int get_prop_batt_voltage_max_design(struct smbchg_chip *chip)
 {
 	int uv, rc;
