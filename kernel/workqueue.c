daff1bded5b89e7758c7a6ed702e8663eb200724
Parvinder Singh <psndna88@gmail.com>
2018-04-16 10:29:57 +0530

    workqueue: disable power_efficient wq's by default and allow sysfs tunable
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -281,13 +281,13 @@ static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0444);
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
-#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
-static bool wq_power_efficient = true;
-#else
-static bool wq_power_efficient;
-#endif
+//#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+//static bool wq_power_efficient = true;
+//#else
+static bool wq_power_efficient = false;
+//#endif
 
-//module_param_named(power_efficient, wq_power_efficient, bool, 0664);
+module_param_named(power_efficient, wq_power_efficient, bool, 0664);
 
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 
