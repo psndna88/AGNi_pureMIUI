969d7b6226ac77fcd31da3a64c79c45ccb47cf76
dev-harah1998 <harshit.india@linaro.org>
2018-04-26 11:57:55 +0200

    mm: Tune process reclaim

--- mm/process_reclaim.c
+++ mm/process_reclaim.c
@@ -30,7 +30,7 @@ static void swap_fn(struct work_struct *work);
 DECLARE_WORK(swap_work, swap_fn);
 
 /* User knob to enable/disable process reclaim feature */
-static int enable_process_reclaim;
+static int enable_process_reclaim = 1;
 module_param_named(enable_process_reclaim, enable_process_reclaim, int,
 	S_IRUGO | S_IWUSR);
 
@@ -43,8 +43,9 @@ module_param_named(reclaim_avg_efficiency, reclaim_avg_efficiency,
 			int, S_IRUGO);
 
 /* The vmpressure region where process reclaim operates */
-static unsigned long pressure_min = 50;
-static unsigned long pressure_max = 90;
+static unsigned long pressure_min = 40;
+static unsigned long pressure_max = 85;
+static unsigned long pressure = 0;
 module_param_named(pressure_min, pressure_min, ulong, S_IRUGO | S_IWUSR);
 module_param_named(pressure_max, pressure_max, ulong, S_IRUGO | S_IWUSR);
 
@@ -220,7 +221,7 @@ static void swap_fn(struct work_struct *work)
 static int vmpressure_notifier(struct notifier_block *nb,
 			unsigned long action, void *data)
 {
-	unsigned long pressure = action;
+	pressure = action;
 
 	if (!enable_process_reclaim)
 		return 0;
