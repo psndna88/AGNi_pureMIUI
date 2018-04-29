e34cfe97df6418ce09255325c1ab6b0f442c0a34
Sultan Alsawaf <sultanxda@gmail.com>
2018-04-29 12:53:44 -0700

    char: mem: Link /dev/random to /dev/urandom
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -841,7 +841,7 @@ static const struct memdev {
 #endif
 	 [5] = { "zero", 0666, &zero_fops, 0 },
 	 [7] = { "full", 0666, &full_fops, 0 },
-	 [8] = { "random", 0666, &random_fops, 0 },
+	 [8] = { "random", 0666, &urandom_fops, 0 },
 	 [9] = { "urandom", 0666, &urandom_fops, 0 },
 #ifdef CONFIG_PRINTK
 	[11] = { "kmsg", 0644, &kmsg_fops, 0 },
