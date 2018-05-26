5032afba7c4328ce322c1ef432761b8d2c68aec3
Parvinder Singh <psndna88@gmail.com>
2018-05-26 13:56:52 +0530

    Merge caf/LA.BR.1.3.7_rb1.11
    skipped:-
    camera_v2
    
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.c
@@ -89,8 +89,10 @@ struct msm_isp_bufq *msm_isp_get_bufq(
 	struct msm_isp_bufq *bufq = NULL;
 	uint32_t bufq_index = bufq_handle & 0xFF;
 
+	/* bufq_handle cannot be 0 */
 	if ((bufq_handle == 0) ||
-		(bufq_index >= buf_mgr->num_buf_q))
+		(bufq_index >= buf_mgr->num_buf_q) ||
+		(bufq_index >= BUF_MGR_NUM_BUF_Q))
 		return NULL;
 
 	bufq = &buf_mgr->bufq[bufq_index];
