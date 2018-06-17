22e51b39904a025c4ad410a814892263a022c5c4
joshuous <joshuous@gmail.com>
2018-06-17 18:22:14 +0200

    sched/tune: Refactor do_stune_boost()
    For added flexibility and in preparation for introducing another function.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -861,13 +861,9 @@ static int dynamic_boost(struct schedtune *st, int boost)
 	return ret;
 }
 
-int do_stune_boost(char *st_name, int boost)
+static int _do_stune_boost(struct schedtune *st, int boost)
 {
 	int ret = 0;
-	struct schedtune *st = getSchedtune(st_name);
-
-	if (!st)
-		return -EINVAL;
 
 	mutex_lock(&stune_boost_mutex);
 
@@ -894,6 +890,17 @@ int reset_stune_boost(char *st_name)
 
 	return ret;
 }
+
+int do_stune_boost(char *st_name, int boost)
+{
+	struct schedtune *st = getSchedtune(st_name);
+
+	if (!st)
+		return -EINVAL;
+
+	return _do_stune_boost(st, boost);
+}
+
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #else /* CONFIG_CGROUP_SCHEDTUNE */
