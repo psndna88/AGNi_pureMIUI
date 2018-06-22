e4fd9bf04010d0045afb8ae7a03903c76d0e9240
joshuous <joshuous@gmail.com>
2018-06-22 08:30:22 +0200

    sched/tune: Track active boosts on a per-Schedtune basis
    It does not make sense to be unable to reset Schedtune boost for a
    particular Schedtune group if another Schedtune group's boost is still
    active. Instead of using a global count, we should use a per-Schedtune
    group count to keep track of active boosts taking place.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -25,7 +25,6 @@ extern struct target_nrg schedtune_target_nrg;
 static DEFINE_MUTEX(stune_boost_mutex);
 static struct schedtune *getSchedtune(char *st_name);
 static int dynamic_boost(struct schedtune *st, int boost);
-static int stune_boost_count = 0;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 /* Performance Boost region (B) threshold params */
@@ -148,6 +147,9 @@ struct schedtune {
 
 	/* Dynamic boost value for tasks on that SchedTune CGroup */
 	int dynamic_boost;
+
+	/* Number of ongoing boosts for this SchedTune CGroup */
+	int boost_count;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 };
 
@@ -184,6 +186,7 @@ root_schedtune = {
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	.boost_default = 0,
 	.dynamic_boost = 0,
+	.boost_count = 0,
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 };
 
@@ -867,7 +870,7 @@ static int _do_stune_boost(struct schedtune *st, int boost)
 	int ret = 0;
 
 	mutex_lock(&stune_boost_mutex);
-	++stune_boost_count;
+	++(st->boost_count);
 
 	/* Boost if new value is greater than current */
 	if (boost > st->boost)
@@ -887,11 +890,11 @@ int reset_stune_boost(char *st_name)
 		return -EINVAL;
 
 	mutex_lock(&stune_boost_mutex);
-	if (stune_boost_count == 1)
+	if (st->boost_count == 1)
 		ret = dynamic_boost(st, st->boost_default);
 
-	if (stune_boost_count >= 1)
-		--stune_boost_count;
+	if (st->boost_count >= 1)
+		--(st->boost_count);
 	mutex_unlock(&stune_boost_mutex);
 
 	return ret;
