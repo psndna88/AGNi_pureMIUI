dc75df2f841ca7b9dce04c4701784d81cc557a9a
Tyler Nijmeh <tylernij@gmail.com>
2018-10-20 21:21:45 -0700

    schedutil: enforce realtime priority
    Even the interactive governor utilizes a realtime priority. It is
    beneficial for schedutil to process it's workload at a >= priority
    than mundane tasks (KGSL/AUDIO/ETC).
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: meg4z0rd <meydiwahendra@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -609,7 +609,7 @@ static void sugov_policy_free(struct sugov_policy *sg_policy)
 static int sugov_kthread_create(struct sugov_policy *sg_policy)
 {
 	struct task_struct *thread;
-	struct sched_param param = { .sched_priority = MAX_USER_RT_PRIO / 2 };
+	struct sched_param param = { .sched_priority = MAX_USER_RT_PRIO - 1 };
 	struct cpufreq_policy *policy = sg_policy->policy;
 	int ret;
 
