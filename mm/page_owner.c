0f4490ddf91ae859541bb4889295d69d49920d33
Miles Chen <miles.chen@mediatek.com>
2018-10-29 13:16:16 +0800

    mm/page_owner: use kvmalloc instead of kmalloc
    The kbuf used by page owner is allocated by kmalloc(), which means it
    can use only normal memory and there might be a "out of memory"
    issue when we're out of normal memory.
    
    To solve this problem, use kvmalloc() to allocate kbuf
    from normal/highmem. But there is one problem here: kvmalloc()
    does not fallback to vmalloc for sub page allocations. So sub
    page allocation fails due to out of normal memory cannot fallback
    to vmalloc.
    
    Modify kvmalloc() to allow sub page allocations fallback to
    vmalloc when CONFIG_HIGHMEM=y and use kvmalloc() to allocate
    kbuf.
    
    Clamp buffer size to PAGE_SIZE to avoid arbitrary size allocation.
    
    Change since v2:
      - improve kvmalloc, allow sub page allocations fallback to
        vmalloc when CONFIG_HIGHMEM=y
    
    Change since v1:
      - use kvmalloc()
      - clamp buffer size to PAGE_SIZE
    
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Cc: Joe Perches <joe@perches.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -1,6 +1,5 @@
 #include <linux/debugfs.h>
 #include <linux/mm.h>
-#include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/bootmem.h>
 #include <linux/stacktrace.h>
@@ -237,7 +236,8 @@ print_page_owner(char __user *buf, size_t count, unsigned long pfn,
 		.skip = 0
 	};
 
-	kbuf = kmalloc(count, GFP_KERNEL);
+	count = count > PAGE_SIZE ? PAGE_SIZE : count;
+	kbuf = kvmalloc(count, GFP_KERNEL);
 	if (!kbuf)
 		return -ENOMEM;
 
@@ -283,11 +283,11 @@ print_page_owner(char __user *buf, size_t count, unsigned long pfn,
 	if (copy_to_user(buf, kbuf, ret))
 		ret = -EFAULT;
 
-	kfree(kbuf);
+	kvfree(kbuf);
 	return ret;
 
 err:
-	kfree(kbuf);
+	kvfree(kbuf);
 	return -ENOMEM;
 }
 
