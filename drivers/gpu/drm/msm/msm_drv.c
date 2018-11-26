81fbc992d161a1fa200ebfd9a4586d267a84f182
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-25 18:25:17 -0800

    drm/msm: Don't bail out when debugfs creation fails
    There's no reason why the absence of debugfs should cause the GPU driver
    to break. Fix it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -940,14 +940,10 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		priv->fbdev = msm_fbdev_init(ddev);
 #endif
 
-	ret = msm_debugfs_late_init(ddev);
-	if (ret)
-		goto fail;
-
-	ret = sde_dbg_debugfs_register(dev);
-	if (ret) {
-		dev_err(dev, "failed to reg sde dbg debugfs: %d\n", ret);
-		goto fail;
+	if (!msm_debugfs_late_init(ddev)) {
+		priv->debug_root = debugfs_create_dir("debug",
+						ddev->primary->debugfs_root);
+		sde_dbg_debugfs_register(priv->debug_root);
 	}
 
 	/* perform subdriver post initialization */
