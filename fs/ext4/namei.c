ed43851a2755081703d8b952738b2c7a84238ae1
Parvinder Singh <psndna88@gmail.com>
2018-11-28 14:46:09 +0530

    ext4 ->tmpfile() support
    In this case we do need a bit more than usual, due to orphan
    list handling.
    
    Change-Id: I355e9c97e04a03f89bb760009fecc160b25caeb7
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2496,7 +2496,6 @@ retry:
 	return err;
 }
 
-#if 0
 static int ext4_tmpfile(struct inode *dir, struct dentry *dentry, umode_t mode)
 {
 	handle_t *handle;
@@ -2534,7 +2533,6 @@ err_unlock_inode:
 	unlock_new_inode(inode);
 	return err;
 }
-#endif
 
 struct ext4_dir_entry_2 *ext4_init_dot_dotdot(struct inode *inode,
 			  struct ext4_dir_entry_2 *de,
@@ -3811,9 +3809,7 @@ const struct inode_operations ext4_dir_inode_operations = {
 	.mkdir		= ext4_mkdir,
 	.rmdir		= ext4_rmdir,
 	.mknod		= ext4_mknod,
-#if 0
 	.tmpfile	= ext4_tmpfile,
-#endif
 	.rename		= ext4_rename2,
 	.setattr	= ext4_setattr,
 	.setxattr	= generic_setxattr,
