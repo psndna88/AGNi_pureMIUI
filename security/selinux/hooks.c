6f47e20c5d514a64bf152a281562653410e9378c
Parvinder Singh <psndna88@gmail.com>
2019-01-15 23:46:29 +0530

    selinux: add FORCE PERMISSIVE option
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -104,7 +104,11 @@ static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!kstrtoul(str, 0, &enforcing))
+#ifdef CONFIG_SECURITY_SELINUX_FORCE_PERMISSIVE
+		selinux_enforcing = 0;
+#else
 		selinux_enforcing = enforcing ? 1 : 0;
+#endif
 	return 1;
 }
 __setup("enforcing=", enforcing_setup);
--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -169,6 +169,9 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+#ifdef CONFIG_SECURITY_SELINUX_FORCE_PERMISSIVE
+	new_value = 0;
+#endif
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
