5071baebb80a803338ee5b18ea1dd4acc4dbb5de
Parvinder Singh <psndna88@gmail.com>
2019-01-30 22:56:13 +0530

    lmk: add MI PocoLauncher to whitelist

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -152,6 +152,7 @@ static bool lowmem_whitelist(char *name)
 		(!strcmp(name, "com.android.contacts")) ||
 		(!strcmp(name, "com.android.mms")) ||
 		(!strcmp(name, "com.xiaomi.hm.health")) ||
+		(!strcmp(name, "com.mi.android.globallauncher")) ||
 		(!strcmp(name, ".android.camera")))
 	{
 		ret = true;
