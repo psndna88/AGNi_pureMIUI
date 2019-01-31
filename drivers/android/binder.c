ae33549225261da4a8d678cdd08ec40a6bee874c
Christian Brauner <christian@brauner.io>
2019-01-31 01:25:02 +0100

    UPSTREAM: binderfs: remove separate device_initcall()
    binderfs should not have a separate device_initcall(). When a kernel is
    compiled with CONFIG_ANDROID_BINDERFS register the filesystem alongside
    CONFIG_ANDROID_IPC. This use-case is especially sensible when users specify
    CONFIG_ANDROID_IPC=y, CONFIG_ANDROID_BINDERFS=y and
    ANDROID_BINDER_DEVICES="".
    When CONFIG_ANDROID_BINDERFS=n then this always succeeds so there's no
    regression potential for legacy workloads.
    
    Signed-off-by: Christian Brauner <christian@brauner.io>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 136497735
    (cherry picked from commit 5b9633af298bfd1de650f6774d3fada546543101)
    Change-Id: I91892655d9d36df5218189f7874312eec7ae3c46
    Signed-off-by: Hridya Valsaraju <hridya@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -6056,9 +6056,10 @@ static int __init init_binder_device(const char *name)
 static int __init binder_init(void)
 {
 	int ret;
-	char *device_name, *device_names, *device_tmp;
+	char *device_name, *device_tmp;
 	struct binder_device *device;
 	struct hlist_node *tmp;
+	char *device_names = NULL;
 
 	ret = binder_alloc_shrinker_init();
 	if (ret)
@@ -6117,6 +6118,10 @@ static int __init binder_init(void)
 		if (ret)
 			goto err_init_binder_device_failed;
 	}
+
+	ret = init_binderfs();
+	if (ret)
+		goto err_init_binder_device_failed;
 
 	return ret;
 
