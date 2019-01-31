d67e341ac3507a7198e942bcf9017e9b98885d77
Parvinder Singh <psndna88@gmail.com>
2019-01-31 15:38:02 +0530

    selinux: option to disable all logging
    This option will make selinux not report any permissive log or denial in kmsg
    
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -742,6 +742,11 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
 
+#ifdef CONFIG_SECURITY_SELINUX_DISABLE_ALL_LOGS
+	return 0;
+#endif
+
+
 	if (!a) {
 		a = &stack_data;
 		a->type = LSM_AUDIT_DATA_NONE;
