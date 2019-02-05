b2cc9272d7959a477d0c5ab150087e9105c11f82
Miguel de Dios <migueldedios@google.com>
2019-02-05 12:52:00 -0800

    sched: fair: Move cpu_is_in_target_set definition
    Causes failures on kernel_test so moving the declaration up.
    
    Bug: 123939451
    Test: manual - build/build_test.sh
    Change-Id: I3865f406ad2363c6a968193052fe421956d94065
    Signed-off-by: Miguel de Dios <migueldedios@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -76,8 +76,6 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
 
 #endif
 
-static inline bool cpu_is_in_target_set(struct task_struct *p, int cpu);
-
 /*
  * Targeted preemption latency for CPU-bound tasks:
  *
@@ -7498,21 +7496,6 @@ enum fastpaths {
 	PREV_CPU_FASTPATH,
 	MANY_WAKEUP,
 };
-
-/*
- * Check whether cpu is in the fastest set of cpu's that p should run on.
- * If p is boosted, prefer that p runs on a faster cpu; otherwise, allow p
- * to run on any cpu.
- */
-static inline bool
-cpu_is_in_target_set(struct task_struct *p, int cpu)
-{
-	struct root_domain *rd = cpu_rq(cpu)->rd;
-	int first_cpu = (schedtune_task_boost(p)) ?
-		rd->mid_cap_orig_cpu : rd->min_cap_orig_cpu;
-	int next_usable_cpu = cpumask_next(first_cpu - 1, &p->cpus_allowed);
-	return cpu >= next_usable_cpu || next_usable_cpu >= nr_cpu_ids;
-}
 
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool boosted, bool sync_boost,
