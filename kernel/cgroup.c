61ed46c1b9bd370945e3ae977901323c90d80865
AmolAmrit <amol.amrit03@outlook.com>
2019-03-07 02:40:50 +0530

    cgroup: Fix merge mismatch
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2107,44 +2107,6 @@ out_free_group_list:
 }
 
 
-static int cgroup_allow_attach(struct cgroup *cgrp, struct cgroup_taskset *tset)
-{
-	struct cgroup_subsys *ss;
-	int ret;
-
-	for_each_subsys(cgrp->root, ss) {
-		if (ss->allow_attach) {
-			ret = ss->allow_attach(cgrp, tset);
-			if (ret)
-				return ret;
-		} else {
-			return -EACCES;
-		}
-	}
-
-	return 0;
-}
-
-int subsys_cgroup_allow_attach(struct cgroup *cgrp, struct cgroup_taskset *tset)
-{
-	const struct cred *cred, *tcred;
-	struct task_struct *task;
-
-	if (capable(CAP_SYS_NICE) || capable(CAP_SYS_ADMIN))
-		return 0;
-
-	cred = current_cred();
-
-	cgroup_taskset_for_each(task, cgrp, tset) {
-		tcred = __task_cred(task);
-
-		if (current != task && cred->euid != tcred->uid &&
-		    cred->euid != tcred->suid)
-			return -EACCES;
-	}
-
-	return 0;
-}
 
 /*
  * Find the task_struct of the task to attach by vpid and pass it along to the
