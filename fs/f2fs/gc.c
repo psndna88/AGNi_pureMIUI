77c62f4234fc7cf2872490c72a593620f6d73971
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-01 21:55:33 +0900

    f2fs: explicitly flush device upon foreground GC
    GC itself will produce some dirty segments.
    This will allow more GCs to be made upon rapid GC or urgent GC.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
    
    # Conflicts:
    #	fs/f2fs/gc.c

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1240,6 +1240,7 @@ int f2fs_gc(struct f2fs_sb_info *sbi, bool sync,
 		.ilist = LIST_HEAD_INIT(gc_list.ilist),
 		.iroot = RADIX_TREE_INIT(GFP_NOFS),
 	};
+	struct super_block *sb = sbi->sb;
 	unsigned long long last_skipped = sbi->skipped_atomic_files[FG_GC];
 	unsigned long long first_skipped;
 	unsigned int skipped_round = 0, round = 0;
@@ -1257,7 +1258,7 @@ int f2fs_gc(struct f2fs_sb_info *sbi, bool sync,
 	sbi->skipped_gc_rwsem = 0;
 	first_skipped = last_skipped;
 gc_more:
-	if (unlikely(!(sbi->sb->s_flags & MS_ACTIVE))) {
+	if (unlikely(!(sb->s_flags & MS_ACTIVE))) {
 		ret = -EINVAL;
 		goto stop;
 	}
@@ -1347,6 +1348,13 @@ stop:
 
 	if (sync && !ret)
 		ret = sec_freed ? 0 : -EAGAIN;
+
+	if (gc_type == FG_GC && down_read_trylock(&sb->s_umount)) {
+		writeback_inodes_sb(sb, WB_REASON_SYNC);
+		sync_inodes_sb(sb);
+		up_read(&sb->s_umount);
+	}
+
 	return ret;
 }
 
