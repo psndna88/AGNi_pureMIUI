d496f74ae54ea7def2fe9ff0ffa86359da076a3a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-02 09:30:02 -0700

    drivers: thermal: limits-dcvs: Always build driver
    The routines in this driver are required to enable stable performance at
    all supported CPU frequencies. Instead of allowing the driver to be
    removed completely, always compile it and simply stub out the actual CPU
    throttle components, in order to enable full CPU performance.
    
    Change-Id: I66397a5aa36a39cb39c68510754a473f359591ab
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/thermal/qcom/msm_lmh_dcvs.c
+++ drivers/thermal/qcom/msm_lmh_dcvs.c
@@ -360,6 +360,13 @@ static int limits_dcvs_probe(struct platform_device *pdev)
 	}
 	request_reg = be32_to_cpu(addr[0]) + LIMITS_CLUSTER_REQ_OFFSET;
 
+	if (!IS_ENABLED(CONFIG_QTI_THERMAL_LIMITS_DCVS)) {
+		limits_isens_vref_ldo_init(pdev, hw);
+		devm_kfree(&pdev->dev, hw->cdev_data);
+		devm_kfree(&pdev->dev, hw);
+		return 0;
+	}
+
 	hw->hw_freq_limit = U32_MAX;
 	snprintf(hw->sensor_name, sizeof(hw->sensor_name), "limits_sensor-%02d",
 			affinity);
