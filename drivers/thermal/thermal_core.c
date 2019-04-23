3cd00cf27e430f727cce0a32ba64a13e8dc17d2b
davidchao <davidchao@google.com>
2019-04-23 16:12:42 +0800

    thermal: core: Use high value to init low temp tracking thermal_zone
    When the thermal_zone property is set to “tracks-low” we should init with
    THERMAL_TEMP_INVALID_LOW (274000), not THERMAL_TEMP_INVALID (-274000).
    
    Bug: 111683694
    Test: Build and thermal mitigation works properly.
    Change-Id: I2824809fdfb47c544d30fde6e4866c3ee4e7b25c
    Signed-off-by: davidchao <davidchao@google.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -503,9 +503,14 @@ static void update_temperature(struct thermal_zone_device *tz)
 static void thermal_zone_device_init(struct thermal_zone_device *tz)
 {
 	struct thermal_instance *pos;
-	tz->temperature = THERMAL_TEMP_INVALID;
 	tz->prev_low_trip = -INT_MAX;
 	tz->prev_high_trip = INT_MAX;
+
+	if (tz->tzp && tz->tzp->tracks_low)
+		tz->temperature = THERMAL_TEMP_INVALID_LOW;
+	else
+		tz->temperature = THERMAL_TEMP_INVALID;
+
 	list_for_each_entry(pos, &tz->thermal_instances, tz_node)
 		pos->initialized = false;
 }
