0a27d302bc5c51301045a977c0d69a7db92c001a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-04 22:08:57 -0700

    cpuidle: Eliminate significant overhead from pm_qos latency notifier
    Currently, the latency notifier kicks all CPUs out of idle all of the
    time, even when the latency requirements are relaxed. Additionally, the
    call to smp_call_function_many is set to wait by default, which causes
    it to block for a while and waste a lot of CPU time since it is called
    with preemption disabled.
    
    Eliminate the mountain of overhead from the latency notifier by making
    the smp_call_function_many non-blocking and only invoking it when the
    CPU's latency requirements are tightened.
    
    Additionally, remove the check for a mask in the v parameter since the
    pm_qos framework does not pass anything for it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -628,16 +628,17 @@ static void smp_callback(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
+	static unsigned long prev_latency = ULONG_MAX;
 	struct cpumask cpus;
 
-	if (v)
-		cpumask_andnot(&cpus, v, cpu_isolated_mask);
-	else
+	if (l < prev_latency) {
 		cpumask_andnot(&cpus, cpu_online_mask, cpu_isolated_mask);
+		preempt_disable();
+		smp_call_function_many(&cpus, smp_callback, NULL, false);
+		preempt_enable();
+	}
 
-	preempt_disable();
-	smp_call_function_many(&cpus, smp_callback, NULL, 1);
-	preempt_enable();
+	prev_latency = l;
 
 	return NOTIFY_OK;
 }
