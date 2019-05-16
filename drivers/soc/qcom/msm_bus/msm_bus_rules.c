ebdb25543b5c8d85aeab7ee6a82136ee57b4ff07
Park Ju Hyung <qkrwngud825@gmail.com>
2019-05-16 22:02:35 +0900

    msm_bus: remove tracer
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_rules.c
+++ drivers/soc/qcom/msm_bus/msm_bus_rules.c
@@ -16,8 +16,6 @@
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/msm-bus.h>
-#define CREATE_TRACE_POINTS
-#include <trace/events/trace_msm_bus.h>
 
 struct node_vote_info {
 	int id;
