c5d23ef01195e30abc169ab29f2bfce10155c7dd
Rick Yiu <rickyiu@google.com>
2019-05-27 19:29:30 +0800

    soc/qcom: move icnss_initialize to async probe
    To reduce boot time to first stage.
    
    Before:
    [    1.221918] init: init first stage started!
    
    After:
    [    1.199159] init: init first stage started!
    
    Bug: 129688998
    Test: reboot 100 times, camera, wifi, basic operation
    Change-Id: Iff04c974605a2a5e5580c60a6b498b1fdbb44d12
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -4066,6 +4066,7 @@ static struct platform_driver icnss_driver = {
 		.pm = &icnss_pm_ops,
 		.owner = THIS_MODULE,
 		.of_match_table = icnss_dt_match,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 
