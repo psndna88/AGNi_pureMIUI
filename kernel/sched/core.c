d3c3a4a79a83cc01a459a161a9606ef08446bb81
Yaroslav Furman <yaro330@gmail.com>
2019-05-28 19:34:06 +0300

    sched: core: silence no longer affine to cpu logspam

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2465,7 +2465,7 @@ out:
 		 * leave kernel.
 		 */
 		if (p->mm && printk_ratelimit()) {
-			printk_deferred("process %d (%s) no longer affine to cpu%d\n",
+			pr_debug("process %d (%s) no longer affine to cpu%d\n",
 					task_pid_nr(p), p->comm, cpu);
 		}
 	}
