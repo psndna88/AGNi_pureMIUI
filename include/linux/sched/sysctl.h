9409a1ea6c29e11e6ad838cd4b66084f9b378ca8
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-05-28 10:03:50 +0530

    sched: Use bitmask for sched_busy_hysteresis_enable_cpus tunable
    The current code is using a bitmap for sched_busy_hysteresis_enable_cpus
    tunable. Since the feature is not enabled for any of the CPUs, the
    default value is printed as "\n". This is very inconvenient for
    user space applications which tries to write a new value and try to
    restore the previous value. Like other scheduler tunables, use
    the bitmask to avoid this issue.
    
    Change-Id: I0c5989606352be5382dd688602aefd753fb62317
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -136,6 +136,6 @@ extern int sysctl_schedstats(struct ctl_table *table, int write,
 extern char sched_lib_name[LIB_PATH_LENGTH];
 extern unsigned int sched_lib_mask_force;
 extern bool is_sched_lib_based_app(pid_t pid);
-extern unsigned long *sched_busy_hysteresis_cpubits;
+extern unsigned int sysctl_sched_busy_hysteresis_enable_cpus;
 
 #endif /* _LINUX_SCHED_SYSCTL_H */
