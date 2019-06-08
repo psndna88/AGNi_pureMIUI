4ecfb6d18712bdeae4fa8a17a3a9bc358ec229c9
Tyler Nijmeh <tylernij@gmail.com>
2019-06-07 19:00:17 -0700

    nvt_touch_d2s: Use a FIFO thread for work processing
    This should reduce any potential preemption by other kernel tasks when
    handling touch input work. See the Maxim STI touchscreen for
    inspiration.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.c
@@ -1027,6 +1027,9 @@ static void nvt_ts_work_func(struct work_struct *work)
 #endif /* MT_PROTOCOL_B */
 	int32_t i = 0;
 	int32_t finger_cnt = 0;
+	struct sched_param param = { .sched_priority = MAX_USER_RT_PRIO / 2 };
+
+	sched_setscheduler(current, SCHED_FIFO, &param);
 
 	mutex_lock(&ts->lock);
 
