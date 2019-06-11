8b76f646b331447ac8d0ff648c171c53cb7a618e
Parvinder Singh <psndna88@gmail.com>
2019-06-11 18:27:25 +0530

    input: goodix_FP: Mark IRQ as performance critical
    In order to improve fingerprint wake performance, mark the fingerprint
    reader's IRQ as performance-critical.
    
    # Conflicts:
    #	drivers/input/fingerprint/gf3208/gf_spi.c

--- drivers/input/fingerprint/gf3258/gf_spi.c
+++ drivers/input/fingerprint/gf3258/gf_spi.c
@@ -364,7 +364,7 @@ static int irq_setup(struct gf_dev *gf_dev)
 
 	gf_dev->irq = gf_irq_num(gf_dev);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL,
 			"gf", gf_dev);
 
 	if (status) {
