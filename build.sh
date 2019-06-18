442316c744dd5e25ef2e4c6a40482c000e9d4f37
AmolAmrit <amol.amrit03@outlook.com>
2019-06-18 15:00:01 +0200

    build.sh: Update script
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# Custom build script for Escrima Kernel
+
+# Constants
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+default='\033[0m'
+
+# Define variables
 KERNEL_DIR=$PWD
 Anykernel_DIR=$KERNEL_DIR/AnyKernel3/
 DATE=$(date +"%d%m%Y")
@@ -6,7 +20,9 @@ KERNEL_NAME="Escrima-X27-Kamui-LightningBlade-PurpleLightning"
 DEVICE="-kenzo-"
 FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TIME"
 
-# Cleanup before 
+BUILD_START=$(date +"%s")
+
+# Cleanup before
 rm -rf $Anykernel_DIR/*zip
 rm -rf $Anykernel_DIR/Image.gz-dtb
 rm -rf arch/arm64/boot/Image
@@ -21,15 +37,27 @@ export CROSS_COMPILE=/home/amol/pie/prebuilts/gcc/linux-x86/aarch64/aarch64-linu
 export ARCH="arm64"
 export USE_CCACHE=1
 
+echo -e "$green***********************************************"
+echo  "           Compiling Escrima Kernel                    "
+echo -e "***********************************************"
+
 # Finally build it
 make clean && make mrproper
 make lineageos_kenzo_defconfig
-make -j4
+make -j6
+
+echo -e "$yellow***********************************************"
+echo  "                 Zipping up                    "
+echo -e "***********************************************"
 
 # Create the flashable zip
 cp arch/arm64/boot/Image.gz-dtb $Anykernel_DIR
 cd $Anykernel_DIR
 zip -r9 $FINAL_ZIP.zip * -x .git README.md *placeholder
+
+echo -e "$cyan***********************************************"
+echo  "            Cleaning up the mess created               "
+echo -e "***********************************************$default"
 
 # Cleanup again
 cd ../
@@ -38,3 +66,8 @@ rm -rf arch/arm64/boot/Image
 rm -rf arch/arm64/boot/dts/qcom/kenzo-msm8956-mtp.dtb
 rm -rf arch/arm64/boot/Image.gz
 rm -rf arch/arm64/boot/Image.gz-dtb
+
+# Build complete
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$default"
