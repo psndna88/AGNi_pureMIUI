4011ed18a36dca824585b1559d6724b326553e1b
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-06-18 14:46:42 +0530

    sched/core_ctl: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I05a6645db80cc04993b45d7ec25a3fb7a112cf3e
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -27,9 +27,6 @@
 #include "sched.h"
 #include "walt.h"
 
-#define MAX_CPUS_PER_CLUSTER 6
-#define MAX_CLUSTERS 3
-
 struct cluster_data {
 	bool inited;
 	unsigned int min_cpus;
@@ -923,7 +920,7 @@ void core_ctl_notifier_unregister(struct notifier_block *n)
 
 static void core_ctl_call_notifier(void)
 {
-	struct core_ctl_notif_data ndata;
+	struct core_ctl_notif_data ndata = {0};
 	struct notifier_block *nb;
 
 	/*
@@ -938,7 +935,9 @@ static void core_ctl_call_notifier(void)
 		return;
 
 	ndata.nr_big = last_nr_big;
-	ndata.coloc_load_pct = walt_get_default_coloc_group_load();
+	walt_fill_ta_data(&ndata);
+	trace_core_ctl_notif_data(ndata.nr_big, ndata.coloc_load_pct,
+			ndata.ta_util_pct, ndata.cur_cap_pct);
 
 	atomic_notifier_call_chain(&core_ctl_notifier, 0, &ndata);
 }
