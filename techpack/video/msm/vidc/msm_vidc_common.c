e9f5ee07f16e1f1d3c04b7fa3a9f7049bcb2901a
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:50:08 +0900

    msm: vidc: use kmem_cache pool for struct msm_vidc_buffer
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- techpack/video/msm/vidc/msm_vidc_common.c
+++ techpack/video/msm/vidc/msm_vidc_common.c
@@ -11,6 +11,8 @@
 #include "msm_vidc_clocks.h"
 #include "msm_vidc_buffer_calculations.h"
 
+static struct kmem_cache *kmem_buf_pool;
+
 #define IS_ALREADY_IN_STATE(__p, __d) (\
 	(__p >= __d)\
 )
@@ -534,6 +536,12 @@ int msm_comm_ctrl_init(struct msm_vidc_inst *inst,
 		return -EINVAL;
 	}
 
+	kmem_buf_pool = KMEM_CACHE(msm_vidc_buffer, SLAB_HWCACHE_ALIGN);
+	if (!kmem_buf_pool) {
+		s_vpr_e(inst->sid, "%s: - failed to allocate kmem pool\n", __func__);
+		return -ENOMEM;
+	}
+
 	inst->ctrls = kcalloc(num_ctrls, sizeof(struct v4l2_ctrl *),
 				GFP_KERNEL);
 	if (!inst->ctrls) {
@@ -621,6 +629,7 @@ int msm_comm_ctrl_deinit(struct msm_vidc_inst *inst)
 
 	kfree(inst->ctrls);
 	v4l2_ctrl_handler_free(&inst->ctrl_handler);
+	kmem_cache_destroy(kmem_buf_pool);
 
 	return 0;
 }
@@ -6982,7 +6991,7 @@ struct msm_vidc_buffer *msm_comm_get_vidc_buffer(struct msm_vidc_inst *inst,
 
 	if (!found) {
 		/* this is new vb2_buffer */
-		mbuf = kzalloc(sizeof(struct msm_vidc_buffer), GFP_KERNEL);
+		mbuf = kmem_cache_zalloc(kmem_buf_pool, GFP_KERNEL);
 		if (!mbuf) {
 			s_vpr_e(inst->sid, "%s: alloc msm_vidc_buffer failed\n",
 				__func__);
@@ -7273,7 +7282,7 @@ static void kref_free_mbuf(struct kref *kref)
 	struct msm_vidc_buffer *mbuf = container_of(kref,
 			struct msm_vidc_buffer, kref);
 
-	kfree(mbuf);
+	kmem_cache_free(kmem_buf_pool, mbuf);
 }
 
 void kref_put_mbuf(struct msm_vidc_buffer *mbuf)
