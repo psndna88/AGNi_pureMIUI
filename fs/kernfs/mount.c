1f2d4cd6bace9b57052087f402ada18a71413d24
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:42:24 +0900

    kernfs: use kmem_cache pool for struct kernfs_open_node/file
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- fs/kernfs/mount.c
+++ fs/kernfs/mount.c
@@ -363,7 +363,7 @@ void kernfs_kill_sb(struct super_block *sb)
 
 void __init kernfs_init(void)
 {
-
+	init_kernfs_file_pool();
 	/*
 	 * the slab is freed in RCU context, so kernfs_find_and_get_node_by_ino
 	 * can access the slab lock free. This could introduce stale nodes,
