705f58d9949505304259d7bc03b0baa4a71179e6
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:45:59 +0900

    glink: use kmem_cache pool for struct deferred_cmd
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/soc/qcom/glink_smem_native_xprt.c
+++ drivers/soc/qcom/glink_smem_native_xprt.c
@@ -247,6 +247,8 @@ struct deferred_cmd {
 	void *data;
 };
 
+static struct kmem_cache *kmem_deferred_cmd_pool;
+
 static uint32_t negotiate_features_v1(struct glink_transport_if *if_ptr,
 				      const struct glink_core_version *version,
 				      uint32_t features);
@@ -788,7 +790,7 @@ static bool queue_cmd(struct edge_info *einfo, void *cmd, void *data)
 	struct command *_cmd = cmd;
 	struct deferred_cmd *d_cmd;
 
-	d_cmd = kmalloc(sizeof(*d_cmd), GFP_ATOMIC);
+	d_cmd = kmem_cache_alloc(kmem_deferred_cmd_pool, GFP_ATOMIC);
 	if (!d_cmd) {
 		GLINK_ERR("%s: Discarding cmd %d\n", __func__, _cmd->id);
 		return false;
@@ -938,7 +940,7 @@ static void __rx_worker(struct edge_info *einfo, bool atomic_ctx)
 			cmd.param1 = d_cmd->param1;
 			cmd.param2 = d_cmd->param2;
 			cmd_data = d_cmd->data;
-			kfree(d_cmd);
+			kmem_cache_free(kmem_deferred_cmd_pool, d_cmd);
 		} else {
 			memset(&cmd, 0, sizeof(cmd));
 			fifo_read(einfo, &cmd, sizeof(cmd));
@@ -1533,7 +1535,7 @@ static int ssr(struct glink_transport_if *if_ptr)
 						struct deferred_cmd, list_node);
 		list_del(&cmd->list_node);
 		kfree(cmd->data);
-		kfree(cmd);
+		kmem_cache_free(kmem_deferred_cmd_pool, cmd);
 	}
 
 	einfo->tx_resume_needed = false;
@@ -3169,6 +3171,8 @@ static struct platform_driver glink_mailbox_driver = {
 static int __init glink_smem_native_xprt_init(void)
 {
 	int rc;
+
+	kmem_deferred_cmd_pool = KMEM_CACHE(deferred_cmd, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
 
 	rc = platform_driver_register(&glink_smem_native_driver);
 	if (rc) {
