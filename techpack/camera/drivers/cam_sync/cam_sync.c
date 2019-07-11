8775e78d77e4552230302bdc3bfc8f3f5b0ac402
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:52:37 +0900

    msm: camera: use kmem_cache pool for struct sync_user_payload
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- techpack/camera/drivers/cam_sync/cam_sync.c
+++ techpack/camera/drivers/cam_sync/cam_sync.c
@@ -20,6 +20,7 @@
 #include "cam_req_mgr_workq.h"
 
 struct sync_device *sync_dev;
+static struct kmem_cache *kmem_payload_pool;
 
 /*
  * Flag to determine whether to enqueue cb of a
@@ -641,7 +642,7 @@ static int cam_sync_handle_register_user_payload(
 	if (sync_obj >= CAM_SYNC_MAX_OBJS || sync_obj <= 0)
 		return -EINVAL;
 
-	user_payload_kernel = kzalloc(sizeof(*user_payload_kernel), GFP_KERNEL);
+	user_payload_kernel = kmem_cache_zalloc(kmem_payload_pool, GFP_KERNEL);
 	if (!user_payload_kernel)
 		return -ENOMEM;
 
@@ -657,7 +658,7 @@ static int cam_sync_handle_register_user_payload(
 			"Error: accessing an uninitialized sync obj = %d",
 			sync_obj);
 		spin_unlock_bh(&sync_dev->row_spinlocks[sync_obj]);
-		kfree(user_payload_kernel);
+		kmem_cache_free(kmem_payload_pool, user_payload_kernel);
 		return -EINVAL;
 	}
 
@@ -673,7 +674,7 @@ static int cam_sync_handle_register_user_payload(
 			CAM_SYNC_COMMON_REG_PAYLOAD_EVENT);
 
 		spin_unlock_bh(&sync_dev->row_spinlocks[sync_obj]);
-		kfree(user_payload_kernel);
+		kmem_cache_free(kmem_payload_pool, user_payload_kernel);
 		return 0;
 	}
 
@@ -687,7 +688,7 @@ static int cam_sync_handle_register_user_payload(
 				user_payload_kernel->payload_data[1]) {
 
 			spin_unlock_bh(&sync_dev->row_spinlocks[sync_obj]);
-			kfree(user_payload_kernel);
+			kmem_cache_free(kmem_payload_pool, user_payload_kernel);
 			return -EALREADY;
 		}
 	}
@@ -742,7 +743,7 @@ static int cam_sync_handle_deregister_user_payload(
 				user_payload_kernel->payload_data[1] ==
 				userpayload_info.payload[1]) {
 			list_del_init(&user_payload_kernel->list);
-			kfree(user_payload_kernel);
+			kmem_cache_free(kmem_payload_pool, user_payload_kernel);
 		}
 	}
 
@@ -1309,12 +1310,16 @@ struct platform_driver cam_sync_driver = {
 
 int cam_sync_init(void)
 {
+	kmem_payload_pool = KMEM_CACHE(sync_user_payload, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
+
 	return platform_driver_register(&cam_sync_driver);
 }
 
 void cam_sync_exit(void)
 {
 	platform_driver_unregister(&cam_sync_driver);
+
+	kmem_cache_destroy(kmem_payload_pool);
 }
 
 MODULE_DESCRIPTION("Camera sync driver");
