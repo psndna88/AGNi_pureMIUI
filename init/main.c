58dd6a264e01d9312123e4033d8f9d4603f986c4
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 01:16:18 +0900

    dma_buf: use kmem_cache pool for struct sync_file
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- init/main.c
+++ init/main.c
@@ -582,6 +582,7 @@ void __init __weak arch_call_rest_init(void)
 	rest_init();
 }
 
+void __init init_sync_kmem_pool(void);
 void __init init_dma_buf_kmem_pool(void);
 asmlinkage __visible void __init start_kernel(void)
 {
@@ -782,6 +783,7 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
+	init_sync_kmem_pool();
 	init_dma_buf_kmem_pool();
 
 	poking_init();
