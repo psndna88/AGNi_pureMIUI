4c0307d77d6861bbd792d84b159fc17359c121f5
flar2 <asegaert@gmail.com>
2019-07-19 20:29:49 -0400

    kcal: remove min limit

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -207,6 +207,7 @@ void sde_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	struct drm_msm_pcc *pcc_cfg;
 	struct drm_msm_pcc_coeff *coeffs = NULL;
 	int i = 0;
+	int kcal_min = 0;
 	u32 base = 0;
 
 	if (!ctx || !cfg) {
