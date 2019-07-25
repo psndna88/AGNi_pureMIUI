fda3fd34415e1ffea82468c86130fc2c9480af28
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2019-07-25 15:38:33 -0700

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I18364c6061ed7525755aaf187bf15a8cb9b54a8a
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    [render: minor fixup]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -1488,6 +1488,7 @@ TRACE_EVENT(sched_task_util,
 		__field(bool, is_rtg			)
 		__field(bool, rtg_skip_min		)
 		__field(int, start_cpu			)
+		__field(int, unfilter			)
 	),
 
 	TP_fast_assign(
@@ -1507,15 +1508,17 @@ TRACE_EVENT(sched_task_util,
 		__entry->is_rtg			= is_rtg;
 		__entry->rtg_skip_min		= rtg_skip_min;
 		__entry->start_cpu		= start_cpu;
+		__entry->unfilter		= p->unfilter;
 	),
 
-	TP_printk("pid=%d comm=%s util=%lu prev_cpu=%d next_cpu=%d backup_cpu=%d target_cpu=%d sync=%d need_idle=%d fastpath=%d placement_boost=%d latency=%llu stune_boosted=%d is_rtg=%d rtg_skip_min=%d start_cpu=%d",
+	TP_printk("pid=%d comm=%s util=%lu prev_cpu=%d next_cpu=%d backup_cpu=%d target_cpu=%d sync=%d need_idle=%d fastpath=%d placement_boost=%d latency=%llu stune_boosted=%d is_rtg=%d rtg_skip_min=%d start_cpu=%d unfilter=%d",
 		__entry->pid, __entry->comm, __entry->util, __entry->prev_cpu,
 		__entry->next_cpu, __entry->backup_cpu, __entry->target_cpu,
 		__entry->sync, __entry->need_idle,
 		__entry->fastpath, __entry->placement_boost,
 		__entry->latency, __entry->stune_boosted,
-		__entry->is_rtg, __entry->rtg_skip_min, __entry->start_cpu)
+		__entry->is_rtg, __entry->rtg_skip_min, __entry->start_cpu,
+		__entry->unfilter)
 )
 
 /*
