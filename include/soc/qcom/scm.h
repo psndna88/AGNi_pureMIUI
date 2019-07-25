4a9665d773a1730a8acc47c923d05af3f1e9e6ce
Minchan Kim <minchan@google.com>
2019-07-25 15:52:01 +0900

    memlat: Don't read perf event if other CPU is under SCM call
    memlat works based on perf event to calculate algorithm.
    Perf event needs IPI to read count from other CPU.
    However, if the CPU is under heavy scm call(+20ms), it could
    be stuck until the CPU could complete the scm call.
    It would cause long latency.
    
    This patch adds global atomic count to check whether scm is
    going. If it's under going, memlat doesn't invoke perf event
    function to avoid IPI stuck. Although there is still race to
    check it, the race should be small enough so effectivey, it
    mitigate such long latency.
    
    Please check to memlat owner what happens when read_event
    sometimes returns zero due to scm call in parallel.
    
    Bug: 134440633
    Change-Id: I0403d151ededcc80d531c8cd81daa783000902d1
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- include/soc/qcom/scm.h
+++ include/soc/qcom/scm.h
@@ -124,7 +124,7 @@ struct scm_hdcp_req {
 };
 
 extern struct mutex scm_lmh_lock;
-
+extern bool under_scm_call(void);
 #else
 
 static inline int scm_call2(u32 cmd_id, struct scm_desc *desc)
@@ -185,6 +185,10 @@ static inline bool scm_is_secure_device(void)
 static inline int scm_enable_mem_protection(void)
 {
 	return 0;
+}
+extern bool under_scm_call(void)
+{
+	return false;
 }
 #endif
 
