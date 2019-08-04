67ada8ceb73dc2994cc4e4522fe29e45a4eadeab
Danny Lin <danny@kdrag0n.dev>
2019-08-04 05:57:49 +0000

    sched/tune: Refactor SchedTune Assist code
     - Return proper values when write wrappers aren't bypassed
     - Revise Kconfig description
     - Improve overall code style
     - Don't write colocate and sched_boost_no_override values when WALT is
       disabled
     - Mark static data as static
     - Improve readability of log messages
     - Propagate cftype struct in write wrappers
     - Use task_is_booster helper rather than hard-coded "init" check
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [adapted for this tree]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1,3 +1,4 @@
+#include <linux/binfmts.h>
 #include <linux/cgroup.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
@@ -621,25 +622,21 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 
 #ifdef CONFIG_STUNE_ASSIST
 static int boost_write_wrapper(struct cgroup_subsys_state *css,
-			struct cftype *cft, s64 boost)
+			       struct cftype *cft, s64 boost)
 {
-	if (!strcmp(current->comm, "init"))
+	if (task_is_booster(current))
 		return 0;
 
-	boost_write(css, NULL, boost);
-
-	return 0;
+	return boost_write(css, cft, boost);
 }
 
 static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
-			struct cftype *cft, u64 prefer_idle)
+				     struct cftype *cft, u64 prefer_idle)
 {
-	if (!strcmp(current->comm, "init"))
+	if (task_is_booster(current))
 		return 0;
 
-	prefer_idle_write(css, NULL, prefer_idle);
-
-	return 0;
+	return prefer_idle_write(css, cft, prefer_idle);
 }
 #endif
 
@@ -677,24 +674,29 @@ schedtune_boostgroup_init(struct schedtune *st)
 }
 
 #ifdef CONFIG_STUNE_ASSIST
+struct st_data {
+	char *name;
+	int boost;
+	bool prefer_idle;
+};
+
 static void write_default_values(struct cgroup_subsys_state *css)
 {
-	u8 i;
-	struct groups_data {
-		char *name;
-		int boost;
-		bool prefer_idle;
-	};
-	struct groups_data groups[2] = {
+	static struct st_data st_targets[] = {
 		{ "top-app",	1, 1 },
-		{ "foreground", 0, 1 }};
+		{ "foreground",	0, 1 }
+	};
+	int i;
 
-	for (i = 0; i < ARRAY_SIZE(groups); i++) {
-		if (!strcmp(css->cgroup->kn->name, groups[i].name)) {
-			pr_info("%s: %i - %i\n", groups[i].name,
-					groups[i].boost, groups[i].prefer_idle);
-			boost_write(css, NULL, groups[i].boost);
-			prefer_idle_write(css, NULL, groups[i].prefer_idle);
+	for (i = 0; i < ARRAY_SIZE(st_targets); i++) {
+		struct st_data tgt = st_targets[i];
+
+		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle);
+
+			boost_write(css, NULL, tgt.boost);
+			prefer_idle_write(css, NULL, tgt.prefer_idle);
 		}
 	}
 }
