5e3d67acafef6f9cc2ead6d76de0e566ca37d2d6
Maria Yu <aiquny@codeaurora.org>
2019-08-13 17:25:28 +0800

    cpufreq: schedutil: Queue sugov irq work on policy online cpu
    Got never update frequency if scheduled the irq
    work on an offlined cpu and it will always pending.
    Queue sugov irq work on any online cpu if current
    cpu is offline.
    
    Change-Id: I33fc691917b5866488b6aeb11ed902a2753130b2
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>
    [clingutla@codeaurora.org: Resolved minor merge conflicts]
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    [render: minor fixup]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -256,7 +256,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 	} else {
 		if (use_pelt())
 			sg_policy->work_in_progress = true;
-		irq_work_queue(&sg_policy->irq_work);
+		sched_irq_work_queue(&sg_policy->irq_work);
 	}
 }
 
