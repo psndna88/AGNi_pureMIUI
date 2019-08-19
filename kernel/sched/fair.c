b1ba868ee93f5217643c272dca24be09b651669f
Julian Liu <wlootlxt123@gmail.com>
2019-08-19 13:30:41 +0800

    sched/fair: Re-allow negative boost
    * also fix distinct types in boosted_cpu_util
    
    # Conflicts:
    #	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6312,36 +6312,40 @@ unsigned long
 boosted_cpu_util(int cpu)
 {
 	unsigned long util = cpu_util_freq(cpu);
-	long margin = schedtune_cpu_margin(util, cpu);
+	int margin;
 
-//	trace_sched_boost_cpu(cpu, util, margin);
-	
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (dsb_boosting || sched_feat(SCHEDTUNE_BOOST_UTIL))
 #else
 	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
 #endif
-		return util + margin;
+		margin = 0;
 	else
-		return util;
+		margin = min(0, schedtune_cpu_margin(util, cpu));
+
+	trace_sched_boost_cpu(cpu, util, margin);
+
+	return util + margin;
 }
 
 static inline unsigned long
 boosted_task_util(struct task_struct *task)
 {
 	unsigned long util = task_util_est(task);
-	long margin = schedtune_task_margin(task);
-
-//	trace_sched_boost_task(task, util, margin);
+	long margin;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (dsb_boosting || sched_feat(SCHEDTUNE_BOOST_UTIL))
 #else
 	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
 #endif
-		return util + margin;
+		margin = 0;
 	else
-		return util;
+		margin = min((long)0, schedtune_task_margin(task));
+
+	trace_sched_boost_task(task, util, margin);
+
+	return util + margin;
 }
 
 static unsigned long capacity_spare_without(int cpu, struct task_struct *p)
