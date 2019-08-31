a3190be707862107848fb026f8ce2ae17e0fcab7
Petri Gynther <pgynther@google.com>
2019-08-30 17:03:14 -0700

    msm: cleanup: union gsi_wdi_channel_scratch3_reg
    Remove __packed from the usage of union gsi_wdi_channel_scratch3_reg.
    __packed is only needed in the union definition.
    
    include/linux/msm_gsi.h:
    union __packed gsi_wdi_channel_scratch3_reg {
        ...
    }
    
    Bug: 139442076
    Bug: 139808631
    Change-Id: I70edc50fbbe4c004309f76c020ac4d77cad91aad
    Signed-off-by: Petri Gynther <pgynther@google.com>

--- include/linux/msm_gsi.h
+++ include/linux/msm_gsi.h
@@ -1321,7 +1321,7 @@ int gsi_write_channel_scratch(unsigned long chan_hdl,
  * @Return gsi_status
  */
 int gsi_write_channel_scratch3_reg(unsigned long chan_hdl,
-		union __packed gsi_wdi_channel_scratch3_reg val);
+		union gsi_wdi_channel_scratch3_reg val);
 
 /**
  * gsi_write_wdi3_channel_scratch2_reg - Peripheral should call this function
@@ -1826,7 +1826,7 @@ static inline int gsi_write_channel_scratch(unsigned long chan_hdl,
 	return -GSI_STATUS_UNSUPPORTED_OP;
 }
 static inline int gsi_write_channel_scratch3_reg(unsigned long chan_hdl,
-		union __packed gsi_wdi_channel_scratch3_reg val)
+		union gsi_wdi_channel_scratch3_reg val)
 {
 	return -GSI_STATUS_UNSUPPORTED_OP;
 }
