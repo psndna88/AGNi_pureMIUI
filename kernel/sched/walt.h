42b4ea8e5da3b85f975f64179f0526b95ada0e79
Amir Vajid <avajid@codeaurora.org>
2019-09-10 15:50:04 -0700

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I8ff4768d56d8e63b2cfa78e5f34cb156ee60e3da
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/walt.h
+++ kernel/sched/walt.h
@@ -332,6 +332,7 @@ static inline bool walt_should_kick_upmigrate(struct task_struct *p, int cpu)
 }
 
 extern bool is_rtgb_active(void);
+extern u64 get_rtgb_active_time(void);
 
 #else /* CONFIG_SCHED_WALT */
 
@@ -419,6 +420,10 @@ static inline bool walt_should_kick_upmigrate(struct task_struct *p, int cpu)
 	return false;
 }
 
+static inline u64 get_rtgb_active_time(void)
+{
+	return 0;
+}
 #endif /* CONFIG_SCHED_WALT */
 
 #endif
