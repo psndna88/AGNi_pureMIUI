4bc5286b8b4f0ecbeaf3b6db37934bfbb5ef2f48
Jiri Pirko <jiri@mellanox.com>
2019-09-30 10:15:10 +0200

    BACKPORT: net: introduce per-netns netdevice notifiers
    Often the code for example in drivers is interested in getting notifier
    call only from certain network namespace. In addition to the existing
    global netdevice notifier chain introduce per-netns chains and allow
    users to register to that. Eventually this would eliminate unnecessary
    overhead in case there are many netdevices in many network namespaces.
    
    Change-Id: I38034dcf12b8d373039ae147425baf433ac3aed4
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -37,6 +37,7 @@
 #include <linux/ns_common.h>
 #include <linux/idr.h>
 #include <linux/skbuff.h>
+#include <linux/notifier.h>
 
 struct user_namespace;
 struct proc_dir_entry;
@@ -105,6 +106,8 @@ struct net {
 
 	struct hlist_head 	*dev_name_head;
 	struct hlist_head	*dev_index_head;
+	struct raw_notifier_head	netdev_chain;
+
 	/* Note that @hash_mix can be read millions times per second,
 	 * it is critical that it is on a read_mostly cache line.
 	 */
