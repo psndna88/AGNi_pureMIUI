d228f8cbca0edb0bf20956ceb17fb03ca7786290
Patrick Bellasi <patrick.bellasi@matbug.com>
2019-10-23 21:56:30 +0100

    sched/fair: util_est: fast ramp-up EWMA on utilization increases
    The estimated utilization for a task:
    
       util_est = max(util_avg, est.enqueue, est.ewma)
    
    is defined based on:
     - util_avg: the PELT defined utilization
     - est.enqueued: the util_avg at the end of the last activation
     - est.ewma:     a exponential moving average on the est.enqueued
                     samples
    
    According to this definition, when a task suddenly change its bandwidth
    requirements from small to big, the EWMA will need to collect multiple
    samples before converging up to track the new big utilization.
    
    This slow convergence towards bigger utilization values is not
    aligned to the default scheduler behavior, which is to optimize for
    performance. Moreover, the est.ewma component fails to compensate for
    temporarely utilization drops which spans just few est.enqueued samples.
    
    To let util_est do a better job in the scenario depicted above, change
    its definition by making util_est directly follow upward motion and
    only decay the est.ewma on downward.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@matbug.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    (am from https://lkml.org/lkml/2019/10/23/1071)
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -96,6 +96,7 @@ SCHED_FEAT(WA_BIAS, true)
  * UtilEstimation. Use estimated CPU utilization.
  */
 SCHED_FEAT(UTIL_EST, true)
+SCHED_FEAT(UTIL_EST_FASTUP, true)
 
 /*
  * Energy aware scheduling. Use platform energy model to guide scheduling
