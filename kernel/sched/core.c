dd4cdad0bc0a6dad37e495ef9fc51e2bd76a8bb4
Blagovest Kolenichev <bkolenichev@codeaurora.org>
2019-10-24 14:53:24 -0700

    Revert "sched/core: Fix migration to invalid CPU in __set_cpus_allowed_ptr()"
    This reverts commit 5d49886483c73838e7aba438f10ce8a7b1123eb7.
    
    This is a preparation change for merging android-4.14-q.149 into
    msm-4.14 branch.The reverted change is committed already as:
    
    98164f45b3 sched/core: Fix warning introduced in __set_cpus_allowed_ptr
    
    Change-Id: I9381e1518e3c4494b8abf6adedcf5c4459662bf3
    Signed-off-by: Blagovest Kolenichev <bkolenichev@codeaurora.org>
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1146,8 +1146,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	cpumask_andnot(&allowed_mask, new_mask, cpu_isolated_mask);
 	cpumask_and(&allowed_mask, &allowed_mask, cpu_valid_mask);
 
-	dest_cpu = cpumask_any(&allowed_mask);
-	if (dest_cpu >= nr_cpu_ids) {
+	if (!cpumask_intersects(new_mask, cpu_valid_mask)) {
 		cpumask_and(&allowed_mask, cpu_valid_mask, new_mask);
 		dest_cpu = cpumask_any(&allowed_mask);
 		if (!cpumask_intersects(new_mask, cpu_valid_mask)) {
@@ -1172,6 +1171,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	if (cpumask_test_cpu(task_cpu(p), &allowed_mask))
 		goto out;
 
+	dest_cpu = cpumask_any_and(cpu_valid_mask, new_mask);
 	if (task_running(rq, p) || p->state == TASK_WAKING) {
 		struct migration_arg arg = { p, dest_cpu };
 		/* Need help from migration thread: drop lock and wait. */
