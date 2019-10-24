8ff79be634580f320702ba2920aeff9da1ef4e49
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-25 02:12:04 +0530

    mdss,adreno: move to async probe
    Reason: https://github.com/aosp-mirror/kernel_msm/commit/6ecb5f869d57d3d134f1e0f562aeb3000be516f2
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -2475,6 +2475,7 @@ static struct platform_driver mdss_fb_driver = {
 		.name = "mdss_fb",
 		.of_match_table = mdss_fb_dt_match,
 		.pm = &mdss_fb_pm_ops,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 
