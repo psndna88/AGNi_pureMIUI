02ac7654b8eb407a5bfcc17f95042b244b0aa112
Jigarkumar Zala <jzala@codeaurora.org>
2019-11-05 12:58:01 -0800

    msm: camera: utils: Add stub function for cx_ipeak
    Add mandate check in order to compile and use the cx_ipeak
    related functionality. Also, update header with stub function.
    
    CRs-Fixed: 2572607
    Change-Id: I2e07101f88b6a47ca1733d04b3aa126fdeae176b
    Signed-off-by: Jigarkumar Zala <jzala@codeaurora.org>

--- drivers/cam_utils/cam_cx_ipeak.h
+++ drivers/cam_utils/cam_cx_ipeak.h
@@ -8,10 +8,30 @@
 
 #include "cam_soc_util.h"
 
+#ifndef CONFIG_QCOM_CX_IPEAK
+static inline int cam_cx_ipeak_register_cx_ipeak
+	(struct cam_hw_soc_info *soc_info)
+{
+	return 0;
+}
+
+static inline int cam_cx_ipeak_update_vote_cx_ipeak
+	(struct cam_hw_soc_info *soc_info, int32_t apply_level)
+{
+	return 0;
+}
+
+static inline int cam_cx_ipeak_unvote_cx_ipeak
+	(struct cam_hw_soc_info *soc_info)
+{
+	return 0;
+}
+#else
 int cam_cx_ipeak_register_cx_ipeak(struct cam_hw_soc_info *soc_info);
 
 int cam_cx_ipeak_update_vote_cx_ipeak(struct cam_hw_soc_info *soc_info,
 	int32_t apply_level);
 int cam_cx_ipeak_unvote_cx_ipeak(struct cam_hw_soc_info *soc_info);
+#endif
 
 #endif /* _CAM_CX_IPEAK_H_ */
