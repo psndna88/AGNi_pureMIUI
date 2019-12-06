fdb9ade9a8e711b162e1054cc14300af25b649a3
Jigarkumar Zala <jzala@codeaurora.org>
2019-12-05 16:30:26 -0800

    msm: camera: sensor: Notify completion in error case
    There is no need to wait till timeout in case of NACK or
    underflow error irq is raised, so notify thread for rd_done
    completion. Also, correct the error mask to catch the NACK
    error for Master 1.
    
    CRs-Fixed: 2598605
    Change-Id: Ib8a16ee119dd81f1384283ac7e9f82d3ac2588bb
    Signed-off-by: Jigarkumar Zala <jzala@codeaurora.org>
    Signed-off-by: Depeng Shao <depengs@codeaurora.org>

--- drivers/cam_sensor_module/cam_cci/cam_cci_soc.c
+++ drivers/cam_sensor_module/cam_cci/cam_cci_soc.c
@@ -55,6 +55,7 @@ int cam_cci_init(struct v4l2_subdev *sd,
 				&cci_dev->cci_master_info[master].report_q[i]);
 			/* Set reset pending flag to true */
 			cci_dev->cci_master_info[master].reset_pending = true;
+			cci_dev->cci_master_info[master].status = 0;
 			/* Set proper mask to RESET CMD address */
 			if (master == MASTER_0)
 				cam_io_w_mb(CCI_M0_RESET_RMSK,
@@ -68,6 +69,7 @@ int cam_cci_init(struct v4l2_subdev *sd,
 				CCI_TIMEOUT);
 			if (rc <= 0)
 				CAM_ERR(CAM_CCI, "wait failed %d", rc);
+			cci_dev->cci_master_info[master].status = 0;
 			mutex_unlock(&cci_dev->cci_master_info[master].mutex);
 		}
 		return 0;
@@ -142,6 +144,7 @@ int cam_cci_init(struct v4l2_subdev *sd,
 	}
 
 	cci_dev->cci_master_info[master].reset_pending = true;
+	cci_dev->cci_master_info[master].status = 0;
 	cam_io_w_mb(CCI_RESET_CMD_RMSK, base +
 			CCI_RESET_CMD_ADDR);
 	cam_io_w_mb(0x1, base + CCI_RESET_CMD_ADDR);
