67c30adf9617279b66e07b0804906491a582d395
Yaroslav Furman <yaro330@gmail.com>
2019-12-06 18:49:23 +0300

    Revert "msm: kgsl: Use event workqueue for event work instead of RT Kthread worker"
    This reverts commit a3fdf9b3115aab6f6c63b02ceaae91043c61cf37.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2018, 2020 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -321,7 +321,7 @@ struct kgsl_event {
 	void *priv;
 	struct list_head node;
 	unsigned int created;
-	struct work_struct work;
+	struct kthread_work work;
 	int result;
 	struct kgsl_event_group *group;
 };
