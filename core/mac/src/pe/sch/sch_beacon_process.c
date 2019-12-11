bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Set wdev for vendor event BEACON_REPORTING_INDEX
Currently host sends wdev as NULL to cfg80211_vendor_event_alloc,
this allows userspace to process nl_cmd for
QCA_NL80211_VENDOR_SUBCMD_BEACON_REPORTING_INDEX event with P2P
interface instead of WLAN.

Set wdev with proper value to avoid processing nl_cmd with P2P
interface by userspace.

Change-Id: If0b99efdc76c1f89b6b07ee72edc6c5e2994a90c
CRs-Fixed: 2568776

core/mac/src/pe/sch/sch_beacon_process.c
@@ -1113,6 +1113,8 @@ void sch_send_beacon_report(struct mac_context *mac_ctx,
 				qdf_do_div(qdf_get_monotonic_boottime(),
 					   QDF_MC_TIMER_TO_MS_UNIT);
 
+		beacon_report.vdev_id = session->vdev_id;
+
 		/* Send report to upper layer */
 		mac_ctx->lim.sme_bcn_rcv_callback(mac_ctx->hdd_handle,
 						  &beacon_report);

psndna88/AGNi_pureMIUI

core/mac/src/pe/sch/sch_beacon_process.c
@@ -1132,6 +1132,8 @@ void sch_send_beacon_report(struct sAniSirGlobal *mac_ctx,
 				qdf_do_div(qdf_get_monotonic_boottime(),
 					   QDF_MC_TIMER_TO_MS_UNIT);
 
+		beacon_report.vdev_id = session->smeSessionId;
+
 		/* Send report to upper layer */
 		mac_ctx->lim.sme_bcn_rcv_callback(mac_ctx->hdd_handle,
 						  &beacon_report);