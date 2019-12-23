3bbd97274f19d710f925864d57878e42dbc557ad
Trishansh Bhardwaj <tbhardwa@codeaurora.org>
2019-12-23 15:34:34 +0530

    msm: camera: cpas: Add support for Bengal camnoc
    Bengal has different version of camnoc which requires
    separate register space and camnoc interface changes.
    This change adds the same.
    
    CRs-Fixed: 2594541
    Change-Id: I2a8e6cb8894444cb669ab1c4da5fa706dc6543ad
    Signed-off-by: Rishabh Jain <risjai@codeaurora.org>
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>

--- drivers/cam_cpas/cpas_top/cam_cpastop_hw.c
+++ drivers/cam_cpas/cpas_top/cam_cpastop_hw.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/delay.h>
@@ -22,6 +22,7 @@
 #include "cpastop_v175_130.h"
 #include "cpastop_v480_100.h"
 #include "cpastop_v580_100.h"
+#include "cpastop_v540_100.h"
 
 struct cam_camnoc_info *camnoc_info;
 
@@ -126,6 +127,10 @@ static int cam_cpastop_get_hw_info(struct cam_hw_info *cpas_hw,
 		(hw_caps->camera_version.minor == 8) &&
 		(hw_caps->camera_version.incr == 0)) {
 		soc_info->hw_version = CAM_CPAS_TITAN_580_V100;
+	} else if ((hw_caps->camera_version.major == 5) &&
+		(hw_caps->camera_version.minor == 4) &&
+		(hw_caps->camera_version.incr == 0)) {
+		soc_info->hw_version = CAM_CPAS_TITAN_540_V100;
 	}
 
 	CAM_DBG(CAM_CPAS, "CPAS HW VERSION %x", soc_info->hw_version);
@@ -521,8 +526,7 @@ static int cam_cpastop_poweron(struct cam_hw_info *cpas_hw)
 	int i;
 	struct cam_cpas_hw_errata_wa_list *errata_wa_list =
 		camnoc_info->errata_wa_list;
-	struct cam_cpas_hw_errata_wa *errata_wa =
-		&errata_wa_list->tcsr_camera_hf_sf_ares_glitch;
+	struct cam_cpas_hw_errata_wa *errata_wa;
 
 	cam_cpastop_reset_irq(cpas_hw);
 	for (i = 0; i < camnoc_info->specific_size; i++) {
@@ -544,8 +548,10 @@ static int cam_cpastop_poweron(struct cam_hw_info *cpas_hw)
 		}
 	}
 
-	if (errata_wa->enable) {
-		cam_cpastop_scm_write(errata_wa);
+	if (errata_wa_list) {
+		errata_wa = &errata_wa_list->tcsr_camera_hf_sf_ares_glitch;
+		if (errata_wa->enable)
+			cam_cpastop_scm_write(errata_wa);
 	}
 
 	return 0;
@@ -628,6 +634,8 @@ static int cam_cpastop_init_hw_version(struct cam_hw_info *cpas_hw,
 		break;
 	case CAM_CPAS_TITAN_580_V100:
 		camnoc_info = &cam580_cpas100_camnoc_info;
+	case CAM_CPAS_TITAN_540_V100:
+		camnoc_info = &cam540_cpas100_camnoc_info;
 		break;
 	default:
 		CAM_ERR(CAM_CPAS, "Camera Version not supported %d.%d.%d",
