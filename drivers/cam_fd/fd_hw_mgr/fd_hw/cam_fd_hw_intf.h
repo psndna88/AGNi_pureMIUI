9ea8b7319440782b60fb8d974ebe657097576c47
Tejas Prajapati <tpraja@codeaurora.org>
2020-01-07 16:06:46 +0530

    msm: camera: fd: skip halt and reset for session start
    Halt and reset can be applied only at probe time and flush
    time to clean up the MSF interface.
    
    CRs-Fixed: 2593760
    Change-Id: I8a19a5a5f4a3b25ef5fe9c18a1e754529418c53c
    Signed-off-by: Tejas Prajapati <tpraja@codeaurora.org>

--- drivers/cam_fd/fd_hw_mgr/fd_hw/cam_fd_hw_intf.h
+++ drivers/cam_fd/fd_hw_mgr/fd_hw/cam_fd_hw_intf.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _CAM_FD_HW_INTF_H_
@@ -158,10 +158,12 @@ struct cam_fd_hw_release_args {
  *
  * @hw_ctx         : HW context for which init is requested
  * @ctx_hw_private : HW layer's private information specific to this hw context
+ * @reset_required : Indicates if the reset is required during init or not
  */
 struct cam_fd_hw_init_args {
 	void    *hw_ctx;
 	void    *ctx_hw_private;
+	bool     reset_required;
 };
 
 /**
