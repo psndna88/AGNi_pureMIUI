4a019e7828598d7802f790fa5db443966745f3b7
Vishalsingh Hajeri <vhajeri@codeaurora.org>
2020-01-08 11:53:18 -0800

    msm: camera: uapi: Add flags for additional IFE HW
    As part of acquire HW, kernel passes the IFE hw_idx to
    userspace. This change will enable the capability to pass
    the hw_idx of additional IFE HW enabled.
    
    CRs-Fixed: 2582959
    Change-Id: I689b99d021c8d0f3440e46d09a5dcda3daaefcfc
    Signed-off-by: Vishalsingh Hajeri <vhajeri@codeaurora.org>

--- include/uapi/camera/media/cam_isp.h
+++ include/uapi/camera/media/cam_isp.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 /*
- * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __UAPI_CAM_ISP_H__
@@ -108,12 +108,12 @@
 
 #define CAM_ISP_IFE0_HW          0x1
 #define CAM_ISP_IFE1_HW          0x2
-#define CAM_ISP_IFE2_HW          0x4
-#define CAM_ISP_IFE0_LITE_HW     0x100
-#define CAM_ISP_IFE1_LITE_HW     0x200
-#define CAM_ISP_IFE2_LITE_HW     0x400
-#define CAM_ISP_IFE3_LITE_HW     0x800
-#define CAM_ISP_IFE4_LITE_HW     0x1000
+#define CAM_ISP_IFE0_LITE_HW     0x4
+#define CAM_ISP_IFE1_LITE_HW     0x8
+#define CAM_ISP_IFE2_LITE_HW     0x10
+#define CAM_ISP_IFE3_LITE_HW     0x20
+#define CAM_ISP_IFE4_LITE_HW     0x40
+#define CAM_ISP_IFE2_HW          0x100
 
 #define CAM_ISP_PXL_PATH          0x1
 #define CAM_ISP_PPP_PATH          0x2
