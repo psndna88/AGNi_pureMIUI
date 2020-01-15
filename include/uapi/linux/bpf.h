ebd5cd559fbab7e2c0524d1f0e5525f66c7cdf7f
Yonghong Song <yhs@fb.com>
2020-01-14 19:50:02 -0800

    BACKPORT: bpf: Add bpf_send_signal_thread() helper
    Commit 8b401f9ed244 ("bpf: implement bpf_send_signal() helper")
    added helper bpf_send_signal() which permits bpf program to
    send a signal to the current process. The signal may be
    delivered to any threads in the process.
    
    We found a use case where sending the signal to the current
    thread is more preferable.
      - A bpf program will collect the stack trace and then
        send signal to the user application.
      - The user application will add some thread specific
        information to the just collected stack trace for
        later analysis.
    
    If bpf_send_signal() is used, user application will need
    to check whether the thread receiving the signal matches
    the thread collecting the stack by checking thread id.
    If not, it will need to send signal to another thread
    through pthread_kill().
    
    This patch proposed a new helper bpf_send_signal_thread(),
    which sends the signal to the thread corresponding to
    the current kernel task. This way, user space is guaranteed that
    bpf_program execution context and user space signal handling
    context are the same thread.
    
    Change-Id: I92c226b1f80e4f9f17e4465c81a36551386d3396
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200115035002.602336-1-yhs@fb.com

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -2718,7 +2718,8 @@ union bpf_attr {
  *
  * int bpf_send_signal(u32 sig)
  *	Description
- *		Send signal *sig* to the current task.
+ *		Send signal *sig* to the process of the current task.
+ *		The signal may be delivered to any of this process's threads.
  *	Return
  *		0 on success or successfully queued.
  *
@@ -2862,6 +2863,19 @@ union bpf_attr {
  *	Return
  *		0 on success, or a negative error in case of failure.
  *
+ * int bpf_send_signal_thread(u32 sig)
+ *	Description
+ *		Send signal *sig* to the thread corresponding to the current task.
+ *	Return
+ *		0 on success or successfully queued.
+ *
+ *		**-EBUSY** if work queue under nmi is full.
+ *
+ *		**-EINVAL** if *sig* is invalid.
+ *
+ *		**-EPERM** if no permission to send the *sig*.
+ *
+ *		**-EAGAIN** if bpf program can try again.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
