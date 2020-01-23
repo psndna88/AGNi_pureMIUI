1ef2d6ebeb59e149c721ea1b301d6d588e95c86b
Yaroslav Furman <yaro330@gmail.com>
2020-01-23 22:33:58 +0300

    i2c: qcom-geni: silence debugging
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -412,7 +412,7 @@ static void gi2c_ev_cb(struct dma_chan *ch, struct msm_gpi_cb const *cb_str,
 		break;
 	}
 	if (cb_str->cb_event != MSM_GPI_QUP_NOTIFY)
-		GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
+		GENI_SE_DBG(gi2c->ipcl, true, gi2c->dev,
 			"GSI QN err:0x%x, status:0x%x, err:%d\n",
 			cb_str->error_log.error_code, m_stat,
 			cb_str->cb_event);
