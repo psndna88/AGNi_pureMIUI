ba51cf38d83b932b154230c2b118f3134c40f006
Wei Wang <wvw@google.com>
2020-02-14 15:24:27 -0800

    sched: separate boost signal from placement hint
    Test: build and boot
    Bug: 144451857
    Bug: 147785606
    Change-Id: Ib2d86a72cad12971a99c7105813473211a7fbd76
    Signed-off-by: Wei Wang <wvw@google.com>
    
    # Conflicts:
    #	kernel/sched/tune.c

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -14,7 +14,6 @@ struct target_nrg {
 
 int schedtune_cpu_boost(int cpu);
 int schedtune_task_boost(struct task_struct *tsk);
-int schedtune_task_boost_rcu_locked(struct task_struct *tsk);
 
 int schedtune_prefer_idle(struct task_struct *tsk);
 int schedtune_prefer_high_cap(struct task_struct *tsk);
