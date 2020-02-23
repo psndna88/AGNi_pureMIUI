88c72b35cb83e8b4d229c763e8689bad4bb9258a
klozz <klozz@vivaldi.net>
2020-02-23 05:06:17 +0000

    dm-crypt: increase threads
    Signed-off-by: klozz <klozz@vivaldi.net>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -2854,7 +2854,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	else
 		cc->crypt_queue = alloc_workqueue("kcryptd",
 						  WQ_HIGHPRI | WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_UNBOUND,
-						  num_online_cpus());
+						  num_online_cpus() * 2);
 	if (!cc->crypt_queue) {
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
