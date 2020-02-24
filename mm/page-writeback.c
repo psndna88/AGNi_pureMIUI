c3d56e9ff8cc8f2ca2cd2948d267b05c0faf5c3a
Tyler Nijmeh <tylernij@gmail.com>
2020-02-23 16:38:23 -0800

    mm: Do not periodically flush dirty pages
    Let dirty_ratio handle this, do not use a constant timer to flush data
    to the disk.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -99,7 +99,7 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
+unsigned int dirty_writeback_interval; /* centiseconds */
 
 EXPORT_SYMBOL_GPL(dirty_writeback_interval);
 
