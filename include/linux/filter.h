5168492dd6a16897f83cead2893cdcb4bdb2c10a
Thomas Gleixner <tglx@linutronix.de>
2020-02-24 15:01:41 +0100

    UPSTREAM: bpf: Provide bpf_prog_run_pin_on_cpu() helper
    BPF programs require to run on one CPU to completion as they use per CPU
    storage, but according to Alexei they don't need reentrancy protection as
    obviously BPF programs running in thread context can always be 'preempted'
    by hard and soft interrupts and instrumentation and the same program can
    run concurrently on a different CPU.
    
    The currently used mechanism to ensure CPUness is to wrap the invocation
    into a preempt_disable/enable() pair. Disabling preemption is also
    disabling migration for a task.
    
    preempt_disable/enable() is used because there is no explicit way to
    reliably disable only migration.
    
    Provide a separate macro to invoke a BPF program which can be used in
    migrateable task context.
    
    It wraps BPF_PROG_RUN() in a migrate_disable/enable() pair which maps on
    non RT enabled kernels to preempt_disable/enable(). On RT enabled kernels
    this merely disables migration. Both methods ensure that the invoked BPF
    program runs on one CPU to completion.
    
    Change-Id: Ie02f0bcc006a1795c7a8b3cb1cf9a95cdf7bbb98
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200224145643.474592620@linutronix.de

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -654,8 +654,30 @@ static inline void bpf_jit_set_header_magic(struct bpf_binary_header *hdr)
 	}								\
 	ret; })
 
-#define BPF_PROG_RUN(prog, ctx) __BPF_PROG_RUN(prog, ctx,		\
-					       bpf_dispatcher_nopfunc)
+#define BPF_PROG_RUN(prog, ctx)						\
+	__BPF_PROG_RUN(prog, ctx, bpf_dispatcher_nopfunc)
+
+/*
+ * Use in preemptible and therefore migratable context to make sure that
+ * the execution of the BPF program runs on one CPU.
+ *
+ * This uses migrate_disable/enable() explicitly to document that the
+ * invocation of a BPF program does not require reentrancy protection
+ * against a BPF program which is invoked from a preempting task.
+ *
+ * For non RT enabled kernels migrate_disable/enable() maps to
+ * preempt_disable/enable(), i.e. it disables also preemption.
+ */
+static inline u32 bpf_prog_run_pin_on_cpu(const struct bpf_prog *prog,
+					  const void *ctx)
+{
+	u32 ret;
+
+	migrate_disable();
+	ret = __BPF_PROG_RUN(prog, ctx, bpf_dispatcher_nopfunc);
+	migrate_enable();
+	return ret;
+}
 
 #define BPF_SKB_CB_LEN QDISC_CB_PRIV_LEN
 
