d37ac5cbd5f890c14eeb114a91ff53e7e00f46c1
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-26 19:41:22 +0900

    drm/msm/sde: allocate kmem_fence_pool once
    sde_fence_init() is called multiple times without matching
    sde_fence_deinit() calls, resulting in memory leaks and mismatched
    allocs and frees.
    
    Just allocate the kmem pool once.
    
    Fixes: 49b09fc235c8 ("drm/msm/sde: use kmem_cache pool for struct sde_fence")
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/gpu/drm/msm/sde/sde_fence.c
+++ drivers/gpu/drm/msm/sde/sde_fence.c
@@ -279,8 +279,6 @@ struct sde_fence_context *sde_fence_init(const char *name, uint32_t drm_id)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
-
 #ifdef CONFIG_FENCE_DEBUG
 	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
 #endif
@@ -303,8 +301,6 @@ void sde_fence_deinit(struct sde_fence_context *ctx)
 	}
 
 	kref_put(&ctx->kref, sde_fence_destroy);
-
-	kmem_cache_destroy(kmem_fence_pool);
 }
 
 void sde_fence_prepare(struct sde_fence_context *ctx)
@@ -524,3 +520,11 @@ void sde_debugfs_timeline_dump(struct sde_fence_context *ctx,
 	}
 	spin_unlock(&ctx->list_lock);
 }
+
+static int __init sde_kmem_pool_init(void)
+{
+	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
+	return 0;
+}
+
+module_init(sde_kmem_pool_init);
