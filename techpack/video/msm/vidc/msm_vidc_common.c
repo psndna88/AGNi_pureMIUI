dfff2ad915353ddd52287f66d9e9171029ab1fd5
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-26 19:24:00 +0900

    msm: vidc: allocate kmem_buf_pool once
    msm_comm_ctrl_init() and msm_comm_ctrl_deinit() can be called multiple
    times without properly cleaning up previous instances.
    
    Just allocate the kmem pool once.
    
    Fixes: b81bb38c1212 ("msm: vidc: use kmem_cache pool for struct msm_vidc_buffer")
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- techpack/video/msm/vidc/msm_vidc_common.c
+++ techpack/video/msm/vidc/msm_vidc_common.c
@@ -13,6 +13,11 @@
 
 static struct kmem_cache *kmem_buf_pool;
 
+void __init init_vidc_kmem_buf_pool(void)
+{
+	kmem_buf_pool = KMEM_CACHE(msm_vidc_buffer, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
+}
+
 #define IS_ALREADY_IN_STATE(__p, __d) (\
 	(__p >= __d)\
 )
@@ -536,12 +541,6 @@ int msm_comm_ctrl_init(struct msm_vidc_inst *inst,
 		return -EINVAL;
 	}
 
-	kmem_buf_pool = KMEM_CACHE(msm_vidc_buffer, SLAB_HWCACHE_ALIGN);
-	if (!kmem_buf_pool) {
-		s_vpr_e(inst->sid, "%s: - failed to allocate kmem pool\n", __func__);
-		return -ENOMEM;
-	}
-
 	inst->ctrls = kcalloc(num_ctrls, sizeof(struct v4l2_ctrl *),
 				GFP_KERNEL);
 	if (!inst->ctrls) {
@@ -629,7 +628,6 @@ int msm_comm_ctrl_deinit(struct msm_vidc_inst *inst)
 
 	kfree(inst->ctrls);
 	v4l2_ctrl_handler_free(&inst->ctrl_handler);
-	kmem_cache_destroy(kmem_buf_pool);
 
 	return 0;
 }
