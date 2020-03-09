249aaf3d67fe39ffea24a0135079b43b6f45b369
Yaroslav Furman <yaro330@gmail.com>
2020-03-09 21:28:05 +0300

    zram_drv: allow overriding zram size from kernel
    Add a CONFIG_ZRAM_SIZE_OVERRIDE option, size will be set
    to 1 GB multiply by CONFIG_ZRAM_SIZE_OVERRIDE.
    
    Signed-off-by: Dmitry Eliseev <dmitry.e@cloudberrylab.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1827,9 +1827,14 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
+#ifndef CONFIG_ZRAM_SIZE_OVERRIDE
 	disksize = memparse(buf, NULL);
 	if (!disksize)
 		return -EINVAL;
+#else
+	disksize = (u64)SZ_1G * CONFIG_ZRAM_SIZE_OVERRIDE;
+	pr_info("Overriding zram size to %li", disksize);
+#endif
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {
