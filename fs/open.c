70f0fed1259b1812e2cfa46ef8c8740c0282f04f
Yaroslav Furman <yaro330@gmail.com>
2020-03-12 14:28:59 +0300

    fs: expand blocker to cover FDE.AI apps
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -1079,6 +1079,7 @@ EXPORT_SYMBOL(filp_clone_open);
 
 #ifdef CONFIG_BLOCK_UNWANTED_FILES
 static char *files_array[] = {
+	"com.feravolt",
 	"fde",
 	"lspeed",
 	"nfsinjector",
@@ -1087,7 +1088,9 @@ static char *files_array[] = {
 };
 
 static char *paths_array[] = {
-	"/data/adb/modules", "/system/etc",
+	"/data/adb/modules",
+	"/system/etc",
+	"/data/app"
 };
 
 static bool inline check_file(const char *name)
