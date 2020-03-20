b3ddbd4d8c304ef3f24a4f463be487ae8e0ad0ba
YueHaibing <yuehaibing@huawei.com>
2020-03-20 10:34:26 +0800

    UPSTREAM: bpf, tcp: Make tcp_bpf_recvmsg static
    After commit f747632b608f ("bpf: sockmap: Move generic sockmap
    hooks from BPF TCP"), tcp_bpf_recvmsg() is not used out of
    tcp_bpf.c, so make it static and remove it from tcp.h. Also move
    it to BPF_STREAM_PARSER #ifdef to fix unused function warnings.
    
    Fixes: f747632b608f ("bpf: sockmap: Move generic sockmap hooks from BPF TCP")
    Change-Id: I17bbfeb60955ae4f868944861dd5bc3c03ab9f71
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20200320023426.60684-3-yuehaibing@huawei.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2293,8 +2293,6 @@ void tcp_bpf_clone(const struct sock *sk, struct sock *newsk);
 
 int tcp_bpf_sendmsg_redir(struct sock *sk, struct sk_msg *msg, u32 bytes,
 			  int flags);
-int tcp_bpf_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
-		    int nonblock, int flags, int *addr_len);
 int __tcp_bpf_recvmsg(struct sock *sk, struct sk_psock *psock,
 		      struct msghdr *msg, int len, int flags);
 #endif /* CONFIG_NET_SOCK_MSG */
