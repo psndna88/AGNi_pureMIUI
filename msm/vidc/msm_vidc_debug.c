9903d41e304c83068314748fdb8b5d0963b0817f
Prakruthi Deepak Heragu <pheragu@codeaurora.org>
2020-04-13 16:15:01 -0700

    msm: vidc: Fix  NULL pointer error when DEBUG_FS is disabled
    Add extra check to avoid NULL pointer derefernce when DEBUG_FS is
    disabled.
    
    Change-Id: Id89e5422c5afce0428dd77280788d50af0a22fec
    Signed-off-by: Prakruthi Deepak Heragu <pheragu@codeaurora.org>

--- msm/vidc/msm_vidc_debug.c
+++ msm/vidc/msm_vidc_debug.c
@@ -270,7 +270,8 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 
 	snprintf(debugfs_name, MAX_DEBUGFS_NAME, "core%d", core->id);
 	dir = debugfs_create_dir(debugfs_name, parent);
-	if (!dir) {
+	if (IS_ERR_OR_NULL(dir)) {
+		dir = NULL;
 		d_vpr_e("Failed to create debugfs for msm_vidc\n");
 		goto failed_create_dir;
 	}
@@ -491,6 +492,7 @@ struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 
 	dir = debugfs_create_dir(debugfs_name, parent);
 	if (IS_ERR_OR_NULL(dir)) {
+		dir = NULL;
 		s_vpr_e(inst->sid, "Failed to create debugfs for msm_vidc\n");
 		goto failed_create_dir;
 	}
