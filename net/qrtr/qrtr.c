2541433c6613cc61d8253c4a340a20b238bf78c5
Deepak Kumar Singh <deesin@codeaurora.org>
2020-04-20 16:49:58 +0530

    net: qrtr: print error case for qrtr receive
    Print error code if there is failure in qrtr_recev
    
    Change-Id: Id08d4fc90994b2bc0b3c5de6c3bb7f1ae56e324f
    Signed-off-by: Deepak Kumar Singh <deesin@codeaurora.org>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -1705,6 +1705,7 @@ static int qrtr_recvmsg(struct socket *sock, struct msghdr *msg,
 
 	if (sock_flag(sk, SOCK_ZAPPED)) {
 		release_sock(sk);
+		pr_err("%s: Invalid addr error\n", __func__);
 		return -EADDRNOTAVAIL;
 	}
 
@@ -1723,8 +1724,10 @@ static int qrtr_recvmsg(struct socket *sock, struct msghdr *msg,
 	}
 
 	rc = skb_copy_datagram_msg(skb, 0, msg, copied);
-	if (rc < 0)
+	if (rc < 0) {
+		pr_err("%s: Failed to copy skb rc[%d]\n", __func__, rc);
 		goto out;
+	}
 	rc = copied;
 
 	if (addr) {
