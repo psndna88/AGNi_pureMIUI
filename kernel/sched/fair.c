695e6cf287ca134c28b02a08bdbdc785a447cbc5
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-04-21 07:52:28 +0530

    sched/walt: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I80606a09c3d09c65b7bcdf3940bee5e155d6318c
    Co-developed-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -190,7 +190,6 @@ unsigned int sched_capacity_margin_down[NR_CPUS] = {
 unsigned int sysctl_sched_min_task_util_for_boost = 51;
 /* 0.68ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_colocation = 35;
-unsigned int sched_task_filter_util = 35;
 #endif
 static unsigned int __maybe_unused sched_small_task_threshold = 102;
 
