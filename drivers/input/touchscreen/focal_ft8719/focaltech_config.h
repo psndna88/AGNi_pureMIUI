ae0571fe1e4d3e486c5465296c70d1a1fd67164f
psndna88 <psndna88@gmail.com>
2020-04-23 14:07:45 +0530

    drivers: input: touchscreen: ft8719: enable gloves mode

--- drivers/input/touchscreen/focal_ft8719/focaltech_config.h
+++ drivers/input/touchscreen/focal_ft8719/focaltech_config.h
@@ -138,7 +138,7 @@
  * Glove mode enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_GLOVE_EN                            0
+#define FTS_GLOVE_EN                            1
 /*
  * cover enable
  * 1: enable, 0:disable(default)
