c6cfa04497ebaae286ea05240442acfdab3622c6
Pzqqt <821026875@qq.com>
2020-04-27 14:11:41 +0800

    drivers: misc: Cleanup usb fast charge mode
    This hook is not needed now, idea from wloot.
    
    Signed-off-by: Pzqqt <821026875@qq.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -910,13 +910,6 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	u8 icl_options;
 	const struct apsd_result *apsd_result = smblib_get_apsd_result(chg);
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-	if (force_fast_charge > 0 && icl_ua == USBIN_500MA)
-	{
-		icl_ua = USBIN_900MA;
-	}
-#endif
-
 	/* power source is SDP */
 	switch (icl_ua) {
 	case USBIN_100MA:
