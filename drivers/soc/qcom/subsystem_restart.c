79841022ae4f68ea08eb62d95f14087a65783a8c
Pzqqt <821026875@qq.com>
2020-04-30 14:21:50 +0800

    soc: qcom: subsystem_restart: Prevent kernel panic caused by modem crash
    Just restart the modem if the modem crash,
    not worth making the kernel panic.
    
    This solves the problem of kernel panic
    after modifying "Lock Bands" in the "Network Signal Guru" application.
    
    Signed-off-by: Pzqqt <821026875@qq.com>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1118,6 +1118,9 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return 0;
 	}
 
+	if (!strcmp(name, "modem"))
+		dev->restart_level = RESET_SUBSYS_COUPLED;
+
 	switch (dev->restart_level) {
 
 	case RESET_SUBSYS_COUPLED:
