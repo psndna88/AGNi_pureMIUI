07e08f1e3949db261002adde01349c898ea9f4ef
celtare21 <celtare21@gmail.com>
2020-05-03 15:17:39 +0200

    soc: qcom: Remove tracing
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_arb_rpmh.c
+++ drivers/soc/qcom/msm_bus/msm_bus_arb_rpmh.c
@@ -1599,8 +1599,6 @@ static int update_context(uint32_t cl, bool active_only,
 		goto exit_update_context;
 	}
 
-//	trace_bus_update_request_end(pdata->name);
-
 exit_update_context:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 	return ret;
@@ -1662,8 +1660,6 @@ static int update_request_adhoc(uint32_t cl, unsigned int index)
 		goto exit_update_request;
 	}
 
-//	trace_bus_update_request_end(pdata->name);
-
 exit_update_request:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 	return ret;
@@ -1719,8 +1715,6 @@ static int query_client_usecase(struct msm_bus_tcs_usecase *tcs_usecase,
 		goto exit_query_client_usecase;
 	}
 
-//	trace_bus_update_request_end(pdata->name);
-
 exit_query_client_usecase:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 	return ret;
@@ -1772,8 +1766,6 @@ static int query_client_usecase_all(struct msm_bus_tcs_handle *tcs_handle,
 		pr_err("%s: Err updating path\n", __func__);
 		goto exit_query_client_usecase_all;
 	}
-
-//	trace_bus_update_request_end(pdata->name);
 
 exit_query_client_usecase_all:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
@@ -1847,7 +1839,6 @@ static int update_bw_adhoc(struct msm_bus_client_handle *cl, u64 ab, u64 ib)
 
 	if (log_transaction)
 		getpath_debug(cl->mas, cl->first_hop, cl->active_only);
-//	trace_bus_update_request_end(cl->name);
 exit_update_request:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 
@@ -1890,7 +1881,6 @@ static int update_bw_context(struct msm_bus_client_handle *cl, u64 act_ab,
 	cl->cur_act_ab = act_ab;
 	cl->cur_dual_ib = dual_ib;
 	cl->cur_dual_ab = dual_ab;
-//	trace_bus_update_request_end(cl->name);
 exit_change_context:
 	rt_mutex_unlock(&msm_bus_adhoc_lock);
 	return ret;
