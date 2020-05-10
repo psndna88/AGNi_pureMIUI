43f631cc88d0035065fb5818d3d3329539352863
psndna88 <psndna88@gmail.com>
2020-05-10 15:04:54 +0530

    AGNi v8.4 RC4

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="RC3"
+export AGNI_VERSION_PREFIX="RC4"
 export AGNI_VERSION="v8.4"
-sed -i 's/v8.4_RC2-EAS/v8.4_RC3-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v8.4_RC3-EAS/v8.4_RC4-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
