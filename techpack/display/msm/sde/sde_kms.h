7e340f50957b26a248e55e2b71e462d8dfa6a411
Danny Lin <danny@kdrag0n.dev>
2020-05-10 09:44:08 +0200

    techpack/display/msm/sde: Omit debug logging code
    We're not going to debug the SDE driver in production. Don't compile the
    code at all to reduce the measurably significant overhead in frame commit
    hotpaths.
    
    Adapted to sm8250 by: Kristof Petho <kristof.petho@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- techpack/display/msm/sde/sde_kms.h
+++ techpack/display/msm/sde/sde_kms.h
@@ -47,22 +47,19 @@
  * SDE_DEBUG - macro for kms/plane/crtc/encoder/connector logs
  * @fmt: Pointer to format string
  */
-#define SDE_DEBUG(fmt, ...)                                                \
-	no_printk(fmt, ##__VA_ARGS__)
+#define SDE_DEBUG(fmt, ...)
 
 /**
  * SDE_INFO - macro for kms/plane/crtc/encoder/connector logs
  * @fmt: Pointer to format string
  */
-#define SDE_INFO(fmt, ...)                                                \
-	no_printk(fmt, ##__VA_ARGS__)
+#define SDE_INFO(fmt, ...)
 
 /**
  * SDE_DEBUG_DRIVER - macro for hardware driver logging
  * @fmt: Pointer to format string
  */
-#define SDE_DEBUG_DRIVER(fmt, ...)                                         \
-	no_printk(fmt, ##__VA_ARGS__)
+#define SDE_DEBUG_DRIVER(fmt, ...)
 
 #define SDE_ERROR(fmt, ...) pr_err("[sde error]" fmt, ##__VA_ARGS__)
 
