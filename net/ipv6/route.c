873f1febe6d69cb52570568f6de3c17fed257607
Yonghong Song <yhs@fb.com>
2020-05-13 11:02:20 -0700

    UPSTREAM: bpf: Change func bpf_iter_unreg_target() signature
    Change func bpf_iter_unreg_target() parameter from target
    name to target reg_info, similar to bpf_iter_reg_target().
    
    Change-Id: I39acb908fe8aac7c982b647edbaa2b2e925c7e99
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200513180220.2949737-1-yhs@fb.com

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -6426,7 +6426,7 @@ static int __init bpf_iter_register(void)
 
 static void bpf_iter_unregister(void)
 {
-	bpf_iter_unreg_target("ipv6_route");
+	bpf_iter_unreg_target(&ipv6_route_reg_info);
 }
 #endif
 #endif
