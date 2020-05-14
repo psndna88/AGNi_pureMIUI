c3b0c7f5b1d137c9e1f04d0f5704afdc4aee192d
tbalden <illespal@gmail.com>
2020-05-14 20:33:27 +0200

    cmdline: replace instead of remove for SafetyNet CTS pass
    Replace with "locked" fake values in fashion of Magisk,
    to make non magisked phones pass CTS safetynet check again.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -41,20 +41,47 @@ static void remove_flag(char *cmd, const char *flag)
 
 static void remove_safetynet_flags(char *cmd)
 {
-	remove_flag(cmd, "androidboot.enable_dm_verity=");
-	remove_flag(cmd, "androidboot.secboot=");
-	remove_flag(cmd, "androidboot.verifiedbootstate=");
 	remove_flag(cmd, "androidboot.veritymode=");
 }
+
+#if 1
+static void replace_flag(char *cmd, const char *flag, const char *flag_new)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are replaced */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memcpy(start_addr, flag_new, strlen(flag));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void replace_safetynet_flags(char *cmd)
+{
+	// WARNING: be aware that you can't replace shorter string with longer ones in the function called here...
+	replace_flag(cmd, "androidboot.vbmeta.device_state=unlocked",
+			  "androidboot.vbmeta.device_state=locked  ");
+	replace_flag(cmd, "androidboot.enable_dm_verity=0",
+			  "androidboot.enable_dm_verity=1");
+	replace_flag(cmd, "androidboot.secboot=disabled",
+			  "androidboot.secboot=enabled ");
+	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
+			  "androidboot.verifiedbootstate=green ");
+}
+#endif
 
 static int __init proc_cmdline_init(void)
 {
 	strcpy(new_command_line, saved_command_line);
 
 	/*
-	 * Remove various flags from command line seen by userspace in order to
+	 * Remove/replace various flags from command line seen by userspace in order to
 	 * pass SafetyNet CTS check.
 	 */
+	replace_safetynet_flags(new_command_line);
 	remove_safetynet_flags(new_command_line);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
