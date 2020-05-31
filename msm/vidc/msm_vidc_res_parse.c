0db29bbac37f4e4754e77bc99d7a921639ca0732
Vikash Garodia <vgarodia@codeaurora.org>
2020-06-01 00:50:26 +0530

    msm: vidc: Configure video context bank for best fit
    With best fit configuration, the probability of getting
    a mapping for video buffer increases even when the system
    is fragmented.
    
    Change-Id: I8689447ce1341fd1189b543eb9fb6a2e03edcc94
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>

--- msm/vidc/msm_vidc_res_parse.c
+++ msm/vidc/msm_vidc_res_parse.c
@@ -4,6 +4,7 @@
  */
 
 #include <linux/iommu.h>
+#include <linux/dma-iommu.h>
 #include <linux/of.h>
 #include <linux/sort.h>
 #include "msm_vidc_debug.h"
@@ -967,6 +968,12 @@ static int msm_vidc_setup_context_bank(struct msm_vidc_platform_resources *res,
 	}
 
 	 cb->domain = iommu_get_domain_for_dev(cb->dev);
+
+	 /*
+	  * When memory is fragmented, below configuration increases the
+	  * possibility to get a mapping for buffer in the configured CB.
+	  */
+	iommu_dma_enable_best_fit_algo(cb->dev);
 
 	 /*
 	 * configure device segment size and segment boundary to ensure
