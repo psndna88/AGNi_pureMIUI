86acedf93dc2c699d8e4a4e9507631850255372b
psndna88 <psndna88@gmail.com>
2020-06-05 17:41:25 +0530

    msm-poweroff: allow edl mode command

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -364,10 +364,7 @@ static void msm_restart_prepare(const char *cmd)
 					     restart_reason);
 			}
 		} else if (!strncmp(cmd, "edl", 3)) {
-			if (0)
-				enable_emergency_dload_mode();
-			else
-				pr_notice("This command already been disabled\n");
+			enable_emergency_dload_mode();
 		} else {
 			qpnp_pon_set_restart_reason(PON_RESTART_REASON_NORMAL);
 			__raw_writel(0x77665501, restart_reason);
