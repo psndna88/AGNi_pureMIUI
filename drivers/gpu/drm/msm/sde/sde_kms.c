ea155d24d77fb967efe64d48757a63c0274dc88b
Yaroslav Furman <yaro330@gmail.com>
2020-06-08 00:18:57 +0300

    disp: msm: sde: fix a warning in kms_update_pm_qos
    There is a chance of sde_kms_handle_power_event happening before hw_init,
    this causes a following warning:
    
    [    1.519001] pm_qos_update_request() called for unknown object
    [    1.519007] ------------[ cut here ]------------
    [    1.519018] WARNING: CPU: 6 PID: 1 at kernel/power/qos.c:685 pm_qos_update_request+0x74/0x84
    [    1.519022] CPU: 6 PID: 1 Comm: swapper/0 Tainted: G S              4.14.183-F1xy-Q-raphael/0a4d53a3c5 #14
    [    1.519025] Hardware name: Qualcomm Technologies, Inc. SM8150 V2 PM8150 RAPHAEL (DT)
    [    1.519028] task: 00000000988e6034 task.stack: 000000000f3f2d5f
    [    1.519031] pc : pm_qos_update_request+0x74/0x84
    [    1.519033] lr : pm_qos_update_request+0x74/0x84
    [    1.519035] sp : ffffff801005b690 pstate : 60c00005
    [    1.519038] x29: ffffff801005b690 x28: 0000000000000005
    [    1.519041] x27: ffffff801005b730 x26: ffffff801005b780
    [    1.519044] x25: 0000000000000000 x24: ffffff801005b7d0
    [    1.519046] x23: 0000000000000005 x22: 0000000000000005
    [    1.519048] x21: ffffffe2ed1d1800 x20: ffffffe2ed1d1678
    [    1.519051] x19: ffffffe2ed1d1000 x18: 0000000000000000
    [    1.519053] x17: 0000000000000039 x16: ffffff915d056044
    [    1.519056] x15: 000000005e5fff5a x14: 000000000000000a
    [    1.519058] x13: ffffff801045f10a x12: ffffff801044d000
    [    1.519061] x11: 0000000000000041 x10: 00000000ffffffff
    [    1.519063] x9 : 81f7450a2fa21d00 x8 : 81f7450a2fa21d00
    [    1.519066] x7 : 6c6c616320292874 x6 : ffffff915e1fce50
    [    1.519068] x5 : 0000000000000000 x4 : 0000000000000004
    [    1.519070] x3 : 0000000000000074 x2 : 0000000000000001
    [    1.519073] x1 : 0000000000000000 x0 : 0000000000000031
    [    1.519076] \x0aPC: 0xffffff915bd275b8:
    [    1.519078] 75b8  6b13011f 54000120 b9807288 d000bb89 91312129 f8687928 f9400100 52800022
    [    1.519084] 75d8  2a1303e3 97fffbf2 a9414ff4 a8c27bfd d65f03c0 9000b440 910dc800 940015d5
    [    1.519089] 75f8  d4210000 a9414ff4 a8c27bfd d65f03c0 a9bd7bfd a90157f6 a9024ff4 910003fd
    [    1.519094] 7618  b4000500 aa0003f6 b9407008 34000528 aa0203f3 2a0103f5 9101e2d4 aa1403e0
    [    1.519099] \x0aLR: 0xffffff915bd275b8:
    [    1.519101] 75b8  6b13011f 54000120 b9807288 d000bb89 91312129 f8687928 f9400100 52800022
    [    1.519106] 75d8  2a1303e3 97fffbf2 a9414ff4 a8c27bfd d65f03c0 9000b440 910dc800 940015d5
    [    1.519111] 75f8  d4210000 a9414ff4 a8c27bfd d65f03c0 a9bd7bfd a90157f6 a9024ff4 910003fd
    [    1.519116] 7618  b4000500 aa0003f6 b9407008 34000528 aa0203f3 2a0103f5 9101e2d4 aa1403e0
    [    1.519121] \x0aSP: 0xffffff801005b650:
    [    1.519123] b650  5bd275f8 ffffff91 60c00005 00000000 1005b638 ffffff80 aaaaaaaa aaaaaaaa
    [    1.519128] b670  ffffffff ffffffff 2fa21d00 81f7450a 1005b690 ffffff80 5bd275f8 ffffff91
    [    1.519133] b690  1005b6d0 ffffff80 5c35184c ffffff91 00000000 00000000 ed1d1000 ffffffe2
    [    1.519138] b6b0  00000000 00000000 00000013 00000000 00000003 ffffff80 2fa21d00 81f7450a
    [    1.519143]
    [    1.519145] Call trace:
    [    1.519147] pm_qos_update_request+0x74/0x84
    [    1.519152] sde_kms_handle_power_event+0x128/0x1e8
    [    1.519155] sde_kms_hw_init+0x24d4/0x25e8
    [    1.519158] msm_drm_bind+0x548/0xc28
    [    1.519163] try_to_bring_up_master.llvm.12005053224231897863+0x138/0x180
    [    1.519166] component_add+0x90/0x164
    [    1.519168] dp_display_probe+0x3b4/0x5d4
    [    1.519171] platform_drv_probe.llvm.6835447479668156938+0x7c/0xbc
    [    1.519174] driver_probe_device+0x650/0x700
    [    1.519176] __driver_attach.llvm.2079861050941666168+0xb8/0xe4
    [    1.519179] bus_add_driver+0x224/0x310
    [    1.519181] driver_register+0xac/0x158
    [    1.519184] dp_display_init+0x44/0x70
    [    1.519187] do_one_initcall+0x160/0x260
    [    1.519190] kernel_init_freeable+0x620/0x6d0
    [    1.519192] kernel_init+0x10/0x2b0
    [    1.519195] ret_from_fork+0x10/0x18
    [    1.519197] ---[ end trace c8ded9077943eafe ]---
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -3171,6 +3171,9 @@ static void _sde_kms_set_lutdma_vbif_remap(struct sde_kms *sde_kms)
 
 static void kms_update_pm_qos(struct pm_qos_request *pm_qos_irq_req, bool enable)
 {
+	if (!pm_qos_request_active(pm_qos_irq_req))
+		return;
+
 	if (enable)
 		pm_qos_update_request(pm_qos_irq_req, SDE_KMS_PM_QOS_CPU_DMA_LATENCY);
 	else
