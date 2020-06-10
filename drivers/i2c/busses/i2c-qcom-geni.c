56f24214e941d279a4a6245185ec2e852cda2195
celtare21 <celtare21@gmail.com>
2020-06-10 22:36:03 +0200

    kernel: silence more logger
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -883,8 +883,7 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 
 		ret = gi2c->err;
 		if (gi2c->err) {
-			GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
-				"i2c error :%d\n", gi2c->err);
+			dev_dbg(gi2c->dev, "i2c error :%d\n", gi2c->err);
 			break;
 		}
 	}
