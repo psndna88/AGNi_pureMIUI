cfe3d2a01faa0dde765a6fe9372883a2b69bacb6
Sudeep Duhoon <rishi.gothic@gmail.com>
2020-06-14 12:35:49 +0530

    techpack: 4.0: Disable DEBUG_FS
    Signed-off-by: Sudeep Duhoon <rishi.gothic@gmail.com>

--- techpack/audio/4.0/dsp/codecs/audio_ape.c
+++ techpack/audio/4.0/dsp/codecs/audio_ape.c
@@ -243,8 +243,10 @@ static int audio_open(struct inode *inode, struct file *file)
 	struct q6audio_aio *audio = NULL;
 	int rc = 0;
 
+#ifdef CONFIG_DEBUG_FS
 	/* 4 bytes represents decoder number, 1 byte for terminate string */
 	char name[sizeof "msm_ape_" + 5];
+#endif
 
 	audio = kzalloc(sizeof(struct q6audio_aio), GFP_KERNEL);
 	if (!audio)
