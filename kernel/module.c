d63a417d03d3b29bb3c28633493acf2201487f5f
psndna88 <psndna88@gmail.com>
2020-06-14 14:36:26 +0530

    wifi: prepare for modules

--- kernel/module.c
+++ kernel/module.c
@@ -1297,6 +1297,8 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+	if(!strncmp("wlan", mod->name, 6))
+		return 1;
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
