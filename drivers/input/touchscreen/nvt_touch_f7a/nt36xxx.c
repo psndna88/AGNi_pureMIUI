1826aa82090b308745a4186da365cfce648af59c
psndna88 <psndna88@gmail.com>
2020-06-15 17:03:41 +0530

    Revert "nvt_touch_f7a: Remove MP CTRLRAM support"
    This reverts commit 4e33eb06fc23358e7e9df552b1c45016d4109406.

--- drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
@@ -58,6 +58,10 @@ static struct work_struct g_resume_work;
 extern int32_t nvt_extra_proc_init(void);
 #endif
 
+#if NVT_TOUCH_MP
+extern int32_t nvt_mp_proc_init(void);
+#endif
+
 struct nvt_ts_data *ts;
 
 //F7A Novatek touch usb pulgin work
@@ -1465,6 +1469,14 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	}
 #endif
 
+#if NVT_TOUCH_MP
+	ret = nvt_mp_proc_init();
+	if (ret != 0) {
+		NVT_ERR("nvt mp proc init failed. ret=%d\n", ret);
+		goto err_init_NVT_ts;
+	}
+#endif
+
 #if defined(CONFIG_FB)
 	ts->fb_notif.notifier_call = fb_notifier_callback;
 	ret = fb_register_client(&ts->fb_notif);
@@ -1504,7 +1516,7 @@ err_register_fb_notif_failed:
 #elif defined(CONFIG_HAS_EARLYSUSPEND)
 err_register_early_suspend_failed:
 #endif
-#if (NVT_TOUCH_PROC || NVT_TOUCH_EXT_PROC)
+#if (NVT_TOUCH_PROC || NVT_TOUCH_EXT_PROC || NVT_TOUCH_MP)
 err_init_NVT_ts:
 #endif
 	free_irq(client->irq, ts);
