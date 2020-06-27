ba7db7af1e00b43f0fdff022ae880f424a06688e
ArbitraryFox <chrisung1992@gmail.com>
2020-06-28 02:43:18 +0800

    netfilter: Added support for TTL fixation
    Signed-off-by: Forenche <prahul2003@gmail.com>

---
+++ include/linux/netfilter_ipv6/ip6t_HL.h
@@ -0,0 +1,19 @@
+/* TTL modification module for IP tables
+* (C) 2000 by Harald Welte <laforge@netfilter.org> */
+#ifndef _IP6T_HL_H
+#define _IP6T_HL_H
+#include <linux/types.h>
+
+enum {
+    IP6T_HL_SET = 0,
+    IP6T_HL_INC,
+    IP6T_HL_DEC
+};
+
+#define IP6T_HL_MAXMODE IP6T_HL_DEC
+
+struct ip6t_HL_info {
+   __u8 mode;
+   __u8 hop_limit;
+};
+#endif 
