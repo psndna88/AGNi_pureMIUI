88e1047591a2568e73bcead84aba84d595f69cea
Arun Kumar Khandavalli <akhandav@codeaurora.org>
2020-06-28 22:15:12 +0530

    qcacld-3.0: Free scan ie's as part of uninit cfg80211 ops
    During SSR with the new changes the stop adapter would be invoked,
    this would free the scan ie's and will not be able to configured
    after the SSR.
    
    Free the scan ie's as part of the uninit which would be called as
    part of the netdev down.
    
    Change-Id: I781eefbf0ae7a9e64bc6cb2ead98f45919587f64
    CRs-Fixed: 2724670

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5855,6 +5855,12 @@ void hdd_deinit_adapter(struct hdd_context *hdd_ctx,
 		break;
 	}
 
+	if (adapter->scan_info.default_scan_ies) {
+		qdf_mem_free(adapter->scan_info.default_scan_ies);
+		adapter->scan_info.default_scan_ies = NULL;
+		adapter->scan_info.default_scan_ies_len = 0;
+	}
+
 	hdd_exit();
 }
 
@@ -7244,12 +7250,6 @@ QDF_STATUS hdd_stop_adapter(struct hdd_context *hdd_ctx,
 		break;
 	default:
 		break;
-	}
-
-	if (adapter->scan_info.default_scan_ies) {
-		qdf_mem_free(adapter->scan_info.default_scan_ies);
-		adapter->scan_info.default_scan_ies = NULL;
-		adapter->scan_info.default_scan_ies_len = 0;
 	}
 
 	/* This function should be invoked at the end of this api*/
