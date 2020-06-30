ae0f62887ff78879b46c8c1162185c951e9db8f4
Demon000 <demonsingur@gmail.com>
2020-06-30 17:07:55 +0300

    batterydata: use generic node if no node is found

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -377,7 +377,19 @@ struct device_node *of_batterydata_get_best_profile(
 	}
 
 	if (best_node == NULL) {
-		pr_err("No battery data found\n");
+		for_each_child_of_node(batterydata_container_node, node) {
+			rc = of_property_read_string(node, "qcom,battery-type", &battery_type);
+			if (!rc && strcmp(battery_type,"unknown-default") == 0) {
+				best_node = node;
+				break;
+			}
+		}
+		
+		if (best_node == NULL)
+			pr_err("No battery data found\n");
+		else
+			pr_err("No battery data found, using unknown\n");
+
 		return best_node;
 	}
 
