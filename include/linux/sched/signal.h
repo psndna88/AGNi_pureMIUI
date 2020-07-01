e36f8fe4fd20d4f27cdab50ac7f79efeef7503a2
Eric W. Biederman <ebiederm@xmission.com>
2020-07-01 07:30:06 -0500

    UPSTREAM: exit: Factor thread_group_exited out of pidfd_poll
    Create an independent helper thread_group_exited which returns true
    when all threads have passed exit_notify in do_exit.  AKA all of the
    threads are at least zombies and might be dead or completely gone.
    
    Create this helper by taking the logic out of pidfd_poll where it is
    already tested, and adding a READ_ONCE on the read of
    task->exit_state.
    
    I will be changing the user mode driver code to use this same logic
    to know when a user mode driver needs to be restarted.
    
    Place the new helper thread_group_exited in kernel/exit.c and
    EXPORT it so it can be used by modules.
    
    Link: https://lkml.kernel.org/r/20200702164140.4468-13-ebiederm@xmission.com
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Tested-by: Alexei Starovoitov <ast@kernel.org>
    Change-Id: I26813e9dad6a0bb78528cf1cff3fbc6c2369231f
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/sched/signal.h
+++ include/linux/sched/signal.h
@@ -675,6 +675,8 @@ static inline int thread_group_empty(struct task_struct *p)
 extern struct sighand_struct *__lock_task_sighand(struct task_struct *task,
 							unsigned long *flags);
 
+extern bool thread_group_exited(struct pid *pid);
+
 static inline struct sighand_struct *lock_task_sighand(struct task_struct *task,
 						       unsigned long *flags)
 {
