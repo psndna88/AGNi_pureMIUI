844c629189527ddf410ba4c783dca5a2d77df4b5
Demon000 <demonsingur@gmail.com>
2020-07-20 01:09:37 +0300

    spi: geni-qcom: expose master of device

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -172,6 +172,18 @@ struct spi_geni_master {
 	struct spi_geni_ssr spi_ssr;
 };
 
+/******************************************************************************
+ * *This functionis for get spi_geni_master->dev
+ * *spi_master: struct spi_device ->master
+ * *return: spi_geni_master->dev
+ ******************************************************************************/
+struct device *lct_get_spi_geni_master_dev(struct spi_master *spi)
+{
+	struct spi_geni_master *geni_mas = spi_master_get_devdata(spi);
+	return geni_mas->dev;
+}
+EXPORT_SYMBOL(lct_get_spi_geni_master_dev);
+
 static void spi_slv_setup(struct spi_geni_master *mas);
 static int ssr_spi_force_suspend(struct device *dev);
 static int ssr_spi_force_resume(struct device *dev);
