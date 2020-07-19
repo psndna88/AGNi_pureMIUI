4dac7c8732bb064f05e7efc75ed75c70a1589fc0
Demon000 <demonsingur@gmail.com>
2020-07-20 00:05:35 +0300

    msm: camera: eeprom: increase memory map count and size

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_eeprom/cam_eeprom_dev.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_eeprom/cam_eeprom_dev.h
@@ -34,8 +34,8 @@
 
 #define PROPERTY_MAXSIZE 32
 
-#define MSM_EEPROM_MEMORY_MAP_MAX_SIZE         80
-#define MSM_EEPROM_MAX_MEM_MAP_CNT             16
+#define MSM_EEPROM_MEMORY_MAP_MAX_SIZE         128
+#define MSM_EEPROM_MAX_MEM_MAP_CNT             64
 #define MSM_EEPROM_MEM_MAP_PROPERTIES_CNT      8
 
 enum cam_eeprom_state {
