d98b7186dc114caca848156b8b76ff531da76f9d
Demon000 <demonsingur@gmail.com>
2020-07-22 01:52:17 +0300

    drm: msm: add custom esd handling
    The touchscreen probably shares the reset pin with the display,
    special care needs to be taken to prevent an ESD from triggering
    panel death while a touchscreen firmware is downloading.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -165,6 +165,10 @@ struct drm_panel_esd_config {
 	u8 *return_buf;
 	u8 *status_buf;
 	u32 groups;
+
+	int esd_err_irq_gpio;
+	int esd_err_irq;
+	int esd_err_irq_flags;
 };
 
 enum dsi_special_panel {
