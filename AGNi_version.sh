f2f89f78ca54fcc32792ce62ed6e7b9f6cec318e
Parvinder Singh <psndna88@gmail.com>
2020-07-23 23:33:13 +0530

    AGNi v9.3 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v9.2"
-sed -i 's/v9.2_beta1-EAS/v9.2_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/ini_set("rom_version",	"v9.2_beta1");/ini_set("rom_version",	"v9.2_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
+export AGNI_VERSION="v9.3"
+sed -i 's/v9.3_beta3-EAS/v9.3_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/ini_set("rom_version",	"v9.3_beta3");/ini_set("rom_version",	"v9.3_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
 
 echo "	AGNi Version info loaded."
 
