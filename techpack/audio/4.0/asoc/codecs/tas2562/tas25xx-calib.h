8df14e55ee43868ffc4acae22803d04aed982f34
Demon000 <demonsingur@gmail.com>
2020-07-25 01:12:40 +0300

    techpack: audio: 4.0: import tas2562 driver

---
+++ techpack/audio/4.0/asoc/codecs/tas2562/tas25xx-calib.h
@@ -0,0 +1,28 @@
+/*
+** =============================================================================
+** Copyright (c) 2016  Texas Instruments Inc.
+**
+** This program is free software; you can redistribute it and/or modify it under
+** the terms of the GNU General Public License as published by the Free Software
+** Foundation; version 2.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT
+** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+**
+** File:
+**     tas25xx-calib.h
+**
+** Description:
+**     header file for tas25xx-calib.c
+**
+** =============================================================================
+*/
+
+#ifndef _TAS25XX_CALIB_H_
+#define _TAS25XX_CALIB_H_
+
+int tas_calib_init(void);
+void tas_calib_exit(void);
+
+#endif /* _TAS25XX_CALIB_H */
