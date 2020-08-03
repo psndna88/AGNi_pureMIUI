b1b03b19363d3031c8d0473fd3daf91edd01d0a0
Ratoriku <a1063021545@gmail.com>
2020-08-03 16:04:32 +0800

    sched: Disable NONTASK_CAPACITY
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -43,7 +43,7 @@ SCHED_FEAT(LB_BIAS, true)
 /*
  * Decrement CPU capacity based on time not spent running tasks
  */
-SCHED_FEAT(NONTASK_CAPACITY, true)
+SCHED_FEAT(NONTASK_CAPACITY, false)
 
 /*
  * Queue remote wakeups on the target CPU and process them
