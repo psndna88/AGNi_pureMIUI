494ace8da7350cffe3f3a9239d32ed6dc459fb28
Depeng Shao <depengs@codeaurora.org>
2020-08-07 14:54:24 +0800

    msm: camera: ife: Get consumed address info during hw_mgr init
    This change checks whether the context supports last consumed
    address during hw_mgr init.
    
    CRs-Fixed: 2749254
    Change-Id: I238893eb2d661014a2a51758bcf71c3e4d00f2d5
    Signed-off-by: Depeng Shao <depengs@codeaurora.org>

--- drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.h
+++ drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.h
@@ -172,6 +172,7 @@ struct cam_ife_hw_mgr_ctx {
  * @ife_dev_caps           ife device capability per core
  * @work q                 work queue for IFE hw manager
  * @debug_cfg              debug configuration
+ * @support_consumed_addr  indicate whether hw supports last consumed address
  */
 struct cam_ife_hw_mgr {
 	struct cam_isp_hw_mgr          mgr_common;
@@ -192,6 +193,7 @@ struct cam_ife_hw_mgr {
 	struct cam_req_mgr_core_workq *workq;
 	struct cam_ife_hw_mgr_debug    debug_cfg;
 	spinlock_t                     ctx_lock;
+	bool                           support_consumed_addr;
 };
 
 /**
