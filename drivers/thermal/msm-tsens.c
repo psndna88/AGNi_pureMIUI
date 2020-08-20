fc99ff654911bb140a8d3e22ef1bd7b15418383b
Parvinder Singh <psndna88@gmail.com>
2020-08-20 17:48:42 +0530

    Revert partial: "msm-tsens: Reschedule work instead of causing uninterruptible sleep"

--- drivers/thermal/msm-tsens.c
+++ drivers/thermal/msm-tsens.c
@@ -2434,8 +2434,8 @@ static int tsens_tm_probe(struct platform_device *pdev)
 
 	tmdev->pdev = pdev;
 
-	tmdev->tsens_critical_wq = create_singlethread_workqueue("tsens_critical_wq");
-
+	tmdev->tsens_critical_wq = alloc_workqueue("tsens_critical_wq",
+							WQ_UNBOUND, 1);
 	if (!tmdev->tsens_critical_wq) {
 		rc = -ENOMEM;
 		goto fail;
