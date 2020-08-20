42730b7b63fe53d97a8fc968d99f0a0ae7ecd691
Martin KaFai Lau <kafai@fb.com>
2020-08-20 12:00:39 -0700

    UPSTREAM: bpf: tcp: Add bpf_skops_established()
    In tcp_init_transfer(), it currently calls the bpf prog to give it a
    chance to handle the just "ESTABLISHED" event (e.g. do setsockopt
    on the newly established sk).  Right now, it is done by calling the
    general purpose tcp_call_bpf().
    
    In the later patch, it also needs to pass the just-received skb which
    concludes the 3 way handshake. E.g. the SYNACK received at the active side.
    The bpf prog can then learn some specific header options written by the
    peer's bpf-prog and potentially do setsockopt on the newly established sk.
    Thus, instead of reusing the general purpose tcp_call_bpf(), a new function
    bpf_skops_established() is added to allow passing the "skb" to the bpf
    prog.  The actual skb passing from bpf_skops_established() to the bpf prog
    will happen together in a later patch which has the necessary bpf pieces.
    
    A "skb" arg is also added to tcp_init_transfer() such that
    it can then be passed to bpf_skops_established().
    
    Calling the new bpf_skops_established() instead of tcp_call_bpf()
    should be a noop in this patch.
    
    Change-Id: I9347e4608ca3c5d6927f7116cad9948c05227b3f
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200820190039.2884750-1-kafai@fb.com

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -410,7 +410,7 @@ bool tcp_peer_is_proven(struct request_sock *req, struct dst_entry *dst);
 void __tcp_close(struct sock *sk, long timeout);
 void tcp_close(struct sock *sk, long timeout);
 void tcp_init_sock(struct sock *sk);
-void tcp_init_transfer(struct sock *sk, int bpf_op);
+void tcp_init_transfer(struct sock *sk, int bpf_op, struct sk_buff *skb);
 __poll_t tcp_poll(struct file *file, struct socket *sock,
 		      struct poll_table_struct *wait);
 int tcp_getsockopt(struct sock *sk, int level, int optname,
