89e54d6f8201d53478d7d865ef0edeeca29d29f2
KP Singh <kpsingh@google.com>
2020-08-25 20:29:17 +0200

    UPSTREAM: bpf: Implement bpf_local_storage for inodes
    Similar to bpf_local_storage for sockets, add local storage for inodes.
    The life-cycle of storage is managed with the life-cycle of the inode.
    i.e. the storage is destroyed along with the owning inode.
    
    The BPF LSM allocates an __rcu pointer to the bpf_local_storage in the
    security blob which are now stackable and can co-exist with other LSMs.
    
    Change-Id: Icdd652ec10897ef218406d76bfadbe321f7d6288
    Signed-off-by: KP Singh <kpsingh@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200825182919.1118197-6-kpsingh@chromium.org

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -4338,6 +4338,11 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 		    func_id != BPF_FUNC_sk_storage_delete)
 			goto error;
 		break;
+	case BPF_MAP_TYPE_INODE_STORAGE:
+		if (func_id != BPF_FUNC_inode_storage_get &&
+		    func_id != BPF_FUNC_inode_storage_delete)
+			goto error;
+		break;
 	default:
 		break;
 	}
@@ -4409,6 +4414,11 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 	case BPF_FUNC_sk_storage_get:
 	case BPF_FUNC_sk_storage_delete:
 		if (map->map_type != BPF_MAP_TYPE_SK_STORAGE)
+			goto error;
+		break;
+	case BPF_FUNC_inode_storage_get:
+	case BPF_FUNC_inode_storage_delete:
+		if (map->map_type != BPF_MAP_TYPE_INODE_STORAGE)
 			goto error;
 		break;
 	default:
