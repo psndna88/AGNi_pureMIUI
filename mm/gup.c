12c84032519302b8b27465fca92f466ebc584c6c
Parvinder Singh <psndna88@gmail.com>
2020-09-08 10:59:20 +0530

    Revert "UPSTREAM: mm/gup.c: fix access_ok() argument type"
    This reverts commit 2422735b4e23743e749daa41cff861b84671d111.

--- mm/gup.c
+++ mm/gup.c
@@ -1362,7 +1362,7 @@ int __get_user_pages_fast(unsigned long start, int nr_pages, int write,
 	end = start + len;
 
 	if (unlikely(!access_ok(write ? VERIFY_WRITE : VERIFY_READ,
-					(void __user *)start, len)))
+					start, len)))
 		return 0;
 
 	/*
