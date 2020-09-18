56f1855e31fd1da8ff8b898f49f031ad2b7dfa33
Shashank Baghel <theradcolor@gmail.com>
2020-09-18 11:23:30 +0530

    treewide: Remove duplicate const specifier
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -234,7 +234,7 @@ enum {
 	MAX_TYPES
 };
 
-static const struct apsd_result const smblib_apsd_results[] = {
+static const struct apsd_result smblib_apsd_results[] = {
 	[UNKNOWN] = {
 		.name	= "UNKNOWN",
 		.bit	= 0,
