ecf280ec6ec23565ee4d0f9308bca5c374932fb2
psndna88 <psndna88@gmail.com>
2020-09-21 09:38:06 +0530

    Revert "soc: qcom: stop spam when IPC_LOGGING is disabled"
    This reverts commit 984e08e7328aa268947fe3481e729d38110c6a69.

--- drivers/platform/msm/mhi_uci/mhi_uci.c
+++ drivers/platform/msm/mhi_uci/mhi_uci.c
@@ -1306,10 +1306,9 @@ static int mhi_uci_create_device(struct uci_client *uci_client)
 		 uci_client->out_attr.mhi_handle->bus,
 		 uci_client->out_attr.mhi_handle->slot,
 		 uci_client->out_attr.chan_id);
-#ifdef CONFIG_IPC_LOGGING
 	uci_client->uci_ipc_log = ipc_log_context_create(MHI_UCI_IPC_LOG_PAGES,
 							 node_name, 0);
-#endif
+
 	return 0;
 }
 
@@ -1447,7 +1446,6 @@ static struct platform_driver mhi_uci_driver = {
 
 static int mhi_uci_init(void)
 {
-#ifdef CONFIG_IPC_LOGGING
 	mhi_uci_drv_ctxt.mhi_uci_ipc_log =
 		ipc_log_context_create(MHI_UCI_IPC_LOG_PAGES,
 				       "mhi-uci",
@@ -1457,7 +1455,7 @@ static int mhi_uci_init(void)
 			UCI_DBG_WARNING,
 			"Failed to create IPC logging context");
 	}
-#endif
+
 	mhi_uci_drv_ctxt.mhi_uci_class =
 		class_create(THIS_MODULE, DEVICE_NAME);
 
