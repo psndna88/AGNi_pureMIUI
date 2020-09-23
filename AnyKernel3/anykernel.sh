88249ac9e09a85eb6306cb7e933005174649f16c
psndna88 <psndna88@gmail.com>
2020-09-23 14:08:12 +0530

    Bringup AGNi Nusantara kenzo v1.0

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Bloody-Kernel-Mod-x69
+kernel.string=Nusantara_kenzo_AGNi-v1.0
 do.devicecheck=1
 do.modules=0
 do.systemless=1
