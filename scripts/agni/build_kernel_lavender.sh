edc038715342e484e1c1db8ac83a8d88f8849fb5
psndna88 <psndna88@gmail.com>
2020-09-29 11:05:33 +0530

    AGNi v10.1 stable

--- scripts/agni/build_kernel_lavender.sh
+++ scripts/agni/build_kernel_lavender.sh
@@ -13,6 +13,10 @@ WLAN_MODA11="$COMPILEDIR/drivers/staging/qcacld-3.0"
 WLAN_MODQ="$COMPILEDIR/drivers/staging/qcacld-3.0_Q"
 WLAN_MODP="$COMPILEDIR/drivers/staging/qcacld-3.0_pie"
 WLAN_MODPO="$COMPILEDIR/drivers/staging/qcacld-3.0_pie_old"
+RTL8188EU="$COMPILEDIR/drivers/staging/rtl8188eu"
+RTL8712U="$COMPILEDIR/drivers/staging/rtl8712"
+RTL8723AU="$COMPILEDIR/drivers/staging/rtl8723au"
+RTL8192EU="$COMPILEDIR/drivers/staging/rtl8192eu"
 
 . $KERNELDIR/AGNi_version.sh
 FILENAME="AGNi_kernel_$DEVICE-$AGNI_VERSION_PREFIX-$AGNI_VERSION.zip"
@@ -53,6 +57,10 @@ rm $WLAN_MODA11/*.ko 2>/dev/null
 rm $WLAN_MODQ/*.ko 2>/dev/null
 rm $WLAN_MODP/*.ko 2>/dev/null
 rm $WLAN_MODPO/*.ko 2>/dev/null
+rm $RTL8188EU/*.ko 2>/dev/null
+rm $RTL8712U/*.ko 2>/dev/null
+rm $RTL8723AU/*.ko 2>/dev/null
+rm $RTL8192EU/*.ko 2>/dev/null
 
 make defconfig O=$COMPILEDIR $CONFIG1
 make -j12 O=$COMPILEDIR # COMPILE
@@ -107,6 +115,10 @@ mv -f $WLAN_MODA11/wlan.ko $KERNELDIR/$DIR/wlan_A11.ko 2>/dev/null
 mv -f $WLAN_MODQ/wlan.ko $KERNELDIR/$DIR/wlan_Q.ko 2>/dev/null
 mv -f $WLAN_MODP/wlan.ko $KERNELDIR/$DIR/wlan_pie.ko 2>/dev/null
 mv -f $WLAN_MODPO/wlan.ko $KERNELDIR/$DIR/wlan_pie_old.ko 2>/dev/null
+mv -f $RTL8188EU/8188eu.ko $KERNELDIR/$DIR 2>/dev/null
+mv -f $RTL8712U/8712u.ko $KERNELDIR/$DIR 2>/dev/null
+mv -f $RTL8723AU/8723au.ko $KERNELDIR/$DIR 2>/dev/null
+mv -f $RTL8192EU/8192eu.ko $KERNELDIR/$DIR 2>/dev/null
 
 ###### COMPILE MIUI-Q cam
 echo ""
@@ -154,10 +166,11 @@ if ([ -f $KERNELDIR/$DIR/Image.gz-dtb-nc ] && [ -f $KERNELDIR/$DIR/Image.gz-dtb-
 	sed -i '/#AGNIFW/d' $KERNELDIR/$DIR/META-INF/com/google/android/aroma-config
 	sed -i 's/SETDEVICETYPE/SDM660_lavender (Redmi Note 7)/' $KERNELDIR/$DIR/META-INF/com/google/android/aroma-config
 	sed -i 's/SDM660/RedmiNote7/' $KERNELDIR/$DIR/tools/sdm660/init.agni*
-	mv $KERNELDIR/$DIR/wlan_pie.ko $KERNELDIR/$DIR/tools/wlan_pie.ko 2>/dev/null
-	mv $KERNELDIR/$DIR/wlan_pie_old.ko $KERNELDIR/$DIR/tools/wlan_pie_old.ko 2>/dev/null
-	mv $KERNELDIR/$DIR/wlan_Q.ko $KERNELDIR/$DIR/tools/wlan_Q.ko 2>/dev/null
-	mv $KERNELDIR/$DIR/wlan_A11.ko $KERNELDIR/$DIR/tools/wlan_A11.ko 2>/dev/null
+	mv -f $KERNELDIR/$DIR/wlan_pie.ko $KERNELDIR/$DIR/tools/wlan_pie.ko 2>/dev/null
+	mv -f $KERNELDIR/$DIR/wlan_pie_old.ko $KERNELDIR/$DIR/tools/wlan_pie_old.ko 2>/dev/null
+	mv -f $KERNELDIR/$DIR/wlan_Q.ko $KERNELDIR/$DIR/tools/wlan_Q.ko 2>/dev/null
+	mv -f $KERNELDIR/$DIR/wlan_A11.ko $KERNELDIR/$DIR/tools/wlan_A11.ko 2>/dev/null
+	mv -f $KERNELDIR/$DIR/8*.ko $KERNELDIR/$DIR/tools 2>/dev/null
 	rm -rf $KERNELDIR/$DIR/tools/thermals-sdm636
 	rm -rf $KERNELDIR/$DIR/tools/sdm636
 	cp -f $KERNELDIR/$DIR/tools/sdm660/* $KERNELDIR/$DIR/tools && rm -rf $KERNELDIR/$DIR/tools/sdm660
