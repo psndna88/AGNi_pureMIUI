e4eebf2f1e7f5f2ba676f102a579cc7ebc3e169a
Divya Sharma <divyash@codeaurora.org>
2020-10-06 08:24:02 -0700

    Revert "qcacmn: Do smmu unmap for IPA TX and RX doorbell registers"
    This reverts commit 10a0fed8152442b106304606962b5bd60b2e5af5.
    
    Change-Id: I76191a97f89ee3d7c09e214f6a610a8afbb6d1e6

--- dp/inc/cdp_txrx_ipa.h
+++ dp/inc/cdp_txrx_ipa.h
@@ -427,15 +427,14 @@ cdp_ipa_setup(ol_txrx_soc_handle soc, uint8_t pdev_id, void *ipa_i2w_cb,
 /**
  * cdp_ipa_cleanup() - Disconnect IPA pipes
  * @soc: data path soc handle
- * @pdev_id: handle to the device instance number
  * @tx_pipe_handle: Tx pipe handle
  * @rx_pipe_handle: Rx pipe handle
  *
  * Return: QDF_STATUS
  */
 static inline QDF_STATUS
-cdp_ipa_cleanup(ol_txrx_soc_handle soc, uint8_t pdev_id,
-		uint32_t tx_pipe_handle, uint32_t rx_pipe_handle)
+cdp_ipa_cleanup(ol_txrx_soc_handle soc, uint32_t tx_pipe_handle,
+		uint32_t rx_pipe_handle)
 {
 	if (!soc || !soc->ops || !soc->ops->ipa_ops) {
 		QDF_TRACE(QDF_MODULE_ID_DP, QDF_TRACE_LEVEL_FATAL,
@@ -444,8 +443,7 @@ cdp_ipa_cleanup(ol_txrx_soc_handle soc, uint8_t pdev_id,
 	}
 
 	if (soc->ops->ipa_ops->ipa_cleanup)
-		return soc->ops->ipa_ops->ipa_cleanup(soc, pdev_id,
-						      tx_pipe_handle,
+		return soc->ops->ipa_ops->ipa_cleanup(tx_pipe_handle,
 						      rx_pipe_handle);
 
 	return QDF_STATUS_SUCCESS;
