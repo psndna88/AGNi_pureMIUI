3f319bb579bbdc42592862a2d564abc06b019ea8
psndna88 <psndna88@gmail.com>
2020-10-08 10:25:19 +0530

    AGNi v10.2 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v10.1"
-sed -i 's/v10.1_beta2-EAS/v10.1_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/ini_set("rom_version",	"v10.1_beta2");/ini_set("rom_version",	"v10.1_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
+export AGNI_VERSION="v10.2"
+sed -i 's/v10.2_beta5-EAS/v10.2_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/ini_set("rom_version",	"v10.2_beta5");/ini_set("rom_version",	"v10.2_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
 
 echo "	AGNi Version info loaded."
 
