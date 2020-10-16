206e3ccd663e8ec43ed54c1f8223b79f2fe2d29c
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-10-16 10:26:46 +0700

    Revert "sched: Add trace point to track preemption disable callers"
    This reverts commit 10e91c5e578fb378b53b8f8e7a3e8d1e6cf6e1f5.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -302,16 +302,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#if defined(CONFIG_PREEMPT_TRACER) || defined(CONFIG_IRQSOFF_TRACER)
-	{
-		.procname       = "preemptoff_tracing_threshold_ns",
-		.data           = &sysctl_preemptoff_tracing_threshold_ns,
-		.maxlen         = sizeof(unsigned int),
-		.mode           = 0644,
-		.proc_handler   = proc_dointvec,
-	},
-
-#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",
