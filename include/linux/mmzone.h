1a13f91f57d6a8a898fe4b56f166b2f73ae3cfd7
Yu Zhao <yuzhao@google.com>
2020-10-15 20:09:55 -0700

    BACKPORT: mm: use self-explanatory macros rather than "2"
    Change-Id: I4bb60eb4191c2a378c3d7c5a8c8170eb72d598ae
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alex Shi <alex.shi@linux.alibaba.com>
    Link: http://lkml.kernel.org/r/20200831175042.3527153-2-yuzhao@google.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit ed0173733dd468883198c3136284394320b8fad6)
    Bug: 261052246

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -300,6 +300,8 @@ static inline int is_active_lru(enum lru_list lru)
 	return (lru == LRU_ACTIVE_ANON || lru == LRU_ACTIVE_FILE);
 }
 
+#define ANON_AND_FILE 2
+
 struct zone_reclaim_stat {
 	/*
 	 * The pageout code in vmscan.c keeps track of how many of the
@@ -309,8 +311,8 @@ struct zone_reclaim_stat {
 	 *
 	 * The anon LRU stats live in [0], file LRU stats in [1]
 	 */
-	unsigned long		recent_rotated[2];
-	unsigned long		recent_scanned[2];
+	unsigned long		recent_rotated[ANON_AND_FILE];
+	unsigned long		recent_scanned[ANON_AND_FILE];
 };
 
 struct lruvec {
@@ -436,6 +438,7 @@ enum zone_type {
 /* The maximum number of pages in MIGRATE_EMERGENCY migration type */
 #define MAX_MANAGED_EMERGENCY 2048
 #endif
+#define ASYNC_AND_SYNC 2
 
 struct zone {
 	/* Read-mostly fields */
@@ -564,8 +567,8 @@ struct zone {
 #if defined CONFIG_COMPACTION || defined CONFIG_CMA
 	/* pfn where compaction free scanner should start */
 	unsigned long		compact_cached_free_pfn;
-	/* pfn where async and sync compaction migration scanner should start */
-	unsigned long		compact_cached_migrate_pfn[2];
+	/* pfn where compaction migration scanner should start */
+	unsigned long		compact_cached_migrate_pfn[ASYNC_AND_SYNC];
 	unsigned long		compact_init_migrate_pfn;
 	unsigned long		compact_init_free_pfn;
 #endif
