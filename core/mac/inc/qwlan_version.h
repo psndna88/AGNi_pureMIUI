461ddd9d7e2f10999b6590abf460e905d771cd6e
snandini <snandini@codeaurora.org>
2020-10-22 14:06:02 -0700

    Release 2.0.8.2
    Release 2.0.8.2
    
    Change-Id: I1eab283b10352ba1a0d7aaa7d804c98ab7d4fb0c
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            8
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            1
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            2
 
-#define QWLAN_VERSIONSTR               "2.0.8.1Z"
+#define QWLAN_VERSIONSTR               "2.0.8.2"
 
 #endif /* QWLAN_VERSION_H */
