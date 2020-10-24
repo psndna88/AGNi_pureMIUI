2a1b40689eec511f5db970ee7c7057477866a28e
celtare21 <celtare21@gmail.com>
2020-10-24 11:59:49 +0530

    kernel: alarmtimer: Relax wakelock
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -63,7 +63,7 @@ static void alarmtimer_triggered_func(void *p)
 
 	if (!(rtc->irq_data & RTC_AF))
 		return;
-	__pm_wakeup_event(ws, 2 * MSEC_PER_SEC);
+	__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
 }
 
 static struct rtc_task alarmtimer_rtc_task = {
@@ -357,7 +357,7 @@ static int alarmtimer_suspend(struct device *dev)
 	/* Set alarm, if in the past reject suspend briefly to handle */
 	ret = rtc_timer_start(rtc, &rtctimer, now, ktime_set(0, 0));
 	if (ret < 0)
-		__pm_wakeup_event(ws, MSEC_PER_SEC);
+		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
 	return ret;
 }
 #endif
