d9852cf701afe7a46e576591ffe69f8f2b24bf06
Sultan Alsawaf <sultan@kerneltoast.com>
2020-10-29 08:10:38 +0700

    msm: sensor: Don't return an error when failing to read pinctrl
    Failing to read actuator and OIS pinctrl is non-fatal, so an error value
    should not be returned from the probe routine when it occurs.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/ois/msm_ois.c
+++ drivers/media/platform/msm/camera_v2/sensor/ois/msm_ois.c
@@ -978,6 +978,7 @@ static int32_t msm_ois_platform_probe(struct platform_device *pdev)
 			pr_err("ERR:%s: Error in reading OIS pinctrl\n",
 				__func__);
 			msm_ois_t->cam_pinctrl_status = 0;
+			rc = 0;
 		}
 	}
 
