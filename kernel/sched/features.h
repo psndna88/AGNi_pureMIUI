0eff7c4a61d2fa0a3162dc19baccef605e2dbf34
dracarys18 <karthihegde010@gmail.com>
2020-11-01 06:05:10 +0000

    sched/features: Disable SCHEDTUNE_BOOST_UTIL
    Just biases tasks to higher capacity CPUs without pushing them to higher
    frequencies. Disable to save some juice
    
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -131,4 +131,4 @@ SCHED_FEAT(FBT_STRICT_ORDER, false)
  * generally leads to usage of higher frequencies.
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
-SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, true)
+SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
