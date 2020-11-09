48da645803e40e0d6215f87ea34771925fc303bb
Swetha Chikkaboraiah <schikk@codeaurora.org>
2020-11-09 13:25:40 +0530

    msm: mhi_dev: Fix compilation with DEBUG_FS is disabled
    Fix compilation error when DEBUG_FS is disabled.
    
    Change-Id: Ib8f4f7cd3f5804313a8f058f21ba4463f481b627
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- drivers/platform/msm/mhi_dev/mhi_sm.c
+++ drivers/platform/msm/mhi_dev/mhi_sm.c
@@ -1307,6 +1307,7 @@ int mhi_dev_sm_syserr(void)
 }
 EXPORT_SYMBOL(mhi_dev_sm_syserr);
 
+#ifdef CONFIG_DEBUG_FS
 static ssize_t mhi_sm_debugfs_read(struct file *file, char __user *ubuf,
 				size_t count, loff_t *ppos)
 {
@@ -1420,3 +1421,4 @@ static ssize_t mhi_sm_debugfs_write(struct file *file,
 
 	return count;
 }
+#endif
