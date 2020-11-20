f7ef5c55116cb2b24ec2c7480ae62658e936f2d9
Ayush Kumar <ayushkr@codeaurora.org>
2020-11-20 20:53:19 +0530

    msm: camera: isp: Add null check for vfe out res data
    Change-Id: I72d8e51257fcfeca8386e626cf04db448bf3b483
    Signed-off-by: Ayush Kumar <ayushkr@codeaurora.org>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_bus/cam_vfe_bus_ver3.c
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_bus/cam_vfe_bus_ver3.c
@@ -862,6 +862,12 @@ static void cam_vfe_bus_ver3_get_constraint_errors(
 
 	for (i = 0; i < bus_priv->num_out; i++) {
 		out_rsrc_node = &bus_priv->vfe_out[i];
+		if (!out_rsrc_node || !out_rsrc_node->res_priv) {
+			CAM_INFO(CAM_ISP,
+				"wm :%d out rsrc node or data is NULL (%p)",
+				i, out_rsrc_node);
+			continue;
+		}
 		out_rsrc_data = out_rsrc_node->res_priv;
 		for (j = 0; j < out_rsrc_data->num_wm; j++) {
 			wm_data = out_rsrc_data->wm_res[j].res_priv;
