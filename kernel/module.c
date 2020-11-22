6383279f0a3772f98374fda39737b07828bf8f3c
psndna88 <psndna88@gmail.com>
2020-11-22 17:35:45 +0530

    kernel/module: allow loading of miui modules without checking

--- kernel/module.c
+++ kernel/module.c
@@ -2776,6 +2776,7 @@ static int module_sig_check(struct load_info *info, int flags)
 	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
 	const void *mod = info->hdr;
 
+	return 0;
 	/*
 	 * Require flags == 0, as a module with version information
 	 * removed is no longer the module that was signed
@@ -3678,6 +3679,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	long err;
 	char *after_dashes;
 
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 	err = module_sig_check(info, flags);
 	if (err)
 		goto free_copy;
