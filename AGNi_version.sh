e608ac9a3c0d757f729ad0ad2ff077fe927ed301
psndna88 <psndna88@gmail.com>
2020-11-24 14:09:43 +0530

    AGNi v11.2 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,20 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v11.1"
-sed -i 's/v11.1_beta2-EAS/v11.1_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/ini_set("rom_version",	"v11.1_beta2");/ini_set("rom_version",	"v11.1_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
+export AGNI_VERSION="v11.2"
+sed -i 's/v11.1_stable-EAS/v11.2_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/ini_set("rom_version",	"v11.1_stable");/ini_set("rom_version",	"v11.2_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
 
 echo "	AGNi Version info loaded."
 
+# CCACHE SHIFTING
+if [ -d ~/.ccache ] && [ -d ~/.ccache_MIATOLL ]; then
+	echo "	   C-CACHE is already set to SDM660."
+elif [ -d ~/.ccache_SDM660 ] && [ -d ~/.ccache ]; then
+	mv ~/.ccache ~/.ccache_MIATOLL
+	mv ~/.ccache_SDM660 ~/.ccache
+	echo "	   C-CACHE was shifted to SDM660 !"
+else
+	mkdir ~/.ccache_MIATOLL
+	echo "	   New C-CACHE created for SDM660 !"
+fi
