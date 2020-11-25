d7318f6f4a7e7ecb786a64b86c6905feaa89e873
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-26 00:13:33 +0100

    proc: cmdline: Patch selinux flag for SafetyNet
    * we've hacked selinux driver, let's be sure userspace doesn't report permissive

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -47,6 +47,7 @@ static void patch_safetynet_flags(char *cmd)
 	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
 	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
 	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
+	patch_flag(cmd, "androidboot.selinux=", "enforcing");
 }
 
 static int __init proc_cmdline_init(void)
