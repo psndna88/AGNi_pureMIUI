923b0799714fd15d2fd8facd910ae5f9f8cfe5a0
psndna88 <psndna88@gmail.com>
2020-11-26 01:10:59 +0530

    touchscreen: ft8756: disable: pressure reporting & production test function, enable gesture function

--- drivers/input/touchscreen/ft8756_spi/focaltech_config.h
+++ drivers/input/touchscreen/ft8756_spi/focaltech_config.h
@@ -148,13 +148,13 @@
  * Report Pressure in multitouch
  * 1:enable(default),0:disable
 */
-#define FTS_REPORT_PRESSURE_EN                  1
+#define FTS_REPORT_PRESSURE_EN                  0
 
 /*
  * Gesture function enable
  * default: disable
  */
-#define FTS_GESTURE_EN                          0
+#define FTS_GESTURE_EN                          1
 
 /*
  * ESD check & protection
@@ -166,7 +166,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN                             1
+#define FTS_TEST_EN                             0
 
 /*
  * Pinctrl enable
