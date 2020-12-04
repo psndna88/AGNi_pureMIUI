238c32efe0f0cf3d7f14657989e115f05285cad5
psndna88 <psndna88@gmail.com>
2020-12-04 17:28:46 +0530

    Revert "adreno: enable fake gpu after 1st boot v2"
    This reverts commit a42918cc0de1c50d712a6cbf650310667ab5a7ba.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -33,7 +33,6 @@
 #include <linux/sysfs.h>
 #include <linux/debugfs.h>
 #include <linux/agni_meminfo.h>
-#include <linux/adrenokgsl_state.h>
 
 #include "zram_drv.h"
 
@@ -1502,9 +1501,6 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-#if defined(CONFIG_KERNEL_CUSTOM_E7S) || defined(CONFIG_KERNEL_CUSTOM_E7T)
-	true_gpu = false;
-#endif
 	disksize = memparse(buf, NULL);
 	if (!disksize) {
 		return -EINVAL;
@@ -1562,9 +1558,6 @@ static ssize_t reset_store(struct device *dev,
 	if (!do_reset)
 		return -EINVAL;
 
-#if defined(CONFIG_KERNEL_CUSTOM_E7S) || defined(CONFIG_KERNEL_CUSTOM_E7T)
-	true_gpu = false;
-#endif
 	zram = dev_to_zram(dev);
 	bdev = bdget_disk(zram->disk, 0);
 	if (!bdev)
