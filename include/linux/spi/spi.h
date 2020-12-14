d7eec39cd4bea6ed6e9b9b375e86d30bef9b830a
psndna88 <psndna88@gmail.com>
2020-12-14 15:07:43 +0530

    Merge kernel 4.4.248
    Tested-by: Pavel Machek (CIP) <pavel@denx.de>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Tested-by: Shuah Khan <skhan@linuxfoundation.org>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/r/20201210142600.887734129@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -595,6 +595,9 @@ static inline struct spi_master *spi_alloc_slave(struct device *host,
 	return __spi_alloc_controller(host, size, true);
 }
 
+extern struct spi_master *
+devm_spi_alloc_master(struct device *dev, unsigned int size);
+
 extern int spi_register_master(struct spi_master *master);
 extern int devm_spi_register_master(struct device *dev,
 				    struct spi_master *master);
