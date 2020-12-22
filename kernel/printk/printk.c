b4521739d0caf7ab79c85ec8a46b35708f2e4e2c
Yaroslav Furman <yaro330@gmail.com>
2020-12-23 00:45:56 +0300

    kernel: printk: suspend-resume stfu
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2178,7 +2178,7 @@ void suspend_console(void)
 {
 	if (!console_suspend_enabled)
 		return;
-	printk("Suspending console(s) (use no_console_suspend to debug)\n");
+
 	console_lock();
 	console_suspended = 1;
 	up_console_sem();
