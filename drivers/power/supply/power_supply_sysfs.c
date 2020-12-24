c268fe6141ffb723b7b58c21e9051ef549a2c989
Erfan Abdi <erfangplus@gmail.com>
2020-12-24 14:54:52 +0530

    power: Drop “_3P5” from type to get recognized in AOSP

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -45,9 +45,9 @@ static const char * const power_supply_type_text[] = {
 	"Unknown", "Battery", "UPS", "Mains", "USB",
 	"USB_DCP", "USB_CDP", "USB_ACA", "USB_C",
 	"USB_PD", "USB_PD_DRP", "BrickID",
-	"USB_HVDCP", "USB_HVDCP_3", "Wireless", "USB_FLOAT",
+	"USB_HVDCP", "USB_HVDCP_3", "USB_HVDCP", "Wireless", "USB_FLOAT",
 	"BMS", "Parallel", "Main", "Wipower", "USB_C_UFP", "USB_C_DFP",
-	"Charge_Pump", "batt_verify",
+	"Charge_Pump",
 };
 
 static const char * const power_supply_status_text[] = {
