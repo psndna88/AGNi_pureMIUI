576b1cbf9376f43c7f5d87900ee86605d15d074b
psndna88 <psndna88@gmail.com>
2020-12-26 22:14:26 +0530

    Revert "soc: qcom: service-locator: Fix memory leak in pd_locator_work"

--- drivers/soc/qcom/service-locator.c
+++ drivers/soc/qcom/service-locator.c
@@ -417,7 +417,6 @@ static void pd_locator_work(struct work_struct *work)
 								pd_loc_work);
 
 	data = pdqw->pdc;
-	data->domain_list = NULL;
 	rc = init_service_locator();
 	if (rc) {
 		pr_err("Unable to connect to service locator!, rc = %d\n", rc);
