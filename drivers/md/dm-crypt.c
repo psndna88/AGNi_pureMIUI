339a69aa13649e416f82077f3e55d8a1e99a3cab
psndna88 <psndna88@gmail.com>
2020-12-27 22:00:34 +0530

    dm-crypt: increase threads and tag WQ_CPU_INTENSIVE

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1865,6 +1865,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	ret = -ENOMEM;
 	cc->io_queue = alloc_workqueue("kcryptd_io",
 				       WQ_HIGHPRI |
+				       WQ_CPU_INTENSIVE |
 				       WQ_MEM_RECLAIM,
 				       1);
 	if (!cc->io_queue) {
@@ -1875,13 +1876,15 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	if (test_bit(DM_CRYPT_SAME_CPU, &cc->flags))
 		cc->crypt_queue = alloc_workqueue("kcryptd",
 						  WQ_HIGHPRI |
+						  WQ_CPU_INTENSIVE |
 						  WQ_MEM_RECLAIM, 1);
 	else
 		cc->crypt_queue = alloc_workqueue("kcryptd",
 						  WQ_HIGHPRI |
+						  WQ_CPU_INTENSIVE |
 						  WQ_MEM_RECLAIM |
 						  WQ_UNBOUND,
-						  num_online_cpus());
+						  num_online_cpus() * 2);
 	if (!cc->crypt_queue) {
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
