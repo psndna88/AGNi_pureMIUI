5f197d409e274fe4649064dc51c2fc78a8f4b03d
psndna88 <psndna88@gmail.com>
2020-12-29 19:33:24 +0530

    Revert "proc: cmdline: Patch vbmeta device state flag for SafetyNet"
    This reverts commit db9d239ccdc25d1facd498e5afb9fbd51d9e84e7.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -46,7 +46,6 @@ static void patch_safetynet_flags(char *cmd)
 	patch_flag(cmd, "androidboot.flash.locked=", "1");
 	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
 	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
-	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
 }
 
 static int __init proc_cmdline_init(void)
