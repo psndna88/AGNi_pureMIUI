06794fdb4500fd17f3eefd828fe246e8dce4adc3
psndna88 <psndna88@gmail.com>
2020-12-29 11:40:51 +0530

    power: supply: AGNi MIATOLL MIUI charging changes v1
    -disable system temperature levels
    -increase parallel charging range from upto 900mA instead of 1400mA
    -increase DCP current
    
    Signed-off-by: psndna88 <psndna88@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -34,19 +34,8 @@
 #include <linux/msm_drm_notify.h>
 #include <linux/fb.h>
 #include <linux/board_id.h>
-#ifdef CONFIG_DEBUG_USB
-#undef dev_dbg
-#undef pr_debug
-#define dev_dbg dev_err
-#define pr_debug pr_err
-#endif
 
-union power_supply_propval lct_therm_lvl_reserved;
-union power_supply_propval lct_therm_level;
-
-bool lct_backlight_off;
 int LctIsInCall = 0;
-int LctThermal =0;
 
 static struct smb_params smb5_pmi632_params = {
 	.fcc			= {
@@ -1953,6 +1942,7 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL,
 };
 
+static int user_temp_level = 0;
 #define DEBUG_ACCESSORY_TEMP_DECIDEGC	250
 static int smb5_batt_get_prop(struct power_supply *psy,
 		enum power_supply_property psp,
@@ -2098,7 +2088,7 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		val->intval = 5020;
 		break;
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = smblib_get_prop_system_temp_level(chg, val);
+		val->intval = user_temp_level;
 		break;
 	default:
 		pr_err("batt power supply prop %d not supported\n", psp);
@@ -2128,7 +2118,7 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 		rc = smblib_set_prop_input_suspend(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
-		rc = smblib_set_prop_system_temp_level(chg, val);
+		user_temp_level = val->intval;
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		rc = smblib_set_prop_batt_capacity(chg, val);
@@ -2205,7 +2195,7 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 					false, 0);
 		break;
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = smblib_set_prop_system_temp_level(chg, val);
+		user_temp_level = val->intval;
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		chg->fcc_stepper_enable = val->intval;
@@ -3979,77 +3969,6 @@ static struct device_attribute attrs2[] = {
 			lct_thermal_call_status_show, lct_thermal_call_status_store),
 };
 
-static void thermal_fb_notifier_resume_work(struct work_struct *work)
-{
-    union power_supply_propval lct_therm_call_level = {LCT_THERM_CALL_LEVEL,};
-    union power_supply_propval lct_therm_lcdoff_level = {LCT_THERM_LCDOFF_LEVEL,};
-	struct smb_charger *chg = container_of(work, struct smb_charger, fb_notify_work);
-	LctThermal = 1;
-
-    if (board_get_33w_supported()) {
-        LCT_THERM_CALL_LEVEL = 14;
-        LCT_THERM_LCDOFF_LEVEL = 13;
-    } else {
-        LCT_THERM_CALL_LEVEL = 7;
-        LCT_THERM_LCDOFF_LEVEL = 4;
-    }
-	if ((lct_backlight_off) && (LctIsInCall == 0)) {
-		if (lct_therm_lvl_reserved.intval > LCT_THERM_LCDOFF_LEVEL)
-			smblib_set_prop_system_temp_level(chg,&lct_therm_lcdoff_level);
-		else
-			smblib_set_prop_system_temp_level(chg,&lct_therm_lvl_reserved);//from thermal-level
-	} else if (LctIsInCall == 1) {
-		smblib_set_prop_system_temp_level(chg,&lct_therm_call_level);
-	} else {
-		smblib_set_prop_system_temp_level(chg,&lct_therm_lvl_reserved);//from thermal-levle
-	}
-
-	LctThermal = 0;
-}
-
-/* frame buffer notifier block control the suspend/resume procedure */
-static int thermal_notifier_callback(struct notifier_block *noti, unsigned long event, void *data)
-{
-	struct fb_event *ev_data = data;
-	struct smb_charger *chg = container_of(noti, struct smb_charger, notifier);
-	int *blank;
-	printk("%s %d",__FUNCTION__,__LINE__);
-	if (ev_data && ev_data->data && chg) {
-		blank = ev_data->data;
-		if (event == MSM_DRM_EARLY_EVENT_BLANK && *blank == MSM_DRM_BLANK_UNBLANK) {
-			lct_backlight_off = false;
-			pr_info("thermal_notifier lct_backlight_off:%d",lct_backlight_off);
-			schedule_work(&chg->fb_notify_work);
-		}
-		else if (event == MSM_DRM_EVENT_BLANK && *blank == MSM_DRM_BLANK_POWERDOWN) {
-			lct_backlight_off = true;
-			pr_info("thermal_notifier lct_backlight_off:%d",lct_backlight_off);
-			schedule_work(&chg->fb_notify_work);
-		}
-	}
-
-	return 0;
-}
-
-static int lct_register_powermanger(struct smb_charger *chg)
-{
-	int ret = 0;
-
-	chg->notifier.notifier_call = thermal_notifier_callback;
-	ret = msm_drm_register_client(&chg->notifier);
-    if (ret)
-        pr_err("[FB]Unable to register fb_notifier: %d", ret);
-
-	return 0;
-}
-
-static int lct_unregister_powermanger(struct smb_charger *chg)
-{
-	msm_drm_unregister_client(&chg->notifier);
-
-	return 0;
-}
-
 static int smb5_show_charger_status(struct smb5 *chip)
 {
 	struct smb_charger *chg = &chip->chg;
@@ -4294,13 +4213,6 @@ static int smb5_probe(struct platform_device *pdev)
 		goto free_irq;
 	}
 
-	lct_therm_lvl_reserved.intval= 0;
-	lct_therm_level.intval= 0;
-	lct_backlight_off = false;
-	INIT_WORK(&chg->fb_notify_work, thermal_fb_notifier_resume_work);
-	/* register suspend and resume fucntion*/
-	lct_register_powermanger(chg);
-
 	if (chg->reg_dump_enable) {
 		schedule_delayed_work(&chg->reg_work, 30 * HZ);
 	}
@@ -4328,7 +4240,6 @@ static int smb5_remove(struct platform_device *pdev)
 			  sysfs_remove_file(&chg->dev->kobj,
 							&attrs2[attr_count2].attr);
 			}
-	lct_unregister_powermanger(chg);
 
 	/* force enable APSD */
 	smblib_masked_write(chg, USBIN_OPTIONS_1_CFG_REG,
