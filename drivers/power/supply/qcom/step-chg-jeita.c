bb96465a1b10178738b4c9375b37d1deb0d56fc5
psndna88 <psndna88@gmail.com>
2020-12-30 16:37:53 +0530

    power: supply: AGNi MIATOLL MIUI charging changes v2
    -temperature dependent fast charge current in batterydata dtsi
    
    excalibur,joyeuse,gram:
    00.0 -  5.0 C : 0900 mA
    05.1 - 10.0 C : 2300 mA
    10.1 - 15.0 C : 3300 mA
    15.1 - 38.0 C : 5500 mA
    38.1 - 39.0 C : 4500 mA
    39.1 - 40.0 C : 4000 mA
    40.1 - 41.0	C : 3800 mA
    41.1 - 42.0 C : 3500 mA
    42.1 - 44.0 C : 2800 mA
    44.1 - 58.0 C : 2300 mA
    
    curtana:
    00.0 -  5.0 C : 0984 mA
    05.1 - 10.0 C : 2460 mA
    10.1 - 15.0 C : 3200 mA
    15.1 - 37.0 C : 3400 mA
    37.1 - 38.0 C : 3000 mA
    38.1 - 39.0 C : 2800 mA
    39.1 - 40.0 C : 2500 mA
    40.1 - 42.0 C : 2200 mA
    42.1 - 44.0 C : 2000 mA
    44.1 - 58.0 C : 1800 mA
    
    Signed-off-by: psndna88 <psndna88@gmail.com>

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -552,7 +552,7 @@ static int get_val(struct range_data *range, int hysteresis, int current_index,
 	return 0;
 }
 
-#define TAPERED_STEP_CHG_FCC_REDUCTION_STEP_MA		50000 /* 50 mA */
+#define TAPERED_STEP_CHG_FCC_REDUCTION_STEP_MA		100000 /* 100 mA */
 static void taper_fcc_step_chg(struct step_chg_info *chip, int index,
 					int current_voltage)
 {
@@ -1126,7 +1126,7 @@ int qcom_step_chg_init(struct device *dev,
 
 	chip->jeita_fcc_config->param.psy_prop = POWER_SUPPLY_PROP_TEMP;
 	chip->jeita_fcc_config->param.prop_name = "BATT_TEMP";
-	chip->jeita_fcc_config->param.hysteresis = 5;
+	chip->jeita_fcc_config->param.hysteresis = 0;
 	chip->jeita_fv_config->param.psy_prop = POWER_SUPPLY_PROP_TEMP;
 	chip->jeita_fv_config->param.prop_name = "BATT_TEMP";
 	chip->jeita_fv_config->param.hysteresis = 5;
