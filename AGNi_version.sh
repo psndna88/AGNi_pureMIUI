3c8a7d2db035160977453606dc22ba8de53994e6
psndna88 <psndna88@gmail.com>
2021-01-01 13:34:33 +0530

    AGNi MIATOLL MIUI v7.6 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,8 +4,8 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v7.5"
-sed -i 's/v7.5-beta1/v7.5-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+export AGNI_VERSION="v7.6"
+sed -i 's/v7.6-beta2/v7.6-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
