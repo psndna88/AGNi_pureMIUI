4b963b2cce108498c44ba6c87256c21bfb1c31b1
Yaroslav Furman <yaro330@gmail.com>
2021-01-03 23:53:57 +0300

    kernel: printk: only allow init messages in dmesg
    Everything else - fuck off.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -813,10 +813,14 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			/* Only allow init: messages in the dmesg */
+			if (strncmp(line, "init:", strlen("init:")))
+				goto free;
 		}
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
+free:
 	kfree(buf);
 	return ret;
 }
