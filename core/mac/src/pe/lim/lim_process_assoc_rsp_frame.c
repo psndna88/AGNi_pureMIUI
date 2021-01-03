a59bc2ffe2d639209262d9199d85dcd599e5b249
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2021-01-04 02:01:18 +0530

    qcacld-3.0: Add support for TWT session state in status query
    Add support to include the session state in TWT status query
    response. Session state is set to active once setup is
    complete and when resume response event is received from
    firmware. Session state is suspend when suspend response
    event is received from firmware.
    
    Change-Id: I1f47947c05f80bd85199fd81b951dc54deacdc09
    CRs-Fixed: 2854500

--- core/mac/src/pe/lim/lim_process_assoc_rsp_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_rsp_frame.c
@@ -1094,7 +1094,6 @@ lim_process_assoc_rsp_frame(struct mac_context *mac_ctx, uint8_t *rx_pkt_info,
 			session_entry->ap_mu_edca_params[QCA_WLAN_AC_VO] =
 				assoc_rsp->mu_edca.acvo;
 		}
-
 	}
 
 	if (beacon->VHTCaps.present)
