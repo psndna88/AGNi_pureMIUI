e95ad676fc4a3d70fa0b6ddefb02a993054f7623
Stanislav Nijnikov <stanislav.nijnikov@wdc.com>
2021-01-07 17:21:09 +0530

    scsi: ufs: sysfs: interconnect descriptor
    This patch introduces a sysfs group entry for the UFS interconnect
    descriptor parameters. The group adds "interconnect_descriptor" folder
    under the UFS driver sysfs entry (/sys/bus/platform/drivers/ufshcd/*).
    The parameters are shown as hexadecimal numbers. The full information
    about the parameters could be found at UFS specifications 2.1.
    
    Change-Id: I9a751e137bbaa3c9664655382e077fb5f743dd13
    Signed-off-by: Stanislav Nijnikov <stanislav.nijnikov@wdc.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Git-commit: 8c2582bfbd9abe84b9cf435996aa720a84f908c4
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Veera Vegivada <vvegivad@codeaurora.org>

--- drivers/scsi/ufs/ufs.h
+++ drivers/scsi/ufs/ufs.h
@@ -271,6 +271,14 @@ enum health_desc_param {
 	HEALTH_DESC_PARAM_LIFE_TIME_EST_B	= 0x4,
 };
 
+/* Interconnect descriptor parameters offsets in bytes*/
+enum interconnect_desc_param {
+	INTERCONNECT_DESC_PARAM_LEN		= 0x0,
+	INTERCONNECT_DESC_PARAM_TYPE		= 0x1,
+	INTERCONNECT_DESC_PARAM_UNIPRO_VER	= 0x2,
+	INTERCONNECT_DESC_PARAM_MPHY_VER	= 0x4,
+};
+
 /*
  * Logical Unit Write Protect
  * 00h: LU not write protected
