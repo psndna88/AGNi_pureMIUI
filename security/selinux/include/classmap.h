336557a1d35d5c6555e45236bed708d164d72fe7
Daniel Colascione <dancol@google.com>
2021-01-08 14:22:22 -0800

    BACKPORT: selinux: teach SELinux about anonymous inodes
    This change uses the anon_inodes and LSM infrastructure introduced in
    the previous patches to give SELinux the ability to control
    anonymous-inode files that are created using the new
    anon_inode_getfd_secure() function.
    
    A SELinux policy author detects and controls these anonymous inodes by
    adding a name-based type_transition rule that assigns a new security
    type to anonymous-inode files created in some domain. The name used
    for the name-based transition is the name associated with the
    anonymous inode for file listings --- e.g., "[userfaultfd]" or
    "[perf_event]".
    
    Example:
    
    type uffd_t;
    type_transition sysadm_t sysadm_t : anon_inode uffd_t "[userfaultfd]";
    allow sysadm_t uffd_t:anon_inode { create };
    
    (The next patch in this series is necessary for making userfaultfd
    support this new interface.  The example above is just
    for exposition.)
    
    Signed-off-by: Daniel Colascione <dancol@google.com>
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit 29cd6591ab6fee3125ea5c1bf350f5013bc615e1)
    
    Conflicts:
        security/selinux/include/classmap.h
    
    (1. Removed 'lockdown' mapping to be in sync with d9cb255af3a03d7b9cdb5ddbab10d9f5c68f97f2)
    
    Signed-off-by: Lokesh Gidra <lokeshgidra@google.com>
    Bug: 160737021
    Bug: 169683130
    Change-Id: Iaa9f236f43bf225f089f00ead17e64326adbb328

--- security/selinux/include/classmap.h
+++ security/selinux/include/classmap.h
@@ -248,6 +248,8 @@ struct security_class_mapping secclass_map[] = {
 	  { COMMON_SOCK_PERMS, NULL } },
 	{ "perf_event",
 	  {"open", "cpu", "kernel", "tracepoint", "read", "write", NULL } },
+	{ "anon_inode",
+	  { COMMON_FILE_PERMS, NULL } },
 	{ NULL }
   };
 
