0466ad4ad8022d2d018b6f910af70e8b340bd607
psndna88 <psndna88@gmail.com>
2021-01-18 20:26:40 +0530

    fs/sdfat: enable discard by default

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -4635,7 +4635,7 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	opts->amap_opt.misaligned_sect = 0;
 	opts->symlink = 0;
 	opts->errors = SDFAT_ERRORS_RO;
-	opts->discard = 0;
+	opts->discard = 1;
 	*debug = 0;
 
 	if (!options)
