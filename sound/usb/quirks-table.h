a379d2df9ea47817a71c394d468bc80b3745ae06
psndna88 <psndna88@gmail.com>
2021-01-25 19:29:47 +0530

    sound: usb: revert a usb audio change

--- sound/usb/quirks-table.h
+++ sound/usb/quirks-table.h
@@ -3293,14 +3293,19 @@ AU0828_DEVICE(0x2040, 0x7270, "Hauppauge", "HVR-950Q"),
 				.ifnum = 0,
 				.type = QUIRK_AUDIO_STANDARD_MIXER,
 			},
-			/* Playback */
+			/* Capture */
 			{
 				.ifnum = 1,
+				.type = QUIRK_IGNORE_INTERFACE,
+			},
+			/* Playback */
+			{
+				.ifnum = 2,
 				.type = QUIRK_AUDIO_FIXED_ENDPOINT,
 				.data = &(const struct audioformat) {
 					.formats = SNDRV_PCM_FMTBIT_S16_LE,
 					.channels = 2,
-					.iface = 1,
+					.iface = 2,
 					.altsetting = 1,
 					.altset_idx = 1,
 					.attributes = UAC_EP_CS_ATTR_FILL_MAX |
