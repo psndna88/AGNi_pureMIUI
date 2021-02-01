4bf479f5fb50e450337b38749a76aec3850fa5d2
Aarqw12 <lcockx@protonmail.com>
2021-02-01 10:51:00 -0500

    drm/msm: fix build errors with disabled debugfs

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -787,12 +787,13 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		priv->fbdev = msm_fbdev_init(ddev);
 #endif
 
+#ifdef CONFIG_DEBUG_FS 
 	if (!msm_debugfs_late_init(ddev)) {
 		priv->debug_root = debugfs_create_dir("debug",
 						ddev->primary->debugfs_root);
 		sde_dbg_debugfs_register(priv->debug_root);
 	}
-
+#endif
 	/* perform subdriver post initialization */
 	if (kms && kms->funcs && kms->funcs->postinit) {
 		ret = kms->funcs->postinit(kms);
