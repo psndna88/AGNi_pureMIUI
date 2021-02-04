5cb28b609d46555395474c5038f86cf4000fd2ac
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-04 20:00:53 +0530

    Revert "Revert "ANDROID: Add hold functionality to schedtune CPU boost""
    This reverts commit 6af6df201e0a96a0e5207ab18314c832dd8538ee.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -132,3 +132,14 @@ SCHED_FEAT(FBT_STRICT_ORDER, false)
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
 SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
+
+/*
+ * Apply schedtune boost hold to tasks of all sched classes.
+ * If enabled, schedtune will hold the boost applied to a CPU
+ * for 50ms regardless of task activation - if the task is
+ * still running 50ms later, the boost hold expires and schedtune
+ * boost will expire immediately the task stops.
+ * If disabled, this behaviour will only apply to tasks of the
+ * RT class.
+ */
+SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, false)
