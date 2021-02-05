b3b4c350b5ea093b0a734dd92bc365a086975754
Jingxiang Ge <jge@codeaurora.org>
2021-02-05 10:56:52 +0800

    qcacld-3.0: check state for netdev_features_update_work
    before deinit netdev_features_update_work, check if it
    has been initialized.
    
    Change-Id: Ib9ba1ed42f4616872ec6eb8a24c6c0d1e316e356
    CRs-Fixed: 2871324

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -983,6 +983,11 @@ enum hdd_mic_work_status {
 	MIC_DISABLED
 };
 
+enum hdd_work_status {
+	HDD_WORK_UNINITIALIZED,
+	HDD_WORK_INITIALIZED,
+};
+
 /**
  * struct hdd_mic_work - mic work info in HDD
  * @mic_error_work: mic error work
@@ -1205,6 +1210,7 @@ struct hdd_context;
  *			   by hdd_netdev_feature_update
  * @netdev_features_update_work: work for handling the netdev features update
  *				 for the adapter.
+ * @netdev_features_update_work_status: status for netdev_features_update_work
  * @delete_in_progress: Flag to indicate that the adapter delete is in
  *			progress, and any operation using rtnl lock inside
  *			the driver can be avoided/skipped.
@@ -1515,6 +1521,7 @@ struct hdd_adapter {
 	ol_txrx_rx_fp rx_stack;
 
 	qdf_work_t netdev_features_update_work;
+	enum hdd_work_status netdev_features_update_work_status;
 	/* Flag to indicate whether it is a pre cac adapter or not */
 	bool is_pre_cac_adapter;
 	bool delete_in_progress;
