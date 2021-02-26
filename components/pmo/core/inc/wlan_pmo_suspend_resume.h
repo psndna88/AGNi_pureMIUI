120775a664b9d5e8999bb4e0ce04d3afc2cf9ba1
Abdul Muqtadeer Ahmed <amuqtade@codeaurora.org>
2021-02-26 15:37:26 +0530

    qcacld-3.0: Add a new feature to support forced dtim
    Add vendor command interface/feature through which the user
    or vendor can set the dtim count as per their requirement.
    
    Change-Id: I26010948c4ed7e3c49bfe0453119235d4f438c6f
    CRs-Fixed: 2894349

--- components/pmo/core/inc/wlan_pmo_suspend_resume.h
+++ components/pmo/core/inc/wlan_pmo_suspend_resume.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018, 2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2018, 2020-2021 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -348,6 +348,18 @@ QDF_STATUS pmo_core_txrx_suspend(struct wlan_objmgr_psoc *psoc);
  * Return: QDF_STATUS
  */
 QDF_STATUS pmo_core_txrx_resume(struct wlan_objmgr_psoc *psoc);
+
+/**
+ * pmo_core_config_forced_dtim() - function to configure forced dtim
+ * @vdev: objmgr vdev handle
+ * @dynamic_dtim: dynamic dtim value passed by user
+ *
+ * This function configures the forced modulated dtim in firmware
+ *
+ * Return: QDF_STATUS
+ */
+QDF_STATUS pmo_core_config_forced_dtim(struct wlan_objmgr_vdev *vdev,
+				       uint32_t dynamic_dtim);
 #endif /* WLAN_POWER_MANAGEMENT_OFFLOAD */
 
 #endif /* end  of _WLAN_PMO_SUSPEND_RESUME_H_ */
