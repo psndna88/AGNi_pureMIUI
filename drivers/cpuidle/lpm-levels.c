92bb76965fed6f32f4a61f1c983dc2c78562c943
psndna88 <psndna88@gmail.com>
2021-03-01 15:23:05 +0530

    AGNI MARKER /sys/module/lpm_levels/parameters/agni_present
    Signed-off-by: psndna88 <psndna88@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -70,6 +70,9 @@ module_param_named(lpm_prediction, lpm_prediction, bool, 0664);
 
 static bool lpm_ipi_prediction = true;
 module_param_named(lpm_ipi_prediction, lpm_ipi_prediction, bool, 0664);
+// AGNi marker sysfs node
+static bool agni_present = true;
+module_param(agni_present, bool, 0444);
 
 struct lpm_history {
 	uint32_t resi[MAXSAMPLES];
