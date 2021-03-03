4b72221d971e78621a42a03a813d456dbeb7a4fd
celtare21 <celtare21@gmail.com>
2021-03-03 11:31:54 +0530

    net/sched: ipa: use interruptible wait

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -316,8 +316,8 @@ static void dev_watchdog(unsigned long arg)
 			}
 
 			if (some_queue_timedout) {
-				WARN_ONCE(1, KERN_INFO "NETDEV WATCHDOG: %s (%s): transmit queue %u timed out\n",
-				       dev->name, netdev_drivername(dev), i);
+//				WARN_ONCE(1, KERN_INFO "NETDEV WATCHDOG: %s (%s): transmit queue %u timed out\n",
+//				       dev->name, netdev_drivername(dev), i);
 				dev->netdev_ops->ndo_tx_timeout(dev);
 			}
 			if (!mod_timer(&dev->watchdog_timer,
