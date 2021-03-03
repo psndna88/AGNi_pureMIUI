52ba3aa19d1b9828a613ac4be5af012c2454a793
Tyler Nijmeh <tylernij@gmail.com>
2021-03-03 10:33:03 +0530

    mm: Always overcommit memory by default
    Android is very memory intensive. This setting will drastically improve
    application performance.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- mm/util.c
+++ mm/util.c
@@ -531,7 +531,7 @@ int __page_mapcount(struct page *page)
 }
 EXPORT_SYMBOL_GPL(__page_mapcount);
 
-int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_NEVER;
+int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_ALWAYS;
 int sysctl_overcommit_ratio __read_mostly = 100;
 unsigned long sysctl_overcommit_kbytes __read_mostly;
 int sysctl_max_map_count __read_mostly = DEFAULT_MAX_MAP_COUNT;
