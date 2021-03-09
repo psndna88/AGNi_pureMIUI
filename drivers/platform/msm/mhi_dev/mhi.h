c259ee8a553c0cfcbafeb678f465cc5126dc810e
Subramanian Ananthanarayanan <skananth@codeaurora.org>
2021-03-09 15:06:45 +0530

    msm: mhi_dev: check for error during ring processing
    The change is to check for error code from cmd processing in case
    of db pending cases, to avoid rd_offset increment.
    
    Change-Id: If213297fe02ec1087bfab572f5af97b1e7bb5791
    Signed-off-by: Subramanian Ananthanarayanan <skananth@codeaurora.org>

--- drivers/platform/msm/mhi_dev/mhi.h
+++ drivers/platform/msm/mhi_dev/mhi.h
@@ -410,7 +410,7 @@ struct mhi_dev_ring {
 	union mhi_dev_ring_ctx			*ring_ctx;
 	/* ring_ctx_shadow -> tracking ring_ctx in the host */
 	union mhi_dev_ring_ctx			*ring_ctx_shadow;
-	void (*ring_cb)(struct mhi_dev *dev,
+	int (*ring_cb)(struct mhi_dev *dev,
 			union mhi_dev_ring_element_type *el,
 			void *ctx);
 };
@@ -775,7 +775,7 @@ int mhi_dev_add_element(struct mhi_dev_ring *ring,
  * @ring_cb:	callback function.
  */
 void mhi_ring_set_cb(struct mhi_dev_ring *ring,
-			void (*ring_cb)(struct mhi_dev *dev,
+			int (*ring_cb)(struct mhi_dev *dev,
 			union mhi_dev_ring_element_type *el, void *ctx));
 
 /**
