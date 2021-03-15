87f208377b84e9d765ebbfdfc9829a57349cb828
Ziqi Chen <ziqichen@codeaurora.org>
2021-03-15 13:04:32 +0800

    scsi: ufs: Disable irq before turning off clocks in suspend path
    In ufs suspend path we are turning off the clocks first and then
    disabling the irq. But in few scenarios link remains in active state
    (say rpm_lvl=2) and there is a chance that link can trigger error
    interrupts. If interrupt gets triggered after disabling clocks and
    before disabling irq, ISR gets invoked and tries to read registers
    which can lead to un-clocked access.
    
    To avoid this scenario, disable irq before disabling clocks.
    
    Change-Id: I90f01c06f72cb10920641e82e99177c99b656117
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
    Signed-off-by: Ziqi Chen <ziqichen@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -10663,6 +10663,12 @@ disable_clks:
 	if (ret)
 		goto set_link_active;
 
+	/*
+	 * Disable the host irq as host controller as there won't be any
+	 * host controller transaction expected till resume.
+	 */
+	ufshcd_disable_irq(hba);
+
 	/* reset the connected UFS device during power down */
 	if (ufshcd_is_link_off(hba)) {
 		ret = ufshcd_assert_device_reset(hba);
@@ -10686,11 +10692,6 @@ disable_clks:
 		trace_ufshcd_clk_gating(dev_name(hba->dev),
 					hba->clk_gating.state);
 	}
-	/*
-	 * Disable the host irq as host controller as there won't be any
-	 * host controller transaction expected till resume.
-	 */
-	ufshcd_disable_irq(hba);
 
 	if (!hba->auto_bkops_enabled ||
 		!(req_dev_pwr_mode == UFS_ACTIVE_PWR_MODE &&
@@ -10702,6 +10703,7 @@ disable_clks:
 	goto out;
 
 set_link_active:
+	ufshcd_enable_irq(hba);
 	if (hba->clk_scaling.is_allowed)
 		ufshcd_resume_clkscaling(hba);
 	ufshcd_vreg_set_hpm(hba);
