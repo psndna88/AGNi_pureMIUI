92b20896c936723f6bd73cb0ecd5753c5f00bfb1
Kiran Kumar Lokere <klokere@codeaurora.org>
2021-03-18 00:01:41 -0700

    qcacld-3.0: Add support for RF test mode and safe mode config
    Add RF test mode and safe mode configuration support for 6GHz.
    
    Change-Id: Icb6382f425b21e89a8cb2481c8f7bf480c4f6c19
    CRs-Fixed: 2904749

--- components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
+++ components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
@@ -1228,6 +1228,7 @@ struct wlan_mlme_ratemask {
  * @dual_sta_roam_fw_support: Firmware support for dual sta roaming feature
  * @sae_connect_retries: sae connect retry bitmask
  * @wls_6ghz_capable: wifi location service(WLS) is 6ghz capable
+ * @enabled_rf_test_mode: Enable/disable the RF test mode config
  * @monitor_mode_concurrency: Monitor mode concurrency supported
  * @ocv_support: FW supports OCV or not
  */
@@ -1271,6 +1272,7 @@ struct wlan_mlme_generic {
 	bool dual_sta_roam_fw_support;
 	uint32_t sae_connect_retries;
 	bool wls_6ghz_capable;
+	bool enabled_rf_test_mode;
 	enum monitor_mode_concurrency monitor_mode_concurrency;
 	bool ocv_support;
 };
