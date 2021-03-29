da6ce11c4b258862a8e0a1481b643b713b742c9b
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-28 18:36:18 -0700

    Revert "msm: kgsl: Add per process GPU memory usage trace"
    This reverts commit c1661af828b27a9177bbcb2cf061f91853bd66a7.
    
    This is debug cruft. Remove it completely since CAF's 4.14 branch
    doesn't have it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I6a49d732b7a52a06416356b079f92b9ac050d38f
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -1411,7 +1411,6 @@ kgsl_allocate_secure_global(struct kgsl_device *device,
 	 * normally
 	 */
 	kgsl_mmu_map_global(device, &md->memdesc, 0);
-	kgsl_trace_gpu_mem_total(device, md->memdesc.size);
 
 	return &md->memdesc;
 }
@@ -1451,7 +1450,6 @@ struct kgsl_memdesc *kgsl_allocate_global(struct kgsl_device *device,
 	list_add_tail(&md->node, &device->globals);
 
 	kgsl_mmu_map_global(device, &md->memdesc, padding);
-	kgsl_trace_gpu_mem_total(device, md->memdesc.size);
 
 	return &md->memdesc;
 }
