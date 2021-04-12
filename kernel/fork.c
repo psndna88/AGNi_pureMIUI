a7f65a5c762c1003121f5af006a6843c5c4c75e6
psndna88 <psndna88@gmail.com>
2021-04-12 09:11:40 +0530

    Revert "cpufreq: Remove cpufreq_times from CAF"
    This reverts commit 4cc75d804f1fd98f96be8ef6029bb712c5f4550a.

--- kernel/fork.c
+++ kernel/fork.c
@@ -78,6 +78,7 @@
 #include <linux/compiler.h>
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
+#include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
 #include <linux/devfreq_boost.h>
 
@@ -230,6 +231,7 @@ static void account_kernel_stack(unsigned long *stack, int account)
 
 void free_task(struct task_struct *tsk)
 {
+	cpufreq_task_times_exit(tsk);
 	account_kernel_stack(tsk->stack, -1);
 	arch_release_thread_stack(tsk->stack);
 	free_thread_stack(tsk->stack);
@@ -1373,6 +1375,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	if (!p)
 		goto fork_out;
 
+	cpufreq_task_times_init(p);
 
 	/*
 	 * This _must_ happen before we call free_task(), i.e. before we jump
@@ -1819,6 +1822,8 @@ long _do_fork(unsigned long clone_flags,
 	if (!IS_ERR(p)) {
 		struct completion vfork;
 		struct pid *pid;
+
+		cpufreq_task_times_alloc(p);
 
 //		trace_sched_process_fork(current, p);
 
