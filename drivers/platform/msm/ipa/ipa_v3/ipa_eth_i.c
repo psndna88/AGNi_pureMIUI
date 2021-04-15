15a402bb4ea0fc59cc49ae3f4b7697cc24361e35
Skylar Chang <chiaweic@codeaurora.org>
2021-04-15 12:23:33 -0700

    msm: ipa: Update metadata register for AQC/RTK
    Update metadata register to pick up metadata value with vlan id
    from header of packets for AQC and RTK pipe.
    
    Change-Id: I5996f54bba9bab1501a679321111418531576830
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_eth_i.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_eth_i.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -455,6 +455,12 @@ int ipa3_eth_rtk_connect(
 	} else {
 		ep->client_notify = pipe->info.notify;
 		ep->priv = pipe->info.priv;
+		/* xlat config in vlan mode */
+		if (vlan_mode) {
+			ep->cfg.hdr.hdr_ofst_metadata_valid = 1;
+			ep->cfg.hdr.hdr_ofst_metadata = ETH_HLEN;
+			ep->cfg.hdr.hdr_metadata_reg_valid = false;
+		}
 	}
 	if (ipa3_cfg_ep(ep_idx, &ep->cfg)) {
 		IPAERR("fail to setup rx pipe cfg\n");
