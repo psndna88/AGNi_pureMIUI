d4cd349dc193df9562f746d0a561a39520cba15b
psndna88 <psndna88@gmail.com>
2021-04-15 11:20:07 +0530

    Revert camera from "ADD some MIUI-Q bits (AOSP)"

--- drivers/media/platform/msm/camera/cam_utils/cam_soc_util.h
+++ drivers/media/platform/msm/camera/cam_utils/cam_soc_util.h
@@ -1,5 +1,4 @@
 /* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
- * Copyright (C) 2020 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -37,7 +36,7 @@
 #define CAM_SOC_MAX_BASE            CAM_SOC_MAX_BLOCK
 
 /* maximum number of device regulator */
-#define CAM_SOC_MAX_REGULATOR       6 // 5
+#define CAM_SOC_MAX_REGULATOR       6
 
 /* maximum number of device clock */
 #define CAM_SOC_MAX_CLK             32
