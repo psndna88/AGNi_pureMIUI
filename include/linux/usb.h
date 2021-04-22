ee3da29a6f6e67eb9f9e4938f43a6a639ef44ebe
Jimmy Hu <hhhuuu@google.com>
2021-04-22 15:48:04 +0800

    usb: fix undefined behavior caused by zero-length array
    The use of zero-length array causes undefined behavior.
    Replace zero-length array with flexible-array member.
    
    Bug: 174330770
    Test: build pass and boot to home
    Change-Id: I080ed1555d5b60075f6a86aa0ac15427529f7a1b
    Signed-off-by: Jimmy Hu <hhhuuu@google.com>
    Signed-off-by: Puma Hsu <pumahsu@google.com>

--- include/linux/usb.h
+++ include/linux/usb.h
@@ -1580,8 +1580,7 @@ struct urb {
 	int error_count;		/* (return) number of ISO errors */
 	void *context;			/* (in) context for completion */
 	usb_complete_t complete;	/* (in) completion routine */
-	struct usb_iso_packet_descriptor iso_frame_desc[0];
-					/* (in) ISO ONLY */
+	struct usb_iso_packet_descriptor iso_frame_desc[]; /* (in) ISO ONLY */
 };
 
 /* ----------------------------------------------------------------------- */
