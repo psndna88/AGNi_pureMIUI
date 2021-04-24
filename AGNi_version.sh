868497cdc646a1062209ca36b7d9b5d5923cdc74
psndna88 <psndna88@gmail.com>
2021-04-24 22:04:20 +0530

    AGNi MIATOLL MIUI Q v9.0 beta2

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta1"
+export AGNI_VERSION_PREFIX="beta2"
 export AGNI_VERSION="v9.0"
-sed -i 's/v8.9-stable/v9.0-beta1/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v9.0-beta1/v9.0-beta2/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
