33e0f6ed7d269656ba5658572851f1ac0402cf31
psndna88 <psndna88@gmail.com>
2021-04-28 22:23:55 +0530

    AGNi MIATOLL AOSP Q/R0 v9.0 beta5

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta4"
+export AGNI_VERSION_PREFIX="beta5"
 export AGNI_VERSION="v9.0"
-sed -i 's/v9.0-beta3/v9.0-beta4/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v9.0-beta4/v9.0-beta5/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
