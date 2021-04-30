82f3da9f83b5eb7ebc45bbf1910852c69251058e
psndna88 <psndna88@gmail.com>
2021-04-30 11:19:50 +0530

    schedutil: disable io boost by default

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -801,7 +801,7 @@ static int sugov_init(struct cpufreq_policy *policy)
                 }
 	}
 
-	tunables->iowait_boost_enable = true;
+	tunables->iowait_boost_enable = false;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
