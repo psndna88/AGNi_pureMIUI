ffab2f07e8711ccbbd27196a7e28c6a3d1b61bf7
psndna88 <psndna88@gmail.com>
2021-05-03 20:56:36 +0530

    AGNi MIATOLL MIUI R v9.1 stable

--- anykernel3/tools/atools.sh
+++ anykernel3/tools/atools.sh
@@ -9,6 +9,7 @@ if ([ ! "` uname -r | grep AGNi`" ] || [ ! -f /sys/module/lpm_levels/parameters/
 	rm -rf /data/adb/modules/AGNiWIFI;
 	rm -rf /data/adb/modules/AGNiCurtanaThermals;
 fi;
+rm -rf /data/adb/modules/AGNi_s2idle;
 
 if ([ -f /data/adb/modules/AGNi_AOSP_DeepSleepHelper/disable ] || [ ! -d /data/adb/modules/AGNi_AOSP_DeepSleepHelper ]); then
 	if [ ! -f /data/media/0/.ANXCamera/.statusfixed ]; then
