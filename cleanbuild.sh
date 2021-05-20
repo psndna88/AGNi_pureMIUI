f5a31a95c672487c5e4d297b6945b46b392c3e48
psndna88 <psndna88@gmail.com>
2021-05-20 15:08:10 +0530

    AGNi MIATOLL AOSP Q/R0 v9.2 stable

--- cleanbuild.sh
+++ cleanbuild.sh
@@ -4,7 +4,7 @@ export KERNELDIR=`readlink -f .`
 if [ -f ~/WORKING_DIRECTORY/AGNi_stamp.sh ]; then
 	. ~/WORKING_DIRECTORY/AGNi_stamp.sh
 fi
-if [ ! -d $COMPILEDIR ]; then
+if [ ! -d $COMPILEDIR_ATOLL ]; then
 	COUT=$KERNELDIR/OUTPUT
 	mkdir $COUT
 else
@@ -14,10 +14,6 @@ fi
 echo "`rm -rf $COUT/*`" > /dev/null
 if [ -f $COUT/.config ]; then
 	echo "`rm -rf $COUT/.*`" > /dev/null
-fi
-if [ -d $KERNELDIR/READY_ZIP ];
-	then
-	echo "`rm -rf $KERNELDIR/READY_ZIP/*`" > /dev/null
 fi
 
 echo "   Compile folder EMPTY !"
