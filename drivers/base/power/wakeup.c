cde7ce94760895cce46964f80219643e391e6a12
psndna88 <psndna88@gmail.com>
2021-05-21 19:52:28 +0530

    Revert "BACKPORT: PM / wakeup: Show wakeup sources stats in sysfs"
    This reverts commit 90001697d1a7786838cc81cc709f04d3125341bf.
    
    # Conflicts:
    #	fs/eventpoll.c

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -88,8 +88,6 @@ static struct wakeup_source deleted_ws = {
 	.lock =  __SPIN_LOCK_UNLOCKED(deleted_ws.lock),
 };
 
-static DEFINE_IDA(wakeup_ida);
-
 /**
  * wakeup_source_prepare - Prepare a new wakeup source for initialization.
  * @ws: Wakeup source to prepare.
@@ -113,32 +111,14 @@ EXPORT_SYMBOL_GPL(wakeup_source_prepare);
  */
 struct wakeup_source *wakeup_source_create(const char *name)
 {
- 	struct wakeup_source *ws;
-	const char *ws_name;
-	int id;
+	struct wakeup_source *ws;
 
-	ws = kzalloc(sizeof(*ws), GFP_KERNEL);
+	ws = kmalloc(sizeof(*ws), GFP_KERNEL);
 	if (!ws)
-		goto err_ws;
+		return NULL;
 
-	ws_name = kstrdup_const(name, GFP_KERNEL);
-	if (!ws_name)
-		goto err_name;
-	ws->name = ws_name;
-
-	id = ida_simple_get(&wakeup_ida, 0, 0, GFP_KERNEL);
-	if (id < 0)
-		goto err_id;
-	ws->id = id;
-
+	wakeup_source_prepare(ws, name ? kstrdup_const(name, GFP_KERNEL) : NULL);
 	return ws;
-
-err_id:
-	kfree_const(ws->name);
-err_name:
-	kfree(ws);
-err_ws:
-	return NULL;
 }
 EXPORT_SYMBOL_GPL(wakeup_source_create);
 
@@ -199,7 +179,6 @@ void wakeup_source_destroy(struct wakeup_source *ws)
 
 	wakeup_source_drop(ws);
 	wakeup_source_record(ws);
-	ida_simple_remove(&wakeup_ida, ws->id);
 	kfree_const(ws->name);
 	kfree(ws);
 }
@@ -253,24 +232,16 @@ EXPORT_SYMBOL_GPL(wakeup_source_remove);
 
 /**
  * wakeup_source_register - Create wakeup source and add it to the list.
- * @dev: Device this wakeup source is associated with (or NULL if virtual).
  * @name: Name of the wakeup source to register.
  */
-struct wakeup_source *wakeup_source_register(struct device *dev,
-					     const char *name)
+struct wakeup_source *wakeup_source_register(const char *name)
 {
 	struct wakeup_source *ws;
-	int ret;
 
 	ws = wakeup_source_create(name);
-	if (ws) {
-		ret = wakeup_source_sysfs_add(dev, ws);
-		if (ret) {
-			wakeup_source_destroy(ws);
-			return NULL;
-		}
+	if (ws)
 		wakeup_source_add(ws);
-	}
+
 	return ws;
 }
 EXPORT_SYMBOL_GPL(wakeup_source_register);
@@ -283,7 +254,6 @@ void wakeup_source_unregister(struct wakeup_source *ws)
 {
 	if (ws) {
 		wakeup_source_remove(ws);
-		wakeup_source_sysfs_remove(ws);
 		wakeup_source_destroy(ws);
 	}
 }
@@ -327,7 +297,7 @@ int device_wakeup_enable(struct device *dev)
 	if (pm_suspend_target_state != PM_SUSPEND_ON)
 		dev_dbg(dev, "Suspicious %s() during system transition!\n", __func__);
 
-	ws = wakeup_source_register(dev, dev_name(dev));
+	ws = wakeup_source_register(dev_name(dev));
 	if (!ws)
 		return -ENOMEM;
 
