9cebdbd6634db525e81fd3047a3d44c9bd142923
psndna88 <psndna88@gmail.com>
2021-05-24 22:51:44 +0530

    block: enable QUEUE_FLAG_SAME_FORCE

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -654,13 +654,15 @@ struct request_queue {
 #define QUEUE_FLAG_PREEMPT_ONLY 30	/* only process REQ_PREEMPT requests */
 
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_NONROT) |		\
-				 (1 << QUEUE_FLAG_STACKABLE)	|	\
+#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE)	|	\
 				 (1 << QUEUE_FLAG_NOMERGES)		|	\
-				 (1 << QUEUE_FLAG_SAME_COMP))
+				 (1 << QUEUE_FLAG_NONROT))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_NONROT)		|	\
 				 (1 << QUEUE_FLAG_POLL))
 
 /*
