f81b9ad398bf04b8a32263f0956bfca5bd86659e
psndna88 <psndna88@gmail.com>
2021-05-25 09:00:42 +0530

    AGNi v13.6 stable

--- anykernel3/anykernel.sh
+++ anykernel3/anykernel.sh
@@ -44,6 +44,7 @@ patch_cmdline agnisoundmod agnisoundmod=0;
 patch_cmdline wiredbtnaltmode wiredbtnaltmode=0;
 patch_cmdline ledmode ledmode=0;
 patch_cmdline losxattr losxattr=0;
+patch_cmdline selfake selfake=0;
 write_boot;
 ## end install
 
