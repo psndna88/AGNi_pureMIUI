68bdb8a0818fe9518609dec6e6f918642576c6cb
psndna88 <psndna88@gmail.com>
2021-05-27 08:30:17 +0530

    Revert "drm/msm/dsi-staging: enable ULPS"
    This reverts commit d0458584dc76e4d9d3369aea894007183bbe3678.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2094,7 +2094,8 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 {
 	struct dsi_parser_utils *utils = &panel->utils;
 
-	panel->ulps_feature_enabled = true;
+	panel->ulps_feature_enabled =
+		utils->read_bool(utils->data, "qcom,ulps-enabled");
 
 	pr_info("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
