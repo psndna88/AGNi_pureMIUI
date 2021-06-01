36bc00a1571716c96ddfd16e43c01f3405e3d0e9
psndna88 <psndna88@gmail.com>
2021-06-01 19:38:42 +0530

    mm: drivers/block: implement vbswap + dependencies from positron v2

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1714,7 +1714,7 @@ static const struct file_operations vmstat_file_operations = {
 
 #ifdef CONFIG_SMP
 static DEFINE_PER_CPU(struct delayed_work, vmstat_work);
-int sysctl_stat_interval __read_mostly = 20 * HZ;
+int sysctl_stat_interval __read_mostly = 60 * HZ;
 
 #ifdef CONFIG_PROC_FS
 static void refresh_vm_stats(struct work_struct *work)
