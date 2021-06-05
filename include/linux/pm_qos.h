ceb1edc6f0963ba828a8ebfabbfff1855ca3b90f
psndna88 <psndna88@gmail.com>
2021-06-05 12:40:26 +0530

    Revert "qos: Remove pm_qos_update_request_timeout() API"
    This reverts commit 0bc68f5d39a2aa71a34ae220c78b38f3eb1e982e.

--- include/linux/pm_qos.h
+++ include/linux/pm_qos.h
@@ -8,6 +8,7 @@
 #include <linux/plist.h>
 #include <linux/notifier.h>
 #include <linux/device.h>
+#include <linux/workqueue.h>
 #include <linux/cpumask.h>
 #include <linux/interrupt.h>
 
@@ -61,6 +62,7 @@ struct pm_qos_request {
 #endif
 	struct plist_node node;
 	int pm_qos_class;
+	struct delayed_work work; /* for pm_qos_update_request_timeout */
 };
 
 struct pm_qos_flags_request {
@@ -140,6 +142,8 @@ void pm_qos_add_request(struct pm_qos_request *req, int pm_qos_class,
 			s32 value);
 void pm_qos_update_request(struct pm_qos_request *req,
 			   s32 new_value);
+void pm_qos_update_request_timeout(struct pm_qos_request *req,
+				   s32 new_value, unsigned long timeout_us);
 void pm_qos_remove_request(struct pm_qos_request *req);
 
 int pm_qos_request(int pm_qos_class);
