f1efa064b62d61125f267e0fa5ec8198e7c19924
psndna88 <psndna88@gmail.com>
2021-06-12 00:03:55 +0530

    Revert "treewide: Adapt for latest perf-crit APIs"
    This reverts commit a4d947517563b2cdbfff59a776fe59655a304cdc.

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -2828,7 +2828,7 @@ int adreno_dispatcher_init(struct adreno_device *adreno_dev)
 
 	init_waitqueue_head(&dispatcher->cmd_waitq);
 	dispatcher->state = (atomic_t)ATOMIC_INIT(THREAD_IDLE);
-	dispatcher->thread = kthread_run_perf_critical(cpu_perf_mask, adreno_dispatcher_thread, adreno_dev,
+	dispatcher->thread = kthread_run(adreno_dispatcher_thread, adreno_dev,
 					 "adreno_dispatch");
 	if (IS_ERR(dispatcher->thread))
 		return PTR_ERR(dispatcher->thread);
