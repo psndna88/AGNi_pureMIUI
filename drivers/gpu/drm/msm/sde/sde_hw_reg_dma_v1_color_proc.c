a3ef86962a9f860f0090ce737f74b142ad445a1a
psndna88 <psndna88@gmail.com>
2021-06-12 19:34:20 +0530

    msm: drm: implement Klapse (squashed commit)
    credits to respective contributors

--- drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
+++ drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
@@ -1031,6 +1031,7 @@ reg_dmav1_setup_dspp_pa_hsicv17_apply(struct sde_hw_dspp *ctx,
 	return rc;
 }
 
+#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 static inline void
 reg_dmav1_setup_dspp_pa_hsicv17_kcal(struct sde_hw_dspp *ctx, void *ctl)
 {
@@ -1041,13 +1042,16 @@ reg_dmav1_setup_dspp_pa_hsicv17_kcal(struct sde_hw_dspp *ctx, void *ctl)
 	if (rc)
 		pr_err("kernel hsic application failed ret %d\n", rc);
 }
+#endif
 
 void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 {
 	struct sde_hw_reg_dma_ops *dma_ops;
 	struct sde_reg_dma_kickoff_cfg kick_off;
 	struct sde_hw_cp_cfg *hw_cfg = cfg;
+#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
+#endif
 	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
 	struct drm_msm_pcc *pcc_cfg;
 	struct drm_msm_pcc_coeff *coeffs = NULL;
@@ -1120,8 +1124,10 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 		data[i + 6] = coeffs->g;
 		data[i + 9] = coeffs->b;
 
+#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 		if (kcal->enabled)
 			sde_hw_kcal_pcc_adjust(data, i);
+#endif
 
 		data[i + 12] = coeffs->rg;
 		data[i + 15] = coeffs->rb;
@@ -1155,25 +1161,27 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	if (rc)
 		DRM_ERROR("failed to kick off ret %d\n", rc);
 
+#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	if (kcal->enabled)
 		reg_dmav1_setup_dspp_pa_hsicv17_kcal(ctx, hw_cfg->ctl);
+#endif
 exit:
 	kfree(data);
 }
 
 void reg_dmav1_setup_dspp_pa_hsicv17(struct sde_hw_dspp *ctx, void *cfg)
 {
-	struct sde_hw_reg_dma_ops *dma_ops;
-	struct sde_reg_dma_kickoff_cfg kick_off;
 	struct sde_hw_cp_cfg *hw_cfg = cfg;
-	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
-	struct drm_msm_pa_hsic *hsic_cfg;
-	u32 reg = 0, opcode = 0, local_opcode = 0;
+#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
+#endif
+	u32 opcode = 0;
 	int rc;
 
+#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	if (kcal->enabled)
 		return;
+#endif
 
 	opcode = SDE_REG_READ(&ctx->hw, ctx->cap->sblk->hsic.base);
 
@@ -1196,94 +1204,10 @@ void reg_dmav1_setup_dspp_pa_hsicv17(struct sde_hw_dspp *ctx, void *cfg)
 		return;
 	}
 
-	hsic_cfg = hw_cfg->payload;
-
-	dma_ops = sde_reg_dma_get_ops();
-	dma_ops->reset_reg_dma_buf(dspp_buf[HSIC][ctx->idx]);
-
-	REG_DMA_INIT_OPS(dma_write_cfg, dspp_mapping[ctx->idx],
-		HSIC, dspp_buf[HSIC][ctx->idx]);
-
-	REG_DMA_SETUP_OPS(dma_write_cfg, 0, NULL, 0, HW_BLK_SELECT, 0, 0, 0);
-	rc = dma_ops->setup_payload(&dma_write_cfg);
-	if (rc) {
-		DRM_ERROR("write decode select failed ret %d\n", rc);
-		return;
-	}
-
-	if (hsic_cfg->flags & PA_HSIC_HUE_ENABLE) {
-		reg = hsic_cfg->hue & PA_HUE_MASK;
-		REG_DMA_SETUP_OPS(dma_write_cfg,
-			ctx->cap->sblk->hsic.base + PA_HUE_OFF,
-			&reg, sizeof(reg), REG_SINGLE_WRITE, 0, 0, 0);
-		rc = dma_ops->setup_payload(&dma_write_cfg);
-		if (rc) {
-			DRM_ERROR("hsic hue write failed ret %d\n", rc);
-			return;
-		}
-		local_opcode |= PA_HUE_EN;
-	}
-
-	if (hsic_cfg->flags & PA_HSIC_SAT_ENABLE) {
-		reg = hsic_cfg->saturation & PA_SAT_MASK;
-		REG_DMA_SETUP_OPS(dma_write_cfg,
-			ctx->cap->sblk->hsic.base + PA_SAT_OFF,
-			&reg, sizeof(reg), REG_SINGLE_WRITE, 0, 0, 0);
-		rc = dma_ops->setup_payload(&dma_write_cfg);
-		if (rc) {
-			DRM_ERROR("hsic saturation write failed ret %d\n", rc);
-			return;
-		}
-		local_opcode |= PA_SAT_EN;
-	}
-
-	if (hsic_cfg->flags & PA_HSIC_VAL_ENABLE) {
-		reg = hsic_cfg->value & PA_VAL_MASK;
-		REG_DMA_SETUP_OPS(dma_write_cfg,
-			ctx->cap->sblk->hsic.base + PA_VAL_OFF,
-			&reg, sizeof(reg), REG_SINGLE_WRITE, 0, 0, 0);
-		rc = dma_ops->setup_payload(&dma_write_cfg);
-		if (rc) {
-			DRM_ERROR("hsic value write failed ret %d\n", rc);
-			return;
-		}
-		local_opcode |= PA_VAL_EN;
-	}
-
-	if (hsic_cfg->flags & PA_HSIC_CONT_ENABLE) {
-		reg = hsic_cfg->contrast & PA_CONT_MASK;
-		REG_DMA_SETUP_OPS(dma_write_cfg,
-			ctx->cap->sblk->hsic.base + PA_CONT_OFF,
-			&reg, sizeof(reg), REG_SINGLE_WRITE, 0, 0, 0);
-		rc = dma_ops->setup_payload(&dma_write_cfg);
-		if (rc) {
-			DRM_ERROR("hsic contrast write failed ret %d\n", rc);
-			return;
-		}
-		local_opcode |= PA_CONT_EN;
-	}
-
-	if (local_opcode) {
-		local_opcode |= PA_EN;
-	} else {
-		DRM_ERROR("Invalid hsic config 0x%x\n", local_opcode);
-		return;
-	}
-
-	REG_DMA_SETUP_OPS(dma_write_cfg,
-		ctx->cap->sblk->hsic.base, &local_opcode, sizeof(local_opcode),
-		REG_SINGLE_MODIFY, 0, 0, REG_DMA_PA_MODE_HSIC_MASK);
-	rc = dma_ops->setup_payload(&dma_write_cfg);
-	if (rc) {
-		DRM_ERROR("setting opcode failed ret %d\n", rc);
-		return;
-	}
-
-	REG_DMA_SETUP_KICKOFF(kick_off, hw_cfg->ctl, dspp_buf[HSIC][ctx->idx],
-			REG_DMA_WRITE, DMA_CTL_QUEUE0, WRITE_IMMEDIATE);
-	rc = dma_ops->kick_off(&kick_off);
+	rc = reg_dmav1_setup_dspp_pa_hsicv17_apply(ctx,
+		hw_cfg->payload, hw_cfg->ctl);
 	if (rc)
-		DRM_ERROR("failed to kick off ret %d\n", rc);
+		DRM_ERROR("hsic application failed ret %d\n", rc);
 }
 
 void reg_dmav1_setup_dspp_sixzonev17(struct sde_hw_dspp *ctx, void *cfg)
