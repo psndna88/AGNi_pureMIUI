99bd64fc9d3a0446187d13dd41b7527eda977dc0
Panchajanya1999 <panchajanya@azure-dev.live>
2021-06-17 15:51:49 -0400

    power/qpnp-smb5: Set Battery technology to Li-ion
    *miatoll use BN55 battery 5020mAh Li-ion
    
    Change-Id: I591c57e416933e73bc6a4903e86dc8ffae2f0c48
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2068,7 +2068,7 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 						POWER_SUPPLY_PROP_TEMP, val);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_DONE:
 		rc = smblib_get_prop_batt_charge_done(chg, val);
