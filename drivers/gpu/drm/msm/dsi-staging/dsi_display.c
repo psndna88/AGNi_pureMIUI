d804e1c1215009cca2ef0d20f6e14306f6eb7eef
psndna88 <psndna88@gmail.com>
2021-06-18 12:05:02 +0530

    drm: msm: allow reading from CABC & HBM sysfs

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -47,6 +47,10 @@
 #define MAX_TE_SOURCE_ID  2
 
 extern char g_lcd_id[128];
+static int dsi_hbm = 0; //Default OFF
+static int dsi_cabc = 2;  //Default OFF
+static int dsi_cabc_still = 2;  //Default OFF
+static int dsi_cabc_movie = 2;  //Default OFF
 static struct dsi_display *whitep_display;
 //Update /proc/tp_info & /proc/tp_lockdown_info node
 extern void update_lct_tp_info(char *tp_info_buf, char *tp_lockdown_info_buf);
@@ -5157,6 +5161,12 @@ static int dsi_display_whitepoint_create_sysfs(void){
         return ret;
 }
 
+static ssize_t dsi_display_get_cabc(struct device *dev, struct device_attribute *attr, char *buf)
+{
+
+	return snprintf(buf, PAGE_SIZE, "cabc=%d\n", dsi_cabc);
+}
+
 static ssize_t dsi_display_set_cabc(struct device *dev,struct device_attribute *attr,const char *buf,size_t len)
 {
 
@@ -5175,6 +5185,7 @@ static ssize_t dsi_display_set_cabc(struct device *dev,struct device_attribute *
                 pr_err("kstrtoint failed. rc=%d\n", rc);
                 return rc;
         }
+		dsi_cabc = param;
 
 	pr_info("xinj:_###_%s,set_cabc_cmd: %d\n",__func__, param);
         switch(param) {
@@ -5191,6 +5202,13 @@ static ssize_t dsi_display_set_cabc(struct device *dev,struct device_attribute *
 	pr_info("xinj:_##### cabc over ###\n");
         return len;
 }
+
+static ssize_t dsi_display_get_cabc_movie(struct device *dev, struct device_attribute *attr, char *buf)
+{
+
+	return snprintf(buf, PAGE_SIZE, "cabc_movie=%d\n", dsi_cabc_movie);
+}
+
 static ssize_t dsi_display_set_cabc_movie(struct device *dev,struct device_attribute *attr,const char *buf,size_t len)
 {
 
@@ -5209,6 +5227,7 @@ static ssize_t dsi_display_set_cabc_movie(struct device *dev,struct device_attri
                 pr_err("kstrtoint failed. rc=%d\n", rc);
                 return rc;
         }
+		dsi_cabc_movie = param;
 
 //        pr_info("xinj:_###_%s,set_cabc_movie_cmd: %d\n",__func__, param);
         switch(param) {
@@ -5224,6 +5243,12 @@ static ssize_t dsi_display_set_cabc_movie(struct device *dev,struct device_attri
         }
 		pr_info("xinj:_##### cabc_movie over ###\n");
         return len;
+}
+
+static ssize_t dsi_display_get_cabc_still(struct device *dev, struct device_attribute *attr, char *buf)
+{
+
+	return snprintf(buf, PAGE_SIZE, "cabc_still=%d\n", dsi_cabc_still);
 }
 
 static ssize_t dsi_display_set_cabc_still(struct device *dev,struct device_attribute *attr,const char *buf,size_t len)
@@ -5244,6 +5269,7 @@ static ssize_t dsi_display_set_cabc_still(struct device *dev,struct device_attri
                 pr_err("kstrtoint failed. rc=%d\n", rc);
                 return rc;
         }
+		dsi_cabc_still = param;
 
 //        pr_info("xinj:_###_%s,set_cabc_still_cmd: %d\n",__func__, param);
         switch(param) {
@@ -5259,6 +5285,12 @@ static ssize_t dsi_display_set_cabc_still(struct device *dev,struct device_attri
         }
 		pr_info("xinj:_##### cabc_still over ###\n");
         return len;
+}
+
+static ssize_t dsi_display_get_hbm(struct device *dev, struct device_attribute *attr, char *buf)
+{
+
+	return snprintf(buf, PAGE_SIZE, "hbm=%d\n", dsi_hbm);
 }
 
 static ssize_t dsi_display_set_hbm(struct device *dev,struct device_attribute *attr,const char *buf,size_t len)
@@ -5279,6 +5311,7 @@ static ssize_t dsi_display_set_hbm(struct device *dev,struct device_attribute *a
                 pr_err("kstrtoint failed. rc=%d\n", rc);
                 return rc;
         }
+		dsi_hbm = param;
 
         switch(param) {
             case 0x1: //hbm1 on
@@ -5300,10 +5333,10 @@ static ssize_t dsi_display_set_hbm(struct device *dev,struct device_attribute *a
         return len;
 }
 
-static DEVICE_ATTR(dsi_display_cabc, 0644, NULL, dsi_display_set_cabc);
-static DEVICE_ATTR(dsi_display_hbm, 0644, NULL, dsi_display_set_hbm);
-static DEVICE_ATTR(dsi_display_cabc_movie, 0644, NULL, dsi_display_set_cabc_movie);
-static DEVICE_ATTR(dsi_display_cabc_still, 0644, NULL, dsi_display_set_cabc_still);
+static DEVICE_ATTR(dsi_display_cabc, 0644, dsi_display_get_cabc, dsi_display_set_cabc);
+static DEVICE_ATTR(dsi_display_hbm, 0644, dsi_display_get_hbm, dsi_display_set_hbm);
+static DEVICE_ATTR(dsi_display_cabc_movie, 0644, dsi_display_get_cabc_movie, dsi_display_set_cabc_movie);
+static DEVICE_ATTR(dsi_display_cabc_still, 0644, dsi_display_get_cabc_still, dsi_display_set_cabc_still);
 
 static struct attribute *dsi_display_feature_attrs[] = {
 	&dev_attr_dsi_display_cabc.attr,
@@ -5820,9 +5853,6 @@ static void dsi_display_firmware_display(const struct firmware *fw,
 		display->name = "dsi_firmware_display";
 	}
 
-	if (dsi_display_init(display))
-		return;
-
 	pr_debug("success\n");
 }
 
@@ -5909,12 +5939,9 @@ int dsi_display_dev_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, display);
 
-	/* initialize display in firmware callback */
-	if (!firm_req) {
-		rc = dsi_display_init(display);
-		if (rc)
-			goto end;
-	}
+	rc = dsi_display_init(display);
+	if (rc)
+		goto end;
 
 	return 0;
 end:
