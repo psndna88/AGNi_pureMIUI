7494ea5078812736665d96bb5237f146d60dbbd7
Da Hoon Pyun <dpyun@codeaurora.org>
2021-06-18 18:02:54 -0400

    msm: npu: Use spinlock for ipc write to avoid context switch
    Use a spinlock to prevent context switch while sending ipc
    command to firmware.
    
    Change-Id: I773ea5f2ed0346b4f7a29719b87fc68aefd78809
    Signed-off-by: Da Hoon Pyun <dpyun@codeaurora.org>

--- drivers/media/platform/msm/npu_v2/npu_common.h
+++ drivers/media/platform/msm/npu_v2/npu_common.h
@@ -134,7 +134,7 @@ struct npu_mbox {
 };
 
 /*
- * struct npul_pwrlevel - Struct holding different pwrlevel info obtained from
+ * struct npu_pwrlevel - Struct holding different pwrlevel info obtained
  * from dtsi file
  * @pwr_level:           NPU power level
  * @freq[]:              NPU frequency vote in Hz
@@ -246,6 +246,7 @@ struct mbox_bridge_data {
 
 struct npu_device {
 	struct mutex dev_lock;
+	spinlock_t ipc_lock;
 
 	struct platform_device *pdev;
 
