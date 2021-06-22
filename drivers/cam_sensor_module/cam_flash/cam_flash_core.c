e1261386ea9c5a220093aa1e47108e012471edf1
Wyes Karny <wkarny@codeaurora.org>
2021-06-22 12:18:26 +0530

    msm: camera: common: Fix for wrong ab ib value send to ICC
    While passing ab and ib value to interconnect
    for conversion from BPS to ICC units wrong kernel macro is used.
    Therefore use correct macro to get the proper ICC units.
    In the same way, update the correct flash on active time (use div_u64
    instead of do_div).
    
    CRs-Fixed: 2971025
    Change-Id: I4cd0de06bd081ea95cdc76eb74887c4d5815241a
    Signed-off-by: Wyes Karny <wkarny@codeaurora.org>

--- drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -10,7 +10,7 @@
 #include "cam_res_mgr_api.h"
 #include "cam_common_util.h"
 #include "cam_packet_util.h"
-#include <asm/div64.h>
+#include <linux/math64.h>
 
 static uint default_on_timer = 2;
 module_param(default_on_timer, uint, 0644);
@@ -1559,7 +1559,7 @@ int cam_flash_pmic_pkt_parser(struct cam_flash_ctrl *fctrl, void *arg)
 				add_req.trigger_eof = true;
 				/* Active time for the preflash */
 				flash_data->flash_active_time_ms =
-				do_div(flash_operation_info->time_on_duration_ns, 1000000);
+				div_u64(flash_operation_info->time_on_duration_ns, 1000000);
 				CAM_DBG(CAM_FLASH,
 					"PRECISE FLASH: active_time: %llu",
 					flash_data->flash_active_time_ms);
