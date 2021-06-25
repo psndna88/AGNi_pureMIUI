132677f6a50d7e2bdac3e6df6fb608db5be98473
Dechen Chai <dchai@codeaurora.org>
2021-06-25 08:02:32 +0530

    asoc: codecs: Add dai driver in rouleur
    Add dai driver in rouleur.
    Changed micbias widget to supply widget.
    
    Change-Id: I7ddfe98ac70eab7f622e78758af8467ccf93b8e3
    Signed-off-by: Dechen Chai <dchai@codeaurora.org>

--- asoc/codecs/rouleur/rouleur.h
+++ asoc/codecs/rouleur/rouleur.h
@@ -1,9 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _ROULEUR_H
 #define _ROULEUR_H
+
+#define ROULEUR_DRV_NAME "rouleur_codec"
 
 #ifdef CONFIG_SND_SOC_ROULEUR
 extern int rouleur_info_create_codec_entry(struct snd_info_entry *codec_root,
