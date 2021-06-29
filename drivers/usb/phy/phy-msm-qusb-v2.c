b09090fa16ddc1844a669ec19c21066426e15c4e
psndna88 <psndna88@gmail.com>
2021-06-29 10:51:01 +0530

    power: supply: AGNi charging temperature control v3.4

--- drivers/usb/phy/phy-msm-qusb-v2.c
+++ drivers/usb/phy/phy-msm-qusb-v2.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
- * Copyright (C) 2021 XiaoMi, Inc.
+ * Copyright (C) 2020 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -445,7 +445,7 @@ static void qusb_phy_host_init(struct usb_phy *phy)
 	qusb_phy_write_seq(qphy->base, qphy->qusb_phy_host_init_seq,
 			qphy->host_init_seq_len, 0);
 
-	if (qphy->efuse_reg) {
+	if (0) {
 		if (!qphy->tune_val)
 			qusb_phy_get_tune1_param(qphy);
 	} else {
@@ -488,6 +488,10 @@ static void qusb_phy_host_init(struct usb_phy *phy)
 	/* Require to get phy pll lock successfully */
 	usleep_range(150, 160);
 
+	reg = readb_relaxed(qphy->base + qphy->phy_reg[PORT_TUNE1]);
+	dev_dbg(phy->dev, "tune1= %x\n", reg);
+	reg = readb_relaxed(qphy->base + qphy->phy_reg[BIAS_CTRL_2]);
+	dev_dbg(phy->dev, "bias_ctrl2= %x\n", reg);
 	reg = readb_relaxed(qphy->base + qphy->phy_reg[PLL_COMMON_STATUS_ONE]);
 	dev_dbg(phy->dev, "QUSB2PHY_PLL_COMMON_STATUS_ONE:%x\n", reg);
 	if (!(reg & CORE_READY_STATUS)) {
