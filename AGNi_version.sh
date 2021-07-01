fa2e27473ea35a26f50e01ac6f1a1ba463a4d14d
psndna88 <psndna88@gmail.com>
2021-07-01 18:54:02 +0530

    AGNi v14.1 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v14.0"
-sed -i 's/v13.9_stable-EAS/v14.0_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/ini_set("rom_version",	"v13.9_stable");/ini_set("rom_version",	"v14.0_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
+export AGNI_VERSION="v14.1"
+sed -i 's/v14.0_stable-EAS/v14.1_stable-EAS/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/ini_set("rom_version",	"v14.0_stable");/ini_set("rom_version",	"v14.1_stable");/' $KERNELDIR/anykernel3/META-INF/com/google/android/aroma-config
 
 echo "	AGNi Version info loaded."
 
