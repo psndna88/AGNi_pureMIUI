8fd69cb240a8757435e3fcaf97cb1b963cb80883
sheenam monga <shebala@codeaurora.org>
2021-07-09 16:54:00 +0530

    qcacmn: Check ext cc event support before pouplating sec chan list
    Currently host is populating secondary channel list from master
    channel list based on ap power type. In case fw doesn't support
    extd cc event then secondary channel list will not have 6g freq,
    which may break legacy 6g handling.
    
    Fix is to add validation check for ext cc event support, copy
    secondary channel list from ap power type in case ext cc event
    is supported by fw else copy secondary channel list from reg
    current channel list.
    
    Change-Id: I2e13be2936b624ccbee5e8e426987355076a0062
    CRs-Fixed: 2987951

--- umac/cmn_services/obj_mgr/inc/wlan_objmgr_psoc_obj.h
+++ umac/cmn_services/obj_mgr/inc/wlan_objmgr_psoc_obj.h
@@ -146,6 +146,8 @@
 #define WLAN_SOC_CEXT_SCAN_PER_CH_CONFIG    0x01000000
 	/* CAPABILITY: csa offload in case of AP */
 #define WLAN_SOC_CEXT_CSA_TX_OFFLOAD      0x02000000
+	/* ext event supported by fw */
+#define WLAN_SOC_EXT_EVENT_SUPPORTED      0x04000000
 
 /* feature_flags */
 	/* CONF: ATH FF enabled */
