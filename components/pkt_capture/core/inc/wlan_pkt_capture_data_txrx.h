81e3c36defdce2c023b8d54654abc1d703ec6232
Surabhi Vishnoi <svishnoi@codeaurora.org>
2021-07-28 00:12:30 +0530

    qcacld-3.0: Fill the ppdu stats in tx status in pkt capture mode
    Ppdu stats related to transmitted msdu are received from
    firmware and queued to a list. Remove the received ppdu stats
    from front of list and fill in tx status of current msdu.
    
    Change-Id: I764aa909497971010202ce3decb3380a732ce12c
    CRs-Fixed: 3004483

--- components/pkt_capture/core/inc/wlan_pkt_capture_data_txrx.h
+++ components/pkt_capture/core/inc/wlan_pkt_capture_data_txrx.h
@@ -169,6 +169,7 @@ void pkt_capture_offload_deliver_indication_handler(
  * @dir: direction rx: 0 and tx: 1
  * @status: tx status
  * @tx_retry_cnt: tx retry count
+ * @ppdu_id: ppdu_id of msdu
  */
 struct pkt_capture_tx_hdr_elem_t {
 	uint32_t timestamp;
@@ -187,6 +188,7 @@ struct pkt_capture_tx_hdr_elem_t {
 	uint8_t tx_retry_cnt;
 	uint16_t framectrl;
 	uint16_t seqno;
+	uint32_t ppdu_id;
 };
 
 /**
