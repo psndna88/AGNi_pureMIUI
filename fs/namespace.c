f422cc6afb07dda20899d503b67da93532b881b8
psndna88 <psndna88@gmail.com>
2021-08-04 21:22:59 +0530

    Revert "Default to NOATIME and NODIRATIME for FS performance improvement."
    This reverts commit 2522dc3afd78d47bef52158b6690ef0dbdb54505.

--- fs/namespace.c
+++ fs/namespace.c
@@ -2858,10 +2858,9 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 	if (retval)
 		goto dput_out;
 
-	/* Default to NOATIME and NODIRATIME */
-	mnt_flags |= MNT_NOATIME;
-	mnt_flags |= MNT_NODIRATIME;
-
+	/* Default to relatime unless overriden */
+	if (!(flags & MS_NOATIME))
+		mnt_flags |= MNT_RELATIME;
 
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
