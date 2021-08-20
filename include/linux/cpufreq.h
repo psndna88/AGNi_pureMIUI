97b6bd2ee23704949f049703fdcbce74e84dd86e
psndna88 <psndna88@gmail.com>
2021-08-20 23:37:43 +0530

    thermal: Remove partial merge changes from mojito MIUI R

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -2,7 +2,6 @@
  * linux/include/linux/cpufreq.h
  *
  * Copyright (C) 2001 Russell King
- * Copyright (C) 2021 XiaoMi, Inc.
  *           (C) 2002 - 2003 Dominik Brodowski <linux@brodo.de>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -431,7 +430,6 @@ static inline void cpufreq_resume(void) {}
 #define CPUFREQ_ADJUST			(0)
 #define CPUFREQ_NOTIFY			(1)
 #define CPUFREQ_START			(2)
-#define CPUFREQ_THERMAL			(3)
 #define CPUFREQ_STOP			(5)
 #define CPUFREQ_INCOMPATIBLE	(6)
 
