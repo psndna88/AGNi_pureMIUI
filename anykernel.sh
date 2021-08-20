b549e2a516fa4043ae47aacfeb040303e716e9d0
EcrosoftXiao <xy19990903@gmail.com>
2021-08-20 16:45:42 +0800

    scripts: Add build scripts

---
+++ anykernel.sh
@@ -0,0 +1,32 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Evasi0nKernel
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+
+write_boot;
+## end install
+
