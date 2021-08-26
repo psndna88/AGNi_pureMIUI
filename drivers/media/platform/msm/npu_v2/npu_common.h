c330603303312ecd8a9d5fa0d9d9a6d4c1acb161
psndna88 <psndna88@gmail.com>
2021-08-27 03:15:15 +0530

    Partial Merge CAF LA.UM.8.9.r1-12300-SMxxx0.0

--- drivers/media/platform/msm/npu_v2/npu_common.h
+++ drivers/media/platform/msm/npu_v2/npu_common.h
@@ -137,7 +137,7 @@ struct npu_mbox {
 };
 
 /*
- * struct npul_pwrlevel - Struct holding different pwrlevel info obtained from
+ * struct npu_pwrlevel - Struct holding different pwrlevel info obtained
  * from dtsi file
  * @pwr_level:           NPU power level
  * @freq[]:              NPU frequency vote in Hz
@@ -248,6 +248,7 @@ struct mbox_bridge_data {
 
 struct npu_device {
 	struct mutex dev_lock;
+	spinlock_t ipc_lock;
 
 	struct platform_device *pdev;
 
