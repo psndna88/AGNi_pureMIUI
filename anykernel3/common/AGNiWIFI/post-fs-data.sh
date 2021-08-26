080c71d2abf90647fc5f3fc97f3336eee74b23b9
psndna88 <psndna88@gmail.com>
2021-08-27 01:14:05 +0530

    AGNi MIATOLL AOSP Q/R0 v10.5 stable

--- anykernel3/common/AGNiWIFI/post-fs-data.sh
+++ anykernel3/common/AGNiWIFI/post-fs-data.sh
@@ -8,25 +8,18 @@ MODDIR=${0%/*}
 
 # AGNi Wifi Tweaks
 if ([ "` uname -r | grep AGNi`" ] || [ -f /sys/module/lpm_levels/parameters/agni_present ]); then
-	if [ ! -d /data/adb/modules/AGNiPSNHOME ]; then
-		echo 0 > /proc/sys/net/ipv4/tcp_ecn
-		echo "fq_codel" > /proc/sys/net/core/default_qdisc
-		echo "veno" > /proc/sys/net/ipv4/tcp_congestion_control
-		echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse 
-		echo 0 > /proc/sys/net/ipv4/tcp_slow_start_after_idle
-		echo 2 > /proc/sys/net/ipv4/tcp_mtu_probing
-		echo 1 > /proc/sys/net/ipv4/tcp_sack
-		echo 1 > /proc/sys/net/ipv4/tcp_timestamps
-		#net.core.netdev_max_backlog = 10000
-		echo 16777216 > /proc/sys/net/core/rmem_max
-		echo 16777216 > /proc/sys/net/core/wmem_max
-		echo "4096 87380 16777216" > /proc/sys/net/ipv4/tcp_rmem
-		echo "4096 87380 16777216" > /proc/sys/net/ipv4/tcp_wmem
-		echo 1 > /proc/sys/net/ipv4/tcp_no_metrics_save
-		echo 1 > /proc/sys/net/ipv4/tcp_moderate_rcvbuf
-		echo 1 > /proc/sys/net/ipv4/tcp_syncookies
-	fi;
+	echo 2 > /proc/sys/net/ipv4/tcp_ecn
+	echo "fq_codel" > /proc/sys/net/core/default_qdisc
+	echo "veno" > /proc/sys/net/ipv4/tcp_congestion_control
+	echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse 
+	echo 0 > /proc/sys/net/ipv4/tcp_slow_start_after_idle
+	echo 2 > /proc/sys/net/ipv4/tcp_mtu_probing
+	echo 1 > /proc/sys/net/ipv4/tcp_sack
+	echo 1 > /proc/sys/net/ipv4/tcp_timestamps
+	echo 1 > /proc/sys/net/ipv4/tcp_no_metrics_save
+	echo 1 > /proc/sys/net/ipv4/tcp_moderate_rcvbuf
+	echo 1 > /proc/sys/net/ipv4/tcp_syncookies
+	echo 1 > /proc/sys/net/ipv4/tcp_low_latency
 else
 	rm -rf /data/adb/modules/AGNiWIFI
 fi;
-
