22b03b769e4216b3a3c704c8e2ea3e3864439663
Jebaitedneko <Jebaitedneko@gmail.com>
2021-09-06 18:14:24 +0530

    fs: ntfs3: fallthrough -> /* fall through */
    match downstream behavior
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- fs/ntfs3/fsntfs.c
+++ fs/ntfs3/fsntfs.c
@@ -938,7 +938,7 @@ int ntfs_set_state(struct ntfs_sb_info *sbi, enum NTFS_DIRTY_FLAGS dirty)
 	case NTFS_DIRTY_ERROR:
 		ntfs_notice(sbi->sb, "Mark volume as dirty due to NTFS errors");
 		sbi->volume.real_dirty = true;
-		fallthrough;
+		/* fall through */
 	case NTFS_DIRTY_DIRTY:
 		info->flags |= VOLUME_FLAG_DIRTY;
 		break;
