11d50ada6df4d3fceb081b75623406d56aa1f9b3
Jebaitedneko <Jebaitedneko@gmail.com>
2021-09-06 19:23:17 +0530

    fs: ntfs3: fallback to iov_iter_copy_from_user_atomic()
    upstream commit: https://github.com/torvalds/linux/commit/f0b65f39ac505e8f1dcdaa165aa7b8c0bd6fd454
    
    we are not ready for this yet
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- fs/ntfs3/file.c
+++ fs/ntfs3/file.c
@@ -1026,8 +1026,8 @@ static ssize_t ntfs_compress_write(struct kiocb *iocb, struct iov_iter *from)
 			size_t cp, tail = PAGE_SIZE - off;
 
 			page = pages[ip];
-			cp = copy_page_from_iter_atomic(page, off,
-							min(tail, bytes), from);
+			cp = iov_iter_copy_from_user_atomic(page, from, off,
+							min(tail, bytes));
 			flush_dcache_page(page);
 
 			copied += cp;
