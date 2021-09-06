894f9d202c0c23f873927c928231bead5caf932d
psndna88 <psndna88@gmail.com>
2021-09-06 18:14:48 +0530

    gpu: adreno: kgsl: re-enable debugfs support without coresight & snapshot & restore sched parameters

--- drivers/gpu/msm/kgsl_debugfs.h
+++ drivers/gpu/msm/kgsl_debugfs.h
@@ -18,6 +18,21 @@
 struct kgsl_device;
 struct kgsl_process_private;
 
+#ifdef CONFIG_DEBUG_FS
+void kgsl_core_debugfs_init(void);
+void kgsl_core_debugfs_close(void);
+
+void kgsl_device_debugfs_init(struct kgsl_device *device);
+void kgsl_device_debugfs_close(struct kgsl_device *device);
+
+extern struct dentry *kgsl_debugfs_dir;
+static inline struct dentry *kgsl_get_debugfs_dir(void)
+{
+	return kgsl_debugfs_dir;
+}
+
+void kgsl_process_init_debugfs(struct kgsl_process_private *);
+#else
 static inline void kgsl_core_debugfs_init(void) { }
 static inline void kgsl_device_debugfs_init(struct kgsl_device *device) { }
 static inline void kgsl_device_debugfs_close(struct kgsl_device *device) { }
@@ -26,5 +41,6 @@ static inline struct dentry *kgsl_get_debugfs_dir(void) { return NULL; }
 static inline void kgsl_process_init_debugfs(struct kgsl_process_private *priv)
 {
 }
+#endif
 
 #endif
