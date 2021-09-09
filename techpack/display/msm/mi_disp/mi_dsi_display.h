b86dd49da16d74bbe67c3d147389886d3e45bd4d
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-09 16:48:51 +0300

    techpack: display: implement mi_get_disp_id

---
+++ techpack/display/msm/mi_disp/mi_dsi_display.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2020 XiaoMi, Inc.
+ */
+
+#ifndef _MI_DSI_DISPLAY_H_
+#define _MI_DSI_DISPLAY_H_
+
+#include "dsi_display.h"
+
+int mi_get_disp_id(struct dsi_display *display);
+
+#endif /*_MI_DSI_DISPLAY_H_*/
