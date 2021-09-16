edf2f43511e82118a027e6e8c05522d275dbf263
Madan Koyyalamudi <mkoyya@codeaurora.org>
2021-09-16 02:45:34 -0700

    Release 2.0.8.24G
    Release 2.0.8.24G
    
    Change-Id: I406da0f31c63f1accf88aa843b0f52d6006ddf85
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            8
-#define QWLAN_VERSION_EXTRA            "F"
+#define QWLAN_VERSION_EXTRA            "G"
 #define QWLAN_VERSION_BUILD            24
 
-#define QWLAN_VERSIONSTR               "2.0.8.24F"
+#define QWLAN_VERSIONSTR               "2.0.8.24G"
 
 #endif /* QWLAN_VERSION_H */
