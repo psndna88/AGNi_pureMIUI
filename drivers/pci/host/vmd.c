09209d7be31a80e1b3ce0ac0a806ad340b3be089
psndna88 <psndna88@gmail.com>
2021-09-22 19:26:49 +0530

    PCI: sync with LA.UM.9.1r1 + 4.14.247

--- drivers/pci/host/vmd.c
+++ drivers/pci/host/vmd.c
@@ -756,12 +756,12 @@ static void vmd_remove(struct pci_dev *dev)
 	struct vmd_dev *vmd = pci_get_drvdata(dev);
 	struct fwnode_handle *fn = vmd->irq_domain->fwnode;
 
-	vmd_detach_resources(vmd);
 	sysfs_remove_link(&vmd->dev->dev.kobj, "domain");
 	pci_stop_root_bus(vmd->bus);
 	pci_remove_root_bus(vmd->bus);
 	vmd_cleanup_srcu(vmd);
 	vmd_teardown_dma_ops(vmd);
+	vmd_detach_resources(vmd);
 	irq_domain_remove(vmd->irq_domain);
 	irq_domain_free_fwnode(fn);
 }
