9506a807b12b69854d62d22e62441e4c5264b952
psndna88 <psndna88@gmail.com>
2021-10-05 23:50:26 +0530

    AGNi MIATOLL AOSP Q/R0 v11.0 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,8 +4,8 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v10.9.2"
-sed -i 's/v10.9.1-stable/v10.9.2-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+export AGNI_VERSION="v11.0"
+sed -i 's/v10.9.2-stable/v11.0-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
