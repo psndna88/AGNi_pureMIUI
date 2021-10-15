4e10e7c5ae0ee7bb6dcb1e2c123cc8f1ec79a9a1
Amit Mehta <amitmeht@codeaurora.org>
2021-10-15 15:11:58 +0530

    qcacmn: Update MPDU retries in case of successful transmission
    In case of successful transmission If MPDU tx
    is retried more than the threshold then
    increment MPDU retry count by transmit_cnt / threshold
    
    Change-Id: Idbc7e2d0579ff68e272132fe4ffe9f951c4aacf5
    CRs-Fixed: 3058464

--- dp/wifi3.0/dp_tx.c
+++ dp/wifi3.0/dp_tx.c
@@ -58,6 +58,8 @@
 /* invalid peer id for reinject*/
 #define DP_INVALID_PEER 0XFFFE
 
+#define DP_RETRY_COUNT 7
+
 /*mapping between hal encrypt type and cdp_sec_type*/
 #define MAX_CDP_SEC_TYPE 12
 static const uint8_t sec_type_map[MAX_CDP_SEC_TYPE] = {
@@ -4113,12 +4115,15 @@ dp_tx_update_peer_stats(struct dp_tx_desc_s *tx_desc,
 	DP_STATS_INCC(peer, tx.stbc, 1, ts->stbc);
 	DP_STATS_INCC(peer, tx.ldpc, 1, ts->ldpc);
 	DP_STATS_INCC(peer, tx.retries, 1, ts->transmit_cnt > 1);
-
 #if defined(FEATURE_PERPKT_INFO) && WDI_EVENT_ENABLE
 	dp_wdi_event_handler(WDI_EVENT_UPDATE_DP_STATS, pdev->soc,
 			     &peer->stats, ts->peer_id,
 			     UPDATE_PEER_STATS, pdev->pdev_id);
 #endif
+	if (ts->first_msdu)
+		DP_STATS_INCC(peer, tx.mpdu_success_with_retries,
+			      qdf_do_div(ts->transmit_cnt, DP_RETRY_COUNT),
+			      ts->transmit_cnt > DP_RETRY_COUNT);
 }
 
 #ifdef QCA_LL_TX_FLOW_CONTROL_V2
