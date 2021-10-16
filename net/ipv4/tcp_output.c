e9d263d71a251d964d130a1b875413eed7b33209
andip71 <andreasp@gmx.de>
2021-10-17 02:55:47 +0530

    tcp_output: set initial TCP window size to 64K (speed improvement)
    Signed-off-by: andip71 <andreasp@gmx.de>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -246,6 +246,10 @@ void tcp_select_initial_window(const struct sock *sk, int __space, __u32 mss,
 		*rcv_wscale = clamp_t(int, ilog2(space) - 15,
 				      0, TCP_MAX_WSCALE);
 	}
+
+	/* Lock the initial TCP window size to 64K*/
+	*rcv_wnd = 64240;
+
 	/* Set the clamp no higher than max representable value */
 	(*window_clamp) = min_t(__u32, U16_MAX << (*rcv_wscale), *window_clamp);
 }
