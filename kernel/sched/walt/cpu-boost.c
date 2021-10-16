9d1375162fee2b0a527bd5567a6d0095ed04b795
psndna88 <psndna88@gmail.com>
2021-10-17 03:12:17 +0530

    sched: disable touch input boosting

--- kernel/sched/walt/cpu-boost.c
+++ kernel/sched/walt/cpu-boost.c
@@ -244,7 +244,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 {
 	u64 now;
 
-	if (!input_boost_enabled)
+//	if (!input_boost_enabled)
 		return;
 
 	now = ktime_to_us(ktime_get());
