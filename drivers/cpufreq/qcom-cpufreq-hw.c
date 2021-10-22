6040c376d55e4561da25ccd11a0b6628bd8319ab
Mike Tipton <mdtipton@codeaurora.org>
2021-10-21 20:17:48 -0700

    cpufreq: qcom-hw: Reduce limits polling delay
    It was suggested that 10ms is an unnecessarily long delay and that 4ms
    is better aligned to our scheduler and may improve performance.
    
    Change-Id: I149c55fe000d058fc6b8b26398816492ce703538
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -31,7 +31,7 @@
 #define CLK_HW_DIV			2
 #define GT_IRQ_STATUS			BIT(2)
 #define MAX_FN_SIZE			20
-#define LIMITS_POLLING_DELAY_MS		10
+#define LIMITS_POLLING_DELAY_MS		4
 #define MAX_ROW				2
 
 #define CYCLE_CNTR_OFFSET(core_id, m, acc_count)				\
