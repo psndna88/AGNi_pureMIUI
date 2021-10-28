fe85b9eda5bc18d461f76896b9ced38ba2396b34
psndna88 <psndna88@gmail.com>
2021-10-28 06:51:37 +0530

    module: option to fake success on loading

--- kernel/module.c
+++ kernel/module.c
@@ -3746,7 +3746,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
 	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 #endif
-#ifdef CONFIG_MODULE_FAKE_LOAD
+#ifdef CONFIG_MODULE_FAKE_LOAD_SUCCESS_WITHOUT_LOADING
 	return 0;
 #endif
 	err = module_sig_check(info, flags);
