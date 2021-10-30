b32de4b1c06354a679bf10749d96eb0273dda953
psndna88 <psndna88@gmail.com>
2021-10-30 16:17:36 +0530

    techpack: display: disable logging

--- techpack/display/msm/sde_dbg.h
+++ techpack/display/msm/sde_dbg.h
@@ -80,8 +80,7 @@ enum sde_dbg_dump_context {
  */
 #define SDE_REG_LOG_RSCC    33
 
-#define SDE_EVTLOG_DEFAULT_ENABLE (SDE_EVTLOG_CRITICAL | SDE_EVTLOG_IRQ | \
-		SDE_EVTLOG_EXTERNAL)
+#define SDE_EVTLOG_DEFAULT_ENABLE 0
 
 /*
  * evtlog will print this number of entries when it is called through
