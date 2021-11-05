7c994d48325c0a9bfedc790a7db1f559f7fdb08a
DevriesL <therkduan@gmail.com>
2021-11-05 13:49:11 +0800

    techpack: display: Add get_main_display

--- techpack/display/msm/dsi/dsi_display.h
+++ techpack/display/msm/dsi/dsi_display.h
@@ -799,4 +799,6 @@ int dsi_display_dump_clks_state(struct dsi_display *display);
  */
 void dsi_display_dfps_update_parent(struct dsi_display *display);
 
+struct dsi_display *get_main_display(void);
+
 #endif /* _DSI_DISPLAY_H_ */
