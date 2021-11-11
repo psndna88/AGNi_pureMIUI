416527be42f2c7bdd0555096f69dabf20fa212e9
Shreyas K K <shrekk@codeaurora.org>
2021-11-11 09:55:35 +0530

    devfreq: Fix PM callbacks to support zero frequency
    It is perfectly valid for a device to have a zero frequency, usually
    it denotes a power collapse. To support this, fix the PM suspend
    callback to allow the suspend_freq to be zero.
    
    Change-Id: Id10f95b59b219e82e0695f80361aa6b35cdc5137
    Signed-off-by: Shreyas K K <shrekk@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -319,7 +319,7 @@ static int devfreq_set_target(struct devfreq *devfreq, unsigned long new_freq,
 
 	devfreq->previous_freq = new_freq;
 
-	if (devfreq->suspend_freq)
+	if (devfreq->suspend_freq >= 0)
 		devfreq->resume_freq = new_freq;
 
 	return err;
@@ -914,7 +914,7 @@ int devfreq_suspend_device(struct devfreq *devfreq)
 			return ret;
 	}
 
-	if (devfreq->suspend_freq) {
+	if (devfreq->suspend_freq >= 0) {
 		mutex_lock(&devfreq->lock);
 		ret = devfreq_set_target(devfreq, devfreq->suspend_freq, 0);
 		mutex_unlock(&devfreq->lock);
