5067044cfc5e7f1a62e7f90fc9abf552dcfc35eb
Tyler Nijmeh <tylernij@gmail.com>
2021-11-13 00:14:20 +0100

    qcacld-3.0: Do not force the compilation of DFS debugging
    Change-Id: I1b46f13ed3913913eff9b37a4bd1336a1073285b
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/staging/qca-wifi-host-cmn/umac/dfs/core/src/misc/dfs_nol.c
+++ drivers/staging/qca-wifi-host-cmn/umac/dfs/core/src/misc/dfs_nol.c
@@ -486,6 +486,7 @@ static os_timer_func(dfs_remove_from_nol)
 #endif
 #endif
 
+#ifdef DFS_DEBUG
 void dfs_print_nol(struct wlan_dfs *dfs)
 {
 	struct dfs_nolelem *nol;
@@ -513,6 +514,7 @@ void dfs_print_nol(struct wlan_dfs *dfs)
 		nol = nol->nol_next;
 	}
 }
+#endif
 
 void dfs_print_nolhistory(struct wlan_dfs *dfs)
 {
