0cc8a490ce9adb716da0bf09304bf2efa4fe0d29
Karthik Kantamneni <quic_vkantamn@quicinc.com>
2021-11-23 15:56:00 +0530

    qcacld-3.0: Control netif sub queues with sub queue pause mask
    Currently global pause mask is used for controlling individual
    AC based sub queues and even all the network queues, due to this
    mask is set/reset improperly which is not giving actual global
    queue status representation. To avoid this introduced new sub queue
    mask which is used while controlling individual network sub queues
    and global mask is over for overall network queues.
    
    Change-Id: I6632a1831cbea84c23441dbb67473c94b487109a
    CRs-Fixed: 3081060

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -1451,6 +1452,7 @@ struct hdd_adapter {
 
 	/* BITMAP indicating pause reason */
 	uint32_t pause_map;
+	uint32_t subqueue_pause_map;
 	spinlock_t pause_map_lock;
 	qdf_time_t start_time;
 	qdf_time_t last_time;
