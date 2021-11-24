e226224030d3792e271998de3b86932b02ea4de8
psndna88 <psndna88@gmail.com>
2021-11-24 13:34:48 +0530

    AGNi MIATOLL MIUI R v11.4 stable

--- cleanbuild.sh
+++ cleanbuild.sh
@@ -13,7 +13,9 @@ fi
 
 echo "`rm -rf $COUT/*`" > /dev/null
 if [ -f $COUT/.config ]; then
-	echo "`rm -rf $COUT/.*`" > /dev/null
+	rm -rf $COUT/.*
+	rm -rf $COUT/.config
+	rm -rf $COUT/.thinlto-cache
 fi
 
 echo "   Compile folder EMPTY !"
@@ -22,4 +24,5 @@ echo "   Compile folder EMPTY !"
 export CCACHE_SDM660="0"
 export CCACHE_MIATOLL_Q="0"
 export CCACHE_MIATOLL_R="0"
+export CCACHE_HAYDN="0"
 . ~/WORKING_DIRECTORY/ccache_shifter.sh
