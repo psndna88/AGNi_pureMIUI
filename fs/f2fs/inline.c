8744c57ac691d5ea14a9ccb4bd1306dfed3ed503
psndna88 <psndna88@gmail.com>
2021-11-28 00:31:01 +0530

    fs: f2fs: revert dev branch & rapidgc

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -186,13 +186,8 @@ int f2fs_convert_inline_inode(struct inode *inode)
 	struct page *ipage, *page;
 	int err = 0;
 
-	if (!f2fs_has_inline_data(inode) ||
-			f2fs_hw_is_readonly(sbi) || f2fs_readonly(sbi->sb))
+	if (!f2fs_has_inline_data(inode))
 		return 0;
-
-	err = dquot_initialize(inode);
-	if (err)
-		return err;
 
 	page = f2fs_grab_cache_page(inode->i_mapping, 0, false);
 	if (!page)
@@ -270,7 +265,7 @@ bool f2fs_recover_inline_data(struct inode *inode, struct page *npage)
 	 * [prev.] [next] of inline_data flag
 	 *    o       o  -> recover inline_data
 	 *    o       x  -> remove inline_data, and then recover data blocks
-	 *    x       o  -> remove data blocks, and then recover inline_data
+	 *    x       o  -> remove inline_data, and then recover inline_data
 	 *    x       x  -> recover data blocks
 	 */
 	if (IS_INODE(npage))
@@ -300,13 +295,11 @@ process_inline:
 		ipage = f2fs_get_node_page(sbi, inode->i_ino);
 		f2fs_bug_on(sbi, IS_ERR(ipage));
 		f2fs_truncate_inline_inode(inode, ipage, 0);
-		stat_dec_inline_inode(inode);
 		clear_inode_flag(inode, FI_INLINE_DATA);
 		f2fs_put_page(ipage, 1);
 	} else if (ri && (ri->i_inline & F2FS_INLINE_DATA)) {
 		if (f2fs_truncate_blocks(inode, 0, false))
 			return false;
-		stat_inc_inline_inode(inode);
 		goto process_inline;
 	}
 	return false;
@@ -525,7 +518,7 @@ static int f2fs_move_rehashed_dirents(struct inode *dir, struct page *ipage,
 			!f2fs_has_inline_xattr(dir))
 		F2FS_I(dir)->i_inline_xattr_size = 0;
 
-	kfree(backup_dentry);
+	kvfree(backup_dentry);
 	return 0;
 recover:
 	lock_page(ipage);
@@ -536,7 +529,7 @@ recover:
 	set_page_dirty(ipage);
 	f2fs_put_page(ipage, 1);
 
-	kfree(backup_dentry);
+	kvfree(backup_dentry);
 	return err;
 }
 
