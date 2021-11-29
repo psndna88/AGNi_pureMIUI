845c9771cf4458b1559f3e5022b89436b6ed6cf6
psndna88 <psndna88@gmail.com>
2021-11-29 19:09:23 +0530

    AGNi MIATOLL AOSP R Kali NetHunter v11.5.1 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,8 +4,8 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v11.5"
-sed -i 's/v11.4-stable/v11.5-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+export AGNI_VERSION="v11.5.1"
+sed -i 's/v11.5-stable/v11.5.1-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
