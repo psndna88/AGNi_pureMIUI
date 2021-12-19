297402d1717abc62fe6e7b01c3092d9e0d7add26
psndna88 <psndna88@gmail.com>
2021-12-20 00:46:02 +0530

    cnss2: disable debug logging

--- drivers/net/wireless/cnss2/debug.h
+++ drivers/net/wireless/cnss2/debug.h
@@ -15,65 +15,25 @@
 extern void *cnss_ipc_log_context;
 extern void *cnss_ipc_log_long_context;
 
-#define cnss_ipc_log_string(_x...)					\
-	ipc_log_string(cnss_ipc_log_context, _x)
-
-#define cnss_ipc_log_long_string(_x...)					\
-	ipc_log_string(cnss_ipc_log_long_context, _x)
-#else
 #define cnss_ipc_log_string(_x...)
 #define cnss_ipc_log_long_string(_x...)
 #endif
 
-#define cnss_pr_err(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_ERR, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
-	} while (0)
+#define cnss_pr_err(_fmt, ...)
 
-#define cnss_pr_warn(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_WARNING, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
-	} while (0)
+#define cnss_pr_warn(_fmt, ...)
 
-#define cnss_pr_info(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_INFO, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
-	} while (0)
+#define cnss_pr_info(_fmt, ...)
 
-#define cnss_pr_dbg(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_DEBUG, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
-	} while (0)
+#define cnss_pr_dbg(_fmt, ...)
 
-#define cnss_pr_vdbg(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_DEBUG, ##__VA_ARGS__);	\
-		cnss_ipc_log_long_string("%scnss: " _fmt, "",		\
-					 ##__VA_ARGS__);		\
-	} while (0)
+#define cnss_pr_vdbg(_fmt, ...)
 
-#define cnss_pr_buf(_fmt, ...)						\
-	cnss_ipc_log_long_string("%scnss: " _fmt, "", ##__VA_ARGS__)	\
+#define cnss_pr_buf(_fmt, ...)
 
-#ifdef CONFIG_CNSS2_DEBUG
-#define CNSS_ASSERT(_condition) do {					\
-		if (!(_condition)) {					\
-			cnss_pr_err("ASSERT at line %d\n",		\
-				    __LINE__);				\
-			BUG();						\
-		}							\
-	} while (0)
-#else
-#define CNSS_ASSERT(_condition) do {					\
-		if (!(_condition)) {					\
-			cnss_pr_err("ASSERT at line %d\n",		\
-				    __LINE__);				\
-			WARN_ON(1);					\
-		}							\
-	} while (0)
-#endif
+#define CNSS_ASSERT(_condition)
 
-#define cnss_fatal_err(_fmt, ...)					\
-	cnss_pr_err("fatal: " _fmt, ##__VA_ARGS__)
+#define cnss_fatal_err(_fmt, ...)
 
 int cnss_debug_init(void);
 void cnss_debug_deinit(void);
