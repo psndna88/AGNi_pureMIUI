877f06e30d1677f945e0536be1c75ccb01dfa2e4
psndna88 <psndna88@gmail.com>
2021-12-31 03:35:18 +0530

    misc: implement mi_memory from zeus-s-oss
    Requirement for MIUI, xiaomi never provided us sources for this kernel module till now!

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -1542,4 +1542,7 @@ static inline void ufshcd_vops_remove_debugfs(struct ufs_hba *hba)
 }
 #endif
 
+extern int ufshcd_read_desc_param(struct ufs_hba *hba, enum desc_idn desc_id,
+				  int desc_index, u8 param_offset,
+				  u8 *param_read_buf, u8 param_size);
 #endif /* End of Header */
