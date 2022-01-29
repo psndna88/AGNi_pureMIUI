8c214b552c159add4c7b65a4f5be9f02c47be56d
psndna88 <psndna88@gmail.com>
2022-01-29 12:37:44 +0530

    mm: hardwire force page_cluster = 1 for zram

--- mm/swap.c
+++ mm/swap.c
@@ -42,7 +42,8 @@
 #include <trace/events/pagemap.h>
 
 /* How many pages do we try to swap or page in/out together? */
-int page_cluster;
+int page_cluster = 0;
+int user_page_cluster = 3;
 
 static DEFINE_PER_CPU(struct pagevec, lru_add_pvec);
 static DEFINE_PER_CPU(struct pagevec, lru_rotate_pvecs);
@@ -1073,6 +1074,9 @@ void __init swap_setup(void)
 {
 	unsigned long megs = totalram_pages() >> (20 - PAGE_SHIFT);
 
+#if defined(CONFIG_ZRAM) || defined(CONFIG_ZSWAP)
+	page_cluster = 1;
+#else
 	/* Use a smaller cluster for small-memory machines */
 	if (megs < 16)
 		page_cluster = 2;
@@ -1082,4 +1086,5 @@ void __init swap_setup(void)
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
 	 */
+#endif
 }
