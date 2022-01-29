8518576276c87aa092611b4463424d9dc38dba36
psndna88 <psndna88@gmail.com>
2022-01-29 17:40:11 +0530

    AGNi MIUI-R v1.0 beta3

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta2"
+export AGNI_VERSION_PREFIX="beta3"
 export AGNI_VERSION="v1.0"
-sed -i 's/v1.0-beta1/v1.0-beta2/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v1.0-beta2/v1.0-beta3/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
