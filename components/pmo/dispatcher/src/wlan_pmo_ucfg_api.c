fc563469de52fa14188855d677b56d1113b7d4e2
Amit Mehta <quic_amitmeht@quicinc.com>
2022-02-13 15:55:23 +0530

    qcacld-3.0: Print wow_nack reason code
    Currently, Fw did not set NACK reason code
    to Host when Fw is not ready for WoW.
    
    As part of the new change, Fw will send NACK
    reason code along with WoW NACK to host.
    Make changes in host to store and print
    NACK reason code.
    
    Change-Id: Ic978af9572c8de37df3fb926083aac5e6ddefda7
    CRs-Fixed: 3481177

--- components/pmo/dispatcher/src/wlan_pmo_ucfg_api.c
+++ components/pmo/dispatcher/src/wlan_pmo_ucfg_api.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -486,9 +487,11 @@ int ucfg_pmo_psoc_clear_target_wake_up(struct wlan_objmgr_psoc *psoc)
 	return pmo_core_psoc_clear_target_wake_up(psoc);
 }
 
-void ucfg_pmo_psoc_target_suspend_acknowledge(void *context, bool wow_nack)
+void ucfg_pmo_psoc_target_suspend_acknowledge(void *context, bool wow_nack,
+					      uint16_t reason_code)
 {
-	pmo_core_psoc_target_suspend_acknowledge(context, wow_nack);
+	pmo_core_psoc_target_suspend_acknowledge(context, wow_nack,
+						 reason_code);
 }
 
 void ucfg_pmo_psoc_wakeup_host_event_received(struct wlan_objmgr_psoc *psoc)
