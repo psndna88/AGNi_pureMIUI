fb552fcf0bae55907ad637207e0c5df1722ca5d0
E V Ravi <quic_evenka@quicinc.com>
2022-02-22 11:54:20 +0530

    asoc: 24 and 32bit format support for ASM loopback and playback
    Add support for 24 and 32bit format for ASM loopback and playback
    session.
    
    Change-Id: Ieb6a6b2328fbe5d84b5b685ecc08adcd6638353c
    Signed-off-by: E V Ravi <quic_evenka@quicinc.com>

--- asoc/msm-pcm-loopback-v2.c
+++ asoc/msm-pcm-loopback-v2.c
@@ -2,6 +2,9 @@
 /* Copyright (c) 2013-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
+/*
+ * Add support for 24 and 32bit format for ASM loopback and playback session.
+ */
 
 #include <linux/init.h>
 #include <linux/err.h>
@@ -302,6 +305,7 @@ static int msm_pcm_open(struct snd_pcm_substream *substream)
 	struct asm_session_mtmx_strtr_param_window_v2_t asm_mtmx_strtr_window;
 	uint32_t param_id;
 	struct msm_pcm_pdata *pdata;
+	uint16_t format = 0;
 
 	if (!component) {
 		pr_err("%s: component is NULL\n", __func__);
@@ -349,6 +353,23 @@ static int msm_pcm_open(struct snd_pcm_substream *substream)
 		pcm->audio_client->fedai_id = rtd->dai_link->id;
 		pcm->audio_client->perf_mode = pdata->perf_mode;
 		pcm->audio_client->stream_type = substream->stream;
+
+		format = msm_pcm_asm_cfg_get(rtd->dai_link->id, MSM_ASM_LOOPBACK_MODE);
+		switch (format) {
+		case SNDRV_PCM_FORMAT_S32_LE:
+			bits_per_sample = 32;
+			break;
+		case SNDRV_PCM_FORMAT_S24_LE:
+		case SNDRV_PCM_FORMAT_S24_3LE:
+			bits_per_sample = 24;
+			break;
+		case SNDRV_PCM_FORMAT_S16_LE:
+		default:
+			bits_per_sample = 16;
+		}
+		pr_debug("%s: fe_id:%d, bits_per_sample:%d\n",__func__,
+			rtd->dai_link->id,bits_per_sample);
+
 		ret = q6asm_open_loopback_with_retry(pcm->audio_client,
 					bits_per_sample);
 		pcm->session_id = pcm->audio_client->session;
