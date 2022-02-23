4915b5f7f50580913485a03f3e2bd8ae315ca06f
Sheenam Monga <quic_shemonga@quicinc.com>
2022-02-23 22:03:20 +0530

    qcacld-3.0: Add TPC Report in probe response
    Currently, TPCReport IE is populated for beacon2 but
    is not populated for probe response. As per Table 9-34
    TPC report should be present in probe response.
    
    Fix is to populate TPC Report IE for probe response from
    beacon2.
    
    Change-Id: I1b384ef71ca99e0144374885cafcd864375a3b58
    CRs-Fixed: 3128799

--- core/mac/src/pe/sch/sch_beacon_gen.c
+++ core/mac/src/pe/sch/sch_beacon_gen.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -937,6 +938,14 @@ void lim_update_probe_rsp_template_ie_bitmap_beacon2(struct mac_context *mac,
 		qdf_mem_copy((void *)&prb_rsp->he_6ghz_band_cap,
 			     (void *)&beacon2->he_6ghz_band_cap,
 			     sizeof(beacon2->he_6ghz_band_cap));
+	}
+
+	if (beacon2->TPCReport.present) {
+		set_probe_rsp_ie_bitmap(DefProbeRspIeBitmap,
+					WLAN_ELEMID_TPCREP);
+		qdf_mem_copy((void *)&prb_rsp->TPCReport,
+			     (void *)&beacon2->TPCReport,
+			     sizeof(beacon2->TPCReport));
 	}
 
 }
