0c20bca0d7b9e33dbb20baa00c8de06dab8f9c7d
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-26 22:03:23 +0530

    drm/msm/sde: Remove debug print from sde_reg_write()
    This unused debug print wastes CPU time when writing to registers,
    resulting in perf top reporting a decent chunk of time spent inside
    sde_reg_write(). Removing the debug print gets sde_reg_write() off perf
    top's radar.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- techpack/display/msm/sde/sde_hw_util.c
+++ techpack/display/msm/sde/sde_hw_util.c
@@ -64,15 +64,12 @@ static u32 sde_hw_util_log_mask = SDE_DBG_MASK_NONE;
 #define QSEED4_DEFAULT_PRELOAD_V 0x2
 #define QSEED4_DEFAULT_PRELOAD_H 0x4
 
-#define GET_REG_BLK_ID(c) (c->log_mask ? (ilog2(c->log_mask) + 1) : 0)
 typedef void (*scaler_lut_type)(struct sde_hw_blk_reg_map *,
 		struct sde_hw_scaler3_cfg *, u32);
 
 void sde_reg_write(struct sde_hw_blk_reg_map *c, u32 reg_off, u32 val)
 {
-	SDE_EVT32_REGWRITE(c->blk_off + reg_off, val, GET_REG_BLK_ID(c));
 	writel_relaxed(val, c->base_off + c->blk_off + reg_off);
-	SDE_REG_LOG(GET_REG_BLK_ID(c), val, c->blk_off + reg_off);
 }
 
 int sde_reg_read(struct sde_hw_blk_reg_map *c, u32 reg_off)
