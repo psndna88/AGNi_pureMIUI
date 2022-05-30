d548fc234adb47d71c219901d89a87df6c5fcc41
psndna88 <psndna88@gmail.com>
2022-05-30 19:25:33 +0530

    AGNi AOSP-S v1.0 beta16

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta15"
+export AGNI_VERSION_PREFIX="beta16"
 export AGNI_VERSION="v1.0"
-sed -i 's/v1.0-beta14/v1.0-beta15/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v1.0-beta15/v1.0-beta16/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
