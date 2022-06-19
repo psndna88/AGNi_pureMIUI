e5e43ee110e79c49381346e86de41ddbdc37da3b
psndna88 <psndna88@gmail.com>
2022-06-19 17:16:00 +0530

    AGNi AOSP-ST v1.0 beta27
    update stock_defconfig with MIUI 13 A12 haydn

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta26"
+export AGNI_VERSION_PREFIX="beta27"
 export AGNI_VERSION="v1.0"
-sed -i 's/v1.0-beta25/v1.0-beta26/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v1.0-beta26/v1.0-beta27/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
--- build_kernel_haydn.sh
+++ build_kernel_haydn.sh
@@ -6,7 +6,7 @@ KERNELDIR=`readlink -f .`
 
 DEVICE="haydn"
 CONFIG1="agni_haydn_defconfig"
-export AGNI_BUILD_TYPE="AOSP-S"
+export AGNI_BUILD_TYPE="AOSP-ST"
 SYNC_CONFIG=1
 
 . $KERNELDIR/AGNi_version.sh
