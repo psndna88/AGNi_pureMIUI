3adeb688553ab5554c0528c9c46f3a1b9c3ff88a
psndna88 <psndna88@gmail.com>
2022-07-04 20:57:36 +0530

    AGNi AOSP-ST v1.3 stable
    set westwood + fq_codel as default
    using fq as default for bbr/bbr2 breaks vowifi

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v1.2"
+export AGNI_VERSION="v1.3"
 sed -i 's/5.4.202/5.4.203/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v1.1-stable/v1.2-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v1.2-stable/v1.3-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
