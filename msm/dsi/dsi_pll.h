ed8c317940ac7643522ff5e68951a2835c924a1d
Naresh Kumar Lingagalla <quic_nlingaga@quicinc.com>
2022-07-07 10:11:26 +0530

    Revert "disp: msm: dsi: Add DSI PLL support for 7nm_v4_1 arch"
    This reverts commit a965033fba11266d97a84465a79c5ac63da296e5.
    
    Change-Id: Ifb3d9b1a10a28cd281459174a9291fea79ec781d
    Signed-off-by: Naresh Kumar Lingagalla <quic_nlingaga@quicinc.com>

--- msm/dsi/dsi_pll.h
+++ msm/dsi/dsi_pll.h
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
  * Copyright (c) 2020, The Linux Foundation. All rights reserved.
  */
 
@@ -56,7 +54,6 @@ struct lpfr_cfg {
 
 enum {
 	DSI_PLL_5NM,
-	DSI_PLL_7NM_V4_1,
 	DSI_PLL_10NM,
 	DSI_UNKNOWN_PLL,
 };
