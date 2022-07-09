6db2c0e997d83a0ab0eeb64efbb381c67ac319fb
psndna88 <psndna88@gmail.com>
2022-07-09 17:23:20 +0530

    AGNi MIATOLL MIUI Q v14.1 beta4

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta3"
+export AGNI_VERSION_PREFIX="beta4"
 export AGNI_VERSION="v14.1"
-sed -i 's/v14.1-beta2/v14.1-beta3/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v14.1-beta3/v14.1-beta4/' $KERNELDIR/arch/arm64/configs/agni_*
 sed -i 's/4.14.286/4.14.287/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
