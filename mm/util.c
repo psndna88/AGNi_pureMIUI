2d522a9605625a4b6540708ad9ba719672558a62
psndna88 <psndna88@gmail.com>
2022-07-09 15:16:40 +0530

    Revert "mm: Always overcommit memory by default"
    This reverts commit a8993f6b19a6c5ccf571b0f45554515235750923.

--- mm/util.c
+++ mm/util.c
@@ -534,7 +534,7 @@ int __page_mapcount(struct page *page)
 EXPORT_SYMBOL_GPL(__page_mapcount);
 
 int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_ALWAYS;
-int sysctl_overcommit_ratio __read_mostly = 100;
+int sysctl_overcommit_ratio __read_mostly = 50;
 unsigned long sysctl_overcommit_kbytes __read_mostly;
 int sysctl_max_map_count __read_mostly = DEFAULT_MAX_MAP_COUNT;
 unsigned long sysctl_user_reserve_kbytes __read_mostly = 1UL << 17; /* 128MB */
