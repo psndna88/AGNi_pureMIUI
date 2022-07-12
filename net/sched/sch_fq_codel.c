9d38e0f966d293fc48e6be67304ff55312f689be
psndna88 <psndna88@gmail.com>
2022-07-12 14:53:03 +0530

    net: sched: reduce fq_codel flows_cnt value
    should be 1/8 of limit

--- net/sched/sch_fq_codel.c
+++ net/sched/sch_fq_codel.c
@@ -465,7 +465,7 @@ static int fq_codel_init(struct Qdisc *sch, struct nlattr *opt)
 	int err;
 
 	sch->limit = 1*1024;
-	q->flows_cnt = 1024;
+	q->flows_cnt = 128;
 	q->memory_limit = 32 << 20; /* 32 MBytes */
 	q->drop_batch_size = 64;
 	q->quantum = psched_mtu(qdisc_dev(sch));
