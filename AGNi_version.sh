1e4404e9cd22a391d3507f74d4d3c568b8f2ba07
psndna88 <psndna88@gmail.com>
2022-08-03 10:47:54 +0530

    AGNi MIUI-S v1.8 beta2

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,10 +3,10 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta1"
+export AGNI_VERSION_PREFIX="beta2"
 export AGNI_VERSION="v1.8"
-sed -i 's/5.4.207/5.4.208/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v1.7-stable/v1.8-beta1/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/5.4.208/5.4.209/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v1.8-beta1/v1.8-beta2/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
