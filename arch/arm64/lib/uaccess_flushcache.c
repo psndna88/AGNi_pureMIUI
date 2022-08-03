397bc799a3380ffd2ff767299848cdd0c363b329
psndna88 <psndna88@gmail.com>
2022-08-04 02:27:34 +0530

    Revert "arm64: uaccess: simplify __copy_user_flushcache()"
    This reverts commit b23178c921a439ece1f2bf66e25bf89d52122566.

--- arch/arm64/lib/uaccess_flushcache.c
+++ arch/arm64/lib/uaccess_flushcache.c
@@ -30,7 +30,9 @@ unsigned long __copy_user_flushcache(void *to, const void __user *from,
 {
 	unsigned long rc;
 
-	rc = raw_copy_from_user(to, from, n);
+	uaccess_enable_not_uao();
+	rc = __arch_copy_from_user(to, from, n);
+	uaccess_disable_not_uao();
 
 	/* See above */
 	__clean_dcache_area_pop(to, n - rc);
