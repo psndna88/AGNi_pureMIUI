0c7366ba7a32ab331cb15bb4db957b9efefdb0cf
psndna88 <psndna88@gmail.com>
2022-08-08 12:07:55 +0530

    Supress IPC logging warning/debugs

--- drivers/thermal/msm-tsens.c
+++ drivers/thermal/msm-tsens.c
@@ -340,29 +340,35 @@ static int tsens_tm_probe(struct platform_device *pdev)
 	snprintf(tsens_name, sizeof(tsens_name), "tsens_%pa_0",
 					&tmdev->phys_addr_tm);
 
+#ifdef CONFIG_IPC_LOGGING
 	tmdev->ipc_log0 = ipc_log_context_create(IPC_LOGPAGES,
 							tsens_name, 0);
 	if (!tmdev->ipc_log0)
 		pr_err("%s : unable to create IPC Logging 0 for tsens %pa\n",
 					__func__, &tmdev->phys_addr_tm);
+#endif
 
 	snprintf(tsens_name, sizeof(tsens_name), "tsens_%pa_1",
 					&tmdev->phys_addr_tm);
 
+#ifdef CONFIG_IPC_LOGGING
 	tmdev->ipc_log1 = ipc_log_context_create(IPC_LOGPAGES,
 							tsens_name, 0);
 	if (!tmdev->ipc_log1)
 		pr_err("%s : unable to create IPC Logging 1 for tsens %pa\n",
 					__func__, &tmdev->phys_addr_tm);
+#endif
 
 	snprintf(tsens_name, sizeof(tsens_name), "tsens_%pa_2",
 					&tmdev->phys_addr_tm);
 
+#ifdef CONFIG_IPC_LOGGING
 	tmdev->ipc_log2 = ipc_log_context_create(IPC_LOGPAGES,
 							tsens_name, 0);
 	if (!tmdev->ipc_log2)
 		pr_err("%s : unable to create IPC Logging 2 for tsens %pa\n",
 					__func__, &tmdev->phys_addr_tm);
+#endif
 
 	list_add_tail(&tmdev->list, &tsens_device_list);
 	platform_set_drvdata(pdev, tmdev);
