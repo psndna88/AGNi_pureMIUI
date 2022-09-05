f2c22e870fb6fa5affee75e39cc63d82b1fd272b
psndna88 <psndna88@gmail.com>
2022-09-05 18:44:57 +0530

    Revert "BACKPORT: crypto: arch - conditionalize crypto api in arch glue for lib code"
    This reverts commit 47c8de47e12546b5f5e019927a10d8429c6095bf.

--- arch/x86/crypto/blake2s-glue.c
+++ arch/x86/crypto/blake2s-glue.c
@@ -108,14 +108,12 @@ static int __init blake2s_mod_init(void)
 			      XFEATURE_MASK_AVX512, NULL))
 		static_branch_enable(&blake2s_use_avx512);
 
-	return IS_REACHABLE(CONFIG_CRYPTO_HASH) ?
-		crypto_register_shashes(blake2s_algs,
-					ARRAY_SIZE(blake2s_algs)) : 0;
+	return crypto_register_shashes(blake2s_algs, ARRAY_SIZE(blake2s_algs));
 }
 
 static void __exit blake2s_mod_exit(void)
 {
-	if (IS_REACHABLE(CONFIG_CRYPTO_HASH) && boot_cpu_has(X86_FEATURE_SSSE3))
+	if (boot_cpu_has(X86_FEATURE_SSSE3))
 		crypto_unregister_shashes(blake2s_algs, ARRAY_SIZE(blake2s_algs));
 }
 
