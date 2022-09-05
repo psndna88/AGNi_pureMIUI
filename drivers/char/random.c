9a903f3d8ec419dd060ea8980346989b812717c7
psndna88 <psndna88@gmail.com>
2022-09-05 18:48:30 +0530

    Revert "UPSTREAM: random: add GRND_INSECURE to return best-effort non-cryptographic bytes"
    This reverts commit 1409928950e8dfdd794c6dd5583e456ca1da7216.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -2003,14 +2003,7 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 {
 	int ret;
 
-	if (flags & ~(GRND_NONBLOCK|GRND_RANDOM|GRND_INSECURE))
-		return -EINVAL;
-
-	/*
-	 * Requesting insecure and blocking randomness at the same time makes
-	 * no sense.
-	 */
-	if ((flags & (GRND_INSECURE|GRND_RANDOM)) == (GRND_INSECURE|GRND_RANDOM))
+	if (flags & ~(GRND_NONBLOCK|GRND_RANDOM))
 		return -EINVAL;
 
 	if (count > INT_MAX)
@@ -2019,7 +2012,7 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 	if (flags & GRND_RANDOM)
 		return _random_read(flags & GRND_NONBLOCK, buf, count);
 
-	if (!(flags & GRND_INSECURE) && !crng_ready()) {
+	if (!crng_ready()) {
 		if (flags & GRND_NONBLOCK)
 			return -EAGAIN;
 		ret = wait_for_random_bytes();
