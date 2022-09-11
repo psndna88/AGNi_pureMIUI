79b2a453e1facffab31191e8990a71d8cc967d65
psndna88 <psndna88@gmail.com>
2022-09-11 22:12:13 +0530

    AGNi MIATOLL AOSP RS v14.3 stable

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v14.2"
-sed -i 's/v14.1-stable/v14.2-stable/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/4.14.289/4.14.290/' $KERNELDIR/arch/arm64/configs/agni_*
+export AGNI_VERSION="v14.3"
+sed -i 's/v14.2-stable/v14.3-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/4.14.290/4.14.292/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
