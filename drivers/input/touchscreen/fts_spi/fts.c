97360f90ec050c8c80707c0b687139c1160660bc
Chenyang Zhong <zhongcy95@gmail.com>
2022-09-18 18:51:16 +0800

    fts: fix high polling rate after suspend & resume
    High polling rate stops working after turning the screen off and
    then turning it back on. It seems that the hardware (or firmware)
    has it disabled after suspend, but the mode is not updated correctly
    in the driver. As a result, after resume, high polling rate appears
    to be enabled, but the actual polling rate is stuck at 240Hz max.
    
    Therefore, reset the mode when going into suspend, and re-enable
    it after resume.
    
    Test: Touch sampling rate reaches 400Hz+ after suspend & resume
    
    * Hypeartist: Adapt to vili
    
    Co-authored-by: EcrosoftXiao <xy19990903@gmail.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -6730,6 +6730,9 @@ static void fts_resume_work(struct work_struct *work)
 #ifdef CONFIG_FACTORY_BUILD
 	int retval = 0;
 #endif
+#ifdef FTS_XIAOMI_TOUCHFEATURE
+	struct xiaomi_touch_pdata *pdata;
+#endif
 	info = container_of(work, struct fts_ts_info, resume_work);
 #ifndef CONFIG_FACTORY_BUILD
 	fts_disableInterrupt();
@@ -6770,6 +6773,19 @@ static void fts_resume_work(struct work_struct *work)
 		fts_palm_sensor_cmd(info->palm_sensor_switch);
 		info->palm_sensor_changed = true;
 	}
+	pdata = dev_get_drvdata(get_xiaomi_touch_dev());
+	if (pdata->bump_sample_rate) {
+		pr_info("%s: bump_sample_rate is true, re-enabling it\n",
+			__func__);
+		pdata->set_update = true;
+		xiaomi_touch_interfaces.setModeValue(0, 1);
+		xiaomi_touch_interfaces.setModeValue(1, 1);
+		xiaomi_touch_interfaces.setModeValue(3, 5);
+		xiaomi_touch_interfaces.setModeValue(2, 99);
+		xiaomi_touch_interfaces.setModeValue(7, 0);
+	} else {
+		pr_info("%s: bump_sample_rate is false\n", __func__);
+	}
 #endif
 
 #ifdef TOUCH_THP_SUPPORT
@@ -6795,6 +6811,9 @@ static void fts_suspend_work(struct work_struct *work)
 #ifdef CONFIG_FACTORY_BUILD
 	int retval = 0;
 #endif
+#ifdef FTS_XIAOMI_TOUCHFEATURE
+	struct xiaomi_touch_pdata *pdata;
+#endif
 
 	info = container_of(work, struct fts_ts_info, suspend_work);
 /*
@@ -6815,6 +6834,13 @@ static void fts_suspend_work(struct work_struct *work)
 		update_palm_sensor_value(0);
 		fts_palm_sensor_cmd(0);
 		info->palm_sensor_switch = false;
+	}
+	pdata = dev_get_drvdata(get_xiaomi_touch_dev());
+	if (pdata->bump_sample_rate) {
+		pr_info("%s: bump_sample_rate is true, resetting mode\n",
+			__func__);
+		pdata->set_update = false;
+		xiaomi_touch_interfaces.resetMode(0);
 	}
 #endif
 	fts_disableInterrupt();
