7f64837fbca89e56b3b8678674f6138332fc0f77
psndna88 <psndna88@gmail.com>
2022-12-04 19:08:03 +0530

    techpack: display: Merge CAF LA.UM.9.14.r1-20800-LAHAINA.QSSI13.0

--- techpack/display/msm/dsi/dsi_drm.c
+++ techpack/display/msm/dsi/dsi_drm.c
@@ -197,7 +197,9 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 		return;
 	}
 
-	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
+	if (bridge->encoder->crtc->state->active_changed)
+		atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
+
 	mi_cfg = &c_bridge->display->panel->mi_cfg;
 
 	power_mode = sde_connector_get_lp(c_bridge->display->drm_conn);
