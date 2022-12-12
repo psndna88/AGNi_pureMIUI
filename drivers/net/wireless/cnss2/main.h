699de1f0f02a223d429bfd1c3cedf2214f218660
Lin Bai <quic_lbai@quicinc.com>
2022-12-13 05:54:12 +0900

    cnss2: Include memory_dump.h when CONFIG_QCOM_MINIDUMP enabled
    MSM_DUMP_DATA_CNSS_WLAN not defined during compilation,
    if CONFIG_QCOM_MEMORY_DUMP_V2 disabled.
    To resolve it, also include memory_dump.h header as well,
    when CONFIG_QCOM_MINIDUMP enabled.
    
    Change-Id: I55fe3ccd606168fb48d6c3a129960f01f1f72a07
    Signed-off-by: Lin Bai <quic_lbai@quicinc.com>

--- drivers/net/wireless/cnss2/main.h
+++ drivers/net/wireless/cnss2/main.h
@@ -15,7 +15,7 @@
 #include <linux/pm_qos.h>
 #include <linux/platform_device.h>
 #include <net/cnss2.h>
-#if IS_ENABLED(CONFIG_QCOM_MEMORY_DUMP_V2)
+#if IS_ENABLED(CONFIG_QCOM_MEMORY_DUMP_V2) || IS_ENABLED(CONFIG_QCOM_MINIDUMP)
 #include <soc/qcom/memory_dump.h>
 #endif
 #if IS_ENABLED(CONFIG_MSM_SUBSYSTEM_RESTART) || \
