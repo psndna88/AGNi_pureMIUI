7af6a8b6a445f3941703a13a4bb9a5167f13f11e
Shalini Manjunatha <quic_c_shalma@quicinc.com>
2022-12-19 19:05:22 +0530

    asoc: wcd938x/wcd937x: add null pointer check for mbhc variable
    Whenever MBHC is disabled through kernel config file,
    particular vaiable of mbhc is not guarded for NULL
    pointer check causing kernel panic,
    Hence add NULL check for that pointer variable.
    
    Change-Id: Iae4950ceb076239a130b4f033cb2f08cd65a92a9
    Signed-off-by: Shalini Manjunatha <quic_c_shalma@quicinc.com>

--- asoc/codecs/wcd938x/wcd938x.c
+++ asoc/codecs/wcd938x/wcd938x.c
@@ -2161,8 +2161,9 @@ static int wcd938x_event_notify(struct notifier_block *block,
 						     NULL);
 		wcd938x->mbhc->wcd_mbhc.deinit_in_progress = true;
 		mbhc = &wcd938x->mbhc->wcd_mbhc;
-		wcd938x->usbc_hs_status = get_usbc_hs_status(component,
-						mbhc->mbhc_cfg);
+		if(mbhc->mbhc_cfg)
+			wcd938x->usbc_hs_status = get_usbc_hs_status(component,
+							mbhc->mbhc_cfg);
 		wcd938x_mbhc_ssr_down(wcd938x->mbhc, component);
 		wcd938x_reset_low(wcd938x->dev);
 		break;
@@ -2184,7 +2185,8 @@ static int wcd938x_event_notify(struct notifier_block *block,
 			dev_err(component->dev, "%s: mbhc initialization failed\n",
 				__func__);
 		} else {
-			wcd938x_mbhc_hs_detect(component, mbhc->mbhc_cfg);
+			if(mbhc->mbhc_cfg)
+				wcd938x_mbhc_hs_detect(component, mbhc->mbhc_cfg);
 		}
 		wcd938x->mbhc->wcd_mbhc.deinit_in_progress = false;
 		wcd938x->dev_up = true;
