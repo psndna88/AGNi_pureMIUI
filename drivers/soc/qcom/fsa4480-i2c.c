d2102096dd5eb74d7dd090953fbcf1e141ff172b
Giovanni Ricca <giovanniricca@proton.me>
2023-02-02 21:20:22 +0530

    misc: Import mmhardware detection
    From branch: redwood-s-oss
    
    Change-Id: I03d8e19d51e7519abb57450a67a18031f727529d

--- drivers/soc/qcom/fsa4480-i2c.c
+++ drivers/soc/qcom/fsa4480-i2c.c
@@ -12,6 +12,7 @@
 #include <linux/usb/ucsi_glink.h>
 #include <linux/soc/qcom/fsa4480-i2c.h>
 #include <linux/iio/consumer.h>
+#include <linux/mmhardware_others.h>
 
 #define FSA4480_I2C_NAME	"fsa4480-driver"
 
@@ -521,6 +522,9 @@ static int fsa4480_probe(struct i2c_client *i2c,
 		((fsa_priv->fsa4480_notifier).rwsem);
 	fsa_priv->fsa4480_notifier.head = NULL;
 
+#ifdef CONFIG_MMHARDWARE_OTHER_DETECTION
+	register_otherkobj_under_mmsysfs(MM_HW_AS, "audioswitch");
+#endif
 	return 0;
 
 err_supply:
