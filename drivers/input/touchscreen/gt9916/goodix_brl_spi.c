1be7dc9aa087cfc86770a3442cd9ea192c4d9907
Giovanni Ricca <giovanniricca@proton.me>
2023-02-12 13:39:29 +0100

    input: touchscreen: gt9916: Make it load on taoyao
    Change-Id: I4b6fb347508380cb13aac291f244bab5aa28e0b2

--- drivers/input/touchscreen/gt9916/goodix_brl_spi.c
+++ drivers/input/touchscreen/gt9916/goodix_brl_spi.c
@@ -291,6 +291,7 @@ static int goodix_spi_remove(struct spi_device *spi)
 #ifdef CONFIG_OF
 static const struct of_device_id spi_matchs[] = {
 	{.compatible = "xiaomi,gt9916r-spi",},
+	{.compatible = "xiaomi,l9-spi",},
 	{},
 };
 #endif
