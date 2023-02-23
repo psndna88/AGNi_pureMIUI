6a25d05577b7631a48194b656b33bddb2ded7e9e
Yojana Juadi <quic_yjuadi@quicinc.com>
2023-02-23 12:19:43 +0530

    disp: msm: sde: update idle_pc_duration based on frame rate
    This change updates the time required to enter idle_pc based
    on frame rate instead of default time. In the current issue,
    customer is facing janks where frame rate is 30fps and race
    happens between sde_encoder_off_work and drm_atomic_commit
    scheduled from userspace. It also sets max and min bound for
    optimized performance.
    
    Change-Id: I5e95e920a2f7b2142b5f63e8ce6b82cf1d482db1
    Signed-off-by: Yojana Juadi <quic_yjuadi@quicinc.com>

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -1593,8 +1593,10 @@ static void _sde_encoder_rc_restart_delayed(struct sde_encoder_virt *sde_enc,
 {
 	struct drm_encoder *drm_enc = &sde_enc->base;
 	struct msm_drm_private *priv;
-	unsigned int lp, idle_pc_duration;
+	unsigned int lp, idle_pc_duration, frame_time_ms, fps;
 	struct msm_drm_thread *disp_thread;
+	unsigned int min_duration = IDLE_POWERCOLLAPSE_DURATION;
+	unsigned int max_duration = IDLE_POWERCOLLAPSE_IN_EARLY_WAKEUP;
 
 	/* set idle timeout based on master connector's lp value */
 	if (sde_enc->cur_master)
@@ -1603,10 +1605,15 @@ static void _sde_encoder_rc_restart_delayed(struct sde_encoder_virt *sde_enc,
 	else
 		lp = SDE_MODE_DPMS_ON;
 
+	fps = sde_enc->mode_info.frame_rate;
 	if ((lp == SDE_MODE_DPMS_LP1) || (lp == SDE_MODE_DPMS_LP2))
 		idle_pc_duration = IDLE_SHORT_TIMEOUT;
-	else
-		idle_pc_duration = IDLE_POWERCOLLAPSE_DURATION;
+	else {
+		frame_time_ms = 1000;
+		do_div(frame_time_ms, fps);
+		idle_pc_duration = max(4 * frame_time_ms, min_duration);
+		idle_pc_duration = min(idle_pc_duration, max_duration);
+	}
 
 	priv = drm_enc->dev->dev_private;
 	disp_thread = &priv->disp_thread[sde_enc->crtc->index];
