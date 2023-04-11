e99b4683833ee939e63bc2c20526bb0e358a66eb
Sultan Alsawaf <sultan@osomprivacy.com>
2023-04-11 11:22:49 -0700

    msm: ipa3: Silence PM notifier log spam
    We don't need to know that PM notifiers are working. Silence the entry/exit
    log spam in ipa_pm_notify().
    
    Change-Id: I9bae89fa5e27b8835683ec1423ecc751491ea13c
    Signed-off-by: Sultan Alsawaf <sultan@osomprivacy.com>

--- techpack/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ techpack/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -456,7 +456,7 @@ EXPORT_SYMBOL(ipa_smmu_free_sgt);
 
 static int ipa_pm_notify(struct notifier_block *b, unsigned long event, void *p)
 {
-	IPAERR("Entry\n");
+	IPADBG("Entry\n");
 	switch (event) {
 		case PM_POST_SUSPEND:
 #ifdef CONFIG_DEEPSLEEP
@@ -468,7 +468,7 @@ static int ipa_pm_notify(struct notifier_block *b, unsigned long event, void *p)
 #endif
 			break;
 	}
-	IPAERR("Exit\n");
+	IPADBG("Exit\n");
 	return NOTIFY_DONE;
 }
 
