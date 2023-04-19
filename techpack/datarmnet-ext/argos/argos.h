c53215acc0ab8f797ec061766931be62bada2db1
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-19 15:18:22 +0600

    techpack/datarmnet-ext: import samsung rmnet argos extension
    This driver is meant to improve rmnet network throughput by
    efficiently boosting using pm_qos framework.
    
    Change-Id: Id6dd68fd7c0a188749a3add64c45916becaf075d
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ techpack/datarmnet-ext/argos/argos.h
@@ -0,0 +1,7 @@
+#ifndef _ARGOS_H
+#define _ARGOS_H
+
+extern int sec_argos_register_notifier(struct notifier_block *n, char *label);
+extern int sec_argos_unregister_notifier(struct notifier_block *n, char *label);
+
+#endif
