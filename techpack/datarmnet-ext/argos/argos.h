4b2e0d1ce2198fc2350edf129e92bbf318d9985a
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-19 21:53:24 +0600

    techpack/datarmnet-ext: argos: workaround notifier block init
    Init the driver independently instead of the samsung chain init
    
    Change-Id: Ib25fcb081c3c3ee3e2fee3177caa9ff9461af43d
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- techpack/datarmnet-ext/argos/argos.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ARGOS_H
-#define _ARGOS_H
-
-extern int sec_argos_register_notifier(struct notifier_block *n, char *label);
-extern int sec_argos_unregister_notifier(struct notifier_block *n, char *label);
-
-#endif
