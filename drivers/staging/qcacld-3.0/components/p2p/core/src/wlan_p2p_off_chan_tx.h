0a88763331dcaf45f571a136660bfa6f6a696271
psndna88 <psndna88@gmail.com>
2023-06-01 20:59:28 +0530

    qcacld-3.0: important changes

--- drivers/staging/qcacld-3.0/components/p2p/core/src/wlan_p2p_off_chan_tx.h
+++ drivers/staging/qcacld-3.0/components/p2p/core/src/wlan_p2p_off_chan_tx.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2017-2019 The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
