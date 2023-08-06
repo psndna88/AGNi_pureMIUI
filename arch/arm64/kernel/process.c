6b3153670860c14c9ea7b47dbcfe2ffb9769a3bd
Murat Kozan <muratkozan350@gmail.com>
2023-08-06 16:50:58 +0300

    arm64: kernel: Use clang stackprotector without conditions
    * This caused an error when building AOSP:
        ld.lld: error: undefined __stack_chk_guard
    
     * Seems that this condition judgement is disabled when building kernel
       with AOSP build system. But our clang indeed has stackprotector
       support. I didn't found the reason. Add this workaround to make it
       build with AOSP.
    
     * This patch should be removed when custom rom kernel build task can
       properly detect our stackprotector feature.

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -57,7 +57,7 @@
 #include <asm/stacktrace.h>
 #include <trace/hooks/minidump.h>
 
-#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+#if 1
 #include <linux/stackprotector.h>
 unsigned long __stack_chk_guard __ro_after_init;
 EXPORT_SYMBOL(__stack_chk_guard);
