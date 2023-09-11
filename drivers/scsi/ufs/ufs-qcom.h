4c4ac64e9a58476a479514b991b8c928e36ffa84
EndCredits <endcredits@crepuscular-aosp.icu>
2023-09-11 15:01:47 +0800

    scsi: ufs: Deprecate Qualcomm UFS QOS implementations
     * Seriously?
    
     * Ideas comes from sultan
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- drivers/scsi/ufs/ufs-qcom.h
+++ drivers/scsi/ufs/ufs-qcom.h
@@ -8,7 +8,6 @@
 #include <linux/reset-controller.h>
 #include <linux/reset.h>
 #include <linux/phy/phy.h>
-#include <linux/pm_qos.h>
 #include "ufshcd.h"
 #ifdef CONFIG_SCSI_UFSHCD_QTI
 #include "unipro.h"
@@ -285,24 +284,10 @@ struct qcom_bus_scale_data {
 struct qos_cpu_group {
 	cpumask_t mask;
 	unsigned int *votes;
-	struct dev_pm_qos_request *qos_req;
 	bool voted;
 	struct work_struct vwork;
 	struct ufs_qcom_host *host;
 	unsigned int curr_vote;
-};
-
-struct ufs_qcom_qos_req {
-	struct qos_cpu_group *qcg;
-	unsigned int num_groups;
-	struct workqueue_struct *workq;
-};
-
-/* Check for QOS_POWER when added to DT */
-enum constraint {
-	QOS_PERF,
-	QOS_POWER,
-	QOS_MAX,
 };
 
 struct ufs_qcom_host {
@@ -377,7 +362,6 @@ struct ufs_qcom_host {
 	/* FlashPVL entries */
 	atomic_t scale_up;
 	atomic_t clks_on;
-	struct ufs_qcom_qos_req *ufs_qos;
 	bool bypass_g4_cfgready;
 	bool is_dt_pm_level_read;
 };
