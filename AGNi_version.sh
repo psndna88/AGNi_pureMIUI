95ac4fd0cf414fcb33bd9958f318a16cc1226099
psndna88 <psndna88@gmail.com>
2023-10-24 21:17:43 +0530

    AGNi MIUI-ST v5.0 stable [5.4.258]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v4.9"
+export AGNI_VERSION="v5.0"
 sed -i 's/5.4.257/5.4.258/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v4.8-stable/v4.9-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v4.9-stable/v5.0-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
