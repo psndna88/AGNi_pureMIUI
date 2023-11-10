a2245336807944bb5d12057f25825aae0d935c2a
psndna88 <psndna88@gmail.com>
2023-11-10 13:11:13 +0530

    Implement YAKT v12 tweaks in kernel sources
    credits to:
    @NotZeetaa
    RedHat
    tytydraco
    KTweak
    kdrag0n
    Arter97
    @darkhz
    
    set read only permissions for tweaked tunables

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -180,7 +180,7 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 100;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
