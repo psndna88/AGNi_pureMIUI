283f2839321bf5daf9f0da4f689c12f66fd5f09c
psndna88 <psndna88@gmail.com>
2024-03-24 05:20:04 +0530

    AGNi MIUI-ST v5.4 stable [5.4.272]
    add a 90Hz only display variant

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v5.6"
+export AGNI_VERSION="v5.7"
 sed -i 's/5.4.271/5.4.272/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v5.5-stable/v5.6-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v5.6-stable/v5.7-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
