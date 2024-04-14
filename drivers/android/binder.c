4ec7afb8692453dd93a15874b3639f8137afaa32
psndna88 <psndna88@gmail.com>
2024-04-14 15:43:49 +0530

    binder: allow BINDER_FREEZE support

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5536,7 +5536,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
-#if 0
 	case BINDER_FREEZE: {
 		struct binder_freeze_info info;
 		struct binder_proc **target_procs = NULL, *target_proc;
@@ -5615,7 +5614,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
-#endif
 	case BINDER_ENABLE_ONEWAY_SPAM_DETECTION: {
 		uint32_t enable;
 
