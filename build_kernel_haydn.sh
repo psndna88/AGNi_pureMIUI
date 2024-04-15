439ef2c339d71b2740241430dd28f1186d02cbd3
psndna88 <psndna88@gmail.com>
2024-04-15 17:25:33 +0530

    AGNi MIUI-STU v5.9 stable [5.4.243]

--- build_kernel_haydn.sh
+++ build_kernel_haydn.sh
@@ -6,7 +6,7 @@ KERNELDIR=`readlink -f .`
 
 DEVICE="haydn"
 CONFIG1="agni_haydn_defconfig"
-export AGNI_BUILD_TYPE="MIUI-ST"
+export AGNI_BUILD_TYPE="MIUI-STU"
 SYNC_CONFIG=1
 
 . $KERNELDIR/AGNi_version.sh
