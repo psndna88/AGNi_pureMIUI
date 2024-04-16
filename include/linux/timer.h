63f93e6b750aa2b3531f47a9d010b3492e4f6bf0
Greg Kroah-Hartman <gregkh@google.com>
2024-04-16 15:30:52 +0000

    Revert "timers: Rename del_timer_sync() to timer_delete_sync()" [5.4.274]
    This reverts commit df4209170b6c0ba2a485f33913c883096624d662 which is
    commit 9b13df3fb64ee95e2397585404e442afee2c7d4f upstream.
    
    It breaks the Android kernel abi by turning del_timer_sync() into an
    inline function, which breaks the abi.  Fix this by putting it back as
    needed AND fix up the only use of this new function in
    drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c which is
    what caused this commit to be backported to 5.4.274 in the first place.
    
    Bug: 161946584
    Change-Id: Icd26c7c81e6172f36eeeb69827989bfab1d32afe
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -195,20 +195,11 @@ extern int try_to_del_timer_sync(struct timer_list *timer);
 
 extern struct timer_base timer_base_deferrable;
 
-extern int timer_delete_sync(struct timer_list *timer);
-
-/**
- * del_timer_sync - Delete a pending timer and wait for a running callback
- * @timer:	The timer to be deleted
- *
- * See timer_delete_sync() for detailed explanation.
- *
- * Do not use in new code. Use timer_delete_sync() instead.
- */
-static inline int del_timer_sync(struct timer_list *timer)
-{
-	return timer_delete_sync(timer);
-}
+#if defined(CONFIG_SMP) || defined(CONFIG_PREEMPT_RT)
+  extern int del_timer_sync(struct timer_list *timer);
+#else
+# define del_timer_sync(t)		del_timer(t)
+#endif
 
 #define del_singleshot_timer_sync(t) del_timer_sync(t)
 
