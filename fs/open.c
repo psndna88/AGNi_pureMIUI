7b27eb034d4f2d3263a839dea56e4743ab9cfa86
psndna88 <psndna88@gmail.com>
2024-04-22 00:48:14 +0530

    fs: implement KernelSU patches
    https://kernelsu.org/guide/how-to-integrate-for-non-gki.html

--- fs/open.c
+++ fs/open.c
@@ -340,9 +340,10 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return ksys_fallocate(fd, mode, offset, len);
 }
 
-// KernelSU hook
+#ifdef CONFIG_KSU
 extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 			 int *flags);
+#endif
 
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
@@ -357,11 +358,13 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct inode *inode;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
 
-	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);  // call KSU hook first
 	override_cred = prepare_creds();
 	if (!override_cred)
 		return -ENOMEM;
