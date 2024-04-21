6ad5ac804cb852227c445c151a6e311385dc6b4c
psndna88 <psndna88@gmail.com>
2024-04-22 00:48:14 +0530

    fs: implement KernelSU patches
    https://kernelsu.org/guide/how-to-integrate-for-non-gki.html

--- fs/exec.c
+++ fs/exec.c
@@ -1924,11 +1924,25 @@ out_ret:
 	return retval;
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
