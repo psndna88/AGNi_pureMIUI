670be0a29424f70c42ff48b8bcfb3175c6a39ab5
psndna88 <psndna88@gmail.com>
2024-05-07 14:59:31 +0530

    Revert "techpack/display: Enforce ulps suspend"
    This reverts commit c7b396ec9d86b62eb4ec48f1a8b1f22f83b7a6ed.

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -2320,7 +2320,8 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	DSI_DEBUG("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
 
-	panel->ulps_suspend_enabled = true;
+	panel->ulps_suspend_enabled =
+		utils->read_bool(utils->data, "qcom,suspend-ulps-enabled");
 
 	DSI_DEBUG("%s: ulps during suspend feature %s\n", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
