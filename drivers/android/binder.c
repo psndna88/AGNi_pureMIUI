1f3cf740401e1d5caa7da05d027122e5513265e4
Michael Bestas <mkbestas@lineageos.org>
2024-06-10 22:17:07 +0530

    fixup! HACK: binder: Disable freeze related ioctls
    Change-Id: I08b4ada30307d43a20be1c89e81467b45cf36ee0

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5817,6 +5817,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
+#if 0
 	case BINDER_FREEZE: {
 		struct binder_freeze_info info;
 		struct binder_proc **target_procs = NULL, *target_proc;
@@ -5895,6 +5896,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	}
+#endif
 	case BINDER_ENABLE_ONEWAY_SPAM_DETECTION: {
 		uint32_t enable;
 
