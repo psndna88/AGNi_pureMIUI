2f8499baf8e2e2333d76af2e9048d3d78e231c5f
psndna88 <psndna88@gmail.com>
2024-06-26 22:53:11 +0530

    Revert "net: qrtr: resolve slab-out-of-bounds in qrtr_endpoint_register"
    This reverts commit 453dac049baf46a90f0440a6ad71a543183a80db.

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -194,7 +194,6 @@ struct qrtr_node {
 	void *ilc;
 
 	u32 nonwake_svc[MAX_NON_WAKE_SVC_LEN];
-	struct qrtr_array no_wake_svc;
 };
 
 struct qrtr_tx_flow_waiter {
@@ -423,7 +422,6 @@ static void __qrtr_node_release(struct kref *kref)
 	kthread_stop(node->task);
 
 	skb_queue_purge(&node->rx_queue);
-	kfree(node->no_wake_svc.arr);
 	kfree(node);
 }
 
@@ -947,12 +945,8 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 			if (svc_id > 0) {
 				for (i = 0; i < MAX_NON_WAKE_SVC_LEN; i++) {
 					if (svc_id == node->nonwake_svc[i]) {
-						for (i = 0; i < node->no_wake_svc.size; i++) {
-							if (svc_id == node->no_wake_svc.arr[i]) {
-								wake = false;
-								break;
-							}
-						}
+						wake = false;
+						break;
 					}
 				}
 			}
@@ -971,6 +965,7 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 
 		qrtr_port_put(ipc);
 	}
+
 	return 0;
 
 err:
@@ -1176,7 +1171,7 @@ static void qrtr_hello_work(struct kthread_work *work)
  * The specified endpoint must have the xmit function pointer set on call.
  */
 int qrtr_endpoint_register(struct qrtr_endpoint *ep, unsigned int net_id,
-			   bool rt, struct qrtr_array *svc_arr)
+			   bool rt, u32 *svc_arr)
 {
 	struct qrtr_node *node;
 	struct sched_param param = {.sched_priority = 1};
@@ -1209,12 +1204,7 @@ int qrtr_endpoint_register(struct qrtr_endpoint *ep, unsigned int net_id,
 
 	if (svc_arr)
 		memcpy(node->nonwake_svc, svc_arr, MAX_NON_WAKE_SVC_LEN * sizeof(int));
-	if (svc_arr && svc_arr->size) {
-		node->no_wake_svc.arr = kmalloc_array(svc_arr->size, sizeof(u32), GFP_KERNEL);
-		memcpy((void *)node->no_wake_svc.arr, (void *)svc_arr->arr,
-				svc_arr->size * sizeof(u32));
-		node->no_wake_svc.size = svc_arr->size;
-	}
+
 	mutex_init(&node->qrtr_tx_lock);
 	INIT_RADIX_TREE(&node->qrtr_tx_flow, GFP_KERNEL);
 	init_waitqueue_head(&node->resume_tx);
