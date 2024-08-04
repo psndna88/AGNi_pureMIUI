591119fac21f22e2c67e2b5d538301d42fc0dc84
psndna88 <psndna88@gmail.com>
2024-08-04 16:50:59 +0530

    fs: erofs: Checkout to AOSP barnch

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (C) 2018 HUAWEI, Inc.
- * Copyright (C) 2021 XiaoMi, Inc.
  *             http://www.huawei.com/
  * Created by Gao Xiang <gaoxiang25@huawei.com>
  */
@@ -701,8 +700,7 @@ err_out:
 	goto out;
 }
 
-static void z_erofs_vle_unzip_wq(struct work_struct *work);
-static void z_erofs_vle_unzip_kickoff(void *ptr, int bios, struct erofs_sb_info *sbi)
+static void z_erofs_vle_unzip_kickoff(void *ptr, int bios)
 {
 	tagptr1_t t = tagptr_init(tagptr1_t, ptr);
 	struct z_erofs_unzip_io *io = tagptr_unfold_ptr(t);
@@ -718,18 +716,8 @@ static void z_erofs_vle_unzip_kickoff(void *ptr, int bios, struct erofs_sb_info 
 		return;
 	}
 
-	if (atomic_add_return(bios, &io->pending_bios))
-		return;
-
-	/* Use workqueue decompression for atomic contexts only */
-	if (in_atomic() || irqs_disabled()) {
+	if (!atomic_add_return(bios, &io->pending_bios))
 		queue_work(z_erofs_workqueue, &io->u.work);
-		if (sbi)
-			sbi->readahead_sync_decompress = true;
-		return;
-	}
-	z_erofs_vle_unzip_wq(&io->u.work);
-
 }
 
 static inline void z_erofs_vle_read_endio(struct bio *bio)
@@ -762,7 +750,7 @@ static inline void z_erofs_vle_read_endio(struct bio *bio)
 			unlock_page(page);
 	}
 
-	z_erofs_vle_unzip_kickoff(bio->bi_private, -1, sbi);
+	z_erofs_vle_unzip_kickoff(bio->bi_private, -1);
 	bio_put(bio);
 }
 
@@ -1314,7 +1302,7 @@ skippage:
 	if (postsubmit_is_all_bypassed(q, nr_bios, force_fg))
 		return true;
 
-	z_erofs_vle_unzip_kickoff(bi_private, nr_bios, sbi);
+	z_erofs_vle_unzip_kickoff(bi_private, nr_bios);
 	return true;
 }
 
@@ -1386,8 +1374,7 @@ static int z_erofs_vle_normalaccess_readpages(struct file *filp,
 	struct inode *const inode = mapping->host;
 	struct erofs_sb_info *const sbi = EROFS_I_SB(inode);
 
-	bool sync = (sbi->readahead_sync_decompress &&
-			        should_decompress_synchronously(sbi, nr_pages));
+	bool sync = should_decompress_synchronously(sbi, nr_pages);
 	struct z_erofs_decompress_frontend f = DECOMPRESS_FRONTEND_INIT(inode);
 	gfp_t gfp = mapping_gfp_constraint(mapping, GFP_KERNEL);
 	struct page *head = NULL;
