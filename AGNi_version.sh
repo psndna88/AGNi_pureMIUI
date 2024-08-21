00c50236aa656a30e5a024da85958ca6faaa6831
psndna88 <psndna88@gmail.com>
2024-08-21 23:22:55 +0530

    AGNi HyperOS v6.8 stable [5.4.282]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v6.7"
-sed -i 's/5.4.279/5.4.281/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v6.6-stable/v6.7-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+export AGNI_VERSION="v6.8"
+sed -i 's/5.4.281/5.4.282/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v6.7-stable/v6.8-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
