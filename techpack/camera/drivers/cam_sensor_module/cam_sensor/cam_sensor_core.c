c45d939e3784148b80cb2722a5a940176807cfce
psndna88 <psndna88@gmail.com>
2024-12-17 12:14:06 +0530

    techpack: camera: Merge LA.UM.9.14.r1-25500-LAHAINA.QSSI15.0

--- techpack/camera/drivers/cam_sensor_module/cam_sensor/cam_sensor_core.c
+++ techpack/camera/drivers/cam_sensor_module/cam_sensor/cam_sensor_core.c
@@ -723,12 +723,13 @@ int32_t cam_sensor_driver_cmd(struct cam_sensor_ctrl_t *s_ctrl,
 {
 	int rc = 0, pkt_opcode = 0;
 	struct cam_control *cmd = (struct cam_control *)arg;
-	struct cam_sensor_power_ctrl_t *power_info =
-		&s_ctrl->sensordata->power_info;
+	struct cam_sensor_power_ctrl_t *power_info = NULL;
 	if (!s_ctrl || !arg) {
 		CAM_ERR(CAM_SENSOR, "s_ctrl is NULL");
 		return -EINVAL;
 	}
+
+	power_info = &s_ctrl->sensordata->power_info;
 
 	if (cmd->op_code != CAM_SENSOR_PROBE_CMD) {
 		if (cmd->handle_type != CAM_HANDLE_USER_POINTER) {
