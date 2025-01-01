e911cd75bf20442a4274e39a5e04c4ef07a5ed2b
DevriesL <therkduan@gmail.com>
2025-01-01 18:11:05 +0100

    techpack: add get_main_display

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -61,6 +61,8 @@ bool is_skip_op_required(struct dsi_display *display)
 	return (display->is_cont_splash_enabled || display->trusted_vm_env);
 }
 
+struct dsi_display *main_display;
+
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -7254,6 +7256,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 exit:
 	*out_modes = display->modes;
 	rc = 0;
+	main_display = display;
 
 error:
 	if (rc)
@@ -8885,6 +8888,10 @@ int dsi_display_unprepare(struct dsi_display *display)
 
 	SDE_EVT32(SDE_EVTLOG_FUNC_EXIT);
 	return rc;
+}
+
+struct dsi_display *get_main_display(void) {
+	return main_display;
 }
 
 void __init dsi_display_register(void)
--- techpack/display/msm/dsi/dsi_display.h
+++ techpack/display/msm/dsi/dsi_display.h
@@ -838,4 +838,6 @@ int dsi_display_ctrl_vreg_on(struct dsi_display *display);
  */
 int dsi_display_ctrl_vreg_off(struct dsi_display *display);
 
+struct dsi_display *get_main_display(void);
+
 #endif /* _DSI_DISPLAY_H_ */
